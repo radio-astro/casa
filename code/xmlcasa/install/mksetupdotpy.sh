518d75c0dde41c2dfbc61181400390b002e3292b
darrell schiebel <ds@nrao.edu>
2009-02-03 03:06:15 +0000

    should be sensitive to the CORELIB makedef variable
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/v2/stable@7113 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/install/mksetupdotpy.sh
+++ code/xmlcasa/install/mksetupdotpy.sh
@@ -6,11 +6,12 @@ ARCHLIB=`echo $CASAPATH | awk '{printf "%s/%s/lib", $1,$2}'`
 ARCH=`echo $CASAPATH | awk '{print $2}'`
 SITE=`echo $CASAPATH | awk '{print $3}'`
 MAKEDEFS=$AIPSROOT/$ARCH/$SITE/makedefs
-VARS="CPPSTD PYTHONLIBD PYTHONVER CCMTOOLSLIBD CCMTOOLSINCD CORELIBD COREINCD WCSLIBLIBD WCSLIBINCD QT4LIBD QT4LIB CFITSIOLIBD CFITSIOINCD XTRNLIBS_rpath"
+VARS="CPPSTD PYTHONLIBD PYTHONVER CCMTOOLSLIBD CCMTOOLSINCD CORELIB CORELIBD COREINCD WCSLIBLIBD WCSLIBINCD QT4LIBD QT4LIB CFITSIOLIBD CFITSIOINCD XTRNLIBS_rpath"
 eval `gmake -f $AIPSROOT/$ARCH/makedefs VARS="$VARS" eval_vars`
 DEFINES2=`for i in $CPPSTD; do echo $i | grep "\-D"; done`
 DEFINES2="$DEFINES2 -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -D_LARGEFILE_64_SOURCE"
 COMMA=""
+SETUPDOTPY="setup.py"
 
 CFITSIO_LIBPATH=""
 if [ "$CFITSIOLIBD" != "" ]
@@ -42,58 +43,61 @@ fi
 
 cd $AIPSROOT/gcode_$ARCH/Python_Converter
 theFiles=`ls *.cc ../impl/*.cc ../CCM_Local/casac/CCM_Session*/*.cc CCM_Local/casac/CCM_Session*/*.cc | grep -v cmpt.cc`
-echo "from distutils.core import setup, Extension" > setup.py
-echo "setup( name=\"$COMPONENT\", version=\"1.0\"," >> setup.py
-echo "      ext_modules=[Extension(\"casac\"," >> setup.py
-echo "               [" >>setup.py
+echo "from distutils.core import setup, Extension" > $SETUPDOTPY
+echo "setup( name=\"$COMPONENT\", version=\"1.0\"," >> $SETUPDOTPY
+echo "      ext_modules=[Extension(\"casac\"," >> $SETUPDOTPY
+echo "               [" >>$SETUPDOTPY
 for afile in $theFiles
 do
-echo "            $COMMA'$afile'" >> setup.py
+echo "            $COMMA'$afile'" >> $SETUPDOTPY
 COMMA=", "
 done
-echo "               ]," >> setup.py
+echo "               ]," >> $SETUPDOTPY
 if [ "$SETUPLIBS" != "" ]; then
    if [ "$QT4LIBD" != "" ]; then
-      echo "               library_dirs=['$ARCHLIB', $SETUPLIBS, '$PYTHONLIBD' $CFITSIO_LIBPATH, '$WCSLIBLIBD', '$QT4LIBD']," >> setup.py
+      echo "               library_dirs=['$ARCHLIB', $SETUPLIBS, '$PYTHONLIBD' $CFITSIO_LIBPATH, '$WCSLIBLIBD', '$QT4LIBD']," >> $SETUPDOTPY
    else
-      echo "               library_dirs=['$ARCHLIB', $SETUPLIBS, '$PYTHONLIBD' $CFITSIO_LIBPATH, '$WCSLIBLIBD']," >> setup.py
+      echo "               library_dirs=['$ARCHLIB', $SETUPLIBS, '$PYTHONLIBD' $CFITSIO_LIBPATH, '$WCSLIBLIBD']," >> $SETUPDOTPY
    fi
 else
    if [ "$QT4LIBD" != "" ]; then
-      echo "               library_dirs=['$ARCHLIB', '$PYTHONLIBD' $CFITSIO_LIBPATH, '$WCSLIBLIBD', '$QT4LIBD']," >> setup.py
+      echo "               library_dirs=['$ARCHLIB', '$PYTHONLIBD' $CFITSIO_LIBPATH, '$WCSLIBLIBD', '$QT4LIBD']," >> $SETUPDOTPY
    else
-      echo "               library_dirs=['$ARCHLIB', '$PYTHONLIBD' $CFITSIO_LIBPATH, '$WCSLIBLIBD']," >> setup.py
+      echo "               library_dirs=['$ARCHLIB', '$PYTHONLIBD' $CFITSIO_LIBPATH, '$WCSLIBLIBD']," >> $SETUPDOTPY
    fi
 fi
-echo "               libraries=[ 'nrao', 'xmlcasa', 'display', 'flagging', 'calibration', 'msvis', 'casa_msfits', 'casa_ms', 'casa_images', 'casa_components', 'casa_coordinates'," >> setup.py
-echo "                           'casa_lattices', 'casa_fits', 'casa_measures', 'casa_tables', 'casa_scimath', 'synthesis', 'graphics', 'casa_casa','casaqt','qwt'," >> setup.py
+
+core_libraries=`echo "$CORELIB" | perl -pe "s/-l//g; s/(\S+)/'\\\$1'/g; \\\$_ = join(', ', split(/\s+/))"`
+echo "               libraries=[ 'nrao', 'xmlcasa', 'display', 'flagging', 'calibration', 'msvis'," >> $SETUPDOTPY
+echo "                           'synthesis', 'graphics', 'casaqt', 'qwt'," >> $SETUPDOTPY
+echo "                           $core_libraries," >> $SETUPDOTPY
 #
 #If it's a flavour of darwin the skip lapack and blas
 tjunk=`echo "$ARCH" | grep "^darwin"`
 if [ "$tjunk" ]; then
-   echo "                           'casa_scimath_f', 'casa_mirlib', 'cfitsio', 'wcs', " >> setup.py
+   echo "                           'cfitsio', 'wcs', " >> $SETUPDOTPY
 else
-   echo "                           'casa_scimath_f', 'lapack', 'blas', 'casa_mirlib', 'cfitsio', 'wcs', " >> setup.py
+   echo "                           'lapack', 'blas', 'cfitsio', 'wcs', " >> $SETUPDOTPY
    if [ "$QT4LIB" != "" ]; then
    qtlibs=''
    for i in `echo $QT4LIB | sed 's/-l//g'`; do
        qtlibs="$qtlibs '$i',";
    done
-   echo "                          $qtlibs" >> setup.py
+   echo "                          $qtlibs" >> $SETUPDOTPY
    fi
 fi
 if [ "$SETUPPYTHONLIB" != "" ]; then
-echo "$SETUPPYTHONLIB," >> setup.py
+echo "$SETUPPYTHONLIB," >> $SETUPDOTPY
 fi
-echo "                           'CCM_Python', 'CCM_Local_HomeFinder', 'WX_Utils_error', 'WX_Utils_code', 'WX_Utils_types', 'c', 'm' ]," >> setup.py
-echo "               extra_compile_args = [" >> setup.py
+echo "                           'CCM_Python', 'CCM_Local_HomeFinder', 'WX_Utils_error', 'WX_Utils_code', 'WX_Utils_types', 'c', 'm' ]," >> $SETUPDOTPY
+echo "               extra_compile_args = [" >> $SETUPDOTPY
 for i in $DEFINES2
 do
-echo                                       \'$i\', >> setup.py
+echo                                       \'$i\', >> $SETUPDOTPY
 done
-echo "                                     ]," >> setup.py
-#_echo "                                     '-DAIPS_NO_TEMPLATE_SRC']," >> setup.py
+echo "                                     ]," >> $SETUPDOTPY
+#_echo "                                     '-DAIPS_NO_TEMPLATE_SRC']," >> $SETUPDOTPY
 if [ "$SETUPEXTRALINK" != "" ]; then
-echo "               extra_link_args = [$SETUPEXTRALINK])], " >> setup.py
+echo "               extra_link_args = [$SETUPEXTRALINK])], " >> $SETUPDOTPY
 fi
-echo "       include_dirs = ['$AIPSROOT/code/include', '$AIPSROOT/code/casa', '$COREINCD', '$WCSLIBINCD', '.', '..', '$CCMTOOLSINCD', '$CFITSIOINCD', '../impl'])" >> setup.py
+echo "       include_dirs = ['$AIPSROOT/code/include', '$AIPSROOT/code/casa', '$COREINCD', '$WCSLIBINCD', '.', '..', '$CCMTOOLSINCD', '$CFITSIOINCD', '../impl'])" >> $SETUPDOTPY
