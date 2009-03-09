02167b4629a75d61ca1f26bd73b1fc58af295f06
shannon jaeger <sdjaeger@ucalgary.ca>
2009-03-09 22:38:18 +0000

    New Development: Yes             JIRA Issue: No       Ready to Release: Yes      Interface Changes: Yes  PROGRAMMER What Interface Changed: Changed various includes to reflect the                         changes/rearranging in the casacore images dir.          Test Programs: compile CASA   Put in Release Notes: No              Module(s): display in this case            Description: See interface changes
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/v2/active/code/synthesis@7323 ac9c0da5-131f-0410-9009-a9c195f183e6

--- implement/MeasurementComponents/PBMosaicFT.cc
+++ implement/MeasurementComponents/PBMosaicFT.cc
@@ -77,10 +77,10 @@
 #include <casa/sstream.h>
 #include <fstream.h>
 #include <strstream>
-#include <images/Images/WCBox.h>
 #include <images/Images/SubImage.h>
-#include <images/Images/ImageRegion.h>
 #include <images/Images/ImageSummary.h>
+#include <images/Regions/WCBox.h>
+#include <images/Regions/ImageRegion.h>
 #include <casa/BasicMath/Random.h>
 
 #include <synthesis/MeasurementComponents/PBMosaicFT.h>
