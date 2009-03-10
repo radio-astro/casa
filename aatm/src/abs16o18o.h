061d6a18d0791412c15e0192bb3719ffbf5123fb
wes young <wyoung@nrao.edu>
2009-03-10 02:15:24 +0000

    Import of aatm into stable branch
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/v2/stable@7327 ac9c0da5-131f-0410-9009-a9c195f183e6

---
+++ aatm/src/abs16o18o.h
@@ -0,0 +1,7 @@
+#if     defined(__cplusplus__)
+extern "C" {
+  void abs16o18o_c(double *,double *,double *,double *,double *,double *,double *,double *,double *);
+}
+#else
+extern void abs16o18o_c(double *,double *,double *,double *,double *,double *,double *,double *,double *);
+#endif
