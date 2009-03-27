baf73cc3752185879f8b9184f76c92d8fd6d9700
wes young <wyoung@nrao.edu>
2009-03-27 16:22:44 +0000

    Initial snap shot for prerelease branch
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/v2/stable@7469 ac9c0da5-131f-0410-9009-a9c195f183e6

--- aatm/src/abs16o17o.h
+++
@@ -1,7 +0,0 @@
-#if     defined(__cplusplus__)
-extern "C" {
-  void abs16o17o_c(double *,double *,double *,double *,double *,double *,double *,double *,double *);
-}
-#else
-extern void abs16o17o_c(double *,double *,double *,double *,double *,double *,double *,double *,double *);
-#endif
