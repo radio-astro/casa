f1cab192d298a3f0b99de2001d417815b3e0ace5
wes young <wyoung@nrao.edu>
2009-06-04 03:14:11 +0000

    Fixes from Kumar
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/v2/stable@8041 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/synthesis/implement/MeasurementEquations/Imager.cc
+++ code/synthesis/implement/MeasurementEquations/Imager.cc
@@ -8548,6 +8548,15 @@ Bool Imager::makePBImage(PBMath& pbMath, ImageInterface<Float>& pbImage){
   ctemp.set(1.0);
   pbMath.applyPB(ctemp, ctemp, wcenter, Quantity(0.0, "deg"), BeamSquint::NONE);
   StokesImageUtil::To(pbImage, ctemp);
+  Float cutoffval=minPB_p;
+  LatticeExpr<Bool> lemask(iif(pbImage < cutoffval, 
+			       False, True));
+  ImageRegion outreg=pbImage.makeMask("mask0", False, True);
+  LCRegion& outmask=outreg.asMask();
+  outmask.copyData(lemask);
+  pbImage.defineRegion("mask0", outreg,RegionHandler::Masks, True); 
+  pbImage.setDefaultMask("mask0");
+  
   return True;
 }
 void Imager::setObsInfo(ObsInfo& obsinfo){
