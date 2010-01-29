fb52e9af28683df09cd21e4ce7f9dd588a208359
sandra castro <sbhatnag@nrao.edu>
2010-01-29 12:48:05 +0000

    New Development: No             JIRA Issue: No       Ready to Release: No      Interface Changes: No What Interface Changed:          Test Programs: unit test   Put in Release Notes: No              Module(s):            Description: Modified to be run from the command line. It handles the 3 existing types of tests. Still to be finalized.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@10146 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/scripts/regressions/admin/runUnitTest.py
+++ code/xmlcasa/scripts/regressions/admin/runUnitTest.py
@@ -1,39 +1,108 @@
+
+""" Script to run unit tests from the command line as:"""
+"""    casapy --nologger --log2term -c runUnitTest.py"""
+
+# There are two classes in testwrapper.py:
+# class UnitTest, methods: runTest(), runFuncTest()
+# class ExecTest, methods: setup(), testrun()
+#
+# UnitTest.runTest() --> for new scripts as described in ....doc....
+# UnitTest.runFuncTest() --> for tests located in regressions/tests
+# ExecTest.testrun() --> for tests in regressions/
+
 import os
 import sys
-
-sys.path.append(os.environ["CASAPATH"].split()[0] + '/code/xmlcasa/scripts/regressions/admin')
-
+import traceback
 from testwrapper import *
 import unittest
-#sys.path.append('/Library/Python/2.6/site-packages/nose-0.11.1-py2.6.egg')
 import nose
 
-ALL_TESTS = ['test_report']
+# List of tests to run
+E2E_TESTS = ['listcal_regression','testcube_regression']
+OLD_TESTS = ['plotants_test','accum_test']
+NEW_TESTS = ['test_report']
+
 
 i = sys.argv.index("-c")
 this_file = sys.argv[i+1]
 testnames = []
+whichtests = 0  #all tests
 
 if sys.argv.__len__() == i+2:
-    testnames = ALL_TESTS
+    whichtests = 0
+#    testnames = ALL_TESTS
 else:
-    testnames = [sys.argv[i+2]]
+    whichtests = 1
+#    testnames = [sys.argv[i+2]]
 
-print testnames
 PWD = os.getcwd()
+NOSE_XML = PWD+'/nosexml/'
+
+# Create a directory for nose's xml files
+xmldir = NOSE_XML
+print ' Creating '+xmldir
+if os.access(xmldir, os.F_OK) is False:
+    os.makedirs(xmldir)
+else:
+    shutil.rmtree(xmldir)
+    os.makedirs(xmldir)
 
 
-for f in testnames:
-    try:
-        tests = UnitTest(f).runTest()
-        xmlfile = f+'.xml'
-        result = nose.run(argv=[sys.argv[0],"--with-xunit", "--xunit-file="+xmlfile], suite=tests)
-        os.chdir(PWD)
-    except:
-        print "Exception: failed to run %s" %f
 
- 
+if not whichtests:
+    # Run new tests
+    testnames = NEW_TESTS
+    for f in testnames:
+        try:
+            # Get a list of tests contained in the class
+            tests = UnitTest(f).runTest()
+            xmlfile = xmldir+f+'.xml'
+            result = nose.run(argv=[sys.argv[0],"--with-xunit","--verbosity=2","--xunit-file="+xmlfile], 
+                              suite=tests)
+            os.chdir(PWD)
+        except:
+            print "Exception: failed to run %s" %f
+            traceback.print_exc()
+
+
+    # Run old tests
+    testnames = OLD_TESTS
+    for f in testnames:
+        try:
+            os.chdir(PWD)
+            # Get the testcase
+            test = UnitTest(f).runFuncTest()
+            xmlfile = xmldir+f+'.xml'
+            result = nose.run(argv=[sys.argv[0],"--with-xunit","--verbosity=2","--xunit-file="+xmlfile], 
+                              suite=[test])
+            os.chdir(PWD)
+        except:
+            print "Exception: failed to run %s" %f
+            traceback.print_exc()
+
+
+    # Run e2e tests
+    testnames = E2E_TESTS
+    for f in testnames:
+        try:
+            os.chdir(PWD)
+            # Create a testcase of an e2e test
+            testcase = ExecTest('testrun')
+            testcase.testname = f       
+            print '-------------- Unit Test for %s ---------------'%f
     
+            testcase.setup()
+           
+            xmlfile = xmldir+f+'.xml'
+            result = nose.run(argv=[sys.argv[0],"--with-xunit","--verbosity=2","--xunit-file="+xmlfile], 
+                              suite=[testcase])
+            os.chdir(PWD)
+        except:
+            print "Exception: failed to run %s" %f
+            traceback.print_exc()
+
+else:
+    print "Run test testname. TBI"
     
     
     
