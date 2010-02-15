289d32c147ef992997c7092b4837265f3451acda
sandra castro <sbhatnag@nrao.edu>
2010-02-15 10:15:39 +0000

    New Development: No             JIRA Issue: Yes, CAS1955       Ready to Release: No      Interface Changes: No What Interface Changed:          Test Programs: unit test   Put in Release Notes: No              Module(s):            Description: Added more tests. The output image is the same for all tests.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@10371 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/scripts/regressions/tests/test_clean.py
+++ code/xmlcasa/scripts/regressions/tests/test_clean.py
@@ -8,161 +8,175 @@ import unittest
 
 class clean_test(unittest.TestCase):
     
-#    msfile = 'at166B.ms'
+    # Input and output names
     msfile = 'Itziar.ms'
-
-#    def shortDescription(self):
-#        return "Unit tests of task clean"
+    res = None
+    img = 'cleantest_im'
 
     def setUp(self):
-        res = None
+        self.res = None
         default(clean)
         shutil.copytree(os.environ.get('CASAPATH').split()[0] +\
                             '/data/regression/exportasdm/input/'+self.msfile, self.msfile)
-#        shutil.copytree('/Users/scastro/casadir/utests/clean/data/'+self.msfile, self.msfile)
-        
-        
     
     def tearDown(self):
-        os.system('rm -rf ' + self.msfile)
+        if (os.path.exists(self.msfile)):
+            os.system('rm -rf ' + self.msfile)
+
+        os.system('rm -rf ' + self.img+'*')
+
+    def getpixval(self,img,pixel):
+        ia.open(img)
+        px = ia.pixelvalue(pixel)
+        ia.close()
+        return px['value']['value']
+        
         
     def test1(self):
         """Test 1: Wrong input should return False"""
         msfile = 'badfilename'
-        self.res = clean(vis=msfile)
+        self.res = clean(vis=msfile, imagename=self.img)
         self.assertFalse(self.res)
         
     def test2(self):
         """Test 2: Good input should return None"""
-        output = 'cleantest2.im'
-        self.res = clean(vis=self.msfile,imagename=output)
+        self.res = clean(vis=self.msfile,imagename=self.img)
         self.assertEqual(self.res,None)
         
     def test3(self):
         """Test 3: Check if output exists"""
-        output = 'cleantest3.im'
-        self.res = clean(vis=self.msfile,imagename=output)
-        self.assertTrue(os.path.exists(output+'.image'))
+        self.res = clean(vis=self.msfile,imagename=self.img)
+        self.assertTrue(os.path.exists(self.img+'.image'))
         
     def test4(self):
         """Test 4: Wrong field type"""
-        output = 'cleantest4.im'
-        self.res = clean(vis=self.msfile,imagename=output,field=0)
+        self.res = clean(vis=self.msfile,imagename=self.img,field=0)
         self.assertFalse(self.res)
         
     def test5(self):
         """Test 5: Wrong spw value"""
-        output = 'cleantest5.im'
-        self.res = clean(vis=self.msfile,imagename=output,spw=10)
-        self.assertFalse(os.path.exists(output+'.image'))
-
-        
+        self.res = clean(vis=self.msfile,imagename=self.img,spw=10)
+        self.assertFalse(os.path.exists(self.img+'.image'))
+       
     def test6(self):
         """Test 6: Empty mode value"""
-        output = 'cleantest6.im'
-        self.res = clean(vis=self.msfile,imagename=output,mode='')
+        self.res = clean(vis=self.msfile,imagename=self.img,mode='')
         self.assertFalse(self.res)
         
     def test7(self):
         """Test 7: Unsupported gridmode"""
-        output = 'cleantest7.im'
-        self.res = clean(vis=self.msfile,imagename=output,gridmode='grid')
+        self.res = clean(vis=self.msfile,imagename=self.img,gridmode='grid')
         self.assertFalse(self.res)
         
     def test8(self):
-        """Test 8: Test gridmode=widefield"""
-        output = 'cleantest8.im'
-        self.res = clean(vis=self.msfile,imagename=output,gridmode='widefield')
-        self.assertEqual(self.res, None) 
-        self.assertTrue(os.path.exists(output+'.image'))
+        """Test 8: Correct gridmode values"""
+        modes = ['widefield','aprojection']
+        for m in modes:
+            out = self.img+'_'+m
+            self.res = clean(vis=self.msfile,imagename=out,gridmode=m)
+            self.assertEqual(self.res, None, 'Failed for gridmode = '+m) 
+            self.assertTrue(os.path.exists(out+'.image'),'Image %s does not exist'%out)
  
-    #FIXME: should this fail????
-    def test9(self):
-        """Test 9: Test gridmode=aprojection"""
-        output = 'cleantest9.im'
-        self.res = clean(vis=self.msfile,imagename=output,
-                                               gridmode='aprojection')
-        pass
                  
-    def test10(self):
-        """Test 10: Wrong niter type"""
-        output = 'cleantest10.im'
-        self.res = clean(vis=self.msfile,imagename=output,niter='1')
-        self.assertFalse(self.res)
-        
-    def test11(self):
-        """Test 11: Unsupported psfmode"""
-        output = 'cleantest11.im'
-        self.res = clean(vis=self.msfile,imagename=output,psfmode='psf')
-        self.assertFalse(self.res)
-        
-    def test12(self):
-        """Test 12: Test psfmode=clark"""
-        output = 'cleantest12.im'
-        self.res = clean(vis=self.msfile,imagename=output,psfmode='clark')
-        self.assertEqual(self.res, None)
-       
-    def test13(self):
-        """Test 13: Test psfmode=hogbom"""
-        output = 'cleantest13.im'
-        self.res = clean(vis=self.msfile,imagename=output,psfmode='hogbom')
-        self.assertEqual(self.res, None)
-       
-    def test14(self):
-        """Test 14: Test psfmode=clarkstokes"""
-        output = 'cleantest14.im'
-        self.res = clean(vis=self.msfile,imagename=output,psfmode='clarkstokes')
-        self.assertEqual(self.res, None)
-
-    def test15(self):
-        """Test 15: Unsupported imagermode"""
-        output = 'cleantest15.im'
-        self.res = clean(vis=self.msfile,imagename=output,imagermode='clark')
-        self.assertFalse(self.res)
-
+#    def test10(self):
+#        """Test 10: Wrong niter type"""
+#        self.res = clean(vis=self.msfile,imagename=self.img,niter='1')
+#        self.assertFalse(self.res)
+#        
+#    def test11(self):
+#        """Test 11: Unsupported psfmode"""
+#        self.res = clean(vis=self.msfile,imagename=self.img,psfmode='psf')
+#        self.assertFalse(self.res)
+#        
+#    def test12(self):
+#        """Test 12: Correct psfmode values"""
+#        modes = ['clark','hogbom','clarkstokes']
+#        for m in modes:
+#            out = self.img+'_'+m
+#            self.res = clean(vis=self.msfile,imagename=out,psfmode=m)
+#            self.assertEqual(self.res, None,'Failed for psfmode = '+m)            
+#            self.assertTrue(os.path.exists(out+'.image'))
+#
+#       
+#    def test15(self):
+#        """Test 15: Unsupported imagermode"""
+#        self.res = clean(vis=self.msfile,imagename=self.img,imagermode='clark')
+#        self.assertFalse(self.res)
+#        
+#
     def test16(self):
-        """Test 16: Test imagermode=csclean"""
-        output = 'cleantest16.im'
-        self.res = clean(vis=self.msfile,imagename=output,imagermode='csclean')
-        self.assertEqual(self.res, None)
+        '''Test 16: Correct imagermode modes'''
+        modes = ['csclean','mosaic']
+        for m in modes:
+            out = self.img+'_'+m
+            self.res = clean(vis=self.msfile,imagename=out,imagermode=m)
+            self.assertEqual(self.res, None, 'Failed for imagermode = '+m)
+            self.assertTrue(os.path.exists(out+'.image'),'Image %s does not exist' %out)
+#
+#    def test17(self):
+#        """Test 17: Test ??? TBD"""
+#
+#    def test18(self):
+#        """Test 18: Zero value of imsize"""
+#        self.res = clean(vis=self.msfile,imagename=self.img,imsize=0)
+#        self.assertFalse(os.path.exists(self.img+'.image'))
+#
+#    def test19(self):
+#        """Test 19: Correct cell values"""
+#        self.res = clean(vis=self.msfile,imagename=self.img, cell=2.0)
+#        self.assertEqual(self.res, None)
+#        self.assertTrue(os.path.exists(self.img+'.image'))
+#        
+#    def test20(self):
+#        """Test 20: Unsupported Stokes parameter"""
+#        self.res = clean(vis=self.msfile,imagename=self.img, stokes='V')
+#        self.assertFalse(self.res)
+#        
+#    def test21(self):
+#        """Test 21: Correct Stokes parameter"""
+#        self.res = clean(vis=self.msfile,imagename=self.img, stokes='XX')
+#        self.assertEqual(self.res, None)
+#        self.assertTrue(os.path.exists(self.img+'.image'))
+#        
+#    def test22(self):
+#        '''Test 22: Unsupported weighting mode'''
+#        self.res = clean(vis=self.msfile,imagename=self.img, weighting='median')
+#        self.assertFalse(self.res)
+#        
+#    def test23(self):
+#        '''Test 23: Correct weighting modes'''
+#        modes = ['natural','uniform','briggs','superuniform','briggsabs','radial']
+#        for m in modes:
+#            out = self.img+'_'+m
+#            self.res = clean(vis=self.msfile,imagename=out, weighting=m)
+#            self.assertEqual(self.res, None, 'Failed for weighting = '+m)
+#            self.assertTrue(os.path.exists(out+'.image'))
 
-    def test17(self):
-        """Test 17: Test imagermode=mosaic"""
-        output = 'cleantest17.im'
-        self.res = clean(vis=self.msfile,imagename=output,imagermode='mosaic')
+    def test24(self):
+        '''Test 24: Correct subparameters of selectdata'''
+        self.res = clean(vis=self.msfile,imagename=self.img,selectdata=True,
+                         timerange='>10:25:00',antenna='8')
         self.assertEqual(self.res, None)
+        self.assertTrue(os.path.exists(self.img+'.image'))
 
-    def test18(self):
-        """Test 18: Zero value of imsize"""
-        output = 'cleantest18.im'
-        self.res = clean(vis=self.msfile,imagename=output,imsize=0)
-        self.assertFalse(os.path.exists(output+'.image'))
+    def test25(self):
+        '''Test 25: Wrong antenna subparameter of selectdata'''
+        self.res = clean(vis=self.msfile,imagename=self.img,selectdata=True,
+                         antenna='88')
+        self.assertFalse(os.path.exists(self.img+'.image'))
 
-    def test19(self):
-        """Test 19: Correct cell values"""
-        output = 'cleantest19.im'
-        self.res = clean(vis=self.msfile,imagename=output, cell=2.0)
+    def test26(self):
+        '''Verify the value of pixel 50'''
+        #run clean with some parameters
+        self.res = clean(vis=self.msfile,imagename=self.img,selectdata=True,
+                         timerange='>10:20:00',field='1~5',imsize=[100,100],niter=10)
+        
         self.assertEqual(self.res, None)
-        self.assertTrue(os.path.exists(output+'.image'))
-        
-    def test20(self):
-        """Test 20: Unsupported Stokes parameter"""
-        output = 'cleantest20.im'
-        self.res = clean(vis=self.msfile,imagename=output, stokes='V')
-        self.assertFalse(self.res)
-        
-#    def test30(self):
-#        """Test 30: X equal 0"""
-#        x = 1
-#        self.assertEqual(x,0)
-        
-    def test21(self):
-        """Test 21: Correct Stokes parameter"""
-        output = 'cleantest21.im'
-        self.res = clean(vis=self.msfile,imagename=output, stokes='XX')
-        self.assertEqual(self.res, None)
-        self.assertTrue(os.path.exists(output+'.image'))
-        
+        self.assertTrue(os.path.exists(self.img+'.image'))
+        ref = 0.031042417511343956
+        value = self.getpixval(self.img+'.image',50)
+        diff = abs(ref - value)
+        self.assertTrue(diff < 10e-5)
         
 
 
