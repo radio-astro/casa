88ad2a614ac5b60b0dec911d942b7837a21a0c69
sandra castro <sbhatnag@nrao.edu>
2010-02-17 09:45:11 +0000

    New Development: No             JIRA Issue:       Ready to Release: Yes      Interface Changes: No What Interface Changed:          Test Programs: unit tests   Put in Release Notes: No              Module(s):            Description: Copied old unit tests to new directory.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@10414 ac9c0da5-131f-0410-9009-a9c195f183e6

---
+++ code/xmlcasa/scripts/tests/smoothcal_test.py
@@ -0,0 +1,41 @@
+########################################################################
+#       This script will call the main regression script               #
+#       smoothcal_regression.py. The 3 returned data sets from         #
+#       data are copied by the runTest() method to the working         #
+#       directory, defaulted to /tmp.                                  #
+#       Exceptions will be handled if smoothal() fails to run or if    #
+#       the regression tests do not pass.                              #
+#                                                                      #
+########################################################################
+
+import sys
+import os
+import string
+from locatescript import locatescript
+import inspect
+
+a=inspect.stack()
+stacklevel=0
+for k in range(len(a)):
+    if (string.find(a[k][1], 'ipython console') > 0):
+        stacklevel=k
+        break
+gl=sys._getframe(stacklevel).f_globals
+
+def description():
+    return "Based on smoothcal_regression.py"
+
+def run():
+    lepath=locatescript('smoothcal_regression.py')
+    gl['regstate']=True
+    execfile(lepath, gl)
+    print 'regstate =', gl['regstate']
+    if not gl['regstate']:
+        raise Exception, 'regstate = False'
+
+    return []
+
+def data():
+    ### return the data files that is needed by the regression script
+    return ['ngc1333.ms','ngc1333.gcal','ngc1333.ref.smoothed']
+#    return []
