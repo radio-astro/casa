df62deb6c77b87dfa8b55299c1df5940baa7a3e6
dirk petry <dpetry@eso.org>
2010-02-17 03:05:05 +0000

    New Development: No             JIRA Issue: CAS-1795       Ready to Release: Yes      Interface Changes: No What Interface Changed:          Test Programs: manual tests   Put in Release Notes: Yes?              Module(s): alma            Description: corrected handling of the numTerm column in the 			Pointing table of the ASDM to follow the documentation. 			Need to get this in to make asdmv1-import regression 			pass. Michel may override in his next installment. 			exportasdm will need to be changed then.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@10408 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/alma/apps/asdm2MS/asdm2MS.cc
+++ code/alma/apps/asdm2MS/asdm2MS.cc
@@ -2072,10 +2072,11 @@ int main(int argc, char *argv[]) {
 
   if (nPointing > 0) {
 
+
     // Check some assertions.
     //
     // All rows of ASDM-Pointing must have their attribute usePolynomials equal to false
-    // and their numTerm attribute equal to 1. Use the opportunity of this check
+    // and their numTerm attribute equal to numSample. Use the opportunity of this check
     // to compute the number of rows to be created in the MS-Pointing by summing
     // all the numSample attributes values.
     //
@@ -2087,14 +2088,18 @@ int main(int argc, char *argv[]) {
 	errstream << "Found usePolynomials equal to true at row #" << i <<". Can't go further"<<endl;
 	error(errstream.str());
       }
-
-      if (v[i]->getNumTerm() != 1) {
-	errstream.str("");
-	errstream << "Found numTerm equal to " << v[i]->getNumTerm() <<" at row #" << i <<" and I was expecting 1. Can't go further" << endl;
-	error(errstream.str());
+      
+      int nSamp = v[i]->getNumSample();
+      
+      if (v[i]->getNumTerm() != nSamp) {
+	infostream.str("");
+	infostream << "Found numTerm equal to " << v[i]->getNumTerm() <<" at row #" << i
+		   <<" and I was expecting " << nSamp << ". Will try to proceed." << endl;
+	info(infostream.str());
       }
+      
+      numMSPointingRows += nSamp;
 
-      numMSPointingRows += v[i]->getNumSample();
     }
 
     //
