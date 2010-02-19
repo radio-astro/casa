11a7b9c64f4bcfc23d87fb72195ae9512dc675e3
kana sugimoto <kana.sugi@nao.ac.jp>
2010-02-19 08:01:19 +0000

    New Development: No             JIRA Issue: Yes (CAS-1903)       Ready to Release: Yes      Interface Changes: No What Interface Changed:          Test Programs: run the script   Put in Release Notes: No              Module(s): none            Description: Updated the test script.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@10458 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/implement/atmosphere/scripts/AtmTypeTest.py
+++ code/xmlcasa/implement/atmosphere/scripts/AtmTypeTest.py
@@ -12,43 +12,66 @@ dpr = quantity(500.,'Pa')
 dpm = 1.25
 h0  = quantity(2000.,'m')
 att = 1
-at.initAtmProfile(alt, tmp, pre, mxA, hum, wvl, dpr, dpm, h0, att)
+myatm=at.initAtmProfile(alt, tmp, pre, mxA, hum, wvl, dpr, dpm, h0, att)
 l = at.getBasicAtmParms()
 print "Test for type ", l['atmType']
-hpt= at.getAtmTypeHPT()
-n = hpt['return']
-for i in range(n):
-	print i, " hx: ", hpt['Hx'].value[i], " px: ", hpt['Px'].value[i], " tx: ", hpt['Tx'].value[i]
+print myatm
+p = at.getProfile()
+print "layer\taltitude\tthickness\ttemperature\twatermassdensity\tpressure"
+height = 0
+for i in range(at.getNumLayers()):
+	height += p['thickness'].value[i]
+	print i, '\t', height, '\t', p['thickness'].value[i],'\t', p['temperature'].value[i],'\t', p['watermassdensity'].value[i],'\t', p['pressure'].value[i]
+print
+
 att = 2
-at.initAtmProfile(alt, tmp, pre, mxA, hum, wvl, dpr, dpm, h0, att)
+myatm=at.initAtmProfile(alt, tmp, pre, mxA, hum, wvl, dpr, dpm, h0, att)
 l = at.getBasicAtmParms()
 print "Test for type ", l['atmType']
-hpt= at.getAtmTypeHPT()
-n = hpt['return']
-for i in range(n):
-	print i, " hx: ", hpt['Hx'].value[i], " px: ", hpt['Px'].value[i], " tx: ", hpt['Tx'].value[i]
+print myatm
+p = at.getProfile()
+print "layer\taltitude\tthickness\ttemperature\twatermassdensity\tpressure"
+height = 0
+for i in range(at.getNumLayers()):
+	height += p['thickness'].value[i]
+	print i, '\t', height, '\t', p['thickness'].value[i],'\t', p['temperature'].value[i],'\t', p['watermassdensity'].value[i],'\t', p['pressure'].value[i]
+print
+
 att = 3
-at.initAtmProfile(alt, tmp, pre, mxA, hum, wvl, dpr, dpm, h0, att)
+myatm=at.initAtmProfile(alt, tmp, pre, mxA, hum, wvl, dpr, dpm, h0, att)
 l = at.getBasicAtmParms()
 print "Test for type ", l['atmType']
-hpt= at.getAtmTypeHPT()
-n = hpt['return']
-for i in range(n):
-	print i, " hx: ", hpt['Hx'].value[i], " px: ", hpt['Px'].value[i], " tx: ", hpt['Tx'].value[i]
+print myatm
+p = at.getProfile()
+print "layer\taltitude\tthickness\ttemperature\twatermassdensity\tpressure"
+height = 0
+for i in range(at.getNumLayers()):
+	height += p['thickness'].value[i]
+	print i, '\t', height, '\t', p['thickness'].value[i],'\t', p['temperature'].value[i],'\t', p['watermassdensity'].value[i],'\t', p['pressure'].value[i]
+print 
+
 att = 4
-at.initAtmProfile(alt, tmp, pre, mxA, hum, wvl, dpr, dpm, h0, att)
+myatm=at.initAtmProfile(alt, tmp, pre, mxA, hum, wvl, dpr, dpm, h0, att)
 l = at.getBasicAtmParms()
 print "Test for type ", l['atmType']
-hpt= at.getAtmTypeHPT()
-n = hpt['return']
-for i in range(n):
-	print i, " hx: ", hpt['Hx'].value[i], " px: ", hpt['Px'].value[i], " tx: ", hpt['Tx'].value[i]
+print myatm
+p = at.getProfile()
+print "layer\taltitude\tthickness\ttemperature\twatermassdensity\tpressure"
+height = 0
+for i in range(at.getNumLayers()):
+	height += p['thickness'].value[i]
+	print i, '\t', height, '\t', p['thickness'].value[i],'\t', p['temperature'].value[i],'\t', p['watermassdensity'].value[i],'\t', p['pressure'].value[i]
+print
+
 att = 5
-at.initAtmProfile(alt, tmp, pre, mxA, hum, wvl, dpr, dpm, h0, att)
+myatm=at.initAtmProfile(alt, tmp, pre, mxA, hum, wvl, dpr, dpm, h0, att)
 l = at.getBasicAtmParms()
 print "Test for type ", l['atmType']
-hpt= at.getAtmTypeHPT()
-n = hpt['return']
-for i in range(n):
-	print i, " hx: ", hpt['Hx'].value[i], " px: ", hpt['Px'].value[i], " tx: ", hpt['Tx'].value[i]
+print myatm
+p = at.getProfile()
+print "layer\taltitude\tthickness\ttemperature\twatermassdensity\tpressure"
+height = 0
+for i in range(at.getNumLayers()):
+	height += p['thickness'].value[i]
+	print i, '\t', height, '\t', p['thickness'].value[i],'\t', p['temperature'].value[i],'\t', p['watermassdensity'].value[i],'\t', p['pressure'].value[i]
 exit()
