d63efb0859b53f311ecb2dd59976f48b33feb5bb
wes young <wyoung@nrao.edu>
2010-03-02 17:24:56 +0000

    New Development: No             JIRA Issue: CAS-1996       Ready to Release: Yes      Interface Changes: No What Interface Changed:          Test Programs: None   Put in Release Notes: No              Module(s): startup            Description: Changes from Juergen to reduce the spew on startup.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@10577 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/scripts/task_help.py
+++ code/xmlcasa/scripts/task_help.py
@@ -1,158 +1,118 @@
 from tasksinfo import *
 def startup():
-# startup guide
-      """ Start up screen for CASA """
-      print """___________________________________________________________
-Available tasks:
-
-   accum	  flagmanager	    listobs	      sdscale
-   applycal	  fluxscale	    listvis	      sdsim
-   bandpass	  ft		    makemask	      sdsmooth
-   blcal	  gaincal	    plotants	      sdstat
-   browsetable	  gencal	    plotcal	      sdtpimaging
-   calstat	  hanningsmooth	    plotmsl	      setjyt
-   clean	  imcontsub	    plotxy	      smoothcal
-   clearcal	  imfit		    sdaverage	      specfit
-   clearplot	  imhead	    sdbaseline	      split
-   clearstat	  immoments	    sdcal	      tget
-   concat	  importfits	    sdcoadd	      tput
-   deconvolve	  importuvfits	    sdfit	      uvcontsub
-   exportfits	  importvla	    sdflag	      uvcontsub2
-   exportuvfits	  imregrid	    sdimaging	      uvmodelfit
-   feather	  imsmooth	    sdimprocess	      viewer
-   filecatalog	  imstat	    sdlist	      vishead
-   find		  imval		    sdmath	      visstat
-   fixvis	  listcal	    sdplot
-   flagdata	  listhistory	    sdsave
-
-
-                
-sd* tasks are available after asap_init() is run
-
-Additional tasks available for ALMA commissioning use
-         (still alpha code as of Beta release):
-
- simdata        importasdm
-
-Available tools:
-
-   cb (calibrater)       cp (cal plot)    fg (flagger)
-   ia (image analysis)   im (imager)      me (measures)
-   mp (MS plot)          ms (MS)          qa (quanta)
-   sm (simulation)       tb (table)       tp (table plot)
-   vp (voltage patterns) pm (plotms)      at (atmosphere)
-
-   pl (pylab functions)
-   sd (ASAP functions - run asap_init() to import into CASA)
-
-   casalogger             - Call up the casalogger (if it goes away)
-   
-___________________________________________________________________
-Help:
-   help taskname          - Full help for task
-   help par.parametername - Full help for parameter name
-   tasklist               - Task list organized by catagory
-   taskhelp               - One line summary of available tasks
-   toolhelp               - One line summary of available tools
-   startup                - The start up screen
-___________________________________________________________________"""
+    # startup guide
+    """ Start up screen for CASA """
+    print """___________________________________________________________________
+    For help use the following commands:
+    tasklist               - Task list organized by category
+    taskhelp               - One line summary of available tasks
+    help taskname          - Full help for task
+    toolhelp               - One line summary of available tools
+    help par.parametername - Full help for parameter name
+    Single Dish sd* tasks are available after asap_init() is run   
+    ___________________________________________________________________"""
 
 def taskhelp(scrap=None):
-      """ Briefly describe all tasks with scrap in their name or one-line description. """
-      if scrap:
-            scrap = str(scrap)
-            foundtasks = [ft for ft in tasksum.keys() if ft.find(scrap) > -1 or tasksum[ft].find(scrap) > -1]
-            if not foundtasks:
-                  print "No tasks were found with '%s' in their name or description." % scrap
-                  return
-      else:
-            print 'Available tasks: \n'
-            foundtasks = tasksum.keys()
-
-      foundtasks.sort()   # Already sorted?!
-      widestftlen = max([len(ft) for ft in foundtasks])
-      fmt = "%%-%ds : %%s" % widestftlen
-      for ft in foundtasks:
+    """ Briefly describe all tasks with scrap in their name or one-line description. """
+    if scrap:
+        scrap = str(scrap)
+        foundtasks = [ft for ft in tasksum.keys() if ft.find(scrap) > -1 or tasksum[ft].find(scrap) > -1]
+        if not foundtasks:
+            print "No tasks were found with '%s' in their name or description." % scrap
+        return
+    else:
+        print 'Available tasks: \n'
+        foundtasks = tasksum.keys()
+        foundtasks.sort()   # Already sorted?!
+        widestftlen = max([len(ft) for ft in foundtasks])
+        fmt = "%%-%ds : %%s" % widestftlen
+        for ft in foundtasks:
             print fmt % (ft, tasksum[ft])            
-
 def toolhelp():
-      """ List all tools with one-line description: """
-      print ' '
-      print 'Available tools: \n'
-      print ' at : Juan Pardo ATM library'
-      print ' cb : Calibration utilities'
-      print ' cp : Cal solution plotting utilities'
-      print ' fg : Flagging/Flag management utilities'
-      print ' ia : Image analysis utilities'
-      print ' im : Imaging utilities'
-      print ' me : Measures utilities'
-      print ' ms : MeasurementSet (MS) utilties'
-      print ' mp : MS plotting (data (amp/phase) versus other quantities)'
-      print ' pm : PlotMS utilities'
-      print ' tb : Table utilities (selection, extraction, etc)'
-      print ' tp : Table plotting utilities'
-      print ' qa : Quanta utilities'
-      print ' sm : Simulation utilities'
-      print ' vp : Voltage pattern/primary beam utilties'
-      print ' ---'
-      print ' pl : pylab functions (e.g., pl.title, etc)'
-      print ' sd : (after running asap_init()) Single dish utilities'
-      print ' ---'
-
+    """ List all tools with one-line description: """
+    print ' '
+    print 'Available tools: \n'
+    print ' at : Juan Pardo ATM library'
+    print ' cb : Calibration utilities'
+    print ' cp : Cal solution plotting utilities'
+    print ' fg : Flagging/Flag management utilities'
+    print ' ia : Image analysis utilities'
+    print ' im : Imaging utilities'
+    print ' me : Measures utilities'
+    print ' ms : MeasurementSet (MS) utilties'
+    print ' mp : MS plotting (data (amp/phase) versus other quantities)'
+    print ' pm : PlotMS utilities'
+    print ' tb : Table utilities (selection, extraction, etc)'
+    print ' tp : Table plotting utilities'
+    print ' qa : Quanta utilities'
+    print ' sm : Simulation utilities'
+    print ' vp : Voltage pattern/primary beam utilties'
+    print ' ---'
+    print ' pl : pylab functions (e.g., pl.title, etc)'
+    print ' sd : (after running asap_init()) Single dish utilities'
+    print ' ---'
 
 def tasklist():
-      """ List tasks, organized by catagory """
-      print 'Available tasks: \n'
-      print 'Import/Export    Information  Data Editing  Display/Plot'
-      print '-------------    -----------  ------------  ------------'
-      print 'importvla        listcal      (fixvis)      clearplot'
-      print '(importasdm)     listhistory  flagautocorr  plotants '
-      print 'importfits       listobs      flagdata      plotcal'    
-      print 'importuvfits     listvis      flagmanager   plotms'
-      print 'exportfits       imhead       plotms        plotxy'
-      print 'exportuvfits     imstat       plotxy        viewer'
-      print '                 vishead'
-      print '                 visstat'
-      print ''
-      print 'Calibration      Imaging      Modelling     Utility'
-      print '-----------      -------      ---------     -------'
-      print 'accum            clean        setjy         help task'
-      print 'applycal         deconvolve   uvcontsub     help par.parameter'
-      print 'bandpass         feather      (uvcontsub2)  taskhelp'
-      print 'blcal            ft           uvmodelfit    tasklist'
-      print 'calstat          makemask                   browsetable'
-      print 'gaincal          mosaic                     clearplot'
-      print 'fluxscale        widefield                  clearstat'
-      print '(fringecal)                                 concat'
-      print 'clearcal                                    filecatalog'
-      print 'listcal                                     startup'
-      print 'smoothcal                                   split'
-      print 'polcal                                      fixvis'
-      print 'hanningsmooth'
-      print '                                                 '
-      print ''
-      print 'Image Analysis   Simulation   Single Dish (after running asap_init())'
-      print '--------------   ----------   -----------'
-      print 'imcontsub        simdata      sdaverage'
-      print 'imhead                        sdbaseline'
-      print 'immath                        sdcal'
-      print 'immoments                     sdcoadd'
-      print 'imregrid                      sdfit'
-      print 'imsmooth                      sdflag'
-      print 'imstat                        sdimaging'
-      print 'imval                         sdimprocess'
-      print '(specfit)                     sdlist'
-      print '                              sdmath'
-      print '                              sdplot'
-      print '                              sdsave'
-      print '                              sdscale'
-      print '                              (sdsim)'
-      print '                              sdsmooth'
-      print '                              sdstat'
-      print '                              sdtpimaging'
-      if globals().has_key('mytasks') :
-         print ''
-         print 'User defined tasks'
-         print '------------------'
-         for key in mytasks.keys() :
-             print key
+    """ List tasks, organized by catagory """
+    print 'Available tasks, organized by category: \n'
+    print 'Import/Export       Information   Data Editing  Display/Plotting'
+    print '-------------       -----------   ------------  ----------------'
+    print 'importvla           imhead        concat        clearplot'
+    print 'importfits          imstat        fixvis        plotants '
+    print 'importuvfits        listcal       flagautocorr  plotcal'    
+    print 'exportfits          listhistory   flagdata      plotms'
+    print 'exportuvfits        listobs       flagmanager   plotxy'
+    print '(importasdm)        listvis       plotms        viewer'
+    print '(importgmrt)        vishead       plotxy        (viewerconnection)'
+    print '                    visstat'
+    print ' '
+    print ''
+    print 'Data Manipulation   Calibration   Imaging       Modelling   '
+    print '-----------------   -----------   -------       ---------   '
+    print 'concat              accum         clean         setjy       '
+    print 'cvel                applycal      deconvolve    uvcontsub   '
+    print 'fixvis              bandpass      feather       uvmodelfit  '
+    print 'hanningsmooth       blcal         ft            uvsub'
+    print 'split               calstat       makemask      (uvcontsub2)'
+    print 'uvcontsub           clearcal      (autoclean)              '
+    print 'uvsub               cvel          (boxit)                  '
+    print '(uvcontsub2)        fluxscale                               '
+    print '                    fixvis                                  '
+    print '                    gaincal                                 '
+    print '                    gencal                                  '
+    print '                    listcal                                 '
+    print '                    polcal                                  '
+    print '                    setjy                                   '
+    print '                    smoothcal                               '
+    print '                    (fringecal                              '
+    print '                    (peel)                                  '
+    print '                                                            '
+    print '                                                            '
+    print ''
+    print 'Image Analysis  Simulation  Utilities           Single Dish'
+    print '--------------  ----------  ---------           (after running asap_init())'
+    print 'imcontsub       simdata     browsetable         ---------------------------'
+    print 'imhead          (simdata2)  casalogger          sdaverage'
+    print 'imfit                       clearplot           sdbaseline'
+    print 'immath                      clearstat           sdcal'
+    print 'immoments                   filecatalog         sdcoadd'
+    print 'imregrid                    find                sdfit'
+    print 'imsmooth                    help task           sdflag'
+    print 'imstat                      help par.parameter  sdimaging'
+    print 'imval                       tasklist            sdimprocess'
+    print '(specfit)                   taskhelp            sdlist'
+    print '                            toolhelp            sdmath'
+    print '                            rmtables            sdplot'
+    print '                            startup             sdsave'
+    print '                                                sdscale'
+    print '                                                sdsmooth '
+    print '                                                sdstat '
+    print '                                                sdtpimaging '
+    print '                                                (sdsim)'
+    if globals().has_key('mytasks') :
+        print ''
+        print 'User defined tasks'
+        print '------------------'
+        for key in mytasks.keys() :
+            print key
+
