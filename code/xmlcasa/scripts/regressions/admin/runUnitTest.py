5d64a63b38f9a1d5f802a7585b4a236b920e2d39
sandra castro <sbhatnag@nrao.edu>
2010-03-12 08:53:39 +0000

    New Development: No             JIRA Issue: No       Ready to Release: Yes      Interface Changes: No What Interface Changed:          Test Programs: runUnitTest   Put in Release Notes:              Module(s):            Description: Removed slow tests to allow faster tests with Hudson.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@10716 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/scripts/regressions/admin/runUnitTest.py
+++ code/xmlcasa/scripts/regressions/admin/runUnitTest.py
@@ -32,7 +32,7 @@ import nose
 #             'imsmooth_test','imval_test','vishead_test',
 #             'visstat_test']
 OLD_TESTS = ['asdm-import','boxit_test',
-             'imfit_test','imhead_test','immath_test',
+             'imfit_test',
              'imregrid_test',
              'imval_test','vishead_test']
 NEW_TESTS = ['test_listhistory','test_clean','test_report','test_exportasdm',
