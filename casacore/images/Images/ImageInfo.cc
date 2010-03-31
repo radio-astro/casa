371ea87525673e6a2abd7c0161a0a5b28f8ac468
darrell schiebel <ds@nrao.edu>
2010-03-31 20:08:25 +0000

    New Development: no             JIRA Issue: no       Ready to Release: yes      Interface Changes: none What Interface Changed: none          Test Programs: none   Put in Release Notes: no
               Description: this cout line dumps the entire history of the fits file to the terminal...
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/test@10939 ac9c0da5-131f-0410-9009-a9c195f183e6

--- casacore/images/Images/ImageInfo.cc
+++ casacore/images/Images/ImageInfo.cc
@@ -214,7 +214,7 @@ Bool ImageInfo::getRestoringBeam (LoggerHolder& logger)
 {
    for (LoggerHolder::const_iterator iter = logger.begin(); iter != logger.end(); iter++) {
       String line = iter->message();
-      cout << line << endl;
+//       cout << line << endl;
       if (line.contains(String("BMAJ")) &&
           line.contains(String("BMIN")) &&
           line.contains(String("BPA"))) {
