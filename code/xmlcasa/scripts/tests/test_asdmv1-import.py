ab0644927cacf0390dcd37c82590e752022a401d
sandra castro <sbhatnag@nrao.edu>
2010-04-22 08:44:05 +0000

    New Development: No             JIRA Issue: No       Ready to Release: Yes      Interface Changes: No What Interface Changed:          Test Programs: test_asdmv1-import, unit tests   Put in Release Notes:              Module(s):            Description: Converted to PyUnit
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@11194 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/scripts/tests/test_asdmv1-import.py
+++ code/xmlcasa/scripts/tests/test_asdmv1-import.py
@@ -22,19 +22,19 @@
 #     one simulated MS dataset                                              #
 #                                                                           #
 #############################################################################
+import os
+import sys
+import shutil
+from __main__ import default
+from tasks import *
+from taskinit import *
+import unittest
 
 myname = 'asdmv1-import_regression'
 
 # default ASDM dataset name
 myasdm_dataset_name = 'uid___X5f_X18951_X1'
 myms_dataset_name = 'M51.ms'
-
-# get the dataset name from the wrapper if possible
-mydict = locals()
-if mydict.has_key("asdm_dataset_name"):
-    myasdm_dataset_name = mydict["asdm_dataset_name"]
-if mydict.has_key("ms_dataset_name"):
-    myms_dataset_name = mydict["ms_dataset_name"]
 
 # name of the resulting MS
 msname = myasdm_dataset_name+'.ms'
@@ -75,10 +75,10 @@ def checktable(thename, theexpectation):
             print "     column ", mycell[0], " row ", mycell[1], " contains ", value
             print "     expected value is ", mycell[2]
             tb.close()
-            raise
+            return False
     tb.close()
     print myname, ": table ", thename, " as expected."
-    return
+    return True
 
 #########################
 
@@ -145,149 +145,193 @@ def verify_asdm(asdmname, withPointing):
 ###########################
 # beginning of actual test 
 
-try:
-    importasdm(myasdm_dataset_name)
-except:
-    print myname, ": Error ", sys.exc_info()[0]
-    raise
-else:
-    print myname, ": Success! Now checking output ..."
-    mscomponents = set(["table.dat",
-                        "table.f0",
-                        "table.f1",
-                        "table.f2",
-                        "table.f3",
-                        "table.f4",
-                        "table.f5",
-                        "table.f6",
-                        "table.f7",
-                        "table.f8",
-                        "ANTENNA/table.dat",
-                        "DATA_DESCRIPTION/table.dat",
-                        "FEED/table.dat",
-                        "FIELD/table.dat",
-                        "FLAG_CMD/table.dat",
-                        "HISTORY/table.dat",
-                        "OBSERVATION/table.dat",
-                        "POINTING/table.dat",
-                        "POLARIZATION/table.dat",
-                        "PROCESSOR/table.dat",
-                        "SOURCE/table.dat",
-                        "SPECTRAL_WINDOW/table.dat",
-                        "STATE/table.dat",
-                        "SYSCAL/table.dat",
-                        "ANTENNA/table.f0",
-                        "DATA_DESCRIPTION/table.f0",
-                        "FEED/table.f0",
-                        "FIELD/table.f0",
-                        "FLAG_CMD/table.f0",
-                        "HISTORY/table.f0",
-                        "OBSERVATION/table.f0",
-                        "POINTING/table.f0",
-                        "POLARIZATION/table.f0",
-                        "PROCESSOR/table.f0",
-                        "SOURCE/table.f0",
-                        "SPECTRAL_WINDOW/table.f0",
-                        "STATE/table.f0",
-                        "SYSCAL/table.f0"
-                        ])
-    for name in mscomponents:
-        if not os.access(msname+"/"+name, os.F_OK):
-            print myname, ": Error  ", msname+"/"+name, "doesn't exist ..."
-            raise
+class asdmv1_import(unittest.TestCase):
+    
+    def setUp(self):
+        res = None
+        if(os.path.exists(myasdm_dataset_name)):
+            shutil.rmtree(myasdm_dataset_name)
+
+        datapath=os.environ.get('CASAPATH').split()[0]+'/data/regression/asdm-import/input/'
+        shutil.copytree(datapath + myasdm_dataset_name, myasdm_dataset_name)
+        datapath=os.environ.get('CASAPATH').split()[0]+'/data/regression/exportasdm/input/'
+        shutil.copytree(datapath + myms_dataset_name, myms_dataset_name)
+        default(importasdm)
+        
+    def tearDown(self):
+        shutil.rmtree(myasdm_dataset_name)
+        shutil.rmtree(myms_dataset_name)
+        shutil.rmtree(msname,ignore_errors=True)
+        shutil.rmtree(msname+'.flagversions',ignore_errors=True)
+        
+    def test1(self):
+        '''Asdmv1-import: Test good input'''
+        retValue = {'success': True, 'msgs': "", 'error_msgs': '' }    
+
+        self.res = importasdm(myasdm_dataset_name)
+        print myname, ": Success! Now checking output ..."
+        mscomponents = set(["table.dat",
+                            "table.f0",
+                            "table.f1",
+                            "table.f2",
+                            "table.f3",
+                            "table.f4",
+                            "table.f5",
+                            "table.f6",
+                            "table.f7",
+                            "table.f8",
+                            "ANTENNA/table.dat",
+                            "DATA_DESCRIPTION/table.dat",
+                            "FEED/table.dat",
+                            "FIELD/table.dat",
+                            "FLAG_CMD/table.dat",
+                            "HISTORY/table.dat",
+                            "OBSERVATION/table.dat",
+                            "POINTING/table.dat",
+                            "POLARIZATION/table.dat",
+                            "PROCESSOR/table.dat",
+                            "SOURCE/table.dat",
+                            "SPECTRAL_WINDOW/table.dat",
+                            "STATE/table.dat",
+                            "SYSCAL/table.dat",
+                            "ANTENNA/table.f0",
+                            "DATA_DESCRIPTION/table.f0",
+                            "FEED/table.f0",
+                            "FIELD/table.f0",
+                            "FLAG_CMD/table.f0",
+                            "HISTORY/table.f0",
+                            "OBSERVATION/table.f0",
+                            "POINTING/table.f0",
+                            "POLARIZATION/table.f0",
+                            "PROCESSOR/table.f0",
+                            "SOURCE/table.f0",
+                            "SPECTRAL_WINDOW/table.f0",
+                            "STATE/table.f0",
+                            "SYSCAL/table.f0"
+                            ])
+        for name in mscomponents:
+            if not os.access(msname+"/"+name, os.F_OK):
+                print myname, ": Error  ", msname+"/"+name, "doesn't exist ..."
+                retValue['success']=False
+                retValue['error_msgs']=retValue['error_msgs']+msname+'/'+name+' does not exist'
+            else:
+                print myname, ": ", name, "present."
+        print myname, ": MS exists. All tables present. Try opening as MS ..."
+        try:
+            ms.open(msname)
+        except:
+            print myname, ": Error  Cannot open MS table", tablename
+            retValue['success']=False
+            retValue['error_msgs']=retValue['error_msgs']+'Cannot open MS table '+tablename
         else:
-            print myname, ": ", name, "present."
-    print myname, ": MS exists. All tables present. Try opening as MS ..."
-    try:
-        ms.open(msname)
-    except:
-        print myname, ": Error  Cannot open MS table", tablename
-        raise
-    else:
-        ms.close()
-        print myname, ": OK. Checking tables in detail ..."
-
-        # check main table first
-        name = ""
-        #             col name, row number, expected value, tolerance
-        expected = [
-                     ['UVW',       42, [ 0., 0., 0. ], 1E-8],
-                     ['EXPOSURE',  42, 1.008, 0],
-                     ['DATA',      42, [ [10.5526886+0.0j] ], 1E-7]
-                     ]
-        checktable(name, expected)
-
-        expected = [
-# old values using TAI     ['UVW',       638, [-65.07623467,   1.05534109, -33.65801386], 1E-8],
-                     ['UVW',       638, [-65.14758508, 1.13423277, -33.51712451], 1E-8],
-                     ['EXPOSURE',  638, 1.008, 0],
-                     ['DATA',      638, [ [0.00362284+0.00340279j] ], 1E-8]
-                     ]
-        checktable(name, expected)
-        
-        name = "ANTENNA"
-        expected = [ ['OFFSET',       1, [ 0.,  0.,  0.], 0],
-                     ['POSITION',     1, [2202242.5520, -5445215.1570, -2485305.0920], 0.0001],
-                     ['DISH_DIAMETER',1, 12.0, 0]
-                     ]
-        checktable(name, expected)
-        
-        name = "POINTING"
-        expected = [ ['DIRECTION',       10, [[ 1.94681283],[ 1.19702955]], 1E-8],
-                     ['INTERVAL',        10, 0.048, 0],
-                     ['TARGET',          10, [[ 1.94681283], [ 1.19702955]], 1E-8],
-                     ['TIME',            10, 4758823736.016000, 0],
-                     ['TIME_ORIGIN',     10, 0., 0],
-                     ['POINTING_OFFSET', 10, [[ 0.],[ 0.]], 0],
-                     ['ENCODER',         10, [ 1.94851533,  1.19867576], 1E-8 ]
-                     ]
-        checktable(name, expected)
-        
-myvis = myms_dataset_name
-os.system('rm -rf exportasdm-output.asdm myinput.ms')
-os.system('cp -R ' + myvis + ' myinput.ms')
-default('exportasdm')
-try:
-    print "\n>>>> Test of exportasdm: input MS  is ", myvis
-    print "(a simulated input MS with pointing table)"
-    rval = exportasdm(
-        vis = 'myinput.ms',
-        asdm = 'exportasdm-output.asdm',
-        archiveid="S002",
-        apcorrected=False
-        )
-    print "rval is ", rval
-    if not rval:
-        raise Exception
-    os.system('rm -rf '+asdmname+'; mv exportasdm-output.asdm '+asdmname)
-    verify_asdm(asdmname, True)
-except:
-    print myname, ': *** Unexpected error exporting MS to ASDM, regression failed ***'   
-    raise
+            ms.close()
+            print myname, ": OK. Checking tables in detail ..."
     
-try:
-    print "Reimporting the created ASDM ...."
-    importasdm(asdm=asdmname, vis=reimp_msname, wvr_corrected_data='no')
-    print "Testing existence of reimported MS ...."
-    if(not os.path.exists(reimp_msname)):
-        print "MS ", reimp_msname, " doesn't exist."
-        raise Exception
-    print "Testing equivalence of the original and the reimported MS."
-    tb.open(myms_dataset_name)
-    nrowsorig = tb.nrows()
-    print "Original MS contains ", nrowsorig, "integrations."
-    tb.close()
-    tb.open(reimp_msname)
-    nrowsreimp = tb.nrows()
-    print "Reimported MS contains ", nrowsreimp, "integrations."
-    if(not nrowsreimp==nrowsorig):
-        print "Numbers of integrations disagree."
-        raise Exception
-except:
-    print myname, ': *** Unexpected error reimporting the exported ASDM, regression failed ***'   
-    raise
+            # check main table first
+            name = ""
+            #             col name, row number, expected value, tolerance
+            expected = [
+                         ['UVW',       42, [ 0., 0., 0. ], 1E-8],
+                         ['EXPOSURE',  42, 1.008, 0],
+                         ['DATA',      42, [ [10.5526886+0.0j] ], 1E-7]
+                         ]
+            results = checktable(name, expected)
+            if not results:
+                retValue['success']=False
+                retValue['error_msgs']=retValue['error_msgs']+'Check of table MAIN failed'
+            else:
+                retValue['success']=True
     
-        
+            expected = [
+    # old values using TAI     ['UVW',       638, [-65.07623467,   1.05534109, -33.65801386], 1E-8],
+                         ['UVW',       638, [-65.14758508, 1.13423277, -33.51712451], 1E-8],
+                         ['EXPOSURE',  638, 1.008, 0],
+                         ['DATA',      638, [ [0.00362284+0.00340279j] ], 1E-8]
+                         ]
+            results = checktable(name, expected)
+            if not results:
+                retValue['success']=False
+                retValue['error_msgs']=retValue['error_msgs']+'Check of table MAIN failed'
+            else:
+                retValue['success']=True
+            
+            name = "ANTENNA"
+            expected = [ ['OFFSET',       1, [ 0.,  0.,  0.], 0],
+                         ['POSITION',     1, [2202242.5520, -5445215.1570, -2485305.0920], 0.0001],
+                         ['DISH_DIAMETER',1, 12.0, 0]
+                         ]
+            results = checktable(name, expected)
+            if not results:
+                retValue['success']=False
+                retValue['error_msgs']=retValue['error_msgs']+'Check of table ANTENNA failed'
+            else:
+                retValue['success']=True
+            
+            name = "POINTING"
+            expected = [ ['DIRECTION',       10, [[ 1.94681283],[ 1.19702955]], 1E-8],
+                         ['INTERVAL',        10, 0.048, 0],
+                         ['TARGET',          10, [[ 1.94681283], [ 1.19702955]], 1E-8],
+                         ['TIME',            10, 4758823736.016000, 0],
+                         ['TIME_ORIGIN',     10, 0., 0],
+                         ['POINTING_OFFSET', 10, [[ 0.],[ 0.]], 0],
+                         ['ENCODER',         10, [ 1.94851533,  1.19867576], 1E-8 ]
+                         ]
+            results = checktable(name, expected)
+            if not results:
+                retValue['success']=False
+                retValue['error_msgs']=retValue['error_msgs']+'Check of table POINTING failed'
+            else:
+                retValue['success']=True
+                
+        self.assertTrue(results)
+                
+# ENABLE the following when exportasdm works in Mac 10.6
+            
+        myvis = myms_dataset_name
+        os.system('rm -rf exportasdm-output.asdm myinput.ms')
+        os.system('cp -R ' + myvis + ' myinput.ms')
+        default('exportasdm')
+        try:
+            print "\n>>>> Test of exportasdm: input MS  is ", myvis
+            print "(a simulated input MS with pointing table)"
+            rval = exportasdm(
+                vis = 'myinput.ms',
+                asdm = 'exportasdm-output.asdm',
+                archiveid="S002",
+                apcorrected=False
+                )
+            print "rval is ", rval
+            if not rval:
+                raise Exception
+            os.system('rm -rf '+asdmname+'; mv exportasdm-output.asdm '+asdmname)
+            verify_asdm(asdmname, True)
+        except:
+            print myname, ': *** Unexpected error exporting MS to ASDM, regression failed ***'   
+            raise
+            
+        try:
+            print "Reimporting the created ASDM ...."
+            importasdm(asdm=asdmname, vis=reimp_msname, wvr_corrected_data='no')
+            print "Testing existence of reimported MS ...."
+            if(not os.path.exists(reimp_msname)):
+                print "MS ", reimp_msname, " doesn't exist."
+                raise Exception
+            print "Testing equivalence of the original and the reimported MS."
+            tb.open(myms_dataset_name)
+            nrowsorig = tb.nrows()
+            print "Original MS contains ", nrowsorig, "integrations."
+            tb.close()
+            tb.open(reimp_msname)
+            nrowsreimp = tb.nrows()
+            print "Reimported MS contains ", nrowsreimp, "integrations."
+            if(not nrowsreimp==nrowsorig):
+                print "Numbers of integrations disagree."
+                raise Exception
+        except:
+            print myname, ': *** Unexpected error reimporting the exported ASDM, regression failed ***'   
+            raise
+    
+def suite():
+    return [asdmv1_import]        
         
     
-
