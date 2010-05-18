f6d65e315f06d4a416597aa3e94476a553d53b96
kumar golap <kgolap@nrao.edu>
2010-05-18 00:22:38 +0000

    New Development: No             JIRA Issue: No List JIRA ticket.       Ready to Release: Yes      Interface Changes: Yes What Interface Changed: added field to record output          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: Describe your changes here...
    Add the request od Sandra ms.summary will now return field info as part of the record of info that comes out
    	
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@11510 ac9c0da5-131f-0410-9009-a9c195f183e6

--- casacore/ms/MeasurementSets/MSSummary.h
+++ casacore/ms/MeasurementSets/MSSummary.h
@@ -135,6 +135,8 @@ public:
    void listAntenna (LogIO& os, Bool verbose=False) const;
    void listFeed (LogIO& os, Bool verbose=False) const;
    void listField (LogIO& os, Bool verbose=False) const;
+   void listField (LogIO& os, Record& outRec, Bool verbose=False,
+		   Bool fillRecord=True) const;
    void listObservation (LogIO& os, Bool verbose=False) const;
    void listHistory (LogIO& os) const;
    void listPolarization (LogIO& os, Bool verbose=False) const;
