a60530294f72c97ebd4953786d3d485ee4c6faa3
sandra castro <sbhatnag@nrao.edu>
2010-05-28 11:19:56 +0000

    New Development: No             JIRA Issue: No       Ready to Release: Yes      Interface Changes: No What Interface Changed:          Test Programs: unit tests framework   Put in Release Notes:              Module(s):            Description: Added path to find testwrapper in test releases. Up to now it was failing 	   to find testwrapper when branch was not active.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@11646 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/scripts/regressions/admin/runUnitTest.py
+++ code/xmlcasa/scripts/regressions/admin/runUnitTest.py
@@ -28,10 +28,21 @@ import unittest
 import string
 import re
 import shutil
-sys.path.append(os.environ["CASAPATH"].split()[0] + '/code/xmlcasa/scripts/regressions/admin')
+import nose
+CASA_DIR = os.environ["CASAPATH"].split()[0]
+UTILS_DIR = CASA_DIR+'/code/xmlcasa/scripts/regressions/admin/'
+if not os.access(UTILS_DIR, os.F_OK):
+    if os.access(CASA_DIR+'/lib64', os.F_OK):
+        UTILS_DIR = CASA_DIR+'/lib64/python2.5/tests/admin'
+    elif os.access(CASA_DIR+'/lib', os.F_OK):
+        UTILS_DIR = CASA_DIR+'/lib/python2.5/tests/admin'
+    else:            #Mac release
+        UTILS_DIR = CASA_DIR+'/Resources/python/regressions/admin'        
+
+sys.path.append(UTILS_DIR)
+
 import testwrapper
 from testwrapper import *
-import nose
 
 
 # List of tests to run
