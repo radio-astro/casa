a27315206ee54579b90cc65545fcef7e1e9e5e88
dave mehringer <dmehring@zia.aoc.nrao.edu>
2010-06-07 13:08:21 +0000

    New Development: No             JIRA Issue: No         Ready for Test: Yes/No      Interface Changes: Yes/No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: create and use private ia tool
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@11716 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/scripts/task_imfit.py
+++ code/xmlcasa/scripts/task_imfit.py
@@ -10,17 +10,19 @@ def imfit(
     newestimates=None
 ):
     casalog.origin('imfit')
+    myia = iatool.create()
     try:
-        if (not ia.open(imagename)):
-            raise Exception, "Cannot create image analysis (ia) tool using " + imagename
-        result_dict = ia.fitcomponents(
+        if (not myia.open(imagename)):
+            raise Exception, "Cannot create image analysis tool using " + imagename
+        result_dict = myia.fitcomponents(
             box, region, chan, stokes, mask, includepix, excludepix,
             residual, model, estimates, logfile, append, newestimates
         )
-        ia.done()
+        myia.done()
         return result_dict
     except Exception, instance:
-        ia.done()
+        if (myia):
+            myia.done()
         casalog.post( str( '*** Error ***') + str(instance), 'SEVERE')
         return None
         
