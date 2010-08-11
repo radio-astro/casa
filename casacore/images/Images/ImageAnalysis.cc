6cbbfce13328aa4c896712bb0102ef6e57925d31
kumar golap <kgolap@nrao.edu>
2010-08-11 00:14:49 +0000

    New Development: Yes/No             JIRA Issue: No/Yes List JIRA ticket.         Ready for Test: Yes/No      Interface Changes: Yes/No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: Describe your changes here...
    Made sure the locks are released ...because destruction of a disk based image object
    does not imply the lock is released if the process is alive. Needed to be released 
    in parallel code
    	
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@12290 ac9c0da5-131f-0410-9009-a9c195f183e6

--- casacore/images/Images/ImageAnalysis.cc
+++ casacore/images/Images/ImageAnalysis.cc
@@ -84,6 +84,7 @@
 #include <images/Images/ImageInterface.h>
 #include <images/Images/ImageMoments.h>
 #include <images/Images/ImageMetaData.h>
+#include <images/Images/ImageOpener.h>
 #include <images/Regions/ImageRegion.h>
 #include <images/Images/ImageRegrid.h>
 #include <images/Images/ImageSourceFinder.h>
@@ -154,7 +155,17 @@ itsLog(new LogIO()), pStatistics_p(0), pHistograms_p(0),
 }
 
 ImageAnalysis::~ImageAnalysis() {
+  
 	if (pImage_p != 0) {
+	  if(pImage_p->isPersistent()){
+	    ImageOpener::ImageTypes type = ImageOpener::imageType(pImage_p->name());
+	    if (type == ImageOpener::AIPSPP) {
+	      (static_cast<PagedImage<Float> *>(pImage_p))->table().relinquishAutoLocks(True);
+	      (static_cast<PagedImage<Float> *>(pImage_p))->table().unlock();
+	    }
+	  }
+
+
 		delete pImage_p;
 		pImage_p = 0;
 	}
