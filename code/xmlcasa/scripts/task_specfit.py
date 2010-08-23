ecca7649f2e287f40e2730422d52f742b8512f9b
dave mehringer <dmehring@zia.aoc.nrao.edu>
2010-08-23 15:54:57 +0000

    New Development: Yes             JIRA Issue: CAS-1157         Ready for Test: Yes      Interface Changes: Yes What Interface Changed: new interface for specfit          Test Programs: specfit_test.py   Put in Release Notes: Yes              Module(s): Module Names change impacts.            Description: new interface for specfit
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@12407 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/scripts/task_specfit.py
+++ code/xmlcasa/scripts/task_specfit.py
@@ -1,58 +1,96 @@
-#import os
+
+##########################################################################
+# task_specfit.py
+#
+# Copyright (C) 2008, 2009
+# Associated Universities, Inc. Washington DC, USA.
+#
+# This script is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Library General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+# License for more details.
+#
+# You should have received a copy of the GNU Library General Public License
+# along with this library; if not, write to the Free Software Foundation,
+# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+#
+# Correspondence concerning AIPS++ should be adressed as follows:
+#        Internet email: aips2-request@nrao.edu.
+#        Postal address: AIPS++ Project Office
+#                        National Radio Astronomy Observatory
+#                        520 Edgemont Road
+#                        Charlottesville, VA 22903-2475 USA
+#
+# <author>
+# Dave Mehringer
+# </author>
+#
+# <summary>
+# Fit 1 dimensional gaussians and/or polynomial
+# </summary>
+#
+# <reviewed reviwer="" date="" tests="" demos="">
+# </reviewed>
+#
+# <prerequisite>
+# <ul>
+#
+# </ul>
+# </prerequisite>
+#
+# <etymology>
+# specfit => spec(trum) fit(ter)
+# but in general it can be used for any image axis
+# </etymology>
+#
+# <synopsis>
+# specfit fits models to 1-d profiles. It is built on top of ia.fitprofile()
+# </synopsis> 
+#
+# <example>
+# specfit(imagename="myline.im", ngauss=2, poly=3, model="mymodel.im", multi=true, residual="myresid.im")
+#
+# </example>
+#
+# <motivation>
+# To make users happy, cf https://bugs.aoc.nrao.edu/browse/CAS-607
+# </motivation>
+#
+
+###########################################################################
 from taskinit import *
-from imregion import *
 
-from odict import odict
+def specfit(
+	imagename, box, region, chans, stokes,
+	axis, mask, ngauss, poly, multifit, model,
+	residual, wantreturn
+):
+    casalog.origin('specfit')
+    retval = None
+    myia = iatool.create()
+    try:
+        if (not myia.open(imagename)):
+            raise Exception, "Cannot create image analysis tool using " + imagename
+        retval = myia.fitprofile(
+			box, region, chans, stokes,
+			axis, mask, ngauss, poly,
+			multifit, model, residual
+		)
+    except Exception, instance:
+        casalog.post( str( '*** Error ***') + str(instance), 'SEVERE')
+        retval = None
+    myia.done()
+    if (wantreturn):
+    	return retval
+    else:
+    	if (retval):
+    	   del retval
+    	return None
 
-def specfit(imagename=None,mode=None,fitorder=None,ngauss=None,box=None,stokes=None,mask=None,estimate=None,estfile=None, sigmafile=None,fitfile=None,residualfile=None):
-	"""Performs profile and polygonial fitting of images.
-        """
 
-	#Python script
-	#
-	try:
-	    casalog.origin('specfit')
-            # Create the region from the information provided by
-            # the user.
-	    reg=imregion( imagename, '', stokes, box, '', '' )
-            print "Done getting region"
 
-            if ( mode=='single' ):
-                print "Fitting a single profile"
-                ia.open(imagename)
-
-                if ( estimate ):
-                    fit = ia.fitprofile( region=reg, mask=mask, ngauss=ngauss, poly=fitorder, fit=False, sigma=sigmafile )
-
-                    print "return keys are: ", fit.keys()
-                    print "return is: ", fit['return']
-                    residual=fit['resid']   
-                    results=fit['values']  # Saved to estimate file
-                    
-                else:
-                    fit=ia.fitprofile( region=reg, mask=mask, estimate=estfile, ngauss=ngauss, poly=fitorder, fit=True, sigma=sigmafile )
-
-                    print "return keys are: ", fit.keys()
-                    print "return is: ", fit['return']
-                    residual=fit['resid']   
-                    results=fit['values']  # Saved to fit file
-                
-            elif( mode=='all' ):
-                print "Fitting all profiles"
-                ia.open(imagename)
-                fit=ia.fitallprofiles( region=reg, mask=mask, ngauss=ngauss, poly=fitorder, sigma=sigmafile, fit=fitfile, resid=residualfile )
-               
-            elif( mode=='poly' ):              
-                print "Doing a polynomial fit"
-                ia.open(imagename)
-                ia.fitpolynomial( region=reg, mask=mask, order=fitorder, sigma=sigmafile, fit=fitfile, resid=residualfile )
-                
-                
-            else:
-                print '*** Error *** Invalid mode: ', mode, 
- 
-        except Exception, instance:
-            print '*** Error ***',instance
-	    return 
-              
-        return 
