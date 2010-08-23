ab6d0d898811ab5a69442e5a548b89713e0a99e2
dave mehringer <dmehring@zia.aoc.nrao.edu>
2010-08-23 15:42:17 +0000

    New Development: Yes             JIRA Issue: CAS-1157         Ready for Test: Yes      Interface Changes: Yes What Interface Changed: added new constructor          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: see above and created enum for function descriptions
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active/casacore@12398 ac9c0da5-131f-0410-9009-a9c195f183e6

--- images/Images/ImageCollapser.h
+++ images/Images/ImageCollapser.h
@@ -28,6 +28,7 @@
 #ifndef IMAGES_IMAGECOLLAPSER_H
 #define IMAGES_IMAGECOLLAPSER_H
 
+#include <casa/Containers/HashMap.h>
 #include <casa/Logging/LogIO.h>
 #include <images/Images/ImageInterface.h>
 #include <casa/namespace.h>
@@ -63,13 +64,49 @@ namespace casa {
 
     public:
 
+    	enum AggregateType {
+    		AVDEV,
+    		MAX,
+    		MEAN,
+    		MEDIAN,
+    		MIN,
+    		RMS,
+    		STDDEV,
+    		SUM,
+    		VARIANCE,
+    		// set all pixels in output image to 0
+    		ZERO,
+    		UNKNOWN
+    	};
+
+    	// if <src>outname</src> is empty, no image will be written
+    	// if <src>overwrite</src> is True, if image already exists it will be removed
+    	// if <src>overwrite</src> is False, if image already exists exception will be thrown
+    	//
+    	// <group>
     	ImageCollapser(
     	    String aggString,const String& imagename,
     	    const String& region, const String& box,
     	    const String& chanInp, const String& stokes,
-    	    const String& maskInp, const uInt compressionAxis,
+    	    const String& maskInp, const uInt axis,
             const String& outname, const Bool overwrite
     	);
+
+    	ImageCollapser(
+    	    String aggString,const String& imagename,
+    	    const String& region, const String& box,
+    	    const String& chanInp, const String& stokes,
+    	    const String& maskInp, const Vector<uInt>& axes,
+            const String& outname, const Bool overwrite
+    	);
+
+    	ImageCollapser(
+            const ImageInterface<Float> * const image,
+    	    const Vector<uInt>& axes, const Bool invertAxesSelection,
+    	    const AggregateType aggregateType,
+    	    const String& outname, const Bool overwrite
+    	);
+    	// </group>
 
         // destructor
         ~ImageCollapser();
@@ -80,16 +117,25 @@ namespace casa {
         // a NULL pointer is returned and pointer deletion is performed internally.
         ImageInterface<Float>* collapse(const Bool wantReturn) const;
 
+        static const HashMap<uInt, Float (*)(const Array<Float>&)>* funcMap();
+        static const HashMap<uInt, String>* funcNameMap();
+        static const HashMap<uInt, String>* minMatchMap();
+
     private:
         LogIO *_log;
-        ImageInterface<Float> *_image;
+        ImageInterface<Float> * _image;
         Record _regionRecord;
-        String _regionString, _chan, _stokesString, _mask, _outname;
-        uInt _compressionAxis;
-        Bool _overwrite;
-        //AggregateType _aggType;
-        Float (*_aggregateFunction)(const Array<Float>&);
+        String _chan, _stokesString, _mask, _outname;
+        Bool _overwrite, _destructImage, _invertAxesSelection;
+        Vector<uInt> _axes;
+        AggregateType _aggType;
 
+        static HashMap<uInt, Float (*)(const Array<Float>&)> *_funcMap;
+        static HashMap<uInt, String> *_funcNameMap;
+        static HashMap<uInt, String> *_minMatchMap;
+
+        // disallow default constructor
+        ImageCollapser();
 
         // does the lion's share of constructing the object, ie checks validity of
         // inputs, etc.
@@ -98,7 +144,9 @@ namespace casa {
         	const String& box, const String& regionName
         );
 
-        void _setAggregateFunction(String& aggString);
+        void _setAggregateType(String& aggString);
+
+        void _invert();
     };
 }
 
