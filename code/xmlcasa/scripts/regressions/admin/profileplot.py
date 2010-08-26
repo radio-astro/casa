4daf97ff979b017dd68d5bd8508cf307692c364e
jonas larsen <usd-help@eso.org>
2010-08-26 18:15:56 +0000

    New Development: Yes/No             JIRA Issue: CAS-2399         Ready for Test: Yes/No      Interface Changes: Yes/No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: No              Module(s): Module Names change impacts.            Description: Make CPU and memory profiling work on Mac.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@12491 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/scripts/regressions/admin/profileplot.py
+++ code/xmlcasa/scripts/regressions/admin/profileplot.py
@@ -17,14 +17,11 @@ y11=[0]
 y22=[0]
 numfile=[0]
 
-testname='unknown_test'
 testname=sys.argv[1]
 RESULT_DIR=sys.argv[2]
 webpage=sys.argv[3]
 asciidata=sys.argv[4]
-procnam='casapy'
-if(len(sys.argv)>5):
-    procnam=sys.argv[5]
+casapy_pid = sys.argv[5]
 
 def handler(signum, frame):
     try:
@@ -32,7 +29,6 @@ def handler(signum, frame):
     except:
         pass
 
-    print 'saving profile plot'
     a=inspect.stack()
     stacklevel=0
     for k in range(len(a)):
@@ -60,7 +56,7 @@ def handler(signum, frame):
     ax2.yaxis.tick_right()
     pl.plot(t,numfile, 'r-.',lw=2)
     pl.legend(['No. of Open FDs'],loc=[0.7,0.8], prop=font)
-    pl.title('memory usage of '+myf['procnam']+' for '+testname)
+    pl.title('memory usage of casapy for '+testname)
 
     #s="test-plot.ps" change to PNG for web browser compatibility
     #s="test-plot.png"
@@ -77,51 +73,62 @@ def handler(signum, frame):
     ht.doBlk(body1, body2,myf['testname']+'_profile.png', 'Profile')
     ht.doFooter()
 
-    print 'finished!\nfile written to %s and %s' % (png_filename,asciidata)
     sys.stdout.flush()
 
     sys.exit()
     return
-def getmem(procName='') :
-    # First get memory info
-    
-    cmd='top -n 1 -b -i | grep ' + procName
-    #a=commands.getoutput(cmd)   #fixme: top command doesn't work on Mac
-    (errorcode, a) = commands.getstatusoutput(cmd)   
-    if(len(a)==0 or errorcode != 0) :
-        return -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0
-    pid = string.split(a)[0]
-    if os.path.isfile('/usr/sbin/lsof'):
-        numoffile=string.atoi(commands.getoutput('/usr/sbin/lsof -p '+pid+' 2>/dev/null | wc -l'))
-    else:
-        numoffile=string.atoi(commands.getoutput('/usr/bin/lsof -p '+pid+' 2>/dev/null | wc -l'))
-    memstr0=string.atof(string.split(string.split(a)[4],'m')[0])*1024.0*1024.0
-    if(len(string.split(string.split(a)[5],'m')) > 1) :
-        memstr1=string.atof(string.split(string.split(a)[5],'m')[0])*1024.0*1024.0
-    elif(len(string.split(string.split(a)[5],'g')) > 1) :
-        memstr1=string.atof(string.split(string.split(a)[5],'g')[0])*1024.0*1024.0*1024.0
-    elif(len(string.split(string.split(a)[5],'k')) > 1) :
-         memstr1=string.atof(string.split(string.split(a)[5],'g')[0])*1024.0
-    else:
-        memstr1=string.atof(string.split(a)[5])
 
+# return memory usage in KB
+def getmem(pid):
+    # First get memory info
+
+    lsof = "/usr/sbin/lsof"
+    if not os.path.isfile(lsof):
+        lsof = "/usr/bin/lsof"
+    if not os.path.isfile(lsof):
+        print "Warning: Could not find lsof at /usr/sbin/lsof or /usr/bin/lsof"
+
+    (errorcode, numoffile) = commands.getstatusoutput(lsof + ' -p ' + str(pid) + ' | wc -l')
+    if errorcode != 0:
+        numoffile = -1
+
+    (errorcode, mem_virtual)  = commands.getstatusoutput('ps -p ' + str(pid) + ' -o vsz | tail -1')
+    if errorcode != 0:
+        mem_virtual = -1
+    (errorcode, mem_resident) = commands.getstatusoutput('ps -p ' + str(pid) + ' -o rss | tail -1')
+    if errorcode != 0:
+        mem_resident = -1
 
     # Get CPU usage.
     # top in batch mode doesn't give accurate numbers
-    # with just 1 iterations, so do 2 iterations with .5 sec
+    # with just 1 iterations, so do 2 iterations with 1 sec
     # delay
-    cmd='top -b d 0.5 -n2 | grep "^Cpu" | tail -1'
-    (errorcode, a) = commands.getstatusoutput(cmd)   
 
-    cpu_us = re.sub('.*[ :]', '', re.sub('%.*', '', a.split(',')[0]))
-    cpu_sy = re.sub('.*[ :]', '', re.sub('%.*', '', a.split(',')[1]))
-    cpu_id = re.sub('.*[ :]', '', re.sub('%.*', '', a.split(',')[3]))
-    cpu_wa = re.sub('.*[ :]', '', re.sub('%.*', '', a.split(',')[4]))
+    if os.uname()[0] == "Linux":
+        cmd = 'top -b -d 1 -n2 | grep -E "^Cpu" | tail -1'
+        (errorcode, a) = commands.getstatusoutput(cmd)   
+        if len(a)==0 or errorcode != 0:
+            return -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0
+        
+        cpu_us = re.sub('.*[ :]', '', re.sub('%.*', '', a.split(',')[0]))
+        cpu_sy = re.sub('.*[ :]', '', re.sub('%.*', '', a.split(',')[1]))
+        cpu_id = re.sub('.*[ :]', '', re.sub('%.*', '', a.split(',')[3]))
+        cpu_wa = re.sub('.*[ :]', '', re.sub('%.*', '', a.split(',')[4]))
 
-    if len(a)==0 or errorcode != 0:
-        return -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0
+    elif os.uname()[0] == "Darwin":
+        cmd = 'top -l 2 -s 1 | grep -Eo "CPU.usage.*" | tail -1'
+        (errorcode, a) = commands.getstatusoutput(cmd)
+        if len(a)==0 or errorcode != 0:
+            return -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0
+        
+        cpu_us = re.sub('.*[ :]', '', re.sub('%.*', '', a.split(',')[0]))
+        cpu_sy = re.sub('.*[ :]', '', re.sub('%.*', '', a.split(',')[1]))
+        cpu_id = re.sub('.*[ :]', '', re.sub('%.*', '', a.split(',')[2]))
+        cpu_wa = 0 # CPU wait not available from top on Mac
+    else:
+        raise Exception, "Unrecognized os.uname()[0] = " + str(os.uname()[0])
 
-    return memstr0, memstr1, numoffile, \
+    return int(mem_virtual), int(mem_resident), numoffile, \
            cpu_us, cpu_sy, cpu_id, cpu_wa
 
 
@@ -130,7 +137,7 @@ signal.signal(signal.SIGHUP, handler)
 signal.signal(signal.SIGTERM, handler)
 signal.signal(signal.SIGINT, handler)
 
-mylabel="memory footprint of "+procnam
+mylabel="memory footprint of casapy"
 t1=time.time()
 y1=0.5
 y2=0.5
@@ -144,19 +151,18 @@ fd.write('#time(s) memory_virtual(Mbytes) memory_resident(Mbytes) no_filedesc CP
 i=0
 
 while True:
-	y1,y2,nfile, cpu_us, cpu_sy, cpu_id, cpu_wa = getmem(procnam)
+	y1,y2,nfile, cpu_us, cpu_sy, cpu_id, cpu_wa = getmem(casapy_pid)
     	if (y1 > 0.0):
         	time.sleep(2.0)
                 tt = time.time()-t1
 	        t.append(tt)
-        	if y1 > 2.5e9: y1=2.5e9
-        	if y2 > 2.5e9: y2=2.5e9
+
 		y11.append(y1)
 		y22.append(y2)
                 numfile.append(nfile)
                 fd.write(str(int(round(tt, 0))) + ' ' + \
-                         str(int(round(y1/1000000, 0))) + ' ' + \
-                         str(int(round(y2/1000000, 0))) + ' ' + \
+                         str(int(round(y1/1024, 0))) + ' ' + \
+                         str(int(round(y2/1024, 0))) + ' ' + \
                          str(nfile) + ' ' + \
                          str(cpu_us) + ' ' + \
                          str(cpu_sy) + ' ' + \
