16dff9aca8be56f67e94938cfd80d703cb8a317b
tak tsutsumi <ttsutsum@nrao.edu>
2010-08-26 22:43:37 +0000

    New Development: No             JIRA Issue: Yes CAS-1972         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: No              Module(s): Module Names change impacts.            Description: cleaned up restoreOverlappingModels
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@12500 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/synthesis/implement/MeasurementComponents/MFCleanImageSkyModel.cc
+++ code/synthesis/implement/MeasurementComponents/MFCleanImageSkyModel.cc
@@ -803,12 +803,11 @@ void MFCleanImageSkyModel::restoreOverlappingModels(){
   for (Int model=0;model<(numberOfModels()-1); ++model) {
     CoordinateSystem cs0=image(model).coordinates();
     IPosition iblc0(image(model).shape().nelements(),0);
+    IPosition itrc0(image(model).shape());
+    itrc0=itrc0-Int(1);
+    LCBox lbox0(iblc0, itrc0, image(model).shape());
+    ImageRegion imagreg0(WCBox(lbox0, cs0));
 
-      IPosition itrc0(image(model).shape());
-      itrc0=itrc0-Int(1);
-      LCBox lbox0(iblc0, itrc0, image(model).shape());
-
-      ImageRegion imagreg0(WCBox(lbox0, cs0));
     for (Int nextmodel=model+1; nextmodel < numberOfModels(); ++nextmodel){
       CoordinateSystem cs=image(nextmodel).coordinates();
       IPosition iblc(image(nextmodel).shape().nelements(),0);
@@ -821,29 +820,17 @@ void MFCleanImageSkyModel::restoreOverlappingModels(){
       ImageRegion imagreg(WCBox(lbox, cs));
       try{
         SubImage<Float> partToMerge(image(nextmodel), imagreg0, True);
-        SubImage<Float> partToMask(image(model), imagreg, True);
+        SubImage<Float> partToUnmask(image(model), imagreg, True);
         
-        LatticeRegion latReg0=imagreg0.toLatticeRegion(deltaImage(nextmodel).coordinates(), deltaImage(nextmodel).shape()
-);
-        ArrayLattice<Bool> pixmerge(latReg0.get());
+        LatticeRegion latReg0=imagreg0.toLatticeRegion(image(nextmodel).coordinates(), image(nextmodel).shape());
         LatticeRegion latReg=imagreg.toLatticeRegion(deltaImage(model).coordinates(), deltaImage(model).shape());
         ArrayLattice<Bool> pixmask(latReg.get());
 
-        LatticeExprNode LEN1 = max(LatticeExpr<Float>(abs(image(model))));
-        Float thisMax = LEN1.getFloat();
-        /////////////////
-	/*
-        Array<Bool> testoo;
-        testoo.assign(pixmask.get());
-        cout << "Images " << model << "  and " << nextmodel << " shape " << pixmask.shape() << "  " << pixmerge.shape() << " number of T " << ntrue(testoo) << endl;
-        */ 
-        ////////////////
-        LatticeExpr<Float> myexpr0(iif(pixmask,partToMerge,partToMask));
-        partToMask.copyData(myexpr0);
+        LatticeExpr<Float> myexpr0(iif(pixmask,partToMerge,partToUnmask));
+        partToUnmask.copyData(myexpr0);
 
       }
       catch(AipsError x){
-        //most probably no overlap
 	/*
            os << LogIO::WARN
               << "no overlap or failure of copying the clean components"
