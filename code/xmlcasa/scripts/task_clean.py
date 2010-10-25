radio-astro/casa

commit be39357a42602bf1766a195544ff8b9528f39c5d

Updates to move us closer to 3.1 release
git-svn-id: https://svn.cv.nrao.edu/svn/casa/stable@13234 ac9c0da5-131f-0410-9009-a9c195f183e6

code/xmlcasa/scripts/task_clean.py
@@ -242,6 +242,7 @@ def clean(vis, imagename,outlierfile, field, spw, selectdata, timerange,
                 maskimage=[]
                 for img in imset.maskimages:
                     maskimage.append(imset.maskimages[img])
+	    casalog.post('Used mask(s) : ' + str(mask) + ' to create mask image(s) : ' + str(maskimage),'INFO');
 
             if dochaniter:
                 imset.checkpsf(chanslice)
@@ -336,6 +337,9 @@ def clean(vis, imagename,outlierfile, field, spw, selectdata, timerange,
                 # imCln.setsmallscalebias(smallscalebias)
                 imCln.settaylorterms(ntaylorterms=nterms,
                                      reffreq=reffreqVal);
+                # forbid pbcorrection with msmfs for now
+                if(pbcor):
+                    raise Exception, 'Primary-beam correction is currently not supported with nterms>1'
 
                 modelimages[0]=modelimages[0]+'.tt'+str(0);
                 restoredimage[0]=restoredimage[0]+'.tt'+str(0);

casacore/VERSION
@@ -1,1 +1,1 @@
-1.0.34.15 Thu 2010/10/21 01:16:52 UTC
+1.0.34.16 Fri 2010/10/22 00:09:52 UTC

casacore/images/Images/ComponentImager.cc
@@ -181,8 +181,9 @@ void ComponentImager::project(ImageInterface<Float>& image, const ComponentList&
   {
     Unit imageUnit = image.units();
     const String& imageUnitName = imageUnit.getName();
-    UnitMap::putUser("pixel", UnitVal(pixelLatSize.radian() * 
+    UnitMap::putUser("1pixel", UnitVal(pixelLatSize.radian() * 
 				      pixelLongSize.radian(), "rad.rad"));
+    // redefine is required to reset Unit Cache 
     const Unit pixel("pixel");
     if (imageUnitName.contains("pixel")) {
       // Get the new definition of the imageUnit which uses the new
@@ -219,9 +220,9 @@ void ComponentImager::project(ImageInterface<Float>& image, const ComponentList&
     // understand that Jy/pixel.pixel == Jy.
     const Unit jy("Jy");
     os << "Adding components to image with units [" << fluxUnits.getName() << "]" << LogIO::POST;
-    if (fluxUnits.getName()=="Jy/pixel.pixel") {
-      fluxUnits=jy;
-    }
+//    if (fluxUnits.getName()=="Jy/pixel.pixel") {
+//      fluxUnits=jy;
+//    }
     if (fluxUnits != jy) {
       os << LogIO::WARN 
 	 << "Image units [" << fluxUnits.getName() << "] are not dimensionally equivalent to "

casacore/images/Images/ImageInputProcessor.cc
@@ -140,7 +140,7 @@ void ImageInputProcessor::_process(
     		stokes, metaData, image->name(), stokesControl
     	);
     	Vector<Double> boxCorners = _setBoxCorners(box);
-    	_setRegion(
+        _setRegion(
     		regionRecord, diagnostics, boxCorners,
     		chanEndPts, polEndPts, metaData, image
     	);
@@ -246,7 +246,7 @@ Vector<uInt> ImageInputProcessor::_setSpectralRanges(
 	}
 
     // First split on commas
-    Vector<String> parts = stringToVector(specification, ',');
+    Vector<String> parts = stringToVector(specification, Regex("[,;]"));
     ranges.resize(2*parts.size());
 	Regex regexuInt("^[0-9]+$");
 	Regex regexRange("^[0-9]+[ \n\t\r\v\f]*~[ \n\t\r\v\f]*[0-9]+$");
@@ -431,10 +431,9 @@ Vector<uInt> ImageInputProcessor::_setPolarizationRanges(
 
 		return ranges;
 	}
-    // First split on commas. Commas seem to have been supported at some point
+    // First split on commas and semi-colons.
 	// in the past for polarization specification.
-    Vector<String> parts = stringToVector(specification, ',');
-
+    Vector<String> parts = stringToVector(specification, Regex("[,;]"));
     Vector<String> polNames = Stokes::allNames(False);
     uInt nNames = polNames.size();
     Vector<uInt> nameLengths(nNames);
@@ -599,8 +598,8 @@ void ImageInputProcessor::_setRegion(
 	LogOrigin origin("ImageInputProcessor", method);
 	*_log << origin;
     IPosition imShape = image->shape();
-    Vector<Double> blc(imShape.nelements());
-    Vector<Double> trc(imShape.nelements());
+    Vector<Double> blc(imShape.nelements(), 0);
+    Vector<Double> trc(imShape.nelements(), 0);
     CoordinateSystem csys = image->coordinates();
     Vector<Int> directionAxisNumbers = md.directionAxesNumbers();
     Int spectralAxisNumber = md.spectralAxisNumber();
@@ -626,8 +625,6 @@ void ImageInputProcessor::_setRegion(
     	yCorners[i] = y;
     }
 
-    RegionManager rm;
-    ImageRegion imRegion;
     Vector<Double> polEndPtsDouble(polEndPts.size());
     for (uInt i=0; i<polEndPts.size(); i++) {
     	polEndPtsDouble[i] = (Double)polEndPts[i];
@@ -637,27 +634,6 @@ void ImageInputProcessor::_setRegion(
     for (uInt i=0; i<chanEndPts.size(); i++) {
     	chanEndPtsDouble[i] = (Double)chanEndPts[i];
     }
-
-    HashMap<uInt, Vector<Double> > axisCornerMap;
-    for (Int axisNumber=0; axisNumber<(Int)image->ndim(); axisNumber++) {
-    	if (directionAxisNumbers.size() > 1 && axisNumber == directionAxisNumbers[0]) {
-			axisCornerMap(axisNumber) = xCorners;
-    	}
-    	else if (directionAxisNumbers.size() > 1 && axisNumber == directionAxisNumbers[1]) {
-    		axisCornerMap(axisNumber) = yCorners;
-    	}
-    	else if (axisNumber == spectralAxisNumber) {
-    		axisCornerMap(axisNumber) = chanEndPtsDouble;
-    	}
-    	else if (axisNumber == polarizationAxisNumber) {
-    		axisCornerMap(axisNumber) = polEndPtsDouble;
-    	}
-    	else {
-    		*_log << "Unhandled image axis number " << axisNumber
-    			<< LogIO::EXCEPTION;
-    	}
-    }
-
     uInt nRegions = 1;
     if (md.hasDirectionCoordinate()) {
     	nRegions *= boxCorners.size()/4;
@@ -668,20 +644,46 @@ void ImageInputProcessor::_setRegion(
     if (md.hasSpectralAxis()) {
     	nRegions *= chanEndPts.size()/2;
     }
-
+    _fillVector(xCorners, nRegions);
+    _fillVector(yCorners, nRegions);
+    _fillVector(polEndPtsDouble, nRegions);
+    _fillVector(chanEndPtsDouble, nRegions);
+    HashMap<uInt, Vector<Double> > axisCornerMap;
+    for (uInt i=0; i<nRegions; i++) {
+        for (uInt axisNumber=0; axisNumber<image->ndim(); axisNumber++) {
+    	    if (directionAxisNumbers.size() > 1 && axisNumber == directionAxisNumbers[0]) {
+			    axisCornerMap(axisNumber) = xCorners;
+    	    }
+    	    else if (directionAxisNumbers.size() > 1 && axisNumber == directionAxisNumbers[1]) {
+    		    axisCornerMap(axisNumber) = yCorners;
+    	    }
+    	    else if (axisNumber == spectralAxisNumber) {
+    		    axisCornerMap(axisNumber) = chanEndPtsDouble;
+    	    }
+    	    else if (axisNumber == polarizationAxisNumber) {
+    		    axisCornerMap(axisNumber) = polEndPtsDouble;
+    	    }
+    	    else {
+    		    *_log << "Unhandled image axis number " << axisNumber
+    			    << LogIO::EXCEPTION;
+    	    }
+        }
+    }
+  
+    RegionManager rm;
+    ImageRegion imRegion;
     for (uInt i=0; i<nRegions; i++) {
     	for (uInt axisNumber=0; axisNumber<image->ndim(); axisNumber++) {
     		blc(axisNumber) = axisCornerMap(axisNumber)[2*i];
     		trc(axisNumber) = axisCornerMap(axisNumber)[2*i + 1];
     	}
-		LCBox lcBox(blc, trc, imShape);
+        LCBox lcBox(blc, trc, imShape);
 		WCBox wcBox(lcBox, csys);
 		ImageRegion thisRegion(wcBox);
 		imRegion = (i == 0)
 			? thisRegion
 			: imRegion = *rm.doUnion(imRegion, thisRegion);
     }
-
     ostringstream os;
 
     os << "Used image region from " << endl;
@@ -789,4 +791,15 @@ void ImageInputProcessor::checkOutputs(
 	}
 }
 
-
+void ImageInputProcessor::_fillVector(
+    Vector<Double> myVector, const uInt nRegions
+) const {
+    if (myVector.size() > 0) {
+        Vector<Double> vCopy = myVector.copy();
+        myVector.resize(nRegions*2, True);
+        for (uInt j=vCopy.size(); j<myVector.size(); j++) {
+            myVector[j] = vCopy[j % vCopy.size()];
+        }
+    }
+}
+ 

casacore/images/Images/ImageInputProcessor.h
@@ -186,6 +186,11 @@ private:
 
     Bool _isFractionalRegion(const Record& region) const;
 
+    void _fillVector(
+        Vector<Double> myVector, const uInt nRegions
+    ) const;
+
+
 };
 
 }

casacore/images/Images/test/tImageCollapser.cc
@@ -166,14 +166,14 @@ int main() {
     	{
     		writeTestString("sum subimage collapse along axis 1");
     		ImageCollapser *collapser = new ImageCollapser(
-    			"sum", goodImage, "", "1,1,2,2", "1-2",
+    			"sum", goodImage, "", "1,1,2,2", "1~2",
     			"qu", "", 2, outname(), False
     		);
     		collapser->collapse(False);
     		delete collapser;
     		// and check that we can overwrite the previous output
     		collapser = new ImageCollapser(
-        		"sum", goodImage, "", "1,1,2,2", "1-2",
+        		"sum", goodImage, "", "1,1,2,2", "1~2",
         		"qu", "", 1, outname(), True
         	);
     		collapser->collapse(False);

casacore/images/Images/test/tImageInputProcessor.cc
@@ -36,8 +36,9 @@ void writeTestString(const String& test) {
 void _checkCorner(const Record& gotRecord, const Vector<Double>& expected) {
 	for (uInt i=0; i<expected.size(); i++) {
 		uInt fieldNumber = i+1;
-		Double got = gotRecord.asRecord(RecordFieldId("*" + String::toString(fieldNumber)))
-			.asDouble(RecordFieldId("value"));
+		Double got = gotRecord.asRecord(RecordFieldId(
+				"*" + String::toString(fieldNumber))
+			).asDouble(RecordFieldId("value"));
 	    AlwaysAssert(fabs((got-expected[i])/expected[i]) < 1e-9, AipsError);
 	}
 }
@@ -90,7 +91,7 @@ void testSuccess(
 	ImageInterface<Float> *image = 0;
 	Record region;
 	String diagnostics;
-	writeTestString("Valid box specification succeeds");
+	writeTestString(test);
 	processor.process(
 		image, region, diagnostics, outputStruct, stokes,
         imagename, regionPtr, regionName, box, chans,
@@ -114,7 +115,7 @@ void runProcess(
 	ImageInterface<Float> *image = 0;
 	Record region;
 	String diagnostics;
-	writeTestString("Valid box specification succeeds");
+	writeTestString(test);
 	processor.process(
 		image, region, diagnostics, outputStruct, stokes,
         imagename, regionPtr, regionName, box, chans,
@@ -126,8 +127,6 @@ void runProcess(
 int main() {
     try {
     	String goodImage = "image_input_processor.im";
-
-
     	ImageInputProcessor processor;
     	ImageInterface<Float> *image = 0;
     	Record region;
@@ -335,6 +334,24 @@ int main() {
         	"0", none, ImageInputProcessor::USE_ALL_STOKES, True,
         	expectedBlc, expectedTrc
         );
+    	testSuccess(
+    		"Valid box specification with valid channel specification #3 succeeds",
+        	0, goodImage, 0, "", "0, 0,  10,10",
+        	"0,0,0", none, ImageInputProcessor::USE_ALL_STOKES, True,
+        	expectedBlc, expectedTrc
+        );
+    	testSuccess(
+    		"Valid box specification with valid channel specification #4 succeeds",
+        	0, goodImage, 0, "", "0, 0,  10,10",
+        	"0;0;0", none, ImageInputProcessor::USE_ALL_STOKES, True,
+        	expectedBlc, expectedTrc
+        );
+    	testSuccess(
+    		"Valid box specification with valid channel specification #5 succeeds",
+        	0, goodImage, 0, "", "0, 0,  10,10",
+        	"0,0;0", none, ImageInputProcessor::USE_ALL_STOKES, True,
+        	expectedBlc, expectedTrc
+        );
         stokes = "QVIU";
         testSuccess(
         	"Valid box specification with valid stokes specification #1 succeeds",
@@ -359,6 +376,39 @@ int main() {
         		"Valid box specification with valid stokes specification #3 succeeds",
         		0, goodImage, 0, "", "0, 0,  10,10",
         		"", stokes, ImageInputProcessor::USE_ALL_STOKES, True,
+            	expectedBlc, expectedTrc
+        	);
+        }
+        {
+        	expectedBlc[3] = 1;
+        	expectedTrc[3] = 3;
+            stokes = "Q,I,U";
+            testSuccess(
+        		"Valid box specification with valid stokes specification #4 succeeds",
+        		0, goodImage, 0, "", "0, 0,  10,10", "",
+        		stokes, ImageInputProcessor::USE_ALL_STOKES, True,
+            	expectedBlc, expectedTrc
+        	);
+        }
+        {
+        	expectedBlc[3] = 1;
+        	expectedTrc[3] = 3;
+            stokes = "Q;I;U";
+            testSuccess(
+        		"Valid box specification with valid stokes specification #5 succeeds",
+        		0, goodImage, 0, "", "0, 0,  10,10", "",
+        		stokes, ImageInputProcessor::USE_ALL_STOKES, True,
+            	expectedBlc, expectedTrc
+        	);
+        }
+        {
+        	expectedBlc[3] = 1;
+        	expectedTrc[3] = 3;
+            stokes = "Q,I;U";
+            testSuccess(
+        		"Valid box specification with valid stokes specification #6 succeeds",
+        		0, goodImage, 0, "", "0, 0,  10,10", "",
+        		stokes, ImageInputProcessor::USE_ALL_STOKES, True,
             	expectedBlc, expectedTrc
         	);
         }

casacore/ms/MeasurementSets/MSLister.cc
@@ -542,7 +542,7 @@ void MSLister::listData(const int pageRows,
         throw(AipsError(errmsg));
       }
       else
-        cout << "Writing output to file: " << listfile << endl;
+        cerr << "Writing output to file: " << listfile << endl;
 
       file.open(listfile.data());
       myout.rdbuf(file.rdbuf());   // DON'T redirect cout to file!
@@ -585,10 +585,17 @@ void MSLister::listData(const int pageRows,
     sort[0] = MS::ANTENNA1;
     sort[1] = MS::ANTENNA2;
     Double timeInterval = 300; // 5 minutes
+    if(pMSSel_p->isNull()){
+        return;
+    }
     MSIter msIter(*pMSSel_p, sort, timeInterval);
     for (msIter.origin(); msIter.more(); msIter++){
-
       MS splitMS = msIter.table();
+      if(splitMS.isNull()){
+        break;
+      }
+      if (!splitMS.nrow())
+          break;
       //myout << "splitMS.nrow()=" << splitMS.nrow() << endl;
 
       getRanges(splitMS);
@@ -999,7 +1006,8 @@ void MSLister::listData(const int pageRows,
 
         // The spectral window ID for this row of the MS is 'spwinid(tableRow)'.
 
-        for (uInt rowCL = 0; rowCL < spwRows; rowCL++) if (chanList_p(rowCL,0) == spwinid(tableRow)) {
+        for (uInt rowCL = 0; rowCL < spwRows; rowCL++){
+           if (chanList_p(rowCL,0) == spwinid(tableRow)) {
             // 'rowCL' is the present row of the 'chanList_p' Matrix.
             // chanList_p(rowCL,0) is the SpwID of the current MS row.
 
@@ -1058,7 +1066,10 @@ void MSLister::listData(const int pageRows,
                 myout.precision(precWeight_p);
                 myout.width(wWeight_p);   myout << weight(IPosition(2,indexPols_p(ipol),tableRow));
                 myout.setf(ios::right); myout.width(2);
-                myout << flagSym(flag(IPosition(3,indexPols_p(ipol),ichan,tableRow)));
+                if(flag(IPosition(3,indexPols_p(ipol),ichan,tableRow)))
+                   myout << flagSym(1);
+                else 
+                   myout << flagSym(0);
                 // Print all loop indices; useful for debugging these loops.
                 // myout << "ipol= " << ipol << " ichan= " << ichan << " rowCL= " << rowCL << " tableRow= " << tableRow;
               } // pol loop
@@ -1068,7 +1079,8 @@ void MSLister::listData(const int pageRows,
               }
               myout << endl;
             } // chan loop
-            if (endOutput) {break;} // break out of spw loop
+          }
+          if (endOutput) {break;} // break out of spw loop
 
           } // spw loop
         if (endOutput) {break;} // break out of row loop

commit 5623e945207b5887c25525d10ae3904f0f9a8127

New Development: No             JIRA Issue: No         Ready for Test: Yes      Interface Changes: No What Interface Changed:          Test Programs: asdm-imexport+analysis regression (runs clean), test_clean   Put in Release Notes: No              Module(s): xmlcasa            Description: Fixed bug introduced in r13234 which made                         clean fail when mask was set to a single                         component, e.g. [10,10,20,20] and not a list                         of components.
git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@13265 ac9c0da5-131f-0410-9009-a9c195f183e6

code/xmlcasa/scripts/task_clean.py
@@ -242,7 +242,7 @@ def clean(vis, imagename,outlierfile, field, spw, selectdata, timerange,
                 maskimage=[]
                 for img in sorted(imset.maskimages):
                     maskimage.append(imset.maskimages[img])
-	    if(len(mask) and len(mask[0])):
+	    if(type(mask)==type([]) and len(mask)):
 	        casalog.post('Used mask(s) : ' + str(mask) + ' to create mask image(s) : ' + str(maskimage),'INFO');
 
             if dochaniter: