a18524d75a1e05324d461f52c2f20a161695463d
rob reid <rreid@nrao.edu>
2010-11-02 03:18:38 +0000

    New Development: Yes             JIRA Issue: Yes CAS-1672         Ready for Test: Yes (but no rush)      Interface Changes: No What Interface Changed:          Test Programs:   Put in Release Notes: No              Module(s): xmlcasa            Description: Doc string improvements
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@13345 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/scripts/recipes/ephemerides/request.py
+++ code/xmlcasa/scripts/recipes/ephemerides/request.py
@@ -1,21 +1,14 @@
-import os
-import re
-import smtplib
-import socket
-import time
-from email.mime.text import MIMEText
-
 """
-Utilities for having JPL-Horizons ephemerides mailed to you (or your enemies).
+Utilities for having JPL-Horizons ephemerides mailed to you.
 See JPL_ephem_reader.py for doing something with them.
 
 Examples:
 
 import recipes.ephemerides.request as jplreq
 
-# I recommend you not ask for more than ~18 months of anything with date_incr
-# ~ 1h, because the result would be split into multiple emails which you would
-# have to stitch together.
+# I recommend you not ask for more than ~18 months of anything with
+# date_incr ~ 1h, because the result would be split into multiple
+# emails which you would have to stitch together.
 
 for thing in jplreq.asteroids.keys() + jplreq.planets_and_moons.keys():
     jplreq.request_from_JPL(thing, '2012-12-31')
@@ -25,6 +18,13 @@ for thing in jplreq.asteroids.keys() + jplreq.planets_and_moons.keys():
     if thing not in jplreq.default_date_incrs:
         jplreq.request_from_JPL(thing, '2012-12-31')
 """
+
+import os
+import re
+import smtplib
+import socket
+import time
+from email.mime.text import MIMEText
 
 # Maps from object names to numbers that JPL-Horizons will recognize without
 # fussing around with choosing between barycenters, substrings, etc..
