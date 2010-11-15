a8cd14fff91bf47539b52538b8cae852daceed93
sanjay bhatnagar <sbhatnag@nrao.edu>
2010-11-15 16:26:20 +0000

    New Development: Yes             JIRA Issue: Yes CAS-2609         Ready for Test: No      Interface Changes: Yes What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: No              Module(s): Module Names change impacts.            Description: Describe your changes here...
    Class to encapsulate the antenna aperture illumination pattern for EVLA.
    	
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@13436 ac9c0da5-131f-0410-9009-a9c195f183e6

---
+++ code/synthesis/implement/MeasurementComponents/EVLAAperture.h
@@ -0,0 +1,94 @@
+// -*- C++ -*-
+//# EVLAAperture.h: Definition of the EVLAAperture class
+//# Copyright (C) 1997,1998,1999,2000,2001,2002,2003
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//# $Id$
+//
+#ifndef SYNTHESIS_EVLAAPERTURE_H
+#define SYNTHESIS_EVLAAPERTURE_H
+
+#include <images/Images/ImageInterface.h>
+#include <synthesis/MeasurementComponents/Utils.h>
+#include <synthesis/MeasurementComponents/BeamCalc.h>
+#include <synthesis/MeasurementComponents/CFStore.h>
+#include <synthesis/MeasurementComponents/ATerm.h>
+#include <synthesis/MeasurementComponents/VLACalcIlluminationConvFunc.h>
+//#include <synthesis/MeasurementComponents/PixelatedConvFunc.h>
+#include <synthesis/MeasurementComponents/ConvolutionFunction.h>
+#include <coordinates/Coordinates/DirectionCoordinate.h>
+#include <coordinates/Coordinates/SpectralCoordinate.h>
+#include <coordinates/Coordinates/StokesCoordinate.h>
+#include <lattices/Lattices/LatticeFFT.h>
+#include <casa/Logging/LogIO.h>
+#include <casa/Logging/LogSink.h>
+#include <casa/Logging/LogOrigin.h>
+
+namespace casa { //# NAMESPACE CASA - BEGIN
+  template<class T> class ImageInterface;
+  template<class T> class Matrix;
+  class VisBuffer;
+  class EVLAAperture : public ATerm
+  //: public PixelatedConvFunc<Complex>
+  {
+  public:
+    EVLAAperture():     
+      ATerm(),bandID_p(-1), polMap_p(), feedStokes_p()
+    {};
+    ~EVLAAperture() {};
+    EVLAAperture& operator=(const EVLAAperture& other);
+    Int getVLABandID(Double& freq,String&telescopeName);
+    Bool findSupport(Array<Complex>& func, Float& threshold,Int& origin, Int& R);
+    void makeConvFunction(const ImageInterface<Complex>& image,
+			  const VisBuffer& vb,
+			  const Int wConvSize,
+			  const Float pa,
+			  CFStore& cfs,
+			  CFStore& cfwts);
+    int getVisParams(const VisBuffer& vb);
+    Int makePBPolnCoords(const VisBuffer&vb,
+			 const Vector<Int>& polMap,
+			 const Int& convSize,
+			 const Int& convSampling,
+			 const CoordinateSystem& skyCoord,
+			 const Int& skyNx, const Int& skyNy,
+			 CoordinateSystem& feedCoord,
+			 Vector<Int>& cfStokes);
+    //
+    // Overloading these functions from ConvolutionFunction class
+    //
+    void setPolMap(const Vector<Int>& polMap);
+    void setFeedStokes(const Vector<Int>& feedStokes);
+  private:
+    Int bandID_p;
+    Float Diameter_p, Nant_p, HPBW, sigma;
+    
+    LogIO& logIO() {return logIO_p;}
+    LogIO logIO_p;
+    Vector<Int> polMap_p;
+    Vector<Int> feedStokes_p;
+    //    CountedPtr<ATerm> ATerm_p;
+  };
+};
+#endif
