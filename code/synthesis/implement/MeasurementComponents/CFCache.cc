689c5604fae32b91f5c6382fc2ea26e8237fe10e
sanjay bhatnagar <sbhatnag@nrao.edu>
2010-12-02 17:02:05 +0000

    New Development: No             JIRA Issue: Yes CAS-2609         Ready for Test: No      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: No              Module(s): Module Names change impacts.            Description: Describe your changes here...
    Cleaned up loging and log messages.  Realized the having private
    (protected) LogIO object results into confused LogOrigin strings in
    the log message.  It is much better to just instantiate a LogIO object
    at the beginning of each method - that way one automatically gets the
    stack-unwinding for log messages as well (and hence corrected
    LogOrigin strings).  I personally prefere that log message strings
    either be accurate and have human understandable info. or not be there
    at all (other possibilites waste time for more than one reason).
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@13626 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/synthesis/implement/MeasurementComponents/CFCache.cc
+++ code/synthesis/implement/MeasurementComponents/CFCache.cc
@@ -48,8 +48,9 @@ namespace casa{
   //
   void CFCache::initCache()
   {
-    LogOrigin logOrigin("CFCache","initCache");
-    logIO() << logOrigin << LogIO::NORMAL;
+    LogOrigin logOrigin("CFCache", "initCache");
+    LogIO log_l(logOrigin);
+
     ostringstream name;
     String line;
     Directory dirObj(Dir);
@@ -260,7 +261,7 @@ namespace casa{
 				  Float convSampling, String nameQualifier,
 				  Bool savePA)
   {
-    logIO() << LogOrigin("CFCache","cacheConvFunction") << LogIO::NORMAL;
+    LogIO log_l(LogOrigin("CFCache","cacheConvFunction"));
     if (Dir.length() == 0) return;
     
     if (which < 0) 
@@ -354,7 +355,8 @@ namespace casa{
   //
   void CFCache::flush()
   {
-    logIO() << LogOrigin("CFCache","flush") << LogIO::NORMAL;
+    LogIO log_l(LogOrigin("CFCache", "flush"));
+
     if (Dir.length() == 0) return;
     ostringstream name;
     
@@ -386,7 +388,8 @@ namespace casa{
   //
   void CFCache::flush(ImageInterface<Float>& avgPB)
   {
-    logIO() << LogOrigin("CFCache","flush") << LogIO::NORMAL;
+    LogIO log_l(LogOrigin("CFCache", "flush"));
+
     if (Dir.length() == 0) return;
     flush();
     ostringstream Name;
@@ -407,7 +410,8 @@ namespace casa{
   //
   Int CFCache::loadAvgPB(ImageInterface<Float>& avgPB)
   {
-    logIO() << LogOrigin("CFCache","loadAvgPB") << LogIO::NORMAL;
+    LogIO log_l(LogOrigin("CFCache", "loadAvgPB"));
+
     if (Dir.length() == 0) 
       throw(SynthesisFTMachineError("Cache dir. name null"));
 
@@ -447,7 +451,8 @@ namespace casa{
 			     Int Nw, CFStoreCacheType &convFuncCache,
 			     CFStore& cfs, String prefix)
   {
-    logIO() << LogOrigin("CFCache","loadFromDisk") << LogIO::NORMAL;
+    LogIO log_l(LogOrigin("CFCache", "loadFromDisk"));
+
     Vector<Int> xconvSupport,yconvSupport;;
     Vector<Float> convSampling;
     Double cfRefFreq; CoordinateSystem coordSys;
@@ -543,8 +548,8 @@ namespace casa{
 				  const Int Nw, const Float pa, const Float dPA,
 				  const Int mosXPos, const Int mosYPos)
   {
-    logIO() << LogOrigin("CFCache","locateConvFunction")
-	    << LogIO::NORMAL;
+    LogIO log_l(LogOrigin("CFCache", "locatedConvFunction"));
+
     Int paKey,retVal=NOTCACHED; 
     Bool found;
     // Search for the PA corresponding to the supplied VB to find a
@@ -562,8 +567,8 @@ namespace casa{
 	    {
 	      //	      *****************	    convWeights.reference(*convWeightsCache[PAIndex]);
 	      if (paKey < (Int)memCache_p.nelements())
-	      	logIO() << "Loaded from disk cache: Conv. func. # "
-	      		<< paKey << LogIO::POST;
+	      	log_l << "Loaded from disk cache: Conv. func. # "
+		      << paKey << LogIO::POST;
 	      else
 	      	throw(SynthesisFTMachineError("Internal error: paKey out of range"));
 	      //	      break;
