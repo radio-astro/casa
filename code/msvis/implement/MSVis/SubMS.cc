811f408c8a5ec32f2a0a0b00c50c49195b3c51f3
rob reid <rreid@nrao.edu>
2011-01-14 00:07:01 +0000

    New Development: No             JIRA Issue: No         Ready for Test: Yes      Interface Changes: No What Interface Changed:          Test Programs: test_split   Put in Release Notes: No              Module(s): msvis            Description: .h: Comments out a couple of unused includes. 	   		.c: Jeff found a Boolean return value that wasn't 			    being passed up the line.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@13915 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/msvis/implement/MSVis/SubMS.cc
+++ code/msvis/implement/MSVis/SubMS.cc
@@ -640,8 +640,8 @@ Bool SubMS::getCorrMaps(MSSelection& mssel, const MeasurementSet& ms,
 
       if(!makeSelection()){
         os << LogIO::SEVERE 
-           << "Failed on selection: combination of spw, field, antenna, correlation and/or timerange"
-           << " may be invalid." 
+           << "Failed on selection: the combination of spw, field, antenna, correlation, "
+           << "and timerange may be invalid." 
            << LogIO::POST;
         ms_p=MeasurementSet();
         return False;
@@ -906,7 +906,7 @@ Bool SubMS::fillAllTables(const Vector<MS::PredefinedColumns>& datacols)
   if(timeBin_p <= 0.0)
     success &= fillMainTable(datacols);
   else
-    fillAverMainTable(datacols);
+    success &= fillAverMainTable(datacols);
   return success;
 }
   
@@ -7223,7 +7223,7 @@ Bool SubMS::doTimeAver(const Vector<MS::PredefinedColumns>& dataColNames)
   //                    chanStep_p[spwind], spw_p[spwind]);
   vi.selectChannel(chanSlices_p);
   vi.selectCorrelation(corrSlices_p);
-  VisBuffer vb(vi);
+  //VisBuffer avb(vi);
 
   Vector<Int> spwindex(max(spw_p) + 1);
   spwindex.set(-1);
