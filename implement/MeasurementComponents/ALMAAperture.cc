fbde78723a51bba511c94cdfcb3f3f9c2f98ae7a
dirk petry <dpetry@eso.org>
2011-02-17 10:56:47 +0000

    New Development: Yes             JIRA Issue: CAS-2653         Ready for Test: No      Interface Changes: Yes What Interface Changed: new class ALMAAperture          Test Programs: none, yet   Put in Release Notes: No              Module(s): synthesis            Description: Initial compiling version with several 			gaps (not yet included in CMakeList.txt)
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active/code/synthesis@14160 ac9c0da5-131f-0410-9009-a9c195f183e6

---
+++ implement/MeasurementComponents/ALMAAperture.cc
@@ -0,0 +1,294 @@
+//# ALMAAperture.cc: Implementation of the ALMAAperture class
+//# Copyright (C) 1997,1998,1999,2000,2001,2002,2003
+//# Associated Universities, Inc. Washington DC, USA.
+//# Copyright (C) 2011 by ESO (in the framework of the ALMA collaboration)
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//# $Id$
+//
+#include <synthesis/MeasurementComponents/Utils.h>
+#include <synthesis/MeasurementComponents/ALMAAperture.h>
+#include <synthesis/MeasurementComponents/SynthesisError.h>
+#include <synthesis/MeasurementComponents/BeamCalc.h>
+#include <synthesis/MeasurementComponents/WTerm.h>
+#include <synthesis/MeasurementComponents/ALMACalcIlluminationConvFunc.h>
+#include <coordinates/Coordinates/DirectionCoordinate.h>
+#include <coordinates/Coordinates/SpectralCoordinate.h>
+#include <coordinates/Coordinates/StokesCoordinate.h>
+
+namespace casa{
+
+  AntennaResponses* ALMAAperture::aR_p = 0;
+
+  ALMAAperture::ALMAAperture(): 
+    ATerm(),
+    polMap_p(),
+    antTypeMap_p(),
+    pairTypeToCFKeyMap_p(-1)
+  {
+
+    haveCannedResponses_p = True;
+
+    if(!aR_p){
+      aR_p = new AntennaResponses();
+    }
+
+    if(!aR_p->isInit()){ // the shared antenna responses object was not yet initialised
+
+      String antRespPath;
+      if(!MeasTable::AntennaResponsesPath(antRespPath, "ALMA")) {
+	// unknown observatory
+	logIO() << LogOrigin("ALMAAperture", "ctor")
+		<< LogIO::WARN
+		<< "We don't have predefined antenna responses for ALMA at any frequency."
+		<< endl << "Will try to use raytracing instead."
+		<< LogIO::POST;
+	haveCannedResponses_p = False;
+      }
+      else if(!aR_p->init(antRespPath)){
+	// init failed
+	String mesg="Initialisation of antenna responses for observatory ALMA failed using path "+antRespPath;
+	SynthesisError err(mesg);
+	throw(err);
+      }
+      else if(MeasTable::AntennaResponsesPath(antRespPath, "ACA")) {
+	// also have responses for ACA
+	aR_p->append(antRespPath); // dont't throw if this fails because the ACA antennas
+	                          // may already be in the ALMA table
+      }
+    }
+	 
+  }
+  
+  ALMAAperture& ALMAAperture::operator=(const ALMAAperture& other)
+  {
+    if(this!=&other) 
+      {
+        // aR_p does not need to be copied because it is static (shared)
+	haveCannedResponses_p = other.haveCannedResponses_p;
+ 	polMap_p.assign(other.polMap_p);
+// 	Diameter_p=other.Diameter_p;
+// 	Nant_p=other.Nant_p;
+// 	HPBW=other.HPBW;
+// 	sigma=other.sigma;
+      }
+    return *this;
+  }
+
+  Int ALMAAperture::getVisParams(const VisBuffer& vb)
+  {
+    Vector<String> telescopeNames=vb.msColumns().observation().telescopeName().getColumn();
+    for(uInt nt=0;nt<telescopeNames.nelements();nt++){
+      if ((telescopeNames(nt) != "ALMA") && (telescopeNames(nt) != "ACA")){
+	String mesg="Can handle only ALMA and ACA antennas.\n";
+	mesg += "Erroneous telescope name = " + telescopeNames(nt) + ".";
+	SynthesisError err(mesg);
+	throw(err);
+      }
+    }
+
+    MVFrequency FreqQ(vb.msColumns().spectralWindow().refFrequencyQuant()(0));
+    Double Freq = FreqQ.getValue();
+
+    Int bandID = -1;
+    if(haveCannedResponses_p){
+      String bandName;
+      if(aR_p->getBandName(bandName, "ALMA", FreqQ)){
+	bandID = atoi(bandName.substr(bandName.find("and")+3).c_str()); // band names start with "band" 
+      }
+      else{
+	logIO() << LogOrigin("ALMAAperture", "getVisParams")
+		<< LogIO::WARN
+		<< "We don't have predefined antenna responses for ALMA at "
+		<< Freq << " Hz. Will try to use raytracing instead."
+		<< LogIO::POST;
+      }      
+    }
+    return bandID;
+  }
+  
+  
+  void ALMAAperture::applySky(ImageInterface<Complex>& outImages,  // the image (cube if there is more than one pol)
+			                                           // upon return: the images (cubes) multiplied by the 
+			                                           // different rotated, regridded,
+			                                           // FTed convolution pairs of AIFs  
+			      const VisBuffer& vb, // for the parallactic angle
+			      const Bool doSquint,
+			      const Int& cfKey)
+  {
+
+    if(getVisParams(vb)==-1){ // need to use ray tracing
+//       ALMACalcIlluminationConvFunc almaPB;
+//       Long cachesize=(HostInfo::memoryTotal(true)/8)*1024;
+//       almaPB.setMaximumCacheSize(cachesize);
+//       almaPB.applyPB(outImages, vb, doSquint);
+    }
+    else{ // use canned antenna responses
+      // extract matrix from image
+      //   (issue warning if too coarse => will be in method ATerm::OK(vb, PAtolerance, timetolerance)
+      //   identify the right AIF based on antenna, freq band, polarizations
+      //   Form all convolution pairs (Efield patterns) of antenna types in the Array
+      //   FT the Efield patterns
+      //   rotate using par. angle,
+      //   then regrid it to the image, 
+      //   multiply with image
+    }
+
+  }
+
+  void ALMAAperture::applySky(ImageInterface<Float>& outImages,
+			      const VisBuffer& vb, 
+			      const Bool doSquint,
+			      const Int& cfKey)
+  {
+    if(getVisParams(vb)==-1){ // need to use ray tracing
+//       ALMACalcIlluminationConvFunc almaPB;
+//       Long cachesize=(HostInfo::memoryTotal(true)/8)*1024;
+//       almaPB.setMaximumCacheSize(cachesize);
+//       almaPB.applyPB(outImages, vb, doSquint);
+    }
+    else{ // use canned antenna responses
+
+    }
+  }
+
+  Int ALMAAperture::makePBPolnCoords(const VisBuffer&vb,
+				     const Int& convSize,
+				     const Int& convSampling,
+				     const CoordinateSystem& skyCoord,
+				     const Int& skyNx, const Int& skyNy,
+				     CoordinateSystem& feedCoord)
+  {
+    feedCoord = skyCoord;
+    //
+    // Make a two dimensional image to calculate auto-correlation of
+    // the ideal illumination pattern. We want this on a fine grid in
+    // the UV plane
+    //
+    Int directionIndex=skyCoord.findCoordinate(Coordinate::DIRECTION);
+    AlwaysAssert(directionIndex>=0, AipsError);
+    DirectionCoordinate dc=skyCoord.directionCoordinate(directionIndex);
+    Vector<Double> sampling;
+    sampling = dc.increment();
+    sampling*=Double(convSampling);
+    sampling*=Double(skyNx)/Double(convSize);
+    dc.setIncrement(sampling);
+    
+    
+    Vector<Double> unitVec(2);
+    unitVec=convSize/2;
+    dc.setReferencePixel(unitVec);
+    
+    // Set the reference value to that of the image
+    feedCoord.replaceCoordinate(dc, directionIndex);
+
+    //
+    // Make an image with linear polarization axis.
+    //
+    Int NPol=0,M,N=0;
+    M=polMap_p.nelements();
+    for(Int i=0;i<M;i++) if (polMap_p(i) > -1) NPol++;
+    Vector<Int> poln(NPol);
+    
+    Int index;
+    Vector<Int> inStokes;
+    index = feedCoord.findCoordinate(Coordinate::STOKES);
+    inStokes = feedCoord.stokesCoordinate(index).stokes();
+    N = 0;
+    try
+      {
+	for(Int i=0;i<M;i++) if (polMap_p(i) > -1) {poln(N) = vb.corrType()(i);N++;}
+	StokesCoordinate polnCoord(poln);
+	Int StokesIndex = feedCoord.findCoordinate(Coordinate::STOKES);
+	feedCoord.replaceCoordinate(polnCoord,StokesIndex);
+      }
+    catch(AipsError& x)
+      {
+	throw(SynthesisFTMachineError("Likely cause: Discrepancy between the poln. "
+				      "axis of the data and the image specifications."));
+      }
+    
+    return NPol;
+  }
+
+
+  Vector<Int> ALMAAperture::vbRow2CFKeyMap(const VisBuffer& vb, Int& nUnique) 
+  {
+    // return index to outputImages for each row in vb
+    Vector<Int> map; 
+    map.resize(vb.nRow()); 
+
+    if(haveCannedResponses_p){
+      // distinguish different antenna types
+      if(antTypeMap_p.nelements()!=vb.numberAnt()){
+	antTypeMap_p.assign(antTypeMap(vb));
+      }
+      pairTypeToCFKeyMap_p.clear();
+      Int cfKeyCount = 0;
+      for(uInt i=0; i<vb.nRow(); i++){
+	Int aT1 = antTypeMap_p(vb.antenna1()(i));
+	Int aT2 = antTypeMap_p(vb.antenna2()(i));
+	Int pair = min(aT1, aT2) + 10000*max(aT1, aT2); // order doesn't matter, convolution commutes
+	map(i) = pairTypeToCFKeyMap_p(pair);
+	if(map(i)<0){ // new pair type
+	  pairTypeToCFKeyMap_p.define(pair, cfKeyCount);
+	  map(i) = cfKeyCount;
+	  cfKeyCount++;
+	}
+      }  
+      nUnique = cfKeyCount;
+    }
+    else{ // raytracing knows only one antenna type
+      map=0; 
+      nUnique=1; 
+    }
+    return map;
+  }
+
+  Vector<Int> ALMAAperture::antTypeMap(const VisBuffer& vb){
+    Vector<Int> map(vb.numberAnt());
+    for(uInt i; i<map.nelements(); i++){
+      map(i) = antTypeFromName(vb.msColumns().antenna().name()(i));
+      if(map(i)<0){
+	logIO() << LogOrigin("ALMAAperture", "antTypeMap")
+		<< LogIO::WARN 
+		<< "Unrecognised antenna type for antenna \"" 
+		<< vb.msColumns().antenna().name()(i) << "\""  
+		<< LogIO::POST;
+	map(i) = -1;
+      }
+    }
+    return map;
+  }
+
+  Int ALMAAperture::antTypeFromName(const String& name){
+    Int rval = -1;
+    String typeN = name.substr(0,2);
+    if(typeN=="DV") rval=0;
+    else if(typeN=="DA") rval=1;
+    else if(typeN=="PM") rval=2;
+    else if(typeN=="CM") rval=3;
+    return rval;
+  }
+
+};
