460bec06d1efdd3e316be65b465067cbe45ffd02
kumar golap <kgolap@nrao.edu>
2011-04-29 01:42:48 +0000

    New Development: No             JIRA Issue: CAS-3114         Ready for Test: Yes (for the next release not this one)      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: No              Module(s): Module Names change impacts.            Description: Describe your changes here...
    Used the new and improved method to capture what is the storage managers and its accessors. This removes a "feature" memory bloat that was discovered on some machines only
    	
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@14936 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/msvis/implement/MSVis/VisSet.cc
+++ code/msvis/implement/MSVis/VisSet.cc
@@ -671,7 +671,7 @@ void VisSet::addCalSet(MeasurementSet& ms, Bool compress) {
             ms.addColumn(tdModelScale, modelScaleStMan);
    
 
-            TiledShapeStMan modelCompStMan("", modelTileShape);
+            TiledShapeStMan modelCompStMan("ModelCompTiled", modelTileShape);
             ms.addColumn(tdModelComp, modelCompStMan);
             ms.addColumn(tdModel, *ccModel);
 
@@ -707,15 +707,15 @@ void VisSet::addCalSet(MeasurementSet& ms, Bool compress) {
             StandardStMan corrScaleStMan("CorrScaleOffset");
             ms.addColumn(tdCorrScale, corrScaleStMan);
 
-            TiledShapeStMan corrCompStMan("", corrTileShape);
+            TiledShapeStMan corrCompStMan("CorrectedCompTiled", corrTileShape);
             ms.addColumn(tdCorrComp, corrCompStMan);
             ms.addColumn(tdCorr, *ccCorr);
 
         } else {
-            TiledShapeStMan corrStMan("", corrTileShape);
+            TiledShapeStMan corrStMan("CorrectedTiled", corrTileShape);
             ms.addColumn(tdCorr, corrStMan);
         };
-        MeasurementSet::addColumnToDesc(td, MeasurementSet::CORRECTED_DATA, 2);
+        //MeasurementSet::addColumnToDesc(td, MeasurementSet::CORRECTED_DATA, 2);
 
         if (ccModel) delete ccModel;
         if (ccCorr) delete ccCorr;
@@ -921,7 +921,7 @@ void VisSet::addCalSet2(MeasurementSet& ms, Bool compress) {
 	ms.addColumn(tdModelScale, modelScaleStMan);
 	
 	
-	TiledShapeStMan modelCompStMan("", modelTileShape);
+	TiledShapeStMan modelCompStMan("ModelCompTiled", modelTileShape);
 	ms.addColumn(tdModelComp, modelCompStMan);
 	ms.addColumn(tdModel, *ccModel);
 	
@@ -955,15 +955,15 @@ void VisSet::addCalSet2(MeasurementSet& ms, Bool compress) {
 	StandardStMan corrScaleStMan("CorrScaleOffset");
 	ms.addColumn(tdCorrScale, corrScaleStMan);
 	
-	TiledShapeStMan corrCompStMan("", corrTileShape);
+	TiledShapeStMan corrCompStMan("CorrectedCompTiled", corrTileShape);
 	ms.addColumn(tdCorrComp, corrCompStMan);
 	ms.addColumn(tdCorr, *ccCorr);
 	
       } else {
-	TiledShapeStMan corrStMan("", corrTileShape);
+	TiledShapeStMan corrStMan("CorrectedTiled", corrTileShape);
 	ms.addColumn(tdCorr, corrStMan);
       };
-      MeasurementSet::addColumnToDesc(td, MeasurementSet::CORRECTED_DATA, 2);
+      //MeasurementSet::addColumnToDesc(td, MeasurementSet::CORRECTED_DATA, 2);
       
       if (ccModel) delete ccModel;
       if (ccCorr) delete ccCorr;
