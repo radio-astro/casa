c427a3a2cf32cfbfeffa3c25aba7b3a9fe0473fc
scott rankin <srankin@nrao.edu>
2011-05-11 23:25:14 +0000

    Clearing the way for a new release build.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/release/current@15061 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/scripts/recipes/ephemerides/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-A package for requesting ephemerides of Solar System objects from
-JPL-Horizons, and, after the results arrive by email, installing
-them as CASA tables in
-os.getenv('CASAPATH').split()[0] + '/data/ephemerides/JPL-Horizons/'.
-"""
-
-import request, JPLephem_reader
