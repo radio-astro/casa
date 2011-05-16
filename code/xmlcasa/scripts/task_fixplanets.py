f552babee76f4920f82cb8b1353354b027a458a4
dirk petry <dpetry@eso.org>
2011-05-16 10:17:38 +0000

    New Development: Yes             JIRA Issue: CSV-600         Ready for Test: Yes      Interface Changes: Yes What Interface Changed: additional parameter "direction"          Test Programs: test_fixplanet   Put in Release Notes: Yes              Module(s): xmlcasa            Description: When ALMA data with the phasedir values 			for ephemeris objects !=(0,0) becomes available 			there is the problem that these cannot be 			concatenated properly. 			The new parameter "direction" in fixplanets 			makes this possible by enabling the user to 			control the position entries. 			This can also be used to control these entries 			in general.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@15108 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/scripts/task_fixplanets.py
+++ code/xmlcasa/scripts/task_fixplanets.py
@@ -1,9 +1,39 @@
 from taskinit import *
 
-# Fix FIELD, SOURCE, and UVW for given fields based on pointing table information
-# example: fixplanets('uid___A002_X1c6e54_X223.ms', 'Titan', True)
+def fixplanets(vis, field, fixuvw=False, direction=''):
+    """
+    Fix FIELD, SOURCE, and UVW for given fields based on pointing table information
 
-def fixplanets(vis, field, fixuvw=False):
+    Input Parameters
+    vis        -- Name of the input visibility set
+    
+    field      -- field selection string
+    
+    fixuvw     -- recalc uvw coordinate? (default: False)
+
+    direction  -- if set, don't use pointing table but set direction to this value
+               example: 'J2000 19h30m00 -40d00m00', default= '' (use pointing table)
+
+    Examples:
+
+    fixplanets('uid___A002_X1c6e54_X223.ms', 'Titan', True)
+          will look up the pointing direction for field 'Titan' in the POINTING table
+          based on the median time in unflagged main table rows for this field,
+          enter this direction in the FIELD and SOURCE tables, and then recalculate
+          the UVW coordinates for this field.
+
+    fixplanets('uid___A002_X1c6e54_X223.ms', 'Titan', False, 'J2000 12h30m15 -02d12m00')
+          will set the directions for field 'Titan' in the FIELD and SOURCE table to the
+          given direction and not recalculate the UVW coordinates.
+          (This can be useful for several purposes, among them preparing a concatenation
+          of datasets. Only fields with the same direction will be recognised as identical.
+          fixplanets can then be run again after the concatenation using parameters as in
+          the first example above.)
+
+    """
+
+    tbt = casac.homefinder.find_home_by_name('tableHome').create()
+
     casalog.origin('fixplanets')
     try:
         fields = ms.msseltoindex(vis=vis,field=field)['field']
@@ -11,94 +41,196 @@ def fixplanets(vis, field, fixuvw=False):
         if(len(fields) == 0):
             casalog.post( "Field selection returned zero results.", 'WARN')
             return
-        tb.open(vis+"/FIELD")
+        tbt.open(vis+"/FIELD")
         oldrefcol = []
-        if('PhaseDir_Ref' in tb.colnames()):
-            oldrefcol = tb.getcol('PhaseDir_Ref')
-        tb.close()
+        if('PhaseDir_Ref' in tbt.colnames()):
+            oldrefcol = tbt.getcol('PhaseDir_Ref')
+        tbt.close()
         for fld in fields:
-            os.system('rm -rf fixplanetstemp')
-            tb.open(vis)
-            tb.query('FIELD_ID=='+str(fld)+' AND FLAG_ROW==False', name='fixplanetstemp', columns='TIME')
-            tb.close()
-            tb.open('fixplanetstemp')
-            mytime = tb.getcell('TIME',tb.nrows()/2)
-            casalog.post( "TIME "+str(mytime), 'NORMAL')
-            tb.close()
+            thenewra_rad = 0.
+            thenewdec_rad = 0.
+            thenewref = -1
+            thenewrefstr = ''
+            if(direction==''):
+                os.system('rm -rf fixplanetstemp')
+                tbt.open(vis)
+                tbt.query('FIELD_ID=='+str(fld)+' AND FLAG_ROW==False', name='fixplanetstemp', columns='TIME')
+                tbt.close()
+                tbt.open('fixplanetstemp')
+                thetime = tbt.getcell('TIME',tbt.nrows()/2)
+                casalog.post( "TIME "+str(thetime), 'NORMAL')
+                tbt.close()
 
-            tb.open(vis+'/POINTING')
-            ttb = tb.query('TRACKING==True AND NEARABS(TIME,'+str(mytime)+',INTERVAL/2.)',
-                           name='fixplanetstemp2')
-            if(ttb.nrows()==0):
-                del ttb
-                os.system('rm -rf fixplanetstemp2')
-                ttb = tb.query('TRACKING==True AND NEARABS(TIME,'+str(mytime)+',3.)', # search within 3 seconds
-                           name='fixplanetstemp2')
+                tbt.open(vis+'/POINTING')
+                ttb = tbt.query('TRACKING==True AND NEARABS(TIME,'+str(thetime)+',INTERVAL/2.)',
+                                name='fixplanetstemp2')
                 if(ttb.nrows()==0):
-                    del ttb
-                    casalog.post( "Cannot find any POINTING table rows with TRACKING==True within 3 seconds of TIME "+str(mytime), 'WARN')
-                    casalog.post( "Will try without requiring TRACKING==True ...", 'WARN')
+                    ttb.close()
                     os.system('rm -rf fixplanetstemp2')
-                    ttb = tb.query('NEARABS(TIME,'+str(mytime)+',INTERVAL/2.)', 
-                                   name='fixplanetstemp2')
-                    if(ttb.nrows()==0):
-                        del ttb
+                    ttb2 = tbt.query('TRACKING==True AND NEARABS(TIME,'+str(thetime)+',3.)', # search within 3 seconds
+                                     name='fixplanetstemp2')
+                    if(ttb2.nrows()==0):
+                        ttb2.close()
+                        casalog.post( "Cannot find any POINTING table rows with TRACKING==True within 3 seconds of TIME "+str(thetime), 'NORMAL')
+                        casalog.post( "Will try without requiring TRACKING==True ...", 'NORMAL')
                         os.system('rm -rf fixplanetstemp2')
-                        ttb = tb.query('NEARABS(TIME,'+str(mytime)+',3.)', # search within 3 seconds
-                                       name='fixplanetstemp2')
-                        if(ttb.nrows()==0):
-                            del ttb
-                            tb.close()
-                            casalog.post( "Cannot find any POINTING table rows within 3 seconds of TIME "+str(mytime), 'SEVERE')
-                            return # give up
-            del ttb
-            tb.close()
-            tb.open('fixplanetstemp2')
-            mydir = tb.getcell('DIRECTION',0)
-            tb.close()
-            casalog.post( ' field id '+str(fld)+ ' AZ EL '+str(mydir[0])+" "+str(mydir[1]), 'NORMAL')
-            mydirme = me.direction(rf='AZELGEO',v0=qa.quantity(mydir[0][0], 'rad'), v1=qa.quantity(mydir[1][0],'rad'))
-            #print mydirme
-            me.doframe(me.epoch(rf='UTC', v0=qa.quantity(mytime,'s')))
-            tb.open(vis+'/OBSERVATION')
-            myobs = tb.getcell('TELESCOPE_NAME',0)
-            tb.close()
-            casalog.post( "Observatory is " + myobs, 'NORMAL')
-            me.doframe(me.observatory(myobs))
-            mydirmemod = me.measure(v=mydirme, rf='J2000')
-            #print mydirmemod
-            mynewra_rad = mydirmemod['m0']['value']
-            mynewdec_rad = mydirmemod['m1']['value']
-            me.done()
+                        ttb3 = tbt.query('NEARABS(TIME,'+str(thetime)+',INTERVAL/2.)', 
+                                         name='fixplanetstemp2')
+                        if(ttb3.nrows()==0):
+                            ttb3.close()
+                            os.system('rm -rf fixplanetstemp2')
+                            ttb4 = tbt.query('NEARABS(TIME,'+str(thetime)+',3.)', # search within 3 seconds
+                                             name='fixplanetstemp2')
+                            if(ttb4.nrows()==0):
+                                ttb4.close()
+                                tbt.close()
+                                casalog.post( "Cannot find any POINTING table rows within 3 seconds of TIME "+str(thetime), 'SEVERE')
+                                return False # give up
+                else:
+                    ttb.close()
+                tbt.close()
+                tbt.open('fixplanetstemp2')
+                thedir = tbt.getcell('DIRECTION',0)
+                tbt.close()
+                casalog.post( ' field id '+str(fld)+ ' AZ EL '+str(thedir[0])+" "+str(thedir[1]), 'NORMAL')
+                thedirme = me.direction(rf='AZELGEO',v0=qa.quantity(thedir[0][0], 'rad'), v1=qa.quantity(thedir[1][0],'rad'))
+                #print thedirme
+                me.doframe(me.epoch(rf='UTC', v0=qa.quantity(thetime,'s')))
+                tbt.open(vis+'/OBSERVATION')
+                theobs = tbt.getcell('TELESCOPE_NAME',0)
+                tbt.close()
+                casalog.post( "Observatory is " + theobs, 'NORMAL')
+                me.doframe(me.observatory(theobs))
+                thedirmemod = me.measure(v=thedirme, rf='J2000')
+                #print thedirmemod
+                thenewra_rad = thedirmemod['m0']['value']
+                thenewdec_rad = thedirmemod['m1']['value']
+                me.done()
+            else: # direction is not an empty string
+                if(type(direction)==str):
+                    dirstr = direction.split(' ')
+                    codes = []
+                    if len(dirstr)==2:
+                        dirstr = ['J2000', dirstr[0], dirstr[1]]
+                        casalog.post('Assuming reference frame J2000 for parameter \'direction\'', 'NORMAL')
+                    elif not len(dirstr)==3:
+                        casalog.post('Incorrect format in parameter \'direction\'', 'SEVERE')
+                        return False
+                    try:
+                        thedir = me.direction(dirstr[0], dirstr[1], dirstr[2])
+                        thenewra_rad = thedir['m0']['value']
+                        thenewdec_rad = thedir['m1']['value']
+                    except Exception, instance:
+                        casalog.post("*** Error \'%s\' when interpreting parameter \'direction\': " % (instance), 'SEVERE')
+                        return False
+                    try:
+                        tbt.open(vis+"/FIELD")
+                        thenewrefindex = tbt.getcolkeyword('PHASE_DIR', 'MEASINFO')['TabRefTypes'].tolist().index(dirstr[0])
+                        thenewref = tbt.getcolkeyword('PHASE_DIR', 'MEASINFO')['TabRefCodes'][thenewrefindex]
+                        thenewrefstr = dirstr[0]
+                        tbt.close()
+                    except Exception, instance:
+                        casalog.post("PHASE_DIR is not a variable reference column. Will leave reference as is.", 'WARN')
+                        thenewref = -1
+                    if(0<thenewref and thenewref<32):
+                        casalog.post("*** Error when interpreting parameter \'direction\':\n presently only J2000 and solar system objects are supported.",
+                                     'SEVERE')
+                        return False
+            #endif
             
             # modify FIELD table
-            tb.open(vis+'/FIELD', nomodify=False)
-            numfields = tb.nrows()
-            myolddir = tb.getcell('PHASE_DIR',fld)
-            planetname = tb.getcell('NAME',fld)
+            tbt.open(vis+'/FIELD', nomodify=False)
+            numfields = tbt.nrows()
+            theolddir = tbt.getcell('PHASE_DIR',fld)
+            planetname = tbt.getcell('NAME',fld)
             casalog.post( 'object: '+planetname, 'NORMAL')
-            casalog.post( 'old RA, DEC (rad) '+str(myolddir[0])+" " +str(myolddir[1]), 'NORMAL')
-            casalog.post( 'new RA, DEC (rad) '+str(mynewra_rad)+" "+ str(mynewdec_rad), 'NORMAL')
-            mynewdir = myolddir
-            mynewdir[0][0] = mynewra_rad
-            mynewdir[1][0] = mynewdec_rad
-            tb.putcell('PHASE_DIR',fld, mynewdir)
-            if('PhaseDir_Ref' in tb.colnames()):
-                tb.putcell('PhaseDir_Ref',fld, 0) # J2000
-            tb.close()
+            casalog.post( 'old RA, DEC (rad) '+str(theolddir[0])+" " +str(theolddir[1]), 'NORMAL')
+            casalog.post( 'new RA, DEC (rad) '+str(thenewra_rad)+" "+ str(thenewdec_rad), 'NORMAL')
+
+            pcol = tbt.getcol('PHASE_DIR')
+            pcol[0][0][fld] = thenewra_rad
+            pcol[1][0][fld] = thenewdec_rad
+            tbt.putcol('PHASE_DIR',pcol)
+
+            pcol = tbt.getcol('DELAY_DIR')
+            pcol[0][0][fld] = thenewra_rad
+            pcol[1][0][fld] = thenewdec_rad
+            tbt.putcol('DELAY_DIR',pcol)
+
+            pcol = tbt.getcol('REFERENCE_DIR')
+            pcol[0][0][fld] = thenewra_rad
+            pcol[1][0][fld] = thenewdec_rad
+            tbt.putcol('REFERENCE_DIR',pcol)
+            casalog.post("FIELD table PHASE_DIR, DELAY_DIR, and REFERENCE_DIR columns changed for field "+str(fld)+".", 'NORMAL')
+
+            if(thenewref!=-1):
+                # modify reference of the direction columns permanently
+                theoldref = -1
+                theoldref2 = -1
+                theoldref3 = -1
+                try:
+                    pcol = tbt.getcol('PhaseDir_Ref')
+                    theoldref = pcol[fld]
+                    pcol[fld] = thenewref
+                    oldrefcol = pcol
+                        
+                    pcol2 = tbt.getcol('DelayDir_Ref')
+                    theoldref2 = pcol2[fld]
+                    pcol2[fld] = thenewref
+                    
+                    pcol3 = tbt.getcol('RefDir_Ref')
+                    theoldref3 = pcol3[fld]
+                    pcol3[fld] = thenewref
+
+                    if not (theoldref==theoldref2 and theoldref==theoldref3):
+                        casalog.post("The three FIELD table direction reference frame entries for field "+str(fld)
+                                     +" are not identical in the input data: "
+                                     +str(theoldref)+", "+str(theoldref2)+", "+str(theoldref3)
+                                     +". Will try to continue ...", 'WARN')
+                        theoldref=-1
+                    else:
+                        oldrefstrindex = tbt.getcolkeyword('PHASE_DIR', 'MEASINFO')['TabRefCodes'].tolist().index(theoldref)
+                        oldrefstr = tbt.getcolkeyword('PHASE_DIR', 'MEASINFO')['TabRefTypes'][oldrefstrindex]
+                        casalog.post("Original FIELD table direction reference frame entries for field "+str(fld)
+                                     +" are all "+str(theoldref)+" ("+oldrefstr+")", 'NORMAL')
+                    tbt.putcol('PhaseDir_Ref', pcol)
+                    tbt.putcol('DelayDir_Ref', pcol2)
+                    tbt.putcol('RefDir_Ref', pcol3)
+
+                except Exception, instance:
+                    casalog.post("*** Error \'%s\' when writing reference frames in FIELD table: " % (instance),
+                                 'SEVERE')
+                    return False
+                if(theoldref != thenewref):
+                    casalog.post("FIELD table direction reference frame entries for field "+str(fld)
+                                 +" set to "+str(thenewref)+" ("+thenewrefstr+")", 'NORMAL')
+                else:
+                    casalog.post("FIELD table direction reference frame entries for field "+str(fld)
+                                 +" unchanged.", 'NORMAL')                        
+                
+
+            if(fixuvw and (oldrefcol!=[]) and (thenewref>0)): 
+                # modify reference of phase dir for fixuvw
+                pcol = tbt.getcol('PhaseDir_Ref')
+                pcol[fld] = 0 # J2000
+                tbt.putcol('PhaseDir_Ref', pcol) # J2000
+
+            tbt.close()
+
             #modify SOURCE table
-            tb.open(vis+'/SOURCE', nomodify=False)
-            sdir = tb.getcol('DIRECTION')
+            tbt.open(vis+'/SOURCE', nomodify=False)
+            sdir = tbt.getcol('DIRECTION')
             newsdir = sdir
-            sname = tb.getcol('NAME')
-            for i in xrange(0,tb.nrows()):
+            sname = tbt.getcol('NAME')
+            for i in xrange(0,tbt.nrows()):
                 if(sname[i]==planetname):
                     #print 'i old dir ', i, " ", sdir[0][i], sdir[1][i]
-                    newsdir[0][i] = mynewra_rad
-                    newsdir[1][i] = mynewdec_rad
+                    newsdir[0][i] = thenewra_rad
+                    newsdir[1][i] = thenewdec_rad
                     #print '  new dir ', newsdir[0][i], newsdir[1][i]
-            tb.putcol('DIRECTION', newsdir)
-            tb.close()
+            tbt.putcol('DIRECTION', newsdir)
+            tbt.close()
+            casalog.post("SOURCE table DIRECTION column changed.", 'NORMAL')
                 
         os.system('rm -rf fixplanetstemp*')
 
@@ -117,15 +249,18 @@ def fixplanets(vis, field, fixuvw=False):
             im.open(vis, usescratch=True) # usescratch=True needed in order to have writable ms
             im.calcuvw(fldids, refcode='J2000', reuse=False)
             im.close()
-        else:
-            casalog.post("UVW coordinates not fixed", 'NORMAL')
 
-        if not oldrefcol==[]: 
-            tb.open(vis+'/FIELD', nomodify=False)
-            tb.putcol('PhaseDir_Ref', oldrefcol)
-            tb.close()
+            if((oldrefcol!=[]) and (thenewref>0)): 
+                tbt.open(vis+'/FIELD', nomodify=False)
+                tbt.putcol('PhaseDir_Ref', oldrefcol)
+                tbt.close()
+
+        else:
+            casalog.post("UVW coordinates not changed.", 'NORMAL')
+
+        return True
 
     except Exception, instance:
         casalog.post("*** Error \'%s\' " % (instance), 'SEVERE')
-        return
+        return False
 
