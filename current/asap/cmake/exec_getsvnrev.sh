c1d90106523ed22145a097eaea63bee45371b9f0
scott rankin <srankin@nrao.edu>
2011-05-24 20:13:17 +0000

    Preparing to update the contents of current/.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/stable@15161 ac9c0da5-131f-0410-9009-a9c195f183e6

--- current/asap/cmake/exec_getsvnrev.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-DIR=.
-if [ $# -ge 1 ]; then
-    DIR=$1
-fi
-
-cd ${DIR}
-${DIR}/getsvnrev.sh
--- current/code/xmlcasa/implement/ms/ms_private.h
+++
@@ -1,9 +0,0 @@
-casa::Bool detached();
-casa::Bool ready2write_();
-
-casa::MeasurementSet *itsMS, *itsOriginalMS;
-casa::MSSelection *itsMSS;
-casa::LogIO *itsLog;
-casa::MSSelector *itsSel;
-casa::MSFlagger *itsFlag;
-
