1815fecd1d3e59861844a84de71e6c39591ca0ec
scott rankin <srankin@nrao.edu>
2011-05-25 22:35:59 +0000

    Preparing for first CASA 3.2.1 Release build.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/release/current@15175 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/implement/atmosphere/scripts/AtmosphereTypeTest.py
+++
@@ -1,4 +0,0 @@
-attool = casac.homefinder.find_home_by_name('atmosphereHome')
-at = attool.create()
-print at.listAtmosphereTypes()
-exit()
--- code/xmlcasa/modules/interrupt/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-
-setup( name="interrupt", version="1.0",
-       ext_modules=[Extension("interrupt",
-			      [ "interrupt_python.cc" ],
-			      library_dirs=['/opt/local/lib'],
-			      libraries = [ ])],
-       include_dirs = [ ".", "/opt/local/include" ]
-)
