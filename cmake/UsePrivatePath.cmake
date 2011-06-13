72c54177bba86146ebcc622207ed5ca3ca0a0db7
honglin ye <hye@aoc.nrao.edu>
2011-06-13 15:43:52 +0000

    New Development: Yes             JIRA Issue: No       Ready to Release: No      Interface Changes: No What Interface Changed: None          Test Programs: cmake, make   Put in Release Notes: No              Module(s):            Description: comment out everything from UsePrivatePath.cmake
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active/casacore@15314 ac9c0da5-131f-0410-9009-a9c195f183e6

--- cmake/UsePrivatePath.cmake
+++ cmake/UsePrivatePath.cmake
@@ -1,4 +1,4 @@
 # This file store special settings a developer possiblly need
 # When checking in code, make sure you comment our everything
 
-set (CFITSIO_ROOT_DIR /usr/include/cfitsio)
+#set (CFITSIO_ROOT_DIR /usr/include/cfitsio)
