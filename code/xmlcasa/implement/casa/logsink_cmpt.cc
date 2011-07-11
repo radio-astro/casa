1827bb1618677ad7d01b21573854ff781bcbbeeb
honglin ye <hye@aoc.nrao.edu>
2011-07-11 22:28:35 +0000

    New Development: no             JIRA Issue: cas-1508       Ready to Release: yes      Interface Changes: No What Interface Changed: None          Test Programs: publish_summary.runTest('ngc5921_regression')   Put in Release Notes: No              Module(s): logging            Description: resolve a long standing seg fault in casalog.setglobal(True)
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@15554 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/implement/casa/logsink_cmpt.cc
+++ code/xmlcasa/implement/casa/logsink_cmpt.cc
@@ -231,17 +231,12 @@ bool logsink::setglobal(const bool isglobal)
    bool rstat(true);
    
    if(isglobal){
-      //cout << "isglobal=" << isglobal << endl;
-      //cout << "thelogsink=" << thelogsink << endl;
+      if (thelogsink)
+         return rstat;
       LogSink().globalSink(thelogsink);
-      globalsink = isglobal;
-      //cout << "setglobal(True) ok" << endl;
    } else {
-      //cout << "isglobal=" << isglobal << endl;
       LogSinkInterface *dummy = new StreamLogSink(LogMessage::NORMAL, &cerr);
-      //cout << "dummy==" << dummy << endl;
-      //LogSink().globalSink(dummy);
-      //cout << "setglobal(True) ok" << endl;
+      LogSink().globalSink(dummy);
    }
    return rstat;
 }
