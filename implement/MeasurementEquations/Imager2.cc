f80e4e1ae9c8a67947508f581fc06c19a8aa391a
tak tsutsumi <ttsutsum@nrao.edu>
2011-08-12 22:49:45 +0000

    New Development: No             JIRA Issue: No         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: No              Module(s): Module Names change impacts.            Description: Fixed the problem in frequency/velocity mode, nchan>1                         (in imagecoordinates2's helper function,calcImFreqs).                         Reverted the previous temporary fix by Kumar and                         made to use correct spw selection to frequency calculation.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active/code/synthesis@15888 ac9c0da5-131f-0410-9009-a9c195f183e6

--- implement/MeasurementEquations/Imager2.cc
+++ implement/MeasurementEquations/Imager2.cc
@@ -466,32 +466,31 @@ Bool Imager::imagecoordinates2(CoordinateSystem& coordInfo, const Bool verbose)
       MFrequency::Types mfreqref=(obsFreqRef==(MFrequency::REST)) ? MFrequency::REST : MFrequency::castType(mfImageStart_p.getRef().getType()) ; 
 
       /////Some problem here it is really goofing up in getting frequency
-      /*Vector<Double> imgridfreqs;
+      // -> fixed was made in calcImFreqs -TT 
+      Vector<Double> imgridfreqs;
       Vector<Double> imfreqres;
       //rstate=calcImFreqs(imgridfreqs, imfreqres, mfreqref, obsEpoch, obsPosition,restFreq);
       // should use obsFreqRef
       rstate=calcImFreqs(imgridfreqs, imfreqres, obsFreqRef, obsEpoch, obsPosition,restFreq);
       //cerr<<"imfreqres(0)="<<imfreqres(0)<<endl;
-      */
+      
 
-      //if (imageNchan_p==1) {
+      if (imageNchan_p==1) {
       mySpectral = new SpectralCoordinate(mfreqref,
       					  mfImageStart_p.get("Hz").getValue(),
       					  mfImageStep_p.get("Hz").getValue(),
       					  refChan, restFreq);
-	//}
-      /*
+      }
       else {
-        Double finc= imgridfreqs(1)-imgridfreqs(0); 
+        Double finc= imgridfreqs(1)-imgridfreqs(0);
         mySpectral = new SpectralCoordinate(mfreqref, imgridfreqs(0), finc, refChan, restFreq);
-	cerr << "after myspectral2 " << mySpectral->referenceValue() << " pixel " <<  mySpectral->referencePixel() << endl;
+	//cerr << "after myspectral2 " << mySpectral->referenceValue() << " pixel " <<  mySpectral->referencePixel() << endl;
         //debug TT
         //Double wrld,pixl;
         //pixl=0.0;
         //mySpectral->toWorld(wrld,pixl);
         //cerr<<"world="<<wrld<<" pixel="<<pixl;
       }
-      */
       os << (verbose ? LogIO::NORMAL : LogIO::NORMAL3)
          << "Start frequency = " // Loglevel INFO
 	 << mfImageStart_p.get("GHz").getValue()
@@ -4372,6 +4371,7 @@ Bool Imager::calcImFreqs(Vector<Double>& imgridfreqs,
 
  
   try {
+    /***
     if(spectralwindowids_p.nelements()==1){
       if(spectralwindowids_p[0]<0){
         spectralwindowids_p.resize();
@@ -4383,14 +4383,21 @@ Bool Imager::calcImFreqs(Vector<Double>& imgridfreqs,
         spectralwindowids_p=dataspectralwindowids_p;
       }
     }
-
-    if(spectralwindowids_p.nelements()==1) {
-      oldChanFreqs=msc.spectralWindow().chanFreq()(spectralwindowids_p[0]);  
-      oldFreqResolution=msc.spectralWindow().chanWidth()(spectralwindowids_p[0]);
+    ***/
+    Vector<Int> spwlist; 
+    if (mode=="frequency" || mode=="velocity") {
+       spwlist = dataspectralwindowids_p;
+    }
+    else {
+       spwlist = spectralwindowids_p;
+    }
+    if(spwlist.nelements()==1) {
+      oldChanFreqs=msc.spectralWindow().chanFreq()(spwlist[0]);  
+      oldFreqResolution=msc.spectralWindow().chanWidth()(spwlist[0]);
     }
     else {
       SubMS thems(*ms_p);
-      if(!thems.combineSpws(spectralwindowids_p,True,oldChanFreqs,oldFreqResolution)){
+      if(!thems.combineSpws(spwlist,True,oldChanFreqs,oldFreqResolution)){
         os << LogIO::SEVERE << "Error combining SpWs" << LogIO::POST;
       }
     }
