f834ea2330966e74efaed1965583812cac2583bb
scott rankin <srankin@nrao.edu>
2011-08-19 21:04:37 +0000

    Removing botched 2011-08 stable branch.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/stable@15942 ac9c0da5-131f-0410-9009-a9c195f183e6

--- 2011/08/test/code/xmlcasa/implement/utils/dbus_forward.h
+++
@@ -1,6 +0,0 @@
-// The Forward declarations of the dbus component.
-
-#include <casa/BasicSL/String.h>
-
-#include <casa/namespace.h>
-using namespace std;
--- 2011/08/test/wcslib/doxygen/getwcstab.sed
+++
@@ -1,6 +0,0 @@
-/^[^*]/{p;d;}
-
-/^\*/s| wtbarr| #wtbarr|g
-/^\*/s| #wtbarr_s| wtbarr_s|g
-
-s|'TAB'|'<TT><B>TAB</B></TT>'|g
