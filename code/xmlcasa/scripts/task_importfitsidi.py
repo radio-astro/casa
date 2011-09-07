b59c9257d4123746aae52982a3338f7aa319dee1
rob reid <rreid@nrao.edu>
2011-09-07 14:00:36 +0000

    New Development: Yes             JIRA Issue: Yes CAS-386         Ready for Test: Yes      Interface Changes: No What Interface Changed:          Test Programs: test_importfitsidi   Put in Release Notes: No              Module(s): xmlcasa            Description: Switches importfitsidi to using write_history() and                         a local ms instance.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@16094 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/scripts/task_importfitsidi.py
+++ code/xmlcasa/scripts/task_importfitsidi.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-from taskinit import *
+from taskinit import casalog, mstool, write_history
 
 def importfitsidi(fitsidifile,vis):
 	"""Convert FITS-IDI visibility file into a CASA visibility file (MS).
@@ -14,39 +14,42 @@ def importfitsidi(fitsidifile,vis):
 	"""
 
 	#Python script
+        retval = True
 	try:
 		casalog.origin('importfitsidi')
 		casalog.post("")
+                myms = mstool.create()
 		if(type(fitsidifile)==str):
 			casalog.post('### Reading file '+fitsidifile, 'INFO')
-			ms.fromfitsidi(vis,fitsidifile)
-			ms.close()
+			myms.fromfitsidi(vis,fitsidifile)
+			myms.close()
 		elif(type(fitsidifile)==list):
 			clist = fitsidifile
 			casalog.post('### Reading file '+clist[0], 'INFO')
-			ms.fromfitsidi(vis,clist[0])
-			ms.close()
+			myms.fromfitsidi(vis,clist[0])
+			myms.close()
 			clist.pop(0)
 			tname = '_importfitsidi_tmp_'+vis
 			shutil.rmtree(tname, ignore_errors=True)
 			for fidifile in clist:
 				casalog.post('### Reading file '+fidifile, 'INFO')
-				ms.fromfitsidi(tname,fidifile)
-				ms.close()
-				ms.open(vis, nomodify=False)
-				ms.concatenate(msfile=tname, freqtol='', dirtol='')
-				ms.close()
+				myms.fromfitsidi(tname,fidifile)
+				myms.close()
+				myms.open(vis, nomodify=False)
+				myms.concatenate(msfile=tname, freqtol='', dirtol='')
+				myms.close()
 				shutil.rmtree(tname, ignore_errors=True)
 		else:
                         raise Exception, 'Parameter fitsidifile should be of type str or list'			
 	        # write history
-		if ((type(vis)==str) & (os.path.exists(vis)) & ms.open(vis,nomodify=False)):
-			ms.writehistory(message='taskname     = importfitsidi',origin='importfitsidi')
-			ms.writehistory(message='fitsidifile  = "'+str(fitsidifile)+'"',origin='importfitsidi')
-			ms.writehistory(message='vis          = "'+str(vis)+'"',origin='importfitsidi')
-			ms.close()
-                else:
-                        raise Exception, 'Cannot write to history table of output MS.'
+                try:
+                        param_names = importfitsidi.func_code.co_varnames[:importfitsidi.func_code.co_argcount]
+                        param_vals = [eval(p) for p in param_names]   
+                        retval &= write_history(myms, vis, 'importfitsidi', param_names,
+                                                param_vals, casalog)
+                except Exception, instance:
+                        casalog.post("*** Error \'%s\' updating HISTORY" % (instance),
+                                     'WARN')
 
 	except Exception, instance: 
 		print '*** Error ***',instance
