940730426dd2eb021811b4f974601aefa3447b9f
rob reid <rreid@nrao.edu>
2011-09-13 17:25:17 +0000

    New Development: Yes             JIRA Issue: Yes CAS-3320         Ready for Test: Yes      Interface Changes: No What Interface Changed:          Test Programs: itself   Put in Release Notes: No              Module(s): xmlcasa            Description: clean up
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@16169 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/scripts/tests/test_fixvis.py
+++ code/xmlcasa/scripts/tests/test_fixvis.py
@@ -38,6 +38,7 @@ class fixvis_test1(unittest.TestCase):
         shutil.rmtree(inpms2)
         shutil.rmtree(outms, ignore_errors=True)
         shutil.rmtree(outms2, ignore_errors=True)
+        os.system('rm -rf test[yz]*')
 
     def test1(self):
         '''Do converted directions in the FIELD table have the right shape?'''
@@ -103,20 +104,20 @@ class fixvis_test1(unittest.TestCase):
 
         mystats = ''
         try:
-            self.res = fixvis(inpms2, outms2, field='0', refcode=refcode, phasecenter='J2000 18h00m02.3092s -29d59m29.9987s')
+            self.res = fixvis(inpms2, outms2, field='0', refcode=refcode,
+                              phasecenter='J2000 18h00m02.3092s -29d59m29.9987s')
             retValue = {'success': True, 'msgs': "", 'error_msgs': '' }
             os.system('rm -rf testy*')
-            clean(vis=outms2,imagename='testy', field='0', niter=10,threshold='0.1mJy',psfmode='clark',imagermode='csclean',
-                  ftmachine='ft',mask=True,imsize=[128, 128],cell=['0.100000080arcsec', '0.100000080arcsec'],
-                  weighting='natural',uvtaper=True,
-                  outertaper=[''],innertaper=[])
-
+            clean(vis=outms2, imagename='testy', field='0', niter=10, threshold='0.1mJy',
+                  psfmode='clark', imagermode='csclean', ftmachine='ft', mask=True,
+                  imsize=[128, 128], cell=['0.100000080arcsec', '0.100000080arcsec'],
+                  weighting='natural', uvtaper=True, outertaper=[''],innertaper=[])
             mystats = imstat('testy.image')
         except:
             print "*** Unexpected error ***"
             
-        self.assertTrue(mystats['maxposf']=='18:00:02.309, -29.59.29.999, I, 2.26e+11Hz' and
-                        (mystats['maxpos']==[64,64,0,0]).all())
+        self.assertTrue(mystats['maxposf'] == '18:00:02.309, -29.59.29.999, I, 2.26e+11Hz' and
+                        (mystats['maxpos'] == [64, 64, 0, 0]).all())
 
     def test3(self):
         '''Apply positive phase center shift along DEC.'''
@@ -124,21 +125,23 @@ class fixvis_test1(unittest.TestCase):
         shutil.rmtree(outms2, ignore_errors=True)
         mystats = ''
         try:
-            self.res = fixvis(inpms2, outms2, field='0', refcode=refcode, phasecenter='J2000 18h00m02.3092s -29d59m26.9987s')
+            self.res = fixvis(inpms2, outms2, field='0', refcode=refcode,
+                              phasecenter='J2000 18h00m02.3092s -29d59m26.9987s')
             retValue = {'success': True, 'msgs': "", 'error_msgs': '' }
             os.system('rm -rf testy*')
-            clean(vis=outms2,imagename='testy',field='0',niter=10,threshold='0.1mJy',psfmode='clark',imagermode='csclean',
-                  ftmachine='ft',mask=True,imsize=[128, 128],cell=['0.100000080arcsec', '0.100000080arcsec'],
+            clean(vis=outms2, imagename='testy', field='0', niter=10, threshold='0.1mJy',
+                  psfmode='clark', imagermode='csclean', ftmachine='ft', mask=True,
+                  imsize=[128, 128], cell=['0.100000080arcsec', '0.100000080arcsec'],
                   phasecenter='J2000 18:00:02.30921 -029.59.26.998716',
-                  weighting='natural',uvtaper=True,
-                  outertaper=[''],innertaper=[])
+                  weighting='natural', uvtaper=True,
+                  outertaper=[''], innertaper=[])
 
             mystats = imstat('testy.image')
         except:
             print "*** Unexpected error ***"
             
-        self.assertTrue(mystats['maxposf']=='18:00:02.309, -29.59.29.999, I, 2.26e+11Hz' and
-                        (mystats['maxpos']==[64,34,0,0]).all())
+        self.assertTrue(mystats['maxposf'] == '18:00:02.309, -29.59.29.999, I, 2.26e+11Hz' and
+                        (mystats['maxpos'] == [64,34,0,0]).all())
 
     def test4(self):
         '''Apply negative phase center shift along DEC using offset syntax.'''
@@ -167,13 +170,15 @@ class fixvis_test1(unittest.TestCase):
         shutil.rmtree(outms2, ignore_errors=True)
         mystats = ''
         try:
-            self.res = fixvis(inpms2, outms2, field='0', refcode=refcode, phasecenter='J2000 18h00m02.5401s -29d59m29.9987s')
+            self.res = fixvis(inpms2, outms2, field='0', refcode=refcode,
+                              phasecenter='J2000 18h00m02.5401s -29d59m29.9987s')
+            self.assertTrue(self.res)
             retValue = {'success': True, 'msgs': "", 'error_msgs': '' }
             os.system('rm -rf testy*')
-            clean(vis=outms2,imagename='testy',field='0',niter=10,threshold='0.1mJy',psfmode='clark',imagermode='csclean',
-                  ftmachine='ft',mask=True,imsize=[128, 128],cell=['0.100000080arcsec', '0.100000080arcsec'],
-                  weighting='natural',uvtaper=True,
-                  outertaper=[''],innertaper=[])
+            clean(vis=outms2, imagename='testy', field='0', niter=10, threshold='0.1mJy',
+                  psfmode='clark', imagermode='csclean', ftmachine='ft', mask=True,
+                  imsize=[128, 128], cell=['0.100000080arcsec', '0.100000080arcsec'],
+                  weighting='natural',uvtaper=True, outertaper=[''], innertaper=[])
 
             mystats = imstat('testy.image')
         except:
@@ -189,6 +194,7 @@ class fixvis_test1(unittest.TestCase):
         mystats = ''
         try:
             self.res = fixvis(inpms2, outms2, field='0', refcode=refcode, phasecenter='-0d0m3s 0deg')
+            self.assertTrue(self.res)
             retValue = {'success': True, 'msgs': "", 'error_msgs': '' }
             os.system('rm -rf testy*')
             clean(vis=outms2,imagename='testy',field='0',niter=10,threshold='0.1mJy',psfmode='clark',imagermode='csclean',
@@ -207,24 +213,29 @@ class fixvis_test1(unittest.TestCase):
         '''Apply negative phase shift along RA in field 1 (using offset syntax, offset is a time), no shift in field 0.'''
         refcode = 'J2000'
         shutil.rmtree(outms2, ignore_errors=True)
-        os.system('cp -R '+inpms2+' '+outms2)
+        os.system('cp -R ' + inpms2 + ' ' + outms2)
 
         mystats0 = ''
         mystats1 = ''
         try:
-            x = qa.div(qa.quantity(-3./3600.,'deg'),qa.cos(qa.quantity(31.,'deg')))['value']*24./360.*3600. # (seconds)
-            phc = str(x)+'s 0deg'
-            self.res = fixvis(vis=outms2, outputvis=outms2, field='1', refcode=refcode, phasecenter=phc)
+            x = qa.div(qa.quantity(-3./3600., 'deg'),
+                       qa.cos(qa.quantity(31.,'deg')))['value']*24./360.*3600. # (seconds)
+            phc = str(x) + 's 0deg'
+            self.res = fixvis(vis=outms2, outputvis=outms2, field='1', refcode=refcode,
+                              phasecenter=phc)
+            self.assertTrue(self.res)
             retValue = {'success': True, 'msgs': "", 'error_msgs': '' }
             os.system('rm -rf testy* testz*')
-            clean(vis=outms2,imagename='testy',field='0',niter=10,threshold='0.1mJy',psfmode='clark',imagermode='csclean',
-                  ftmachine='ft',mask=True,imsize=[128, 128],cell=['0.100000080arcsec', '0.100000080arcsec'],
-                  weighting='natural',uvtaper=True,
-                  outertaper=[''],innertaper=[])
-            clean(vis=outms2,imagename='testz',field='1',niter=10,threshold='0.1mJy',psfmode='clark',imagermode='csclean',
-                  ftmachine='ft',mask=True,imsize=[128, 128],cell=['0.100000080arcsec', '0.100000080arcsec'],
-                  weighting='natural',uvtaper=True,
-                  outertaper=[''],innertaper=[])
+            clean(vis=outms2, imagename='testy', field='0', niter=10, threshold='0.1mJy',
+                  psfmode='clark', imagermode='csclean', ftmachine='ft', mask=True,
+                  imsize=[128, 128], cell=['0.100000080arcsec', '0.100000080arcsec'],
+                  weighting='natural',uvtaper=True, outertaper=[''],innertaper=[])
+            clean(vis=outms2, imagename='testz', field='1', niter=10, threshold='0.1mJy',
+                  psfmode='clark',imagermode='csclean',
+                  ftmachine='ft', mask=True, imsize=[128, 128],
+                  cell=['0.100000080arcsec', '0.100000080arcsec'],
+                  weighting='natural', uvtaper=True,
+                  outertaper=[''], innertaper=[])
 
             mystats0 = imstat('testy.image')
             mystats1 = imstat('testz.image')
