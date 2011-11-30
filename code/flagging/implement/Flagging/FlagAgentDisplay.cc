79dd6eeeae7d58b5d69ef84cfe6e0af65aa1c96e
urvashi rao <rurvashi@nrao.edu>
2011-11-30 21:36:06 +0000

    New Development: Yes             JIRA Issue: Yes - CAS-3229         Ready for Test: Yes      Interface Changes: Yes What Interface Changed: New flag agent.          Test Programs: Flagging/test/tFlagAgentDisplay.cc   Put in Release Notes: No              Module(s): Module Names change impacts.            Description: Added a basic Display agent for the new flagger. It is a subset of the old/temporary Display agent and functionality will be added when required info becomes accessible.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@17061 ac9c0da5-131f-0410-9009-a9c195f183e6

---
+++ code/flagging/implement/Flagging/FlagAgentDisplay.cc
@@ -0,0 +1,409 @@
+//# FlagAgentDisplay.cc: This file contains the implementation of the FlagAgentDisplay class.
+//#
+//#  CASA - Common Astronomy Software Applications (http://casa.nrao.edu/)
+//#  Copyright (C) Associated Universities, Inc. Washington DC, USA 2011, All rights reserved.
+//#  Copyright (C) European Southern Observatory, 2011, All rights reserved.
+//#
+//#  This library is free software; you can redistribute it and/or
+//#  modify it under the terms of the GNU Lesser General Public
+//#  License as published by the Free software Foundation; either
+//#  version 2.1 of the License, or (at your option) any later version.
+//#
+//#  This library is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY, without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//#  Lesser General Public License for more details.
+//#
+//#  You should have received a copy of the GNU Lesser General Public
+//#  License along with this library; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+//#  MA 02111-1307  USA
+//# $Id: rurvashi 28 Nov 2011$
+
+#include <flagging/Flagging/FlagAgentDisplay.h>
+
+namespace casa { //# NAMESPACE CASA - BEGIN
+  
+  FlagAgentDisplay::FlagAgentDisplay(FlagDataHandler *dh, Record config, Bool writePrivateFlagCube):
+    FlagAgentBase(dh,config,ANTENNA_PAIRS,writePrivateFlagCube), plotter_p(NULL), ShowPlots(True)
+  {
+    setAgentParameters(config);
+    
+    // Request loading polarization map to FlagDataHandler
+    flagDataHandler_p->setMapPolarizations(true);
+  }
+  
+  FlagAgentDisplay::~FlagAgentDisplay()
+  {
+    // Compiler automagically calls FlagAgentBase::~FlagAgentBase()
+    
+    if(plotter_p != NULL) { plotter_p->done(); plotter_p=NULL; }
+  }
+  
+  void FlagAgentDisplay::setAgentParameters(Record config)
+  {
+    int exists;
+    
+    exists = config.fieldNumber ("pause");
+    if (exists >= 0)
+      {
+	pause_p = config.asBool("pause");
+      }
+    else
+      {
+	pause_p = True;
+      }
+    
+    *logger_p << LogIO::NORMAL << "FlagAgentDisplay::" << __FUNCTION__ << " pause is " << pause_p << LogIO::POST;
+    
+    
+  }
+  
+  void
+  FlagAgentDisplay::computeAntennaPairFlags(VisMapper &visibilities,FlagMapper &flags,Int antenna1,Int antenna2)
+  {
+    // Gather shapes
+    IPosition flagCubeShape = visibilities.shape();
+    uInt nChannels = flagCubeShape(0);
+    uInt nTimes = flagCubeShape(1);
+    Vector<uInt> polarizations = flags.getSelectedCorrelations();
+    nPolarizations_p = polarizations.size();
+    nPolarizations_p = 1; /////// TODO : Get rid of this once I can access multiple pols of the data !
+    
+    // Read antenna names for the current baseline
+    String antenna1Name = flagDataHandler_p->antennaNames_p->operator()(antenna1);
+    String antenna2Name = flagDataHandler_p->antennaNames_p->operator()(antenna2);
+    String baselineName = antenna1Name + "&&" + antenna2Name;
+    
+    // Read current Field name, SPW id, and correlation string from visBuffer Info.
+    ///// TODO : Read this from somewhere !!
+    uInt fieldID = 0;
+    String fieldName("FLD");
+    String spwID("SPW");
+    Vector<String> corrTypes(4);
+    corrTypes[0]=String("RR");  corrTypes[1]=String("RL");
+    corrTypes[2]=String("LR");  corrTypes[3]=String("LL");
+    Vector<Double> freqList(nChannels);
+    for(uInt ch=0;ch<nChannels;ch++) freqList[ch]=(Double)ch;
+    
+    // Print where we are...
+    *logger_p << LogIO::NORMAL << "FlagAgentDisplay::" << __FUNCTION__ << " Baseline : " << baselineName << " Field : " << fieldName << " Spw : " << spwID << LogIO::POST;
+    
+    // Build the Plot Window for the first time
+    if(ShowPlots && plotter_p==NULL) BuildPlotWindow();
+    
+    // Initialize Plot Arrays and other vars
+    char choice = 'a';
+    Float runningsum=0, runningflag=0,runningpreflag=0;
+    Vector<Float> vecflagdat(0), vecdispdat(0);
+    Vector<Float> origspectrum(0), flagspectrum(0), precountspec(0), countspec(0);
+    if(ShowPlots)
+      {
+	vecflagdat.resize(nChannels * nTimes); vecdispdat.resize(nChannels * nTimes); 
+	origspectrum.resize(nChannels); flagspectrum.resize(nChannels);
+	precountspec.resize(nChannels); countspec.resize(nChannels);
+      }
+    
+    if(ShowPlots)
+      {
+	// Make and send plots for each polarization
+	for(int pl=0;pl<nPolarizations_p;pl++)  // Start Correlation Loop
+	  {
+	    runningsum=0; runningflag=0;  runningpreflag=0;
+	    origspectrum=0.0; flagspectrum=0.0; precountspec=0.0; countspec=0.0;
+	    for(int ch=0;ch<nChannels;ch++)  // Start Channel Loop
+	      { 
+		for(uInt tm=0;tm<nTimes;tm++)  // Start Time Loop
+		  {       
+		    // UUU FOR TEST ONLY -- Later, enable additional ManualFlagAgent in the tFlagAgentDisplay
+		    /////if(ch>10 && ch<20) flags.applyFlag(ch,tm);
+		    
+		    vecdispdat( ch*nTimes + tm ) = visibilities(ch,tm) * ( ! flags.getOriginalFlags(pl,ch,tm) );
+		    vecflagdat( ch*nTimes + tm ) = visibilities(ch,tm) * ( ! flags.getModifiedFlags(pl,ch,tm) );
+		    
+		    origspectrum[ch] += visibilities(ch,tm) * ( ! flags.getOriginalFlags(pl,ch,tm) );
+		    flagspectrum[ch] += visibilities(ch,tm) * ( ! flags.getModifiedFlags(pl,ch,tm) );
+		    
+		    precountspec[ch] += ( ! flags.getOriginalFlags(pl,ch,tm) );
+		    countspec[ch] += ( ! flags.getModifiedFlags(pl,ch,tm) );
+		    
+		    runningsum += visibilities(ch,tm);
+		    runningflag += (Float)(flags.getModifiedFlags(pl,ch,tm));
+		    runningpreflag += (Float)(flags.getOriginalFlags(pl,ch,tm));
+		    
+		  }// End Time Loop
+	      }//End Channel Loop
+	    
+	    // Make the Labels
+	    stringstream ostr1,ostr2;
+	    ostr1 << "(" << fieldID << ") " << fieldName << "\n[spw:" << spwID << "] " << baselineName << "  ( " << corrTypes[pl] << " )";
+	    ostr2 << fixed;
+	    ostr2.precision(1);
+	    ostr2 << " flag:" << 100 * runningflag/(nChannels*nTimes) << "% (pre-flag:" << 100 * runningpreflag/(nChannels*nTimes) << "%)";
+	    
+	    *logger_p << "[" << corrTypes[pl] << "]:" << 100 * runningflag/(nChannels*nTimes) << "%(" << 100 * runningpreflag/(nChannels*nTimes) << "%) "; 
+	    
+	    // Make the Before/After Raster Plots
+	    DisplayRaster(nChannels,nTimes,vecdispdat,panels_p[pl].getInt());
+	    plotter_p->setlabel(" ",pl?" ":"Time",ostr1.str(),panels_p[pl].getInt());
+	    DisplayRaster(nChannels,nTimes,vecflagdat,panels_p[pl+nPolarizations_p].getInt());
+	    plotter_p->setlabel("Frequency",pl?" ":"Time",ostr2.str(),panels_p[pl+nPolarizations_p].getInt());
+	   
+	    // Make the Before/After bandpass plots
+	    for(uInt ch=0;ch<nChannels;ch++)
+	      {
+		if(precountspec[ch]==0) {origspectrum[ch]=0.0; precountspec[ch]=1.0;}
+		if(countspec[ch]==0) {flagspectrum[ch]=0.0; countspec[ch]=1.0;}
+	      }
+	    
+	    origspectrum = (origspectrum/precountspec);
+	    flagspectrum = (flagspectrum/countspec);
+	    AlwaysAssert( freqList.nelements()==nChannels , AipsError); 
+	    
+	    DisplayLine(nChannels, freqList, origspectrum, String("before:")+corrTypes[pl], 
+			String("red"), False, panels_p[pl+(2*nPolarizations_p)].getInt());
+	    DisplayScatter(nChannels, freqList, flagspectrum, String("after:")+corrTypes[pl], 
+			   String("blue"), True, panels_p[pl+(2*nPolarizations_p)].getInt());
+
+	    //// TODO : Can I query the tfcrop agent for a "view" to overlay here. 
+	    // If available, get a plot from the agents
+	    /*
+	      for (uInt fmeth=0; fmeth<flagmethods.nelements(); fmeth++)
+	      {
+	      if(flagmethods[fmeth]->getMonitorSpectrum(flagspectrum,pl,bs))
+	      {
+	      //		    flagspectrum = log10(flagspectrum);
+	      DisplayLine(nChannels, freqlist_p, flagspectrum, flagmethods[fmeth]->methodName(), 
+	      String("green"), True, panels_p[pl+(2*nPolarizations_p)].getInt());
+	      }
+	      }
+	    */
+	    
+	  }//End Correlation Loop
+	
+	*logger_p << LogIO::POST;
+	
+	// Wait for User Input
+	choice = GetUserInput();
+	
+	// React to user-input
+	if(choice=='q')
+	  {
+	    ShowPlots = False; 
+	    StopAndExit = True;
+	    cout << "Exiting flagger : Not yet implemented. Will just stop display" << endl;
+	    if(plotter_p!=NULL) { plotter_p->done(); plotter_p=NULL; }
+	    //return False;
+	  }
+	else if(choice=='s')
+	  {
+	    ShowPlots = False;
+	    cout << "Stopping display. Continuing flagging." << endl;
+	    if(plotter_p!=NULL) { plotter_p->done(); plotter_p=NULL; }
+	  }
+	else if(choice=='p')
+	  {
+	    cout << "Previous Baseline : Not Implemented ! " << endl;
+	    //	bs -= 2;
+	    //if(bs < 0) {bs=-1; cout << "Reached first baseline !" << endl; }
+	  }
+	else if(choice=='c')
+	  {
+	    cout << "Next chunk " << endl; // Right now, a chunk is one baseline !
+	    ////break; // break out of baseline loop
+	  }
+	
+      }// end if ShowPlots
+    
+    return;
+  }
+  
+  //----------------------------------------------------------------------------------------------------------
+  
+  /***********************************************************************/  
+  /******************     Plot Functions      ******************************/  
+  /***********************************************************************/  
+  
+  Bool FlagAgentDisplay::BuildPlotWindow()
+  {
+    
+    dock_xml_p = "\
+<?xml version=\"1.0\" encoding=\"UTF-8\"?>	\
+<ui version=\"4.0\">				\
+ <class>dock01</class>				\
+ <widget class=\"QDockWidget\" name=\"dock01\">	\
+  <property name=\"geometry\">			\
+   <rect>					\
+    <x>0</x>					\
+    <y>0</y>					\
+    <width>485</width>				\
+    <height>80</height>				\
+   </rect>					\
+  </property>					\
+  <property name=\"minimumSize\">		\
+   <size>					\
+    <width>485</width>				\
+    <height>80</height>				\
+   </size>							\
+  </property>							\
+  <property name=\"windowTitle\">				\
+   <string/>							\
+  </property>							\
+  <widget class=\"QWidget\" name=\"dockWidgetContents\">	\
+   <layout class=\"QGridLayout\" name=\"gridLayout_2\">		\
+    <item row=\"0\" column=\"0\">				\
+     <layout class=\"QGridLayout\" name=\"gridLayout\">		\
+      <item row=\"0\" column=\"0\">				\
+       <widget class=\"QPushButton\" name=\"PrevBaseline\">	\
+        <property name=\"text\">				\
+         <string>Prev Baseline</string>				\
+        </property>						\
+       </widget>						\
+      </item>							\
+      <item row=\"0\" column=\"1\">				\
+       <widget class=\"QPushButton\" name=\"NextBaseline\">	\
+        <property name=\"text\">				\
+         <string>Next Baseline</string>				\
+        </property>						\
+       </widget>						\
+      </item>							\
+      <item row=\"0\" column=\"2\">				\
+       <widget class=\"QPushButton\" name=\"NextChunk\">	\
+        <property name=\"text\">				\
+         <string>Next Chunk</string>				\
+        </property>						\
+       </widget>						\
+      </item>							\
+      <item row=\"0\" column=\"3\">				\
+       <widget class=\"QPushButton\" name=\"StopDisplay\">	\
+        <property name=\"text\">				\
+         <string>Stop Display</string>				\
+        </property>						\
+       </widget>						\
+      </item>							\
+      <item row=\"0\" column=\"4\">				\
+       <widget class=\"QPushButton\" name=\"Quit\">		\
+        <property name=\"text\">				\
+         <string>Quit</string>					\
+        </property>						\
+       </widget>						\
+      </item>							\
+     </layout>							\
+    </item>							\
+   </layout>							\
+  </widget>							\
+ </widget>							\
+ <resources/>							\
+ <connections/>							\
+</ui>								\
+";
+    
+    plotter_p = dbus::launch<FlagPlotServerProxy>();
+    
+    panels_p.resize(nPolarizations_p*3);
+    string zoomloc="";
+    string legendloc="bottom";
+    panels_p[0] = plotter_p->panel( "", "", "", "",
+				    std::vector<int>( ),legendloc,zoomloc,0,false,false);
+    
+    if(nPolarizations_p>1)
+      {
+	for(Int i=1;i<nPolarizations_p;i++)
+	  {
+	    panels_p[i] = plotter_p->panel( "", "", "", "",
+					    std::vector<int>( ),legendloc,zoomloc,panels_p[i-1].getInt(),false,false);
+	  }
+      }
+    
+    panels_p[nPolarizations_p] = plotter_p->panel( "", "", "", "",
+						   std::vector<int>( ),legendloc,zoomloc, panels_p[0].getInt(),true,false);
+    
+    if(nPolarizations_p>1)
+      {
+	for(int i=nPolarizations_p+1;i<2*nPolarizations_p;i++)
+	  {
+	    panels_p[i] = plotter_p->panel( "", "", "", "",
+					    std::vector<int>( ),legendloc,zoomloc, panels_p[i-1].getInt(),false,false);
+	  }
+      }
+    panels_p[2*nPolarizations_p] = plotter_p->panel( "", "", "", "",
+						     std::vector<int>( ),legendloc,zoomloc, panels_p[0].getInt(),true,false);
+    if(nPolarizations_p>1)
+      {
+	for(int i=2*nPolarizations_p+1;i<3*nPolarizations_p;i++)
+	  {
+	    panels_p[i] = plotter_p->panel( "", "", "", "",
+					    std::vector<int>( ),legendloc,zoomloc, panels_p[i-1].getInt(),false,false);
+	  }
+      }
+    /*
+    // Misc panel
+    panels_p[8] = plotter_p->panel( "BandPass", "Frequency", "Amp", "",
+    std::vector<int>( ),legendloc,zoomloc, panels_p[3].getInt(),false,false);
+    
+    // Dummy panel
+    panels_p[9] = plotter_p->panel( "---", "----", "---", "",
+    std::vector<int>( ),legendloc,zoomloc, panels_p[7].getInt(),false,false);
+    */
+    
+    for (uInt i=0; i<panels_p.nelements(); i++)
+      {
+	if ( panels_p[i].type( ) != dbus::variant::INT ) {
+	  throw( AipsError("Error in panel construction") );
+	}
+      }
+    
+    std::vector<std::string> loc;
+    //loc.push_back("top");
+    loc.push_back("top");
+    plotter_p->loaddock(dock_xml_p,"bottom",loc,panels_p[0].getInt());
+    
+    
+    
+  }
+  
+  
+  Char FlagAgentDisplay :: GetUserInput()
+  {
+    Char choice='c';
+    String returnvalue = plotter_p->eventloop();
+    if(returnvalue == "NextBaseline") choice = 'n';
+    else if(returnvalue == "PrevBaseline") choice = 'p';
+    else if(returnvalue == "NextChunk") choice = 'c';
+    else if(returnvalue == "StopDisplay") choice = 's';
+    else if(returnvalue == "Quit") choice = 'q';
+    
+    //cout << "Choice : " << returnvalue << endl;
+    
+    return choice;
+  }
+  
+  void FlagAgentDisplay::DisplayRaster(Int xdim, Int ydim, Vector<Float> &data, uInt frame)
+  {
+    //    cout << "panel id : " << frame << endl;;
+    
+    // plotter_p->release( panel_p.getInt( ) );
+    plotter_p->erase( frame );
+    
+    //	plotter_p->line(x, y, "blue", "time", panel_p.getInt() );
+    plotter_p->raster(dbus::af(data), xdim,ydim, "Hot Metal 1", frame);
+  }
+  
+  void FlagAgentDisplay::DisplayLine(Int xdim, Vector<Double> &xdata, Vector<Float> &ydata, String label, String color, Bool hold, uInt frame)
+  {
+    if( hold==False ) plotter_p->erase( frame );
+    plotter_p->line(dbus::af(xdata), dbus::af(ydata),color,label,frame);
+  }
+  
+  void FlagAgentDisplay::DisplayScatter(Int xdim, Vector<Double> &xdata, Vector<Float> &ydata, String label, String color, Bool hold, uInt frame)
+  {
+    if( hold==False ) plotter_p->erase( frame );
+    plotter_p->scatter(dbus::af(xdata), dbus::af(ydata),color,label,"dot",1,4,frame);
+  }
+  
+  
+  
+  
+} //# NAMESPACE CASA - END
+
+
---
+++ code/flagging/implement/Flagging/FlagAgentDisplay.h
@@ -0,0 +1,82 @@
+//# FlagAgentDisplay.h: This file contains the interface definition of the FlagAgentDisplay class.
+//#
+//#  CASA - Common Astronomy Software Applications (http://casa.nrao.edu/)
+//#  Copyright (C) Associated Universities, Inc. Washington DC, USA 2011, All rights reserved.
+//#  Copyright (C) European Southern Observatory, 2011, All rights reserved.
+//#
+//#  This library is free software; you can redistribute it and/or
+//#  modify it under the terms of the GNU Lesser General Public
+//#  License as published by the Free software Foundation; either
+//#  version 2.1 of the License, or (at your option) any later version.
+//#
+//#  This library is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY, without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//#  Lesser General Public License for more details.
+//#
+//#  You should have received a copy of the GNU Lesser General Public
+//#  License along with this library; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+//#  MA 02111-1307  USA
+//# $Id: $
+
+#ifndef FlagAgentDisplay_H_
+#define FlagAgentDisplay_H_
+
+#include <flagging/Flagging/FlagAgentBase.h>
+
+#include <casadbus/viewer/ViewerProxy.h>
+#include <casadbus/plotserver/PlotServerProxy.h>
+#include <casadbus/utilities/BusAccess.h>
+#include <casadbus/session/DBusSession.h>
+
+#include <flagging/Flagging/LFPlotServerProxy.h>
+
+
+namespace casa { //# NAMESPACE CASA - BEGIN
+
+class FlagAgentDisplay : public FlagAgentBase {
+
+public:
+
+	FlagAgentDisplay(FlagDataHandler *dh, Record config, Bool writePrivateFlagCube = false);
+	~FlagAgentDisplay();
+
+protected:
+
+	// Compute flags for a given (time,freq) map
+	void computeAntennaPairFlags(VisMapper &visibilities,FlagMapper &flag,Int antenna1,Int antenna2);
+
+	// Parse configuration parameters
+	void setAgentParameters(Record config);
+
+private:
+
+    virtual Bool BuildPlotWindow();
+    //    virtual Bool ShowFlagPlots();
+
+     virtual Char GetUserInput();
+     char *dock_xml_p;
+
+     void DisplayRaster(Int xdim, Int ydim, Vector<Float> &data, uInt frame);
+    void DisplayLine(Int xdim, Vector<Double> &xdata, Vector<Float> &ydata, String label, String color, Bool hold,  uInt frame);
+    void DisplayScatter(Int xdim, Vector<Double> &xdata, Vector<Float> &ydata, String label, String color, Bool hold,  uInt frame);
+
+        // Plotter members
+  FlagPlotServerProxy *plotter_p;  // UUU Make this a CountedPtr
+    Vector<dbus::variant> panels_p;
+
+    Bool ShowPlots, StopAndExit;
+
+    // Additional private members
+  uInt nPolarizations_p;
+
+  Bool pause_p;
+
+};
+
+
+} //# NAMESPACE CASA - END
+
+#endif /* FLAGAGENTDISPLAY_H_ */
+
