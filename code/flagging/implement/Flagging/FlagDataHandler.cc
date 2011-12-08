b5b8ab388b0ad9f1f8490bbd987e81bb8c6af383
sandra castro <sbhatnag@nrao.edu>
2011-12-08 21:26:58 +0000

    New Development: No             JIRA Issue: CAS-3425         Ready for Test: Yes      Interface Changes: No What Interface Changed:          Test Programs: tFlagDataHandler   Put in Release Notes: No              Module(s): Module Names change impacts.            Description: Added a new public method to set the iteration approach 	   so that the flagger tool can access it and change the iteration for 	   the tfcrop agent. The new method is called from the FlagDataHandler 	   constructor too.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@17180 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/flagging/implement/Flagging/FlagDataHandler.cc
+++ code/flagging/implement/Flagging/FlagDataHandler.cc
@@ -94,6 +94,98 @@ FlagDataHandler::FlagDataHandler(string msname, uShort iterationApproach, Double
 	mapScanStartStop_p = false;
 	mapScanStartStopFlagged_p = false;
 
+	// Set the iteration approach based on the agent
+	setIterationApproach(iterationApproach);
+
+	// Initialize selection ranges
+	timeSelection_p = String("");
+	baselineSelection_p = String("");
+	fieldSelection_p = String("");
+	spwSelection_p = String("*");
+	uvwSelection_p = String("");
+	polarizationSelection_p = String("");
+	scanSelection_p = String("");
+	arraySelection_p = String("");
+	observationSelection_p = String("");
+	scanIntentSelection_p = String("");
+
+	// Initialize iteration parameters
+	chunksInitialized_p = false;
+	buffersInitialized_p = false;
+	iteratorGenerated_p = false;
+	stopIteration_p = false;
+	chunkNo = 0;
+	bufferNo = 0;
+
+	// Initialize stats
+	stats_p = false;
+	cubeAccessCounter_p = 0;
+
+	// Set all the initialized pointers to NULL
+	selectedMeasurementSet_p = NULL;
+	measurementSetSelection_p = NULL;
+	originalMeasurementSet_p = NULL;
+
+	rwVisibilityIterator_p = NULL;
+	roVisibilityIterator_p = NULL;
+	visibilityBuffer_p = NULL;
+
+	antennaNames_p = NULL;
+	antennaDiameters_p = NULL;
+	fieldNames_p = NULL;
+	antennaPairMap_p = NULL;
+	subIntegrationMap_p = NULL;
+	polarizationMap_p = NULL;
+	polarizationIndexMap_p = NULL;
+	antennaPointingMap_p = NULL;
+	scanStartStopMap_p = NULL;
+
+	// Initialize Pre-Load columns
+	preLoadColumns_p.clear();
+
+	return;
+}
+
+
+// -----------------------------------------------------------------------
+// Default destructor
+// -----------------------------------------------------------------------
+FlagDataHandler::~FlagDataHandler()
+{
+	// Delete mapping members
+	if (antennaNames_p) delete antennaNames_p;
+	if (antennaDiameters_p) delete antennaDiameters_p;
+	if (antennaPairMap_p) delete antennaPairMap_p;
+	if (subIntegrationMap_p) delete subIntegrationMap_p;
+	if (polarizationMap_p) delete polarizationMap_p;
+	if (polarizationIndexMap_p) delete polarizationIndexMap_p;
+	if (antennaPointingMap_p) delete antennaPointingMap_p;
+
+	// Delete VisBuffers and iterators
+	if (visibilityBuffer_p) delete visibilityBuffer_p;
+	// ROVisIter is in fact RWVisIter
+	if (rwVisibilityIterator_p) delete rwVisibilityIterator_p;
+
+	// Delete MS objects
+	if (selectedMeasurementSet_p) delete selectedMeasurementSet_p;
+	if (measurementSetSelection_p) delete measurementSetSelection_p;
+	if (originalMeasurementSet_p) delete originalMeasurementSet_p;
+
+	// Delete logger
+	if (logger_p) delete logger_p;
+
+	return;
+}
+
+// -----------------------------------------------------------------------
+// Set iteration Approach
+// -----------------------------------------------------------------------
+void
+FlagDataHandler::setIterationApproach(uShort iterationApproach)
+{
+
+	iterationApproach_p = iterationApproach;
+
 	switch (iterationApproach_p)
 	{
 		case COMPLETE_SCAN_MAPPED:
@@ -238,84 +330,6 @@ FlagDataHandler::FlagDataHandler(string msname, uShort iterationApproach, Double
 			break;
 		}
 	}
-
-	// Initialize selection ranges
-	timeSelection_p = String("");
-	baselineSelection_p = String("");
-	fieldSelection_p = String("");
-	spwSelection_p = String("*");
-	uvwSelection_p = String("");
-	polarizationSelection_p = String("");
-	scanSelection_p = String("");
-	arraySelection_p = String("");
-	observationSelection_p = String("");
-	scanIntentSelection_p = String("");
-
-	// Initialize iteration parameters
-	chunksInitialized_p = false;
-	buffersInitialized_p = false;
-	iteratorGenerated_p = false;
-	stopIteration_p = false;
-	chunkNo = 0;
-	bufferNo = 0;
-
-	// Initialize stats
-	stats_p = false;
-	cubeAccessCounter_p = 0;
-
-	// Set all the initialized pointers to NULL
-	selectedMeasurementSet_p = NULL;
-	measurementSetSelection_p = NULL;
-	originalMeasurementSet_p = NULL;
-
-	rwVisibilityIterator_p = NULL;
-	roVisibilityIterator_p = NULL;
-	visibilityBuffer_p = NULL;
-
-	antennaNames_p = NULL;
-	antennaDiameters_p = NULL;
-	fieldNames_p = NULL;
-	antennaPairMap_p = NULL;
-	subIntegrationMap_p = NULL;
-	polarizationMap_p = NULL;
-	polarizationIndexMap_p = NULL;
-	antennaPointingMap_p = NULL;
-	scanStartStopMap_p = NULL;
-
-	// Initialize Pre-Load columns
-	preLoadColumns_p.clear();
-
-	return;
-}
-
-
-// -----------------------------------------------------------------------
-// Default destructor
-// -----------------------------------------------------------------------
-FlagDataHandler::~FlagDataHandler()
-{
-	// Delete mapping members
-	if (antennaNames_p) delete antennaNames_p;
-	if (antennaDiameters_p) delete antennaDiameters_p;
-	if (antennaPairMap_p) delete antennaPairMap_p;
-	if (subIntegrationMap_p) delete subIntegrationMap_p;
-	if (polarizationMap_p) delete polarizationMap_p;
-	if (polarizationIndexMap_p) delete polarizationIndexMap_p;
-	if (antennaPointingMap_p) delete antennaPointingMap_p;
-
-	// Delete VisBuffers and iterators
-	if (visibilityBuffer_p) delete visibilityBuffer_p;
-	// ROVisIter is in fact RWVisIter
-	if (rwVisibilityIterator_p) delete rwVisibilityIterator_p;
-
-	// Delete MS objects
-	if (selectedMeasurementSet_p) delete selectedMeasurementSet_p;
-	if (measurementSetSelection_p) delete measurementSetSelection_p;
-	if (originalMeasurementSet_p) delete originalMeasurementSet_p;
-
-	// Delete logger
-	if (logger_p) delete logger_p;
-
 	return;
 }
 
