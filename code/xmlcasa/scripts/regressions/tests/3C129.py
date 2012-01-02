5d7b8b43f94d8c47bad30e2bf530ceedfe1a3213
darrell schiebel <ds@nrao.edu>
2012-01-02 17:34:14 +0000

    New Development: no             JIRA Issue: no       Ready to Release: yes      Interface Changes: none What Interface Changed: none          Test Programs: none   Put in Release Notes: no
               Description: optionally fetch data (IMO this should default to True)...
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@17445 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/scripts/regressions/tests/3C129.py
+++ code/xmlcasa/scripts/regressions/tests/3C129.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import string
+from locatescript import copydata
 from locatescript import locatescript
 import inspect
 
@@ -16,7 +17,17 @@ gl=sys._getframe(stacklevel).f_globals
 def description():
     return "Reduces B- and C-configuration VLA continuum polarimetry data at 5 GHz on 3C129 and calibrators, and images the dual-config 3C129 in full polarization"
 
-def run():
+def data():
+    ### return the data files that is needed by the regression script
+    return ['AT166_1', 'AT166_2', 'AT166_3']
+
+def run( fetch=False ):
+
+    #####fetch data
+    if fetch:
+        for f in data( ):
+            copydata( f, os.getcwd( ) )
+    
     #####locate the regression script
     lepath=locatescript('3c129_tutorial_regression.py')
     print 'Script used is ',lepath
@@ -25,10 +36,7 @@ def run():
     print 'regstate =', gl['regstate']
     if not gl['regstate']:
         raise Exception, 'regstate = False'
+
 ###return the images that will be templated and compared in future runs
 #    return ['3C129BC.clean.image', 'at166B.3c129.image', 'at166C.3c129.image']
     return ['3C129BC.core.I']
-
-def data():
-    ### return the data files that is needed by the regression script
-    return ['AT166_1', 'AT166_2', 'AT166_3']
