237585e418f912904c6362f53e10a982339d622c
jim jacobs <jjacobs@nrao.edu>
2012-01-12 17:24:43 +0000

    New Development: No             JIRA Issue: No         Ready for Test: Yes      Interface Changes: What Interface Changed:          Test Programs:   Put in Release Notes: No              Module(s):            Description: Reverting MeasFrame.{h,cc} and MeasRef.{h,tcc} to r17488                         to allow me to fix problems (memory leak, etc.)                         apparently related to mods to these files.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@17640 ac9c0da5-131f-0410-9009-a9c195f183e6

--- casacore/measures/Measures/MeasRef.h
+++ casacore/measures/Measures/MeasRef.h
@@ -179,7 +179,7 @@ private:
   public:
     // Constructor
     // <note role=warning> Next one must be in-line for (some?) compilers </note>
-    RefRep() : type(Ms::DEFAULT), offmp(0), frame() {}
+    RefRep() : type(Ms::DEFAULT), offmp(0), frame(), cnt(1) {}
     // Destructor
     // <note role=warning> Next one must be in-line for (some?) compilers </note>
     ~RefRep() {delete offmp;} 
@@ -192,10 +192,12 @@ private:
     // Reference frame
     MeasFrame frame;
     // </group>
+    // Usage count
+    Int cnt;
   };
   
   //# Data
-  CountedPtr<RefRep> rep;
+  RefRep *rep;
   
   //# Member functions
   // Create an instance of MeasRef
