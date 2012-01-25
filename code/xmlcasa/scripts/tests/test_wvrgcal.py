e3f82f06d691833d00120a31dccbaf5285d87d7d
dirk petry <dpetry@eso.org>
2012-01-25 16:30:06 +0000

    New Development: Yes             JIRA Issue: CAS-3513         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: test_wvrgcal.py   Put in Release Notes: Yes              Module(s): xmlcasa            Description: Added new task "wvrgcal" with all useful 			parameters of the wvrgcal executable. 			Created rudimentary test. Needs more test cases.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@17816 ac9c0da5-131f-0410-9009-a9c195f183e6

---
+++ code/xmlcasa/scripts/tests/test_wvrgcal.py
@@ -0,0 +1,50 @@
+# unit test for the wvrgcal task
+
+import os
+import shutil
+
+from __main__ import default
+from tasks import *
+from taskinit import *
+import unittest
+
+class wvrgcal_test(unittest.TestCase):
+    
+    vis_g = 'M51.ms'
+    out = 'mycaltable.wvr'
+    rval = False
+    
+    def setUp(self):    
+        self.rval = False
+
+        if(not os.path.exists(self.vis_g)):
+            os.system('cp -R '+os.environ['CASAPATH'].split()[0]+'/data/regression/exportasdm/input/M51.ms .')
+
+        default(wvrgcal)
+
+    def tearDown(self):
+        os.system('rm -rf myinput.ms')
+        os.system('rm -rf ' + self.out)
+
+# Test cases    
+    def test1(self):
+        '''Test 1: Testing default'''
+        self.rval = wvrgcal()
+        print "Expected error ..."
+        self.assertFalse(self.rval)
+
+    def test2(self):
+        '''Test 1: Testing with some random values for each parameter'''
+        myvis = self.vis_g
+        os.system('cp -R ' + myvis + ' myinput.ms')
+        self.rval = wvrgcal(vis="myinput.ms",caltable="xy.wvr",toffset=2.3,nsol=2,segsource=False,
+                            reversespw="0~3",disperse=True,cont=False,wvrflag=['DV02', 'DV03'],
+                            sourceflag=['0', '1'],statfield="0",statsource="1",tiesource=[0, 1],
+                            smooth=3,scale=3.5)
+        print "Expected error ..."
+        self.assertFalse(self.rval)
+
+
+def suite():
+    return [wvrgcal_test]
+
