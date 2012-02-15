86a991bcb9e7e80b9b18ec967074ff7307ae9407
wes young <wyoung@nrao.edu>
2012-02-15 04:52:22 +0000

    Tonight's work
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@18216 ac9c0da5-131f-0410-9009-a9c195f183e6

--- swig/bindings/install/mkinitpy.sh
+++ swig/bindings/install/mkinitpy.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 OUTFILE=__init__.py
+echo "__name__ = 'casac'"
 echo '__all__ = ['
 for file in $*
 do
@@ -6,3 +8,8 @@ do
 	echo "     '$pack',"
 done
 echo ']'
+#for file in $*
+#do
+	#pack=`basename $file .xml`
+	#echo "import $pack"
+#done
