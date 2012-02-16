b737265fc4e7499149d135b9bb6501ea0dc8dc4c
dave mehringer <dmehring@zia.aoc.nrao.edu>
2012-02-16 20:24:28 +0000

    New Development: No             JIRA Issue: CAS-3737         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: doc tweaks, added test for tabular spectral axis images
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@18246 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/scripts/tests/test_imcollapse.py
+++ code/xmlcasa/scripts/tests/test_imcollapse.py
@@ -108,7 +108,8 @@ class imcollapse_test(unittest.TestCase):
     
     def setUp(self):
         shutil.copy(datapath + good_image, good_image)
-    
+        self.tabular_spectral_image = datapath + "longZax"
+
     def tearDown(self):
         os.remove(good_image)
 
@@ -451,6 +452,29 @@ class imcollapse_test(unittest.TestCase):
                     "", maskim + ">0", False, True, True
                 )
             self.assertTrue(type(yy) == type(ia))
+            
+    def test_CAS3737(self):
+        """ imcollapse: test tabular spectral axis has correct collapsed reference value """
+        image = self.tabular_spectral_image
+        chans = "2445~2555"
+        mytool = run_collapse(
+            image, "mean", 2, "", "", "",
+            chans, "", "", False
+        )
+        expected = 98318505973583.641
+        got = mytool.toworld([0,0,0])["numeric"][2]
+        mytool.done()
+        frac = got/expected - 1
+        self.assertTrue(frac < 1e-6 and frac > -1e-6)
+        
+        mytool = run_imcollapse(
+            image, "mean", 2, "", "", "",
+            chans, "", "", False, True
+        )
+        got = mytool.toworld([0,0,0])["numeric"][2]
+        mytool.done()
+        frac = got/expected - 1
+        self.assertTrue(frac < 1e-6 and frac > -1e-6) 
 
 def suite():
     return [imcollapse_test]
