18213e73ffb8268d9be4cbf1c1784ce40e910903
tak tsutsumi <ttsutsum@nrao.edu>
2012-03-02 19:24:48 +0000

    New Development: Yes             JIRA Issue: Yes CAS-3796         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes              Module(s): Module Names change impacts.            Description: Enabled automated lookup of antenna                         position corrections for EVLA.                         The gencal task calls a Python                         function (correct_nt_posns) written by                         Bryan Butler. Updated help doc for that.                         Also made gencal to use local cb tool.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@18513 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/scripts/task_gencal.py
+++ code/xmlcasa/scripts/task_gencal.py
@@ -1,7 +1,7 @@
 import sys
 import os
 from taskinit import *
-
+(cb,) = gentools(['cb'])
 def gencal(vis=None,caltable=None,caltype=None,
            spw=None,antenna=None,pol=None,
            parameter=None):
@@ -21,14 +21,28 @@ def gencal(vis=None,caltable=None,caltype=None,
               if (caltable==''):
                      raise Exception, 'A caltable name must be specified'
 
-
+              # call a Python function to retreive ant position offsets automatically (currently EVLA only)
+              if (caltype=='antpos' and antenna==''):
+                casalog.post(" Determine antenna position offests from the baseline correction database")
+                import correct_ant_posns as getantposns 
+                # correct_ant_posns returns a list , [return_code, antennas, offsets]
+                antenna_offsets=getantposns.correct_ant_posns(vis,False)
+                if len(antenna_offsets)==0:
+                   # no offsets were found
+                   raise Exception, 'No offsets found. The caltable is not created'
+                if antenna_offsets[0] == 0:
+                   antenna = antenna_offsets[1]
+                   parameter = antenna_offsets[2] 
+                else:
+                   raise Exception, 'No lookup information'
+              
               cb.specifycal(caltable=caltable,time="",spw=spw,antenna=antenna,pol=pol,
                             caltype=caltype,parameter=parameter)
 
               cb.close()
 
        except Exception, instance:
-              print '*** Error ***',instance
+              #print '*** Error ***',instance
               cb.close()
               raise Exception, instance
 
