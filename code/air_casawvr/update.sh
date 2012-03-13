c699781bc532c29b8b5e43426cbf38c6c3a8f130
dirk petry <dpetry@eso.org>
2012-03-13 17:45:01 +0000

    New Development: No             JIRA Issue: CAS-3513         Ready for Test: Yes      Interface Changes: Yes What Interface Changed: now require almawvr 1.1.0          Test Programs: test_wvrgcal   Put in Release Notes: Yes              Module(s): air_casawvr            Description: Now requiring almawvr version 1.1.0. 	   		Removed air_casawvr/src directory. 			Made update.sh script also run on Macs.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@18726 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/air_casawvr/update.sh
+++ code/air_casawvr/update.sh
@@ -5,7 +5,7 @@ echo "version of libair (typically got from http://www.mrao.cam.ac.uk/~bn204/sof
 echo "Initial version: D. Petry, ESO, March 2012"
 echo "============================================================================="
 export LIBAIRROOT=$1
-if [$1 = ]; then
+if [ $LIBAIRROOT"" == "" ]; then
     echo " Usage:" 
     echo "   cd air_casawvr"
     echo "   ./update.sh <path to the now libair version."
@@ -20,36 +20,43 @@ if ! [ -e $LIBAIRROOT ]; then
 fi
 # for safety, check we are in the right directory
 cd ../air_casawvr/
-if [ $? = 0 ]; then
+if [ $? == 0 ]; then
     echo Using $LIBAIRROOT
-    for j in $( ls -I*.* ); do
+    
+    export BACKUPEXTENSION=""
+    uname -a | grep "Darwin"
+    if [ $? == 0 ]; then
+	# we are on a Mac, need to use sed with a backup extension
+	export BACKUPEXTENSION=".bak"
+    fi
+
+    for j in $( ls -p | grep "/" ); do
 	cd $j
 	echo Now in directory $j
 	for i in $( ls *.cpp *.hpp ); do
 	    echo item: $i
 	    export CORRESPONDING=`find $LIBAIRROOT -name $i`
-	    if [ $CORRESPONDING = "" ]; then
+	    if [ $CORRESPONDING == "" ]; then
 		echo ==============================================
 		echo WARNING: File $i not found in $LIBAIRROOT
 		echo ==============================================
 	    else
 		diff $i $CORRESPONDING
-		if [ $? = 0 ]; then
+		if [ $? == 0 ]; then
 		    echo No difference between $i and $CORRESPONDING
 		else
 		    echo Testing with fixed include paths ...
 		    cp $CORRESPONDING ${i}.new
 		    
-		    sed -i "s|calibration/CalTables/|synthesis/CalTables/|g" $i.new
-		    sed -i "s|casawvr/|../casawvr/|g" $i.new
-		    sed -i "s|../src/apps/|almawvr/|g" $i.new
-		    sed -i "s|src/apps/|almawvr/|g" $i.new
-		    sed -i "s|../src/|almawvr/|g" $i.new
-		    sed -i "s|src/|almawvr/|g" $i.new
-		    sed -i "s|almawvr/dispersion.hpp|../src/dispersion.hpp|g" $i.new
+		    sed -i $BACKUPEXTENSION "s|calibration/CalTables/|synthesis/CalTables/|g" $i.new
+		    sed -i $BACKUPEXTENSION "s|casawvr/|../casawvr/|g" $i.new
+		    sed -i $BACKUPEXTENSION "s|../src/apps/|almawvr/|g" $i.new
+		    sed -i $BACKUPEXTENSION "s|src/apps/|almawvr/|g" $i.new
+		    sed -i $BACKUPEXTENSION "s|../src/|almawvr/|g" $i.new
+		    sed -i $BACKUPEXTENSION "s|src/|almawvr/|g" $i.new
 
 		    diff $i $i.new
-		    if [ $? = 0 ]; then
+		    if [ $? == 0 ]; then
 			echo No difference between $i and $CORRESPONDING with fixed paths.
 			rm $i.new
 		    else
@@ -58,6 +65,7 @@ if [ $? = 0 ]; then
 			echo Replaced $i by a copy of $CORRESPONDING and fixed include paths.
 			echo ==============================================
 		    fi
+		    rm -f *.new$BACKUPEXTENSION 
 		fi
 	    fi
 	done
