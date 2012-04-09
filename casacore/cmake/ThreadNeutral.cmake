9fef72b1a868e040d65275d89fe7b21be0a5bbf7
scott rankin <srankin@nrao.edu>
2012-04-09 18:29:17 +0000

    Merged revisions 19204,19216 via svnmerge from https://svn.cv.nrao.edu/svn/casa/active/casacore
      r19204 | jim.jacobs | 2012-04-05 06:37:24 -0600 (Thu, 05 Apr 2012) | 32 lines
      
      
             New Development: No
                  JIRA Issue: No
              Ready for Test: Yes
           Interface Changes: No
      What Interface Changed: 
               Test Programs: 
        Put in Release Notes: No
                   Module(s): 
                 Description: Build system mods to casacore.  These changes should
                              allow the system to build with and without thread 
                              neutrality.  When enabled, thread neutrality uses boost
                              shared pointers to implement the CountedPtr class that
                              underlies many of the CasaCore classes.  When configure
                              two symbols are defined in the file 
                              casacore/build/casa/Utilities/CasaThreadNeutrality.h.
                              This file is the means to enure that CountedPtr 
                              template instances compiled in the code project use
                              the same enable/disable state as casacore.  Changing
                              the configuration requires giving 
                              -DCASA_THREAD_NEUTRAL=YES to cmake when configuring
                              casacore.  The subsequent make will probably end up
                              rebuilding most of casacore.  It is important to
                              rebuild the code project after rebuilding casacore.
                              Setting the above symbol to "NO" and reconfiguring/
                              rebuilding everything should return the code back to
                              the original implementation.
                            
      
      	
    
      r19216 | jim.jacobs | 2012-04-05 13:34:04 -0600 (Thu, 05 Apr 2012) | 22 lines
      
      
             New Development: No
                  JIRA Issue: No
              Ready for Test: Yes
           Interface Changes: No
      What Interface Changed: 
               Test Programs: 
        Put in Release Notes: No
                   Module(s): 
                 Description: Simple change to capture the thread-neutrality configuration
                              of the library in global casa::CasaThreadNeutral.  This
                              is a String with two possible values:
                              CasaThreadNeutral:YES|NO.  By using the shell "strings"
                              method on libcasa_casa.so the configuration can
                              be seen given the binary.  The idea is that this
                              might make it easier to detect situations where a
                              casacore build and a code build are using different
                              thread neutrality configurations.
      
      	
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/test@19219 ac9c0da5-131f-0410-9009-a9c195f183e6

--- casacore/cmake/ThreadNeutral.cmake
+++ casacore/cmake/ThreadNeutral.cmake
@@ -1,126 +1,146 @@
-# CasaDistributionFinder.cmake
+################################################################
 #
-# Sets the variable casa_packages to point to the appropriate
-# directory where casa-distributed packages are located.
+# ThreadNeutral.cmake
 #
-# Apple
-#     Darwin 9
-#         All    --> /opt/casa/core2-apple-darwin8/3rd-party
-#     Darwin 10
-#         32-bit --> /opt/casa/core2-apple-darwin10
-#         64-bit --> /opt/casa/darwin10-64b
-#     Darwin 11
-#         32-bit --> *Not Supported*
-#         64-bit --> /opt/casa/darwin11
-# Linux
-#     32-bit --> /usr/lib/casapy
-#     64-bit --> /usr/lib64/casapy
+# Determines the location of the CASA distribution which will provide
+# the Boost headers and libraries and setups things to allow
+# thread-neutral build of both casacore and casa.
+# 
+# 1) Determine where the CASA distribution is located.
+# 2) Find Boost in the distribution
+# 3) Configure a header file that causes CASA build to
+#    use thread neutrality when compiling.
+#
+################################################################
+
+option (CASA_THREAD_NEUTRAL "" NO) 
 
 if (CASA_THREAD_NEUTRAL)
-   if (APPLE)
 
-    # I blieve this could be avoided by defining CMAKE_FIND_ROOT_PATH, wky
+    ################################################################
+    #
+    #   Step 1 - Locate CASA installation directory
+    #
+    #   Determine location of the Casa installed packages.  This
+    #   is the only place that can be relied on to have the needed
+    #   boost distribution.
+    #
+    # Sets the variable casa_packages to point to the appropriate
+    # directory where casa-distributed packages are located.
+    #
+    # Apple
+    #     Darwin 9
+    #         All    --> /opt/casa/core2-apple-darwin8/3rd-party
+    #     Darwin 10
+    #         64-bit --> /opt/casa/darwin10-64b
+    #     Darwin 11
+    #         64-bit --> /opt/casa/darwin11
+    #     Darwin 12
+    #	      64-bit --> /opt/casa/darwin12
+    # Linux
+    #     32-bit --> /usr/lib/casapy
+    #     64-bit --> /usr/lib64/casapy
+    #
+    #################################################################
 
-    # Determine location of packages on various Apple
-    # OS-version/processor combinations.
+    if (BOOST_ROOT)
 
-       if (CMAKE_SYSTEM MATCHES ^Darwin-9)
-           set (casa_packages /opt/casa/core2-apple-darwin8/3rd-party)
-       elseif (CMAKE_SYSTEM MATCHES ^Darwin-10)
-               set  (casa_packages /opt/casa/darwin10-64b) # Darwin 10 64-bit
-       elseif  (CMAKE_SYSTEM MATCHES ^Darwin-11)
-	       set  (casa_packages /opt/casa/darwin11) # Darwin 11 64-bit
-       elseif  (CMAKE_SYSTEM MATCHES ^Darwin-12)
-	       set  (casa_packages /opt/casa/darwin12) # Darwin 11 64-bit
-       else()
-           message (WARNING "Unsupported Mac: ${CMAKE_SYSTEM}")
-       endif()
+        # A location for BOOST was explicitly provided so don't both
+        # to look for the CASA distribution.
 
-   elseif (CMAKE_SYSTEM_NAME STREQUAL Linux)
- 
-       # Determine location of packages on Linux
+    else (BOOST_ROOT)
 
-       if (CMAKE_SYSTEM_PROCESSOR STREQUAL x86_64)
-           set (casa_packages /usr/lib64/casapy) # Linux 64-bit
-       else()
-           set (casa_packages /usr/lib/casapy) # Linux 32-bit
-       endif()
+	if (APPLE)
 
-   else()
-         message (WARNING "Unsupported system: ${CMAKE_SYSTEM_NAME}")
-   endif()
-   
-# CASA_THREAD_NEUTRAL
-#
-# Sets up the build configuration to support CASA thread neutrality
-# by finding the appropriate boost headers and libraries. There are
-# two steps: 1) If the user has not provided the location to start 
-# looking for boost (via BOOST_ROOT) then the location of the CASA
-# distributed packages is determined and made to be the first place
-# searched by the find_package module
+	    # Determine location of packages on various Apple
+	    # OS-version/processor combinations.
 
-# Determine the location of CASA-distributed packages if a primary 
-# directory was not provided.
+            # I believe this could be avoided by defining CMAKE_FIND_ROOT_PATH, wky
 
+	    if (CMAKE_SYSTEM MATCHES ^Darwin-9)
+		set (casa_packages /opt/casa/core2-apple-darwin8/3rd-party)
+	    elseif (CMAKE_SYSTEM MATCHES ^Darwin-10)
+		    set  (casa_packages /opt/casa/darwin10-64b) # Darwin 10 64-bit
+	    elseif  (CMAKE_SYSTEM MATCHES ^Darwin-11)
+		    set  (casa_packages /opt/casa/darwin11) # Darwin 11 64-bit
+	    elseif  (CMAKE_SYSTEM MATCHES ^Darwin-12)
+		    set  (casa_packages /opt/casa/darwin12) # Darwin 12 64-bit
+	    else()
+		message (FATAL_ERROR "Unsupported Mac: ${CMAKE_SYSTEM}")
+	    endif()
 
-# if (NOT BOOST_ROOT)
-#
-#   # Normally this file will determine where to start looking for the boost
-#   # packages however, the user can override that via the BOOST_ROOT variable.
-#   # N.B.: In theory the user could also specify it using the shell environment but
-#   # by if a value is set before calling find_package, the environmental
-#   # variable is ignored by the FindBoost module.
-#
-#   if (APPLE)
-#
-#     if (CASA_THREAD_NEUTRAL)
-#
-#       # The Apple logic for determining the location is available in code's
-#       # CMakeLists.txt, but there are lots of other quirky things in that which
-#       # ought to be considered before dropping it in here.  So for now just
-#       # issue a warning and ignore this feature on the Apple.
-#
-#       message( WARNING "CASA_THREAD_NEUTRAL not yet supported on Apple hosts." )
-#       set (CASA_THREAD_NEUTRAL NO)
-#
-#     endif (CASA_THREAD_NEUTRAL)
-#
-#   elseif( CMAKE_SYSTEM_NAME STREQUAL Linux )
-#
-#     if( CMAKE_SYSTEM_PROCESSOR STREQUAL x86_64 )
-#       set( casa_packages /usr/lib64/casapy )
-#     else()
-#       set( casa_packages /usr/lib/casapy )
-#
-#   else()
-#     message( WARNING "Unsupported system: ${CMAKE_SYSTEM_NAME}" )
-#   endif()
+	elseif (CMAKE_SYSTEM_NAME STREQUAL Linux)
 
-# endif (NOT BOOST_ROOT)
+	    # Determine location of packages on Linux
 
-   if (NOT BOOST_ROOT)
-      set( BOOST_ROOT ${casa_packages}/include)
-   endif(NOT BOOST_ROOT)
-   set( boost_components thread serialization)
-   set( min_boost_version 1.41.0)
-   find_package( Boost ${min_boost_version} REQUIRED ${boost_components} )
-   if (NOT Boost_FOUND)
-      message( FATAL_ERROR "Boost with version >= ${min_boost_version} could not be found. Please check!" )
-   endif (NOT Boost_FOUND)
-   # include_directories{$(Boost_INCLUDE_DIR})
-   set (CASA_THREAD_NEUTRALITY "Yes") # for reporting only
-   # Set up these to variables to be used to flesh out a header file template later in
-   # this file.
-   set (CASA_THREAD_NEUTRAL_DEFINE "#define CASA_THREAD_NEUTRAL")
-   set (USE_BOOST_SHARED_PTR_DEFINE "#define USE_BOOST_SHARED_PTR")
-else (CASA_THREAD_NEUTRAL)
-   set (CASA_THREAD_NEUTRALITY "No") # for reporting only
+	    if (CMAKE_SYSTEM_PROCESSOR STREQUAL x86_64)
+
+		set (casa_packages /usr/lib64/casapy) # Linux 64-bit
+
+	    else()
+
+		set (casa_packages /usr/lib/casapy) # Linux 32-bit
+
+	    endif()
+
+	else() # Neither Linux nor Apple so fail!!!
+
+	      message (FATAL_ERROR "Unsupported system: ${CMAKE_SYSTEM_NAME}")
+
+	endif()
+
+	set( BOOST_ROOT ${casa_packages}/include)
+
+    endif (BOOST_ROOT)
+
+    #############################################################
+    #
+    #   Step 2 - Locate boost
+    #
+    #   Now that the distribution is located, find and set up the 
+    #   boost-related variables
+    #
+    #############################################################
+
+    set( boost_components thread serialization)
+    set( min_boost_version 1.41.0)
+
+    find_package( Boost ${min_boost_version} REQUIRED ${boost_components} )
+
+    if (NOT Boost_FOUND)
+	message( FATAL_ERROR "Boost with version >= ${min_boost_version} needed for CASA-thread-neutrality!" )
+    endif (NOT Boost_FOUND)
+
+    include_directories (${Boost_INCLUDE_DIRS})
+
+    set (CASA_THREAD_NEUTRALITY "Yes") # for reporting only
+
+    ######################################################
+    #
+    # Step 3 - Configure header file to capture thread neutrality for CASA.
+    #
+    # Now configure the include file so that it records the thread neutrality-related settings for use
+    # by outside code using casacore.  When thread neutrality is not enabled the file will
+    # end up having no defines it it.  However, if it is not present then the compilations will fail.
+
+    # Set up these to variables to be used to flesh out a header file template later in
+    # this file.
+    #
+    ######################################################
+
+    set (CASA_THREAD_NEUTRAL_DEFINE "#define CASA_THREAD_NEUTRAL")
+    set (USE_BOOST_SHARED_PTR_DEFINE "#define USE_BOOST_SHARED_PTR")
+
 endif (CASA_THREAD_NEUTRAL)
 
-# Now configure the include file so that it records the thread neutrality-related settings for use
-# by outside code using casacore.  When thread neutrality is not enabled the file will
-# end up having no defines it it.  However, if it is not present then the compilations will fail.
+# Now configure use the variables to create the header file.
+#
+# If thread neutrality is on then the header file will contain these two lines:
+#
+# #define CASA_THREAD_NEUTRAL
+# #define USE_BOOST_SHARED_PTR
+#
+# Otherwise it will not define these two so the header file will be essentially 
+# empty; this is necessary to allow switching back from enabled to disable builds.
 
 configure_file (casa/Utilities/CasaThreadNeutrality.h.in ${CMAKE_BINARY_DIR}/casa/Utilities/CasaThreadNeutrality.h)
-
-
