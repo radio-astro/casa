898b93257fd8995bd2a60f43d6bbcda03031d815
dave mehringer <dmehring@zia.aoc.nrao.edu>
2012-04-17 15:20:20 +0000

    New Development: Yes             JIRA Issue: CAS-3322         Ready for Test: Yes      Interface Changes: Yes What Interface Changed: added shape param to imregrid task interface          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: as above, and fixed bogus way of setting output shape equal to template shape which was causing all sorts of undesirable issues
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@19331 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/implement/images/image_cmpt.cc
+++ code/xmlcasa/implement/images/image_cmpt.cc
@@ -1483,17 +1483,15 @@ image* image::transpose(
 			break;
 		default:
 			*_log << "Unsupported type for order parameter " << order.type()
-					<< ". Supported types are a non-negative integer,a single "
+					<< ". Supported types are a non-negative integer, a single "
 					<< "string containing all digits or a list of strings which "
-					<< "unambiguously match the image axis names"
+					<< "unambiguously match the image axis names."
 					<< LogIO::EXCEPTION;
 		}
 		return new image(
 			transposer->transpose(), False
 		);
 	} catch (AipsError x) {
-		//*_log << LogIO::SEVERE << "Exception Reported: " << x.getMesg()
-		//		<< LogIO::POST;
 		RETHROW(x);
 	}
 }
@@ -2487,8 +2485,6 @@ image* image::regrid(
 		);
 		return new image(pImOut.get());
 	} catch (AipsError x) {
-		//*_log << LogIO::SEVERE << "Exception Reported: " << x.getMesg()
-		//		<< LogIO::POST;
 		RETHROW(x);
 	}
 }
