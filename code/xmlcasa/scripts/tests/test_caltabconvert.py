6791b56b2cf7c19b5e01136a6db748ec061e6e28
dirk petry <dpetry@eso.org>
2012-05-21 12:10:55 +0000

    New Development: Yes/No             JIRA Issue: CAS-4138         Ready for Test: Yes      Interface Changes: No What Interface Changed:          Test Programs: this is a test program   Put in Release Notes: No              Module(s): xmlcasa            Description: Added a python file "testhelper.py" in scripts                         to contain general helper functions for task unit                         tests. 			First member of this module is the function 			compTables which compares all columns of two 			given tables. Optionally individual columns can 			be ignored. A tolerance for numerical values can 			be specified (default 0.1 percent). 			This function is now used in the tests 			test_wvrgcal and test_caltabconvert.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@19785 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/scripts/tests/test_caltabconvert.py
+++ code/xmlcasa/scripts/tests/test_caltabconvert.py
@@ -8,6 +8,7 @@ from __main__ import default
 from tasks import *
 from taskinit import *
 import unittest
+import testhelper as th
 
 class caltabconvert_test(unittest.TestCase):
 
@@ -32,93 +33,6 @@ class caltabconvert_test(unittest.TestCase):
     def tearDown(self):
         os.system('rm -rf ' + self.out)
 
-    def compTables(self, referencetab, testtab, excludecols):
-
-        rval = True
-
-        tb2 = casac.homefinder.find_home_by_name('tableHome').create()
-        
-        tb.open(referencetab)
-        cnames = tb.colnames()
-
-        #print cnames
-
-        tb2.open(testtab)
-        
-        for c in cnames:
-            if c in excludecols:
-                continue
-            print c
-            a = tb.getcol(c)
-            #print a
-            b = 0
-            try:
-                b = tb2.getcol(c)
-            except:
-                rval = False
-                print 'Error accessing column ', c, ' in table ', testtab
-                print sys.exc_info()[0]
-                break
-            #print b
-            if not (len(a)==len(b)):
-                print 'Column ',c,' has different length in tables ', referencetab, ' and ', testtab
-                print a
-                print b
-                rval = False
-                break
-            else:
-                if not (a==b).all():
-                    differs = False
-                    for i in range(0,len(a)):
-                        if (type(a[i])==float):
-                            if (abs(a[i]-b[i]) > 0.001*abs(a[i]+b[i])):
-                                print 'Column ',c,' differs in tables ', referencetab, ' and ', testtab
-                                print i
-                                print a[i]
-                                print b[i]
-                                differs = True
-                        elif (type(a[i])==int):
-                            if (abs(a[i]-b[i]) > 0):
-                                print 'Column ',c,' differs in tables ', referencetab, ' and ', testtab
-                                print i
-                                print a[i]
-                                print b[i]
-                                differs = True
-                        elif (type(a[i])==str):
-                            if not (a[i]==b[i]):
-                                print 'Column ',c,' differs in tables ', referencetab, ' and ', testtab
-                                print i
-                                print a[i]
-                                print b[i]
-                                differs = True
-                        elif (type(a[i])==list or type(a[i])==np.ndarray):
-                            for j in range(0,len(a[i])):
-                                if (type(a[i][j])==float):
-                                    if (abs(a[i][j]-b[i][j]) > 0.001*abs(a[i][j]+b[i][j])):
-                                        print 'Column ',c,' differs in tables ', referencetab, ' and ', testtab
-                                        print i, j
-                                        print a[i][j]
-                                        print b[i][j]
-                                        differs = True
-                                elif (type(a[i][j])==list or type(a[i][j])==np.ndarray):
-                                    for k in range(0,len(a[i][j])):
-                                        if (abs(a[i][j][k]-b[i][j][k]) > 0.001*abs(a[i][j][k]+b[i][j][k])):
-                                            print 'Column ',c,' differs in tables ', referencetab, ' and ', testtab
-                                            print i, j, k
-                                            print a[i][j][k]
-                                            print b[i][j][k]
-                                            differs = True
-                    if differs:
-                        rval = False
-                        break
-
-        tb.close()
-        tb2.close()
-        
-        if rval:
-            print 'Tables ', referencetab, ' and ', testtab, ' agree.'
-
-        return rval
 
 # Test cases    
     def test1(self):
@@ -136,13 +50,13 @@ class caltabconvert_test(unittest.TestCase):
                                   caltabnew=self.out)
 
         if(self.rval):
-            self.rval = self.compTables(self.ref2, self.out,['WEIGHT', 'PARAMERR']) # WEIGHT column is empty
+            self.rval = th.compTables(self.ref2, self.out,['WEIGHT', 'PARAMERR']) # WEIGHT column is empty
 
-            self.rval = self.rval and self.compTables(self.ref2+'/ANTENNA', self.out+'/ANTENNA', [])
+            self.rval = self.rval and th.compTables(self.ref2+'/ANTENNA', self.out+'/ANTENNA', [])
 
-            self.rval = self.rval and self.compTables(self.ref2+'/FIELD', self.out+'/FIELD', [])
+            self.rval = self.rval and th.compTables(self.ref2+'/FIELD', self.out+'/FIELD', [])
 
-            self.rval = self.rval and self.compTables(self.ref2+'/SPECTRAL_WINDOW', self.out+'/SPECTRAL_WINDOW', [])
+            self.rval = self.rval and th.compTables(self.ref2+'/SPECTRAL_WINDOW', self.out+'/SPECTRAL_WINDOW', [])
                                                           
 
         self.assertTrue(self.rval)
