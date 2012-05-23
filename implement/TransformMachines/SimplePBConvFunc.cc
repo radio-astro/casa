3e6117464b7b8b9c7c50b18c626a3e2287f52abe
jim jacobs <jjacobs@nrao.edu>
2012-05-23 22:46:03 +0000

    New Development: No             JIRA Issue: No         Ready for Test: No      Interface Changes: No What Interface Changed:          Test Programs:   Put in Release Notes: No              Module(s):            Description: Suppressed compiler warnings.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active/code/synthesis@19817 ac9c0da5-131f-0410-9009-a9c195f183e6

--- implement/TransformMachines/SimplePBConvFunc.cc
+++ implement/TransformMachines/SimplePBConvFunc.cc
@@ -69,29 +69,29 @@
 
 namespace casa { //# NAMESPACE CASA - BEGIN
 
-  SimplePBConvFunc::SimplePBConvFunc(): nchan_p(-1),
-					npol_p(-1), pointToPix_p(), directionIndex_p(-1), thePix_p(0),
-					filledFluxScale_p(False),doneMainConv_p(False),
-					convFunctionMap_p(-1), 
-					actualConvIndex_p(-1), convSize_p(0), 
-					calcFluxScale_p(True), convSupport_p(0)  {
+SimplePBConvFunc::SimplePBConvFunc(): nchan_p(-1),
+        npol_p(-1), pointToPix_p(), directionIndex_p(-1), thePix_p(0),
+        filledFluxScale_p(False),doneMainConv_p(False),
+        convFunctionMap_p(-1),
+        actualConvIndex_p(-1),
+        calcFluxScale_p(True), convSize_p(0), convSupport_p(0)  {
     //
 
-     pbClass_p=PBMathInterface::COMMONPB;
-  }
+    pbClass_p=PBMathInterface::COMMONPB;
+}
 
   SimplePBConvFunc::SimplePBConvFunc(const PBMathInterface::PBClass typeToUse): 
     nchan_p(-1),npol_p(-1),pointToPix_p(),
     directionIndex_p(-1), thePix_p(0), filledFluxScale_p(False),doneMainConv_p(False), 
-    convFunctionMap_p(-1), actualConvIndex_p(-1), convSize_p(0), calcFluxScale_p(True), convSupport_p(0) {
+    convFunctionMap_p(-1), actualConvIndex_p(-1), calcFluxScale_p(True), convSize_p(0), convSupport_p(0) {
     //
     pbClass_p=typeToUse;
 
   }
   SimplePBConvFunc::SimplePBConvFunc(const RecordInterface& rec, const Bool calcfluxneeded)
   : nchan_p(-1),npol_p(-1),pointToPix_p(), directionIndex_p(-1), thePix_p(0), filledFluxScale_p(False),
-    doneMainConv_p(False), convFunctionMap_p(-1), actualConvIndex_p(-1), convSize_p(0),
-    calcFluxScale_p(calcfluxneeded), convSupport_p(0)
+    doneMainConv_p(False), convFunctionMap_p(-1), actualConvIndex_p(-1),
+    calcFluxScale_p(calcfluxneeded), convSize_p(0), convSupport_p(0)
   {
     String err;
     fromRecord(err, rec, calcfluxneeded);
