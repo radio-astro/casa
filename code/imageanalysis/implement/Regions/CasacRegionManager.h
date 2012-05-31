a95aeaa3e536795f6dc90ae0f8e470096f0dc421
dave mehringer <dmehring@zia.aoc.nrao.edu>
2012-05-31 19:04:08 +0000

    New Development: No             JIRA Issue: No         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: added API documentation
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@19883 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/imageanalysis/implement/Regions/CasacRegionManager.h
+++ code/imageanalysis/implement/Regions/CasacRegionManager.h
@@ -56,15 +56,32 @@ public:
 	virtual ~CasacRegionManager();
 
 
-	/*
-	 * regionName should be of the form imagename:regionname if the region
-	 * is a TableDescriptor in an image
-	 */
+	// convert to a record a region specified by a rectangular directional <src>box</src>,
+	// <src>chans</src>, and <src>stokes</src>, or althernatively a pointer to
+	// a region record. If box, chans, or stokes is not blank, <src>regionPtr</src> should
+	// be null. Processing happens in the following order:
+	// 1. if <src>box</src> is not empty it, along with <src>chans</src> and <src>stokes</src>
+	// if specified, are used to determine the returned record. In this case <src>stokes</src>
+	// is not altered.
+	// 2. else if <src>regionPtr</src> is not null, it is used to determine the returned Record.
+	// In this case, stokes may be modified to reflect the stokes parameters included in the
+	// corresponding region.
+	// 3. else if <src>regionName</src> is not empty, the region file of the same is read and
+	// used to create the record, or if <src>regionName</src> is of the form "imagename:regionname" the region
+	// record is read from the corresponding iamge. In this case, stokes may be modified
+	// to reflect the stokes parameters included in the corresponding region.
+	// 4. else <src>chans</src> and <src>stokes</src> are used to determine the region Record, or
+	// if not given, the whole <src>imShape</src> is used.
+	// <src>box</src> is specified in comma seperated quadruplets representing blc and trc pixel
+	// locations, eg "100, 110, 200, 205". <src>stokes</src> is specified as the concatentation of
+	// stokes parameters, eg "IQUV". <src>chans</src> is specified in ways supported by CASA, eg
+	// "1~10" for channels 1 through 10.
+
 	Record fromBCS(
-			String& diagnostics, uInt& nSelectedChannels, String& stokes,
-			const Record  * const regionPtr, const String& regionName,
-			const String& chans, const StokesControl stokesControl,
-			const String& box, const IPosition& imShape, const String& imageName=""
+		String& diagnostics, uInt& nSelectedChannels, String& stokes,
+		const Record  * const regionPtr, const String& regionName,
+		const String& chans, const StokesControl stokesControl,
+		const String& box, const IPosition& imShape, const String& imageName=""
 	);
 
 	static vector<uInt> consolidateAndOrderRanges(
