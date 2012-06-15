cc23bae2e2724f757fa13903571fdbfabb32ed18
wes young <wyoung@nrao.edu>
2012-06-15 22:38:27 +0000

    More clean up from removing implement dirs
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@20056 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/calanalysis/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /export/home/spike2/nelias/personal/code/calanalysis
-# Build directory: /export/home/spike2/nelias/personal/code/xmlcasa/calanalysis
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
