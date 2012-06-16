d95441371165d74fc76f957f4d43cf5d6bf6de1a
wes young <wyoung@nrao.edu>
2012-06-16 18:01:56 +0000

    Changed bindings to gcwrap
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@20062 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/install/setsvneditor.sh
+++ gcwrap/install/setsvneditor.sh
@@ -9,7 +9,7 @@
 #Routine for setting the SVN_EDITOR to get commit template
 AIPSROOT=`echo $CASAPATH | awk '{print $1}'`
 
-export SVN_EDITOR=$AIPSROOT/bindings/install/svn_editor
+export SVN_EDITOR=$AIPSROOT/gcwrap/install/svn_editor
 echo "SVN_EDITOR is set to $SVN_EDITOR"
 
 if [ "$1" != "" ]; then
