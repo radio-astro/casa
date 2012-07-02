457cac6377ecf02f272070eaf2ca51adc6e2f23a
scott rankin <srankin@nrao.edu>
2012-07-02 18:46:47 +0000

    Moving old releases to Subversion standard directory structure.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/release/3.0.0@20198 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/xmlcasa/implement/atmosphere/scripts/AtmProfileTest.py
+++
@@ -1,7 +0,0 @@
-attool = casac.homefinder.find_home_by_name('atmosphereHome')
-at = attool.create()
-print at.initAtmProfile()
-l = at.getProfile()
-print l['return']
-print "Output record keys: ", l.keys()
-exit()
