280b000c722591aad4e50587996f732d98fb0687
takeshi nakazato <takeshi.nakazato@nao.ac.jp>
2012-07-23 20:15:33 +0000

    New Development: No             JIRA Issue: Yes CAS-4335         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: __rethrow_casa_exceptions=True                         default(sdgrid)                         infiles='<any file>'                         outfile'<any existing file>'                         sdgrid()   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: Describe your changes here...
    Changed sdgrid task to rethrow exception caught.
    	
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@20413 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/tests/test_sdgrid.py
+++ gcwrap/python/scripts/tests/test_sdgrid.py
@@ -4,7 +4,6 @@ import shutil
 from __main__ import default
 from tasks import *
 from taskinit import *
-from asap_init import *
 import unittest
 import sha
 import time
@@ -12,8 +11,7 @@ import numpy
 import re
 import string
 
-asap_init()
-from sdgrid import sdgrid
+from sdgrid_cli import sdgrid_cli as sdgrid
 import asap as sd
 
 #
