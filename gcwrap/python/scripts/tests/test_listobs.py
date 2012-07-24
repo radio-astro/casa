3f0fc0a5e5806025717e790f5f316093cb663abd
sandra castro <sbhatnag@nrao.edu>
2012-07-24 13:50:57 +0000

    New Development: No             JIRA Issue: CAS-4106         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: test_listobs   Put in Release Notes: No              Module(s): Module Names change impacts.            Description: Modified the tests to run on MMS data sets.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@20433 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/tests/test_listobs.py
+++ gcwrap/python/scripts/tests/test_listobs.py
@@ -17,9 +17,20 @@ Unit tests for task listobs. It tests the following parameters:
     
 '''
 
+datapath = os.environ.get('CASAPATH').split()[0] + '/data/regression/unittest/listobs/'
+
+# Pick up alternative data directory to run tests on MMSs
+testmms = False
+if os.environ.has_key('TEST_DATADIR'):   
+    testmms = True
+    DATADIR = str(os.environ.get('TEST_DATADIR'))
+    if os.path.isdir(DATADIR):
+        datapath = DATADIR+'/listobs/'
+
+print 'Listobs tests will use data from '+datapath         
+
 # Reference files
-refpath = os.environ.get('CASAPATH').split()[0] + '/data/regression/unittest/listobs/'
-reffile = refpath+'reflistobs'
+reffile = datapath+'reflistobs'
 
 # Input and output names
 msfile1 = 'ngc5921_ut.ms'
@@ -29,7 +40,6 @@ class listobs_test1(unittest.TestCase):
 
     def setUp(self):
         self.res = None
-        datapath = os.environ.get('CASAPATH').split()[0] + '/data/regression/unittest/listobs/'
         if (not os.path.exists(msfile1)):            
             shutil.copytree(datapath+msfile1, msfile1)
 
