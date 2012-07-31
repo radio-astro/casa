50c70664f979cd0ae2167c985b9d644bdfdb0dd7
sandra castro <sbhatnag@nrao.edu>
2012-07-31 14:42:14 +0000

    New Development: No             JIRA Issue:         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: test_listobs   Put in Release Notes: No              Module(s): Module Names change impacts.            Description: It will print a warning if directory from DATADIR does not exist.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@20534 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/tests/test_listobs.py
+++ gcwrap/python/scripts/tests/test_listobs.py
@@ -22,10 +22,12 @@ datapath = os.environ.get('CASAPATH').split()[0] + '/data/regression/unittest/li
 # Pick up alternative data directory to run tests on MMSs
 testmms = False
 if os.environ.has_key('TEST_DATADIR'):   
-    testmms = True
-    DATADIR = str(os.environ.get('TEST_DATADIR'))
+    DATADIR = str(os.environ.get('TEST_DATADIR'))+'/listobs/'
     if os.path.isdir(DATADIR):
-        datapath = DATADIR+'/listobs/'
+        testmms = True
+        datapath = DATADIR
+    else:
+        print 'WARN: directory '+DATADIR+' does not exist'
 
 print 'Listobs tests will use data from '+datapath         
 
