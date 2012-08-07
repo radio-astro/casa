4f07dfb2ea4acb4738d3280341d3f073f710c3b3
sandra castro <sbhatnag@nrao.edu>
2012-08-07 14:40:44 +0000

    New Development: No             JIRA Issue: CAS-4138         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs:   Put in Release Notes: No              Module(s): Module Names change impacts.            Description: Added a new class to take the difference of two Python dictionaries.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@20607 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/testhelper.py
+++ gcwrap/python/scripts/testhelper.py
@@ -11,6 +11,8 @@ import numpy as np
 '''
 A set of helper functions for unit tests:
    compTables - compare two CASA tables
+   DictDiffer - a class with methods to take a difference of two 
+                Python dictionaries
 '''
 
 def compTables(referencetab, testtab, excludecols, tolerance=0.001):
@@ -114,3 +116,27 @@ def compTables(referencetab, testtab, excludecols, tolerance=0.001):
 
     return rval
 
+class DictDiffer(object):
+    """
+    Calculate the difference between two dictionaries as:
+    (1) items added
+    (2) items removed
+    (3) keys same in both but changed values
+    (4) keys same in both and unchanged values
+    Example:
+            mydiff = DictDiffer(dict1, dict2)
+            mydiff.changed()  # to show what has changed
+    """
+    def __init__(self, current_dict, past_dict):
+        self.current_dict, self.past_dict = current_dict, past_dict
+        self.set_current, self.set_past = set(current_dict.keys()), set(past_dict.keys())
+        self.intersect = self.set_current.intersection(self.set_past)
+    def added(self):
+        return self.set_current - self.intersect 
+    def removed(self):
+        return self.set_past - self.intersect 
+    def changed(self):
+        return set(o for o in self.intersect if self.past_dict[o] != self.current_dict[o])            
+    def unchanged(self):
+        return set(o for o in self.intersect if self.past_dict[o] == self.current_dict[o])
+    
