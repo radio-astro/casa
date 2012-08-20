deaf63188a54bcdefd52b9d413c523e65fd208f7
dave mehringer <dmehring@zia.aoc.nrao.edu>
2012-08-20 20:34:18 +0000

    New Development: No             JIRA Issue: CAS-4425         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: test to verify CAS-4425 fix.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@20741 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/tests/test_rg_fromtextfile.py
+++ gcwrap/python/scripts/tests/test_rg_fromtextfile.py
@@ -198,7 +198,20 @@ class rg_fromtextfile_test(unittest.TestCase):
         )
         self.assertTrue(len(zz.keys()) > 0)
 
-
+    def test_CAS_4425(self):
+        """ Verify CAS-4425 (pixel units now accounted for in range and no units throws exception)"""
+        shape = [100, 100, 80]
+        self.ia.fromshape("", shape)
+        csys = self.ia.coordsys()
+        zz = rg.fromtext("box[[30pix, 30pix], [39pix, 39pix]], range=[55pix,59pix]", shape, csys.torecord())
+        self.assertTrue(self.ia.statistics(region=zz)["npts"] == 500)
+        zz = rg.fromtext("box[[30pix, 30pix], [39pix, 39pix]], range=[59pix,55pix]", shape, csys.torecord())
+        self.assertTrue(self.ia.statistics(region=zz)["npts"] == 500)
+        self.assertRaises(
+            Exception, rg.fromtext, "box[[30pix, 30pix], [39pix, 39pix]], range=[59,55]",
+            shape, csys.torecord()
+        )
+         
         
 
 def suite():
