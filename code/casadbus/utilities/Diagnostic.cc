1fef079491fbbab74543af7a6cad73309efbcc67
scott rankin <srankin@nrao.edu>
2012-10-02 16:15:40 +0000

    Merged revisions 21299 via svnmerge from https://svn.cv.nrao.edu/svn/casa/active/code
    ........
      r21299 | darrell.schiebel | 2012-10-02 08:22:40 -0600 (Tue, 02 Oct 2012) | 11 lines
      
             New Development: no
                  JIRA Issue: no
            Ready to Release: yes
           Interface Changes: none
      What Interface Changed: none
               Test Programs: none
        Put in Release Notes: no
      
                 Description: dbus::diagnostic singleton failed with osx dynamic libraries
    ........
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/stable-2012-09@21302 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/casadbus/utilities/Diagnostic.cc
+++ code/casadbus/utilities/Diagnostic.cc
@@ -41,9 +41,23 @@ extern "C" char **environ;
 namespace casa {
     namespace dbus {
 
-	unsigned long init_diagnostic_object_t::count = 0;
+  	unsigned long init_diagnostic_object_t::count = 0;
 
+	Diagnostic::kernel_t::kernel_t( ) : fptr(0), pid(getpid( )) { }
 	Diagnostic::kernel_t::kernel_t( FILE *f ) : fptr(f), pid(getpid( )) { }
+
+	Diagnostic::kernel_t &Diagnostic::lock_kernel( ) {
+	    // appropriate semaphores can be added when necessary...
+	    static Diagnostic::kernel_t *global_kernel = 0;
+	    if ( global_kernel == 0 ) {
+		char *outfile = getenv("CASA_DIAGNOSTIC_FILE");
+		if ( outfile == NULL )
+		    global_kernel = new kernel_t(0);
+		else
+		    global_kernel = new kernel_t(fopen(outfile,"a"));
+	    }
+	    return *global_kernel;
+	}
 
 #define ARGV(TYPE)								\
 	void Diagnostic::kernel_t::argv( int argc_, TYPE *argv_[] ) {		\
@@ -61,49 +75,43 @@ namespace casa {
 	ARGV(const char)
 	ARGV(char)
 
-	void init_diagnostic_object_t::do_initialize( ) {
-
-	    char *outfile = getenv("CASA_DIAGNOSTIC_FILE");
-	    if ( outfile == NULL )
-		diagnostic.kernel_ = new Diagnostic::kernel_t(0);
-	    else
-		diagnostic.kernel_ = new Diagnostic::kernel_t(fopen(outfile,"a"));
-
-	    if ( diagnostic.kernel_->fptr ) {
+	void Diagnostic::output_prologue( ) {
+	    kernel_t &k = lock_kernel( );
+	    if ( k.fptr ) {
 		char buf[PATH_MAX+1];
 		// --- --- current time    --- --- --- --- --- --- --- ---
 		struct timeval tv = {0,0};
 		gettimeofday( &tv, 0 );
 		strftime( buf, PATH_MAX+1, "begin:   %F %T", localtime(&tv.tv_sec) );
-		fprintf( diagnostic.kernel_->fptr, "%5d %s\n", diagnostic.kernel_->pid, buf );
-		fflush( diagnostic.kernel_->fptr );
+		fprintf( k.fptr, "%5d %s\n", k.pid, buf );
+		fflush( k.fptr );
 		// --- --- current environment --- --- --- --- --- --- ---
 #if defined(__APPLE__)
 		char*** envPtr = _NSGetEnviron();	// pointer to the real 'environ'
 		char** environ = *envPtr;
 #endif
 		for ( char **ep=environ; *ep; ++ep )
-		    fprintf( diagnostic.kernel_->fptr, "%5d env:     %s\n", diagnostic.kernel_->pid, *ep );
-		fflush( diagnostic.kernel_->fptr );
+		    fprintf( k.fptr, "%5d env:     %s\n", k.pid, *ep );
+		fflush( k.fptr );
 		// --- --- current path    --- --- --- --- --- --- --- ---
 		if ( getcwd(buf,PATH_MAX+1) != NULL )
-		    fprintf( diagnostic.kernel_->fptr, "%5d cwd:     %s\n", diagnostic.kernel_->pid, buf );
+		    fprintf( k.fptr, "%5d cwd:     %s\n", k.pid, buf );
+	    }
+	    release_kernel( );
+	}
+
+	void Diagnostic::output_epilogue( ) {
+	    kernel_t &k = lock_kernel( );
+	    if ( k.fptr ) {
+		char buf[2048];
+		struct timeval tv = {0,0};
+		gettimeofday( &tv, 0 );
+		strftime( buf, 2048, "end:     %F %T", localtime(&tv.tv_sec) );
+		fprintf( k.fptr, "%5d %s\n", k.pid, buf );
+		fflush( k.fptr );
 	    }
 	}
 
-	void init_diagnostic_object_t::do_finalize( ) {
-	    if ( diagnostic.kernel_ ) {
-		if ( diagnostic.kernel_->fptr ) {
-		    char buf[2048];
-		    struct timeval tv = {0,0};
-		    gettimeofday( &tv, 0 );
-		    strftime( buf, 2048, "end:     %F %T", localtime(&tv.tv_sec) );
-		    fprintf( diagnostic.kernel_->fptr, "%5d %s\n", diagnostic.kernel_->pid, buf );
-		    fflush( diagnostic.kernel_->fptr );
-		}
-		delete diagnostic.kernel_;
-	    }
-	}
     }
 }
 
