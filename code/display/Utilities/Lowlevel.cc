7fd56bc56dc284f6cc87fb19e6f0fe1587a7ce3a
darrell schiebel <ds@nrao.edu>
2012-10-05 18:23:48 +0000

    New Development: no             JIRA Issue: no       Ready to Release: yes      Interface Changes: none What Interface Changed: none          Test Programs: none   Put in Release Notes: no
               Description: add support for --rcdir to casaviewer
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@21400 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/display/Utilities/Lowlevel.cc
+++ code/display/Utilities/Lowlevel.cc
@@ -33,14 +33,17 @@
 #include <sys/stat.h>
 #include <time.h>
 #include <sys/time.h>
+#include <libgen.h>
 #include <QApplication>
+#include <errno.h>
 
 namespace casa { //# NAMESPACE CASA - BEGIN
     namespace viewer { //# NAMESPACE VIEWER - BEGIN
 	//
 	//  helper function to create ~/.casa/ipython/security
+	//  returns true upon success, false upon failure...
 	//
-	int create_dir( const char *path ) {
+	bool create_dir( const char *path, bool recurse ) {
 	    struct stat buf;
 	    if ( stat( path, &buf ) == 0 ) {
 		if ( ! S_ISDIR(buf.st_mode) ) {
@@ -54,20 +57,46 @@ namespace casa { //# NAMESPACE CASA - BEGIN
 
 		    if ( rename( path, savepath ) != 0 ) {
 			free(savepath);
-			return 1;
+			return false;
 		    }
 		    if ( mkdir( path, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH ) != 0 ) {
 			free(savepath);
-			return 1;
+			return false;
 		    }
 		    free(savepath);
 		}
 	    } else {
 		if ( mkdir( path, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH ) != 0 ) {
-		    return 1;
+		    if ( recurse ) {
+
+			// can't write to the parent directory...
+			if ( errno == EACCES ) return false;
+
+			char *orig = strdup(path);
+			char *dir = dirname(orig);
+			if ( ! strcmp("/",dir) || ! strcmp(".",dir) ) {
+			    free(orig);
+			    return true;
+			} else if ( ! strcmp("..",dir) ) {
+			    free(orig);
+			    char buf[10];
+			    if ( ! strcmp("/",getcwd(buf,10)) )
+				return false;
+			    else
+				return true;
+			}
+			if ( create_dir(dir,true) ) {
+			    free( orig );
+			    return create_dir(path,false);
+			} else {
+			    return false;
+			}
+		    } else {
+			return false;
+		    }
 		}
 	    }
-	    return 0;
+	    return true;
 	}
 
 	std::string time( ) {
@@ -88,16 +117,31 @@ namespace casa { //# NAMESPACE CASA - BEGIN
 	    return std::string(buf);
 	}
 
+	static std::string rc_home;
+	bool setrcDir( const std::string &home ) {
+	    if ( ! rc_home.empty( ) ) {
+		return false;
+	    } else if ( create_dir( home + "/viewer", true ) ) {
+		rc_home = home + "/viewer";
+		return true;
+	    } else {
+		return false;
+	    }
+	}
+
 	Casarc &getrc( ) {
 	    char *home = getenv("HOME");
+	    if ( ! rc_home.empty( ) ) {
+		return Casarc::instance(rc_home + "/rc");
+	    }
 	    if ( home ) {
 		std::string rcf(home);
 		rcf += "/.casa";
 		create_dir( rcf );
 		rcf += "/viewer";
 		create_dir( rcf );
-		rcf += "/rc";
-		return Casarc::instance(rcf);
+		rc_home = rcf;
+		return Casarc::instance(rc_home + "/rc");
 	    } else {
 		fprintf( stderr, "HOME is not defined... using current directory to store state...\n" );
 		return Casarc::instance("casaviewerrc");
@@ -114,4 +158,3 @@ namespace casa { //# NAMESPACE CASA - BEGIN
 
     } //# NAMESPACE VIEWER - BEGIN
 } //# NAMESPACE CASA - BEGIN
-
