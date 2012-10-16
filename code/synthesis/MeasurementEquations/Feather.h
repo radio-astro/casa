484a5b572db88f972b53620dff1571f4ce001a29
kumar golap <kgolap@nrao.edu>
2012-10-16 20:06:29 +0000

    New Development: Yes             JIRA Issue: No List JIRA ticket.         Ready for Test: Yes      Interface Changes: Yes/No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: Describe your changes here...
    	Added dummy new API for feather
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@21660 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/synthesis/MeasurementEquations/Feather.h
+++ code/synthesis/MeasurementEquations/Feather.h
@@ -43,6 +43,36 @@ namespace casa { //# NAMESPACE CASA - BEGIN
   class Feather{
 
   public:
+
+    //default constructor
+    Feather();
+    //Constructor 
+    Feather(const ImageInterface<Float>& SDImage, const ImageInterface<Float>& INTImage);
+    //Destructor
+    virtual ~Feather();
+    //set the SDimage and Int images 
+    void setSDImage(const ImageInterface<Float>& SDImage);
+    void setINTImage(const ImageInterface<Float>& IntImage);
+    //set and get effective dish diameter to be used in feathering function
+    //setEffectiveDishDiam will return False if you are trying to assign a finer resolution than what 
+    //the original data came with 
+    Bool setEffectiveDishDiam(const Float xdiam, const Float ydiam=-1.0);
+    void getEffectiveDishDiam(Float& xdiam, Float& ydiam);
+    //Get the 1-D slices of amplitude  along the x and y axis of the FFT of images  
+    //Note the SD image is already feathered by its beam..you cannot get unfeathered SD data as it implies deconvolution
+    void getFTCutSDImage(Vector<Float>& ux, Vector<Float>& xamp, Vector<Float>& uy, Vector<Float>& yamp);
+    void getFTCutIntImage(Vector<Float>& ux, Vector<Float>& xamp, Vector<Float>& uy, Vector<Float>& yamp);
+    //Get the 1-D slices of the feathering function that will be applied on SD and INTerf data
+    void getFeatherSD(Vector<Float>& ux, Vector<Float>& xamp, Vector<Float>& uy, Vector<Float>& yamp);
+    void getFeatherINT(Vector<Float>& ux, Vector<Float>& xamp, Vector<Float>& uy, Vector<Float>& yamp);
+    //Get 1-D slices of the feathered data ...note for SD is effectiveDishdiam is left to the original of image this
+    //should return the same values as getFTCutSDImage
+    void getFeatheredCutSD(Vector<Float>& ux, Vector<Float>& xamp, Vector<Float>& uy, Vector<Float>& yamp);
+    void getFeatheredCutINT(Vector<Float>& ux, Vector<Float>& xamp, Vector<Float>& uy, Vector<Float>& yamp);
+    //write the feathered image to disk
+    Bool saveFeatheredImage(const String& imagename);
+
+    ///////////////Old methods left for now till new version is implemented
     static void feather(const String& image, const ImageInterface<Float>& high, const ImageInterface<Float>& low, const Float& sdScale=1.0, const String& lowPSF="", const Bool useDefault=True, const String& vpTable="" , Float effSDDiam=-1.0, const Bool doPlot=False);
 
     static Double worldFreq(const CoordinateSystem& cs, Int spectralpix=0);
