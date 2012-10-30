62d29899bf98e2be2ad476dc16bd10695394b774
takeshi nakazato <takeshi.nakazato@nao.ac.jp>
2012-10-30 02:22:24 +0000

    New Development: No             JIRA Issue: Yes CAS-2822         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: test_sdflagmanager   Put in Release Notes: No              Module(s): Module Names change impacts.            Description: Describe your changes here...
    Refactoring sdflagmanager task.
    
    	
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@21879 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/task_sdflagmanager.py
+++ gcwrap/python/scripts/task_sdflagmanager.py
@@ -13,39 +13,56 @@ def sdflagmanager(infile, mode, versionname, oldname, comment, merge):
                 sdutil.assert_infile_exists(infile)
                 infilename=sdutil.get_abspath(infile)
 
-		if (mode=='list') or (mode=='save') or (mode=='restore') or (mode=='delete') or (mode=='rename'):
-			import datetime
-			dt = datetime.datetime.today()
-			dtstr = '%d%02d%02d%02d%02d%02d' % (dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
-			
-			msfilename = infilename + '-sdflagmanager-temp-' + dtstr + '.ms'
-			# if directories 'msfilename' and 'msfilename.flagversions' already exist accidentally,
-			# rename it for backup (backupms, backupmsfver)
+                domanage(infilename, mode.lower(), versionname, oldname, comment, merge)
 
-			sdfverfile = infilename + '.flagversions'
-			msfverfile = msfilename + '.flagversions'
-
-			if os.path.exists(sdfverfile):
-				os.system('mv %s %s' % (sdfverfile, msfverfile))
-			
-                        sdutil.save(sd.scantable(infilename,False), msfilename, 'MS2', False)
-			task_flagmanager.flagmanager(msfilename, mode, versionname, oldname, comment, merge)
-
-			if mode=='restore':
-				backupinfile = infilename + '-sdflagmanager-backup-' + dtstr + '.asap'
-				# if a directory with the same name as backupinfile exists, rename it for backup
-				os.system('mv %s %s' % (infilename, backupinfile))
-                                sdutil.save(sd.scantable(msfilename,False), infilename, 'ASAP', False)
-			
-			os.system('rm -rf %s' % (msfilename))
-			os.system('mv %s %s' % (msfverfile, sdfverfile))
-			
-			# if backupms and backupmsfver exist, rename them back
-			# to msfilename and msfilename.flagversions, respectively
-			
-		else:
-			raise Exception, "Unknown mode" + str(mode)
-		
 	except Exception, instance:
                 sdutil.process_exception(instance)
                 raise Exception, instance
+
+def domanage(infilename, mode, versionname, oldname, comment, merge):
+    availablemodes = ['list','save','restore','delete','rename']
+    if mode in availablemodes:
+        namer = filenamer('sdflagmanager',infilename)
+        msfilename = namer.configure_name(kind='temp',suffix='ms')
+        # if directories 'msfilename' and 'msfilename.flagversions' already exist accidentally,
+        # rename it for backup (backupms, backupmsfver)
+        sdfverfile = namer.flagversion_name(infilename)
+        msfverfile = namer.flagversion_name(msfilename)
+
+        if os.path.exists(sdfverfile):
+            move(sdfverfile, msfverfile)
+
+        sdutil.save(sd.scantable(infilename,False), msfilename, 'MS2', False)
+        task_flagmanager.flagmanager(msfilename, mode, versionname, oldname, comment, merge)
+
+        if mode=='restore':
+            backupinfile = namer.configure_name(kind='backup',suffix='asap')
+            # if a directory with the same name as backupinfile exists, rename it for backup
+            move(infilename, backupinfile)
+            sdutil.save(sd.scantable(msfilename,False), infilename, 'ASAP', False)
+
+        remove(msfilename)
+        move(msfverfile, sdfverfile)
+
+        # if backupms and backupmsfver exist, rename them back
+        # to msfilename and msfilename.flagversions, respectively
+    else:
+        raise Exception, "Unknown mode" + str(mode)
+
+
+class filenamer(object):
+    def __init__(self, task, infile):
+        import datetime
+        dt = datetime.datetime.today()
+        timestamp = '%d%02d%02d%02d%02d%02d' % (dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
+        self.tmp = string.Template('%s-%s-${kind}-%s.${suffix}'%(infile,task,timestamp))
+        self.flagver = 'flagversions'
+    def configure_name(self,*args,**kwargs):
+        return self.tmp.safe_substitute(**kwargs)
+    def flagversion_name(self,filename):
+        return string.join([filename.rstrip('/'),self.flagver],'.')
+
+def move(fromfile, tofile):
+    os.system('mv %s %s'%(fromfile,tofile))
+def remove(filename):
+    os.system('rm -rf %s'%(filename))
