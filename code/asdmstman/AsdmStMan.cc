5e7608b62042d4a6a7cbb13e0eb96a14fc99b2df
michel caillat <michel.caillat@nrao.edu>
2012-11-20 10:53:56 +0000

    New Development: Yes             JIRA Issue: No         Ready for Test: No      Interface Changes: No What Interface Changed:          Test Programs:   Put in Release Notes: No              Module(s): asdmstman            Description: Changed the logic in the getData method so that it takes into account that an AsdmIndex entry represents data for *one* spectral window ( in the code the variable spw is set systematically to 0, eventually it'll be removed if it works like this)
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@22076 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/asdmstman/AsdmStMan.cc
+++ code/asdmstman/AsdmStMan.cc
@@ -281,8 +281,12 @@ const AsdmIndex& AsdmStMan::findIndex (Int64 rownr)
 void AsdmStMan::getShort (const AsdmIndex& ix, Complex* buf, uInt bl, uInt spw)
 {
   // Get pointer to the data in the block.
-  Short* data = (reinterpret_cast<Short*>(&(itsData[0])) +
-                 2 * (spw*ix.stepSpw + bl*ix.stepBl));
+  //Short* data = (reinterpret_cast<Short*>(&(itsData[0])) +
+  //             2 * (spw*ix.stepSpw + bl*ix.stepBl));
+  Short* data = reinterpret_cast<Short*>(&itsData[ix.blockOffset + 2 * bl * ix.stepBl * sizeof(Short)]);  // Michel Caillat - 19  Nov 2012
+
+  //cout << "getShort works at this adress : " << (unsigned long long int) data << endl;
+
   if (itsDoSwap) {
     Short real,imag;
     for (uInt j=0; j<ix.nChan; ++j) {
@@ -308,8 +312,10 @@ void AsdmStMan::getShort (const AsdmIndex& ix, Complex* buf, uInt bl, uInt spw)
 void AsdmStMan::getInt (const AsdmIndex& ix, Complex* buf, uInt bl, uInt spw)
 {
   // Get pointer to the data in the block.
-  Int* data = (reinterpret_cast<Int*>(&(itsData[0])) +
-               2 * (spw*ix.stepSpw + bl*ix.stepBl));
+  //Int* data = (reinterpret_cast<Int*>(&(itsData[0])) +
+  //           2 * (spw*ix.stepSpw + bl*ix.stepBl));
+  Int* data = (reinterpret_cast<Int*>(&(itsData[ix.blockOffset])) +  // 19 Nov 2012
+               2 * (spw*ix.stepSpw + bl*ix.stepBl));                 // Michel Caillat
   if (itsDoSwap) {
     Int real,imag;
     for (uInt j=0; j<ix.nChan; ++j) {
@@ -335,8 +341,10 @@ void AsdmStMan::getInt (const AsdmIndex& ix, Complex* buf, uInt bl, uInt spw)
 void AsdmStMan::getFloat (const AsdmIndex& ix, Complex* buf, uInt bl, uInt spw)
 {
   // Get pointer to the data in the block.
-  Float* data = (reinterpret_cast<Float*>(&(itsData[0])) +
-                 2 * (spw*ix.stepSpw + bl*ix.stepBl));
+  //  Float* data = (reinterpret_cast<Float*>(&(itsData[0])) +
+  //             2 * (spw*ix.stepSpw + bl*ix.stepBl));
+  Float* data = (reinterpret_cast<Float*>(&(itsData[ix.blockOffset])) +  // 19 Nov 2012
+                 2 * (spw*ix.stepSpw + bl*ix.stepBl));                   // Michel Caillat
   if (itsDoSwap) {
     Float real,imag;
     for (uInt j=0; j<ix.nChan; ++j) {
@@ -362,8 +370,10 @@ void AsdmStMan::getFloat (const AsdmIndex& ix, Complex* buf, uInt bl, uInt spw)
 void AsdmStMan::getAuto (const AsdmIndex& ix, Complex* buf, uInt bl, uInt spw)
 {
   // Get pointer to the data in the block.
-  Float* data = (reinterpret_cast<Float*>(&(itsData[0])) +
-                 spw*ix.stepSpw + bl*ix.stepBl);
+  //  Float* data = (reinterpret_cast<Float*>(&(itsData[0])) +
+  //             spw*ix.stepSpw + bl*ix.stepBl);
+  Float* data = (reinterpret_cast<Float*>(&(itsData[ix.blockOffset])) +  // 19 Nov 2012 . Michel Caillat replaced the 
+		 spw*ix.stepSpw + bl*ix.stepBl);            // two lines above by these ones.
   // The autocorr can have 1, 2, 3 or 4 npol.
   // 1 and 2 are XX and/or YY which are real numbers.
   // 3 are all 4 pols with XY a complex number and YX=conj(XY).
@@ -442,6 +452,7 @@ IPosition AsdmStMan::getShape (uInt rownr)
 void AsdmStMan::getData (uInt rownr, Complex* buf)
 {
   const AsdmIndex& ix = findIndex (rownr);
+
   // Open the BDF if needed.
   if (Int(ix.fileNr) != itsOpenBDF) {
     closeBDF();
@@ -457,8 +468,11 @@ void AsdmStMan::getData (uInt rownr, Complex* buf)
   }
   // Determine the spw and baseline from the row.
   // The rows are stored in order of spw,baseline.
-  uInt spw = (rownr - ix.row) / ix.nBl;
+  uInt spw = 0 ; // 19 Nov 2012 : Michel Caillat changed this assignement : (rownr - ix.row) / ix.nBl;
   uInt bl  = (rownr - ix.row) - spw*ix.nBl;
+  //if (rownr >= 110 && rownr <= 113) {
+  //  cout << "rownr = " << rownr << ", bl = " << bl << ", ix = " << ix << endl;
+  //}
   switch (ix.dataType) {
   case 0:
     getShort (ix, buf, bl, spw);
