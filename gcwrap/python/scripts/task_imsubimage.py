71f7d1091608fd73ae9ea2fde717a48b2d99d2cd
dave mehringer <dmehring@zia.aoc.nrao.edu>
2012-12-11 22:05:36 +0000

    New Development: No             JIRA Issue: No         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: better exception handling
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@22260 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/task_imsubimage.py
+++ gcwrap/python/scripts/task_imsubimage.py
@@ -1,12 +1,12 @@
 from taskinit import *
 
 def imsubimage(
-    imagename=None, outfile=None, region=None,
-    mask=None, dropdeg=None, overwrite=None, verbose=None,
-    stretch=None, wantreturn=None
+    imagename, outfile, region, mask, dropdeg,
+    overwrite, verbose, stretch, wantreturn
 ):
     casalog.origin('imsubimage')
     myia = iatool()
+    outia = None
     try:
         if (not myia.open(imagename)):
             raise Exception, "Cannot create image analysis tool using " + imagename
@@ -14,17 +14,17 @@ def imsubimage(
             outfile=outfile, region=region, mask=mask, dropdeg=dropdeg,
             overwrite=overwrite, list=verbose, stretch=stretch
         )
-        myia.done()
         if (wantreturn):
             return outia
         else:
             outia.done()
             return True
     except Exception, instance:
-        if (myia):
-            myia.done()
         if (outia):
             outia.done()
         casalog.post( str( '*** Error ***') + str(instance), 'SEVERE')
-        return False
+        raise
+    finally:
+        if (myia):
+            myia.done()
         
