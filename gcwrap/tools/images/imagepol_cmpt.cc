63d1e6e49683b967177143be0825075214c1c759
dave mehringer <dmehring@zia.aoc.nrao.edu>
2012-12-13 19:21:22 +0000

    New Development: No             JIRA Issue: CAS-4767         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: moved ImageAnalysis from casacore -> code
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@22295 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/tools/images/imagepol_cmpt.cc
+++ gcwrap/tools/images/imagepol_cmpt.cc
@@ -16,7 +16,7 @@
 #include <casa/Exceptions/Error.h>
 #include <casa/Logging/LogIO.h>
 #include <images/Images/ImageInterface.h>
-#include <images/Images/ImageAnalysis.h>
+#include <imageanalysis/ImageAnalysis/ImageAnalysis.h>
 #include <images/Images/ImageUtilities.h>
 #include <images/Images/ImageExpr.h>
 #include <images/Images/ImagePolProxy.h>
