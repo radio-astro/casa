ef7e39641550a88d8e7a36d5236c8382ddfc68ec
sanjay bhatnagar <sbhatnag@nrao.edu>
2012-12-20 23:25:41 +0000

    New Development: Yes             JIRA Issue: Yes (CAS-4616)         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes              Module(s): Module Names change impacts.            Description: Describe your changes here...
    Added the function for gridding using double complex grid.	
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/active@22369 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/synthesis/fortran/faccumulateToGrid.f
+++ code/synthesis/fortran/faccumulateToGrid.f
@@ -76,6 +76,84 @@
       enddo
       end
       
+      subroutine dfaccumulateToGrid(grid, convFuncV, nvalue, wVal, 
+     $     scaledSupport,scaledSampling,
+     $     off, convOrigin, cfShape, loc,
+     $     igrdpos, sinDPA, cosDPA,
+     $     finitePointingOffset,
+     $     doPSFOnly,
+     $     norm,
+     $     phaseGrad,
+     $     imNX, imNY, imNP, imNC,
+     $     cfNX, cfNY, cfNP, cfNC,
+     $     phNX, phNY)
+
+      implicit none
+      integer imNX, imNY, imNC, imNP,
+     $     vNX, vNY, vNC, vNP,
+     $     cfNX, cfNY, cfNP, cfNC,
+     $     phNX, phNY
+
+      double complex grid(imNX, imNY, imNP, imNC)
+      complex convFuncV(cfNX, cfNY, cfNP, cfNC)
+      complex nvalue
+      double precision wVal
+      integer scaledSupport(2)
+      real scaledSampling(2)
+      double precision off(2)
+      integer convOrigin(4), cfShape(4), loc(4), igrdpos(4)
+      double precision sinDPA, cosDPA
+      integer finitePointingOffset, doPSFOnly
+      double precision norm
+      complex phaseGrad(phNX, phNY)
+
+      integer l_phaseGradOriginX, l_phaseGradOriginY
+      integer iloc(4), iCFPos(4)
+      complex wt, cfArea
+
+      complex fGetCFArea
+      integer ix,iy
+      integer l_igrdpos(4)
+
+      data iloc/1,1,1,1/, iCFPos/1,1,1,1/
+      l_igrdpos(3) = igrdpos(3)+1
+      l_igrdpos(4) = igrdpos(4)+1
+      norm=0.0
+      l_phaseGradOriginX=phNX/2 + 1
+      l_phaseGradOriginY=phNY/2 + 1
+
+      cfArea = fGetCFArea(convFuncV, wVal, scaledSupport, 
+     $     scaledSampling, off, convOrigin, cfShape,
+     $     cfNX, cfNY, cfNP, cfNC)
+      
+      do iy=-scaledSupport(2),scaledSupport(2) 
+         iloc(2)=nint(scaledSampling(2)*iy+off(2)-1)
+         iCFPos(2)=iloc(2)+convOrigin(2)+1
+         l_igrdpos(2) = loc(2)+iy+1
+         do ix=-scaledSupport(1),scaledSupport(1)
+            iloc(1)=nint(scaledSampling(1)*ix+off(1)-1)
+            iCFPos(1) = iloc(1) + convOrigin(1) + 1
+            l_igrdpos(1) = loc(1) + ix + 1
+            
+            wt = convFuncV(iCFPos(1), iCFPos(2), 
+     $           iCFPos(3), iCFPos(4))/cfArea
+            if (wVal > 0.0) wt = conjg(wt)
+
+            norm = norm + real(wt)
+
+            if ((finitePointingOffset .eq. 1) .and.
+     $              (doPSFOnly .eq. 0)) then
+               wt = wt * phaseGrad(iloc(1) + l_phaseGradOriginX,
+     $              iloc(2) + l_phaseGradOriginY)
+            endif
+
+            grid(l_igrdpos(1), l_igrdpos(2), l_igrdpos(3), l_igrdpos(4)) 
+     $           =grid(l_igrdpos(1), l_igrdpos(2), 
+     $           l_igrdpos(3), l_igrdpos(4)) + nvalue * wt
+         enddo
+      enddo
+      end
+      
       complex function fGetCFArea(convFuncV, wVal, support, sampling,
      $     off, convOrigin, cfShape,
      $     cfNX, cfNY, cfNP, cfNC)
