b4fb394c2927ec347c454572b422a492a292961a
urvashi rao <rurvashi@nrao.edu>
2013-01-02 21:47:23 +0000

    New Development: Yes             JIRA Issue: cas-4442         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: No              Module(s): Module Names change impacts.            Description: Disabled coefficient-residual calculations when clean is called with niter=-1. This is to make MTMFS work with both single-node runs where residuals must be modified during restoration, and with parallel-clean where residuals must not be modified during restoration.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@22416 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/synthesis/MeasurementEquations/Imager.cc
+++ code/synthesis/MeasurementEquations/Imager.cc
@@ -6911,7 +6911,7 @@ Int Imager::interactivemask(const String& image, const String& mask,
 	}
 
 	os << "Restoring Image(s) with the clean-beam" << LogIO::POST;
-	restoreImages(aimage);
+	restoreImages(aimage, (niter>=0) );
 	this->writeHistory(os);
 	try{
 	  // write data processing history into image logtable
