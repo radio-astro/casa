04cd82fc0ba4101c7c955b3ed64061cff89880e8
dave mehringer <dmehring@zia.aoc.nrao.edu>
2013-01-28 20:45:53 +0000

    New Development: Yes             JIRA Issue: CAS-4828         Ready for Test: Yes      Interface Changes: Yes What Interface Changed: added box/chans/stokes/overwrite parameters          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: as above
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@22655 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/task_imsubimage.py
+++ gcwrap/python/scripts/task_imsubimage.py
@@ -1,7 +1,7 @@
 from taskinit import *
 
 def imsubimage(
-    imagename, outfile, region, mask, dropdeg,
+    imagename, outfile, box, region, chans, stokes, mask, dropdeg,
     overwrite, verbose, stretch, wantreturn
 ):
     casalog.origin('imsubimage')
@@ -10,6 +10,11 @@ def imsubimage(
     try:
         if (not myia.open(imagename)):
             raise Exception, "Cannot create image analysis tool using " + imagename
+        if (type(region) != type({})):
+            region = rg.frombcs(
+                csys=myia.coordsys().torecord(), shape=myia.shape(), box=box,
+                chans=chans, stokes=stokes, stokescontrol="a", region=region
+            )
         outia = myia.subimage(
             outfile=outfile, region=region, mask=mask, dropdeg=dropdeg,
             overwrite=overwrite, list=verbose, stretch=stretch
