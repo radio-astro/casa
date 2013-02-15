3e16f079428561250d6581993e6bef304edb9b84
dave mehringer <dmehring@zia.aoc.nrao.edu>
2013-02-15 15:28:28 +0000

    New Development: No             JIRA Issue: No         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: remove debugging statements
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@22916 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/task_impv.py
+++ gcwrap/python/scripts/task_impv.py
@@ -5,26 +5,20 @@ def impv(
     region, chans, stokes, mask, stretch
 ):
     casalog.origin('impv')
-    
     try:
         if len(outfile) == 0:
             raise Exception, "outfile must be specified."
         myia = iatool()
         if (not myia.open(imagename)):
             raise Exception, "Cannot create image analysis tool using " + imagename
-        print "**** here 0"
-
         myia.pv(
             outfile=outfile, start=start, end=end,
             halfwidth=halfwidth, overwrite=overwrite, region=region,
             chans=chans, stokes=stokes, mask=mask, stretch=stretch,
             wantreturn=False
         )
-        myia.done()
-        print "**** here 1"
         return True
     except Exception, instance:
-        print "**** here 2"
         casalog.post( str( '*** Error ***') + str(instance), 'SEVERE')
         raise
     finally:
