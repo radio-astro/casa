3c51124eaa0432591a354a8d8b514af9f2c7f704
kumar golap <kgolap@nrao.edu>
2013-04-02 01:09:58 +0000

    New Development: No             JIRA Issue: CAS-4956         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: Describe your changes here...
    Fixed/work around  a few quirkyness of ia.regrid along with a upper loewer case bug 
    	
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@23511 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/task_imreframe.py
+++ gcwrap/python/scripts/task_imreframe.py
@@ -1,6 +1,8 @@
 import os
 import shutil
+import string
 from taskinit import *
+import pdb
 
 def imreframe(imagename=None, output=None, outframe=None, epoch=None, restfreq=None):
     try:
@@ -8,11 +10,16 @@ def imreframe(imagename=None, output=None, outframe=None, epoch=None, restfreq=N
         if(output==imagename):
             output=''
         needregrid=False
+        outframe=string.lower(outframe)
         if(((outframe == 'topo') or (outframe=='geo')) and (epoch != '')):
             needregrid=True
         myia,me=gentools(['ia', 'me'])
         myia.open(imagename)
         c=myia.coordsys()
+        me.doframe(me.observatory(c.telescope()))
+        me.doframe(c.referencevalue('m', 'direction')['measure']['direction'])
+        me.doframe(c.epoch())
+        reffreq=c.referencevalue('m', 'spectral')['measure']['spectral']['frequency']
         hasspec,pixax,worldax=c.findcoordinate('spectral')
         if(not hasspec):
             raise Exception, 'Could not find spectral axis'
@@ -22,6 +29,7 @@ def imreframe(imagename=None, output=None, outframe=None, epoch=None, restfreq=N
             c.setrestfrequency(value=qa.quantity(restfreq, 'Hz'))
         if(epoch != ''):
             c.setepoch(me.epoch('utc', epoch))
+            me.doframe(me.epoch('utc', epoch))
         if(not needregrid):
             if(output != ''):
                 myia.fromimage(outfile=output, infile=imagename, overwrite=True)
@@ -30,10 +38,14 @@ def imreframe(imagename=None, output=None, outframe=None, epoch=None, restfreq=N
             myia.setcoordsys(c.torecord())
             myia.done()
         else:
+            c.setreferencecode(outframe, 'spectral', True)
+            newreffreq=me.measure(reffreq, outframe)
+            c.setreferencevalue(qa.tos(newreffreq['m0']), 'spectral')
             outname='_temp_regrid_image' if(output=='') else output
             shp=myia.shape()             
             ib=myia.regrid(outfile=outname, shape=shp, csys=c.torecord(), 
                            axes=pixax, overwrite=True)
+            ib.setcoordsys(c.torecord())
             if(output==''):
                 myia.done()
                 ib.rename(name=imagename, overwrite=True)
