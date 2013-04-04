6cf01a490c19bb920f5038b16363339972c7752f
lindsey davis <ldavis@nrao.edu>
2013-04-04 17:30:30 +0000

    No comment.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project/pipeline@23581 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/hif/tasks/gaincal/__init__.py
+++ pipeline/hif/tasks/gaincal/__init__.py
@@ -4,4 +4,5 @@ from .gaincalworker import GaincalWorker
 from .gtypegaincal import GTypeGaincal
 from .gsplinegaincal import GSplineGaincal
 from .gaincalmode import GaincalMode
+from .newgaincal import NewGaincal
 #from .phaseupgaincal import PhaseUpGaincal
