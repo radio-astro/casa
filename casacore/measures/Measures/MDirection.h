a6b137ab7e9998a40aa328b18e0fe10f4263eb6f
dirk petry <dpetry@eso.org>
2013-04-04 14:48:06 +0000

    New Development: No             JIRA Issue: CAS-3046         Ready for Test: Yes      Interface Changes: Yes What Interface Changed: MDirection.h          Test Programs: measures unit tests   Put in Release Notes: Yes/No              Module(s): measures            Description: Reverted previous introduction of 			MDirection::INVALID. MSFieldColumns is now 			throwing an exception when accessing an ephemeris 			outside its validity range. Hence 			the new type is not needed anymore.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@23573 ac9c0da5-131f-0410-9009-a9c195f183e6

--- casacore/measures/Measures/MDirection.h
+++ casacore/measures/Measures/MDirection.h
@@ -111,11 +111,7 @@ template <class M> class ScalarMeasColumn;
 //  <li>MDirection::DEFAULT = J2000
 // </ul>
 // <p>
-// In addition, an MDirection::INVALID is defined to be used internally
-// to mark directions which could not be determined, e.g. when trying
-// to use an ephemeris outside its validity range.  
-// <p>
-// Conversion between the different valid types is done with the standard
+// Conversion between the different types is done with the standard
 // <linkto class=MeasConvert>MeasConvert</linkto> class 
 // (<src>MDirection::Convert</src> in this case).<br>
 // For some conversion additional <linkto class=MeasFrame>MeasFrame</linkto>
@@ -233,10 +229,7 @@ public:
       DEFAULT=J2000,
       // Synonyms
       AZELNE=AZEL,
-      AZELNEGEO=AZELGEO,
-      // Invalid direction
-      INVALID = 255
-    };
+      AZELNEGEO=AZELGEO };
   // Global types
   enum GlobalTypes {
     GRADEC,
