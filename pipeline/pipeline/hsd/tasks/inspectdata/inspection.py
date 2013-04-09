f361a08f9a5152521b5512b98942a6d8d71afeef
takeshi nakazato <takeshi.nakazato@nao.ac.jp>
2013-04-09 03:25:32 +0000

    New Development: No             JIRA Issue: No         Ready for Test: Yes      Interface Changes: Yes/No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: Describe your changes here...
    Resoved circular dependency between domain and hsd by moving 
    part of the code in domain.singledish.py to hsd.tasks.inspectdata. 
    
    
    	
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@23680 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hsd/tasks/inspectdata/inspection.py
+++ pipeline/pipeline/hsd/tasks/inspectdata/inspection.py
@@ -1,12 +1,19 @@
 from __future__ import absolute_import
 
-import pipeline.infrastructure as infrastructure
+import re
+
+import pipeline.infrastructure.api as api
 import pipeline.infrastructure.casatools as casatools
+import pipeline.infrastructure.basetask as basetask
+import pipeline.infrastructure.logging as logging
+import pipeline.infrastructure.jobrequest as jobrequest
 from pipeline.domain.datatable import DataTableImpl as DataTable
-from pipeline.hsd.tasks.common import common
+from pipeline.domain.datatable import DataTableColumnMaskList as ColMaskList
+from ..common import common
+from . import reader
+from . import analyser
 
-LOG = infrastructure.get_logger(__name__)
-
+LOG = logging.get_logger(__name__)
 
 import os
 import asap as sd
@@ -14,7 +21,7 @@ import numpy
 
 class SDInspectDataInputs(common.SingleDishInputs):
     """
-    Inputs for single dish calibraton
+    Inputs for single dish calibration
     """
     def __init__(self, context, infiles=None):
         self._init_properties(vars())
@@ -36,12 +43,28 @@ class SDInspectDataResults(common.SingleDishResults):
         name = self.__dict__['outcome']['name']
         if self.__dict__['outcome']['instance'] is None:
             datatable = DataTable(name)
-            self.__dict__['outcome']['instance'] = datatable
-        
+            self.__dict__['outcome']['instance'] = datatable        
     
     def merge_with_context(self, context):
-        context.observing_run.set_datatable(**self.outcome)
+        #context.observing_run.set_datatable(**self.outcome)
         super(SDInspectDataResults,self).merge_with_context(context)
+        
+        # merge outcome with datatable
+        datatable = self.outcome['instance']
+        datatable.putcol('POSGRP', self.outcome.pop('position_group'))
+        datatable.putkeyword('POSGRP_REP', self.outcome.pop('position_group_rep'))
+        datatable.putkeyword('POSGRP_LIST', self.outcome.pop('position_group_list'))
+        datatable.putkeyword('TIMEGRP_LIST', self.outcome.pop('time_group_list'))
+        time_group = self.outcome.pop('time_group')
+        time_gap = self.outcome.pop('time_gap')
+        datatable.putcol('TIMEGRP_S', time_group[0])
+        datatable.putcol('TIMEGRP_L', time_group[1])
+        datatable.putkeyword('TIMEGAP_S', time_gap[0])
+        datatable.putkeyword('TIMEGAP_L', time_gap[1])
+        datatable.exportdata(minimal=False)
+
+        # merge
+        context.observing_run.merge_inspection(**self.outcome)
 
     def _outcome_name(self):
         name = self.outcome['name']
@@ -67,194 +90,69 @@ class SDInspectData(common.SingleDishTaskTemplate):
             LOG.info('remove existing DataTable...')
             os.system('rm -rf %s'%(table_name))
         
-        self.DataTable = DataTable(name=table_name)
+
+        # create DataTableReader instance
+        worker = reader.DataTableReader(table_name=table_name)
         LOG.info('table_name=%s'%(table_name))
 
 
         # loop over infiles
-        self.vAnt = 0
-        self.Row2ID = {}
         infiles = inputs.infiles
         if isinstance(infiles, list):
             for f in infiles:
-                self._read_metadata(f)
+                job = jobrequest.JobRequest(worker.execute, name=f)
+                self._executor.execute(job)
         else:
-            self._read_metadata(infiles)
-        
+            job = jobrequest.JobRequest(worker.execute, name=infiles)
+            self._executor.execute(job)
+
         # finally, export DataTable
-        self.DataTable.exportdata(minimal=False)
+        worker.export_datatable(minimal=False)
 
         # done, restore scantable.storage
         sd.rcParams['scantable.storage'] = storage_save
 
         outcome = {}
         outcome['name'] = table_name
-        outcome['instance'] = self.DataTable
+        outcome['instance'] = worker.get_datatable()#self.DataTable
 
-        result = SDInspectDataResults(success=True, outcome=outcome)
+        result = SDInspectDataResults(task=self.__class__,
+                                      success=True,
+                                      outcome=outcome)
+        result.task = self.__class__
+
+        if inputs.context.subtask_counter is 0: 
+            result.stage_number = inputs.context.task_counter - 1
+        else:
+            result.stage_number = inputs.context.task_counter               
 
         return result
 
     def analyse(self, result):
 
+        datatable = result.outcome['instance']
+        scantablelist = self.inputs.context.observing_run
+        worker = analyser.DataTableAnalyser(scantablelist,
+                                            datatable)
+
+        # analyse datatable with observing_run
+        job = jobrequest.JobRequest(worker.execute)
+        self._executor.execute(job)
+
+        # update result
+        result.outcome['position_group'] = worker.posgrp
+        result.outcome['time_group'] = worker.timegrp
+        result.outcome['position_group_rep'] = worker.posgrp_rep
+        result.outcome['position_group_list'] = worker.posgrp_list
+        result.outcome['time_group_list'] = worker.timegrp_list
+        result.outcome['time_gap'] = worker.timegap
+        result.outcome['reduction_group'] = worker.reduction_group
+        result.outcome['calibration_strategy'] = worker.calibration_strategy
+        result.outcome['beam_size'] = worker.beam_size
+        result.outcome['grid_position'] = worker.grid_position
+        result.outcome['observing_pattern'] = worker.observing_pattern
+        
         return result
 
 
-    def _read_metadata(self, name):
-        Rad2Deg = 180. / 3.141592653
-        
-        LOG.info('name=%s'%(name))
-        if self.DataTable.has_key('FILENAMES'):
-            filenames = self.DataTable.getkeyword('FILENAMES')
-            filenames = numpy.concatenate((filenames,[name]))
-        else:
-            filenames = [name]
-        self.DataTable.putkeyword('FILENAMES',filenames)
-        s = sd.scantable(name, average=False)
-        nrow = s.nrow()
-        npol = s.npol()
-        nbeam = s.nbeam()
-        nif = s.nif()
-        vorg=sd.rcParams['verbose']
-        sd.rcParams['verbose']=False
-        if self.casa_version > 302:
-            sd.asaplog.disable()
-        Tsys = s.get_tsys()
-        sd.rcParams['verbose']=vorg
-        if self.casa_version > 302:
-            sd.asaplog.enable()
-        if s.get_azimuth()[0] == 0: s.recalc_azel()
-        
-        with casatools.TableReader(name) as tb:
-            Texpt = tb.getcol('INTERVAL')
-            # ASAP doesn't know the rows for cal are included in s.nrow()
-            # nrow = len(Ttime)
-            Tscan = tb.getcol('SCANNO')
-            Tif = tb.getcol('IFNO')
-            Tpol = tb.getcol('POLNO')
-            Tbeam = tb.getcol('BEAMNO')
-            Tsrctype = tb.getcol('SRCTYPE')
-            # 2009/10/19 nchan for scantable is not correctly set
-            NchanArray = numpy.zeros(nrow, numpy.int)
-            for row in range(nrow):
-                NchanArray[row] = len(tb.getcell('SPECTRA', row))
-
-        # 2011/10/23 GK List of DataTable for multiple antennas
-        #self.DataTable = {}
-        # Save file name to be able to load the special setup needed for the
-        # flagging based on the expected RMS.
-        # 2011/10/23 GK List of DataTable for multiple antennas
-        #if self.DataTable.has_key('FileName') == False: self.DataTable['FileName']  = ['']
-        #self.DataTable.putkeyword('FileName',[''])
-
-        #self.DataTable['FileName'] = rawFile
-        #self.DataTable['FileName'].append(rawFile)
-
-        #if 'FileName' in self.DataTable.tb.keywordnames():
-        #    l = self.DataTable.getkeyword('FileName').tolist()
-        #    self.DataTable.putkeyword('FileName',l+[rawFile])
-        #else:
-        #    self.DataTable.putkeyword('FileName',[rawFile])
-
-##         rawFileList.append(rawFile)
-        self.Row2ID[self.vAnt] = {}
-##         outfile = open(TableOut, 'w')
-##         outfile.write("!ID,Row,Scan,IF,Pol,Beam,Date,MJD,ElapsedTime,ExpTime,RA,Dec,Az,El,Nchan,Tsys,TargetName,AntennaID\n")
-
-        # 2011/10/23 GK List of DataTable for multiple antennas
-        #ID = len(self.DataTable)-1
-        ID = len(self.DataTable)
-        #ID = len(self.DataTable)
-##         self.LogMessage('INFO',Msg='ID=%s'%(ID))
-        LOG.info('ID=%s'%(ID))
-        #ID = 0
-        ROWs = []
-        IDs = []
-        # 2009/7/16 to speed-up, get values as a list
-        # 2011/11/8 get_direction -> get_directionval
-        Sdir = s.get_directionval()
-        #Sdir = s.get_direction()
-        if ( sd.__version__ == '2.1.1' ):
-            Stim = s.get_time()
-        else:
-            Stim = s.get_time(-1, True)
-        Ssrc = s.get_sourcename()
-        Saz = s.get_azimuth()
-        Sel = s.get_elevation()
-
-        # 2012/08/31 Temporary
-##         if os.path.isdir(outTbl):
-##             os.system('rm -rf %s' % outTbl)
-##         TBL = createExportTable(outTbl, nrow)
-        #TBL = gentools(['tb'])[0]
-        #TBL.open(self.TableOut, nomodify=False)
-        #TBL.addrows(nrow)
-
-        self.DataTable.addrows( nrow )
-        # column based storing
-        intArr = numpy.arange(nrow,dtype=int)
-        self.DataTable.putcol('ROW',intArr,startrow=ID)
-        self.DataTable.putcol('SCAN',Tscan,startrow=ID)
-        self.DataTable.putcol('IF',Tif,startrow=ID)
-        self.DataTable.putcol('POL',Tpol,startrow=ID)
-        self.DataTable.putcol('BEAM',Tbeam,startrow=ID)
-        self.DataTable.putcol('EXPOSURE',Texpt,startrow=ID)
-        dirNP = numpy.array(Sdir,dtype=float) * Rad2Deg
-        self.DataTable.putcol('RA',dirNP[:,0],startrow=ID)
-        self.DataTable.putcol('DEC',dirNP[:,1],startrow=ID)
-        azNP = numpy.array(Saz,dtype=float) * Rad2Deg
-        self.DataTable.putcol('AZ',azNP,startrow=ID)
-        elNP = numpy.array(Sel,dtype=float) * Rad2Deg
-        self.DataTable.putcol('EL',elNP,startrow=ID)
-        self.DataTable.putcol('NCHAN',NchanArray,startrow=ID)
-        self.DataTable.putcol('TSYS',Tsys,startrow=ID)
-        self.DataTable.putcol('TARGET',Ssrc,startrow=ID)
-        intArr[:] = 1
-        self.DataTable.putcol('FLAG_SUMMARY',intArr,startrow=ID)
-        intArr[:] = 0
-        self.DataTable.putcol('NMASK',intArr,startrow=ID)
-        intArr[:] = -1
-        self.DataTable.putcol('NOCHANGE',intArr,startrow=ID)
-        self.DataTable.putcol('POSGRP',intArr,startrow=ID)
-        self.DataTable.putcol('TIMEGRP_S',intArr,startrow=ID)
-        self.DataTable.putcol('TIMEGRP_L',intArr,startrow=ID)
-        intArr[:] = self.vAnt
-        self.DataTable.putcol('ANTENNA',intArr,startrow=ID)
-        self.DataTable.putcol('SRCTYPE',Tsrctype,startrow=ID)
-        # row base storing
-        stats = [-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0]
-        flags = [1, 1, 1, 1, 1, 1, 1]
-        pflags = [1, 1, 1]
-        masklist = [[0,0]]
-        for x in range(nrow):
-            Ttime = Stim[x]
-            sDate = ("%4d-%02d-%02d" % (Ttime.year, Ttime.month, Ttime.day))
-            # Calculate MJD
-            sTime = ("%4d/%02d/%02d/%02d:%02d:%.1f" % (Ttime.year, Ttime.month, Ttime.day, Ttime.hour, Ttime.minute, Ttime.second))
-            qTime = casatools.quanta.quantity(sTime)
-            MJD = qTime['value']
-            if x == 0: MJD0 = MJD
-            self.DataTable.putcell('DATE',ID,sDate)
-            self.DataTable.putcell('TIME',ID,MJD)
-            self.DataTable.putcell('ELAPSED',ID,(MJD-MJD0)*86400.0)
-            self.DataTable.putcell('STATISTICS',ID,stats)
-            self.DataTable.putcell('FLAG',ID,flags)
-            self.DataTable.putcell('FLAG_PERMANENT',ID,pflags)
-            self.DataTable.putcell('MASKLIST',ID,masklist)
-##             if Ssrc[x].find('_calon') < 0:
-##                 outfile.write("%d,%d,%d,%d,%d,%d,%s,%.8f,%.3f,%.3f,%.8f,%.8f,%.3f,%.3f,%d,%f,%s,%d\n" % \
-##                          (ID, x, Tscan[x], Tif[x], Tpol[x], Tbeam[x], \
-##                          sDate, MJD, (MJD - MJD0) * 86400., Texpt[x], \
-##                           dirNP[x,0],dirNP[x,1], \
-##                           azNP[x], elNP[x], \
-##                          NchanArray[x], Tsys[x], Ssrc[x], vAnt))
-
-##                 TBL.putcol('Row', int(x), int(x), 1, 1)
-##                 TBL.putcol('Ant', vAnt, int(x), 1, 1)
-##                 ROWs.append(int(x))
-##                 IDs.append(int(ID))
-##                 self.Row2ID[vAnt][int(x)] = int(ID)
-            ID += 1
-
-        self.vAnt += 1
 
---
+++ pipeline/pipeline/hsd/tasks/inspectdata/reader.py
@@ -0,0 +1,187 @@
+from __future__ import absolute_import
+
+import os
+import re
+import numpy
+
+import asap as sd
+
+import pipeline.infrastructure.casatools as casatools
+import pipeline.infrastructure.logging as logging
+from pipeline.domain.datatable import DataTableImpl as DataTable
+from pipeline.domain.datatable import DataTableColumnMaskList as ColMaskList
+
+LOG = logging.get_logger(__name__)
+
+class DataTableReader(object):
+    def __init__(self, table_name):
+        self.table_name = table_name
+        self.datatable = DataTable(name=self.table_name)
+        self.vAnt = 0
+
+    def get_datatable(self):
+        return self.datatable
+
+    def export_datatable(self, minimal=False):
+        self.datatable.exportdata(minimal=minimal)
+
+    def execute(self, name):
+        Rad2Deg = 180. / 3.141592653
+        
+        LOG.info('name=%s'%(name))
+        if self.datatable.has_key('FILENAMES'):
+            filenames = self.datatable.getkeyword('FILENAMES')
+            filenames = numpy.concatenate((filenames,[name]))
+        else:
+            filenames = [name]
+        self.datatable.putkeyword('FILENAMES',filenames)
+        s = sd.scantable(name, average=False)
+        nrow = s.nrow()
+        npol = s.npol()
+        nbeam = s.nbeam()
+        nif = s.nif()
+        vorg=sd.rcParams['verbose']
+        sd.rcParams['verbose']=False
+        #if self.casa_version > 302:
+        #    sd.asaplog.disable()
+        sd.asaplog.disable()
+        Tsys = s.get_tsys()
+        sd.rcParams['verbose']=vorg
+        #if self.casa_version > 302:
+        #    sd.asaplog.enable()
+        sd.asaplog.enable()
+        if s.get_azimuth()[0] == 0: s.recalc_azel()
+        
+        with casatools.TableReader(name) as tb:
+            Texpt = tb.getcol('INTERVAL')
+            # ASAP doesn't know the rows for cal are included in s.nrow()
+            # nrow = len(Ttime)
+            Tscan = tb.getcol('SCANNO')
+            Tif = tb.getcol('IFNO')
+            Tpol = tb.getcol('POLNO')
+            Tbeam = tb.getcol('BEAMNO')
+            Tsrctype = tb.getcol('SRCTYPE')
+            # 2009/10/19 nchan for scantable is not correctly set
+            NchanArray = numpy.zeros(nrow, numpy.int)
+            for row in range(nrow):
+                NchanArray[row] = len(tb.getcell('SPECTRA', row))
+
+        # 2011/10/23 GK List of DataTable for multiple antennas
+        #self.datatable = {}
+        # Save file name to be able to load the special setup needed for the
+        # flagging based on the expected RMS.
+        # 2011/10/23 GK List of DataTable for multiple antennas
+        #if self.datatable.has_key('FileName') == False: self.datatable['FileName']  = ['']
+        #self.datatable.putkeyword('FileName',[''])
+
+        #self.datatable['FileName'] = rawFile
+        #self.datatable['FileName'].append(rawFile)
+
+        #if 'FileName' in self.datatable.tb.keywordnames():
+        #    l = self.datatable.getkeyword('FileName').tolist()
+        #    self.datatable.putkeyword('FileName',l+[rawFile])
+        #else:
+        #    self.datatable.putkeyword('FileName',[rawFile])
+
+##         rawFileList.append(rawFile)
+        #self.Row2ID[self.vAnt] = {}
+##         outfile = open(TableOut, 'w')
+##         outfile.write("!ID,Row,Scan,IF,Pol,Beam,Date,MJD,ElapsedTime,ExpTime,RA,Dec,Az,El,Nchan,Tsys,TargetName,AntennaID\n")
+
+        # 2011/10/23 GK List of DataTable for multiple antennas
+        #ID = len(self.datatable)-1
+        ID = len(self.datatable)
+        #ID = len(self.datatable)
+##         self.LogMessage('INFO',Msg='ID=%s'%(ID))
+        LOG.info('ID=%s'%(ID))
+        #ID = 0
+        ROWs = []
+        IDs = []
+        # 2009/7/16 to speed-up, get values as a list
+        # 2011/11/8 get_direction -> get_directionval
+        Sdir = s.get_directionval()
+        #Sdir = s.get_direction()
+        if ( sd.__version__ == '2.1.1' ):
+            Stim = s.get_time()
+        else:
+            Stim = s.get_time(-1, True)
+        Ssrc = s.get_sourcename()
+        Saz = s.get_azimuth()
+        Sel = s.get_elevation()
+
+        # 2012/08/31 Temporary
+##         if os.path.isdir(outTbl):
+##             os.system('rm -rf %s' % outTbl)
+##         TBL = createExportTable(outTbl, nrow)
+        #TBL = gentools(['tb'])[0]
+        #TBL.open(self.TableOut, nomodify=False)
+        #TBL.addrows(nrow)
+
+        self.datatable.addrows( nrow )
+        # column based storing
+        intArr = numpy.arange(nrow,dtype=int)
+        self.datatable.putcol('ROW',intArr,startrow=ID)
+        self.datatable.putcol('SCAN',Tscan,startrow=ID)
+        self.datatable.putcol('IF',Tif,startrow=ID)
+        self.datatable.putcol('POL',Tpol,startrow=ID)
+        self.datatable.putcol('BEAM',Tbeam,startrow=ID)
+        self.datatable.putcol('EXPOSURE',Texpt,startrow=ID)
+        dirNP = numpy.array(Sdir,dtype=float) * Rad2Deg
+        self.datatable.putcol('RA',dirNP[:,0],startrow=ID)
+        self.datatable.putcol('DEC',dirNP[:,1],startrow=ID)
+        azNP = numpy.array(Saz,dtype=float) * Rad2Deg
+        self.datatable.putcol('AZ',azNP,startrow=ID)
+        elNP = numpy.array(Sel,dtype=float) * Rad2Deg
+        self.datatable.putcol('EL',elNP,startrow=ID)
+        self.datatable.putcol('NCHAN',NchanArray,startrow=ID)
+        self.datatable.putcol('TSYS',Tsys,startrow=ID)
+        self.datatable.putcol('TARGET',Ssrc,startrow=ID)
+        intArr[:] = 1
+        self.datatable.putcol('FLAG_SUMMARY',intArr,startrow=ID)
+        intArr[:] = 0
+        self.datatable.putcol('NMASK',intArr,startrow=ID)
+        intArr[:] = -1
+        self.datatable.putcol('NOCHANGE',intArr,startrow=ID)
+        self.datatable.putcol('POSGRP',intArr,startrow=ID)
+        self.datatable.putcol('TIMEGRP_S',intArr,startrow=ID)
+        self.datatable.putcol('TIMEGRP_L',intArr,startrow=ID)
+        intArr[:] = self.vAnt
+        self.datatable.putcol('ANTENNA',intArr,startrow=ID)
+        self.datatable.putcol('SRCTYPE',Tsrctype,startrow=ID)
+        # row base storing
+        stats = [-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0]
+        flags = [1, 1, 1, 1, 1, 1, 1]
+        pflags = [1, 1, 1]
+        masklist = ColMaskList.NoMask
+        for x in range(nrow):
+            Ttime = Stim[x]
+            sDate = ("%4d-%02d-%02d" % (Ttime.year, Ttime.month, Ttime.day))
+            # Calculate MJD
+            sTime = ("%4d/%02d/%02d/%02d:%02d:%.1f" % (Ttime.year, Ttime.month, Ttime.day, Ttime.hour, Ttime.minute, Ttime.second))
+            qTime = casatools.quanta.quantity(sTime)
+            MJD = qTime['value']
+            if x == 0: MJD0 = MJD
+            self.datatable.putcell('DATE',ID,sDate)
+            self.datatable.putcell('TIME',ID,MJD)
+            self.datatable.putcell('ELAPSED',ID,(MJD-MJD0)*86400.0)
+            self.datatable.putcell('STATISTICS',ID,stats)
+            self.datatable.putcell('FLAG',ID,flags)
+            self.datatable.putcell('FLAG_PERMANENT',ID,pflags)
+            self.datatable.putcell('MASKLIST',ID,masklist)
+##             if Ssrc[x].find('_calon') < 0:
+##                 outfile.write("%d,%d,%d,%d,%d,%d,%s,%.8f,%.3f,%.3f,%.8f,%.8f,%.3f,%.3f,%d,%f,%s,%d\n" % \
+##                          (ID, x, Tscan[x], Tif[x], Tpol[x], Tbeam[x], \
+##                          sDate, MJD, (MJD - MJD0) * 86400., Texpt[x], \
+##                           dirNP[x,0],dirNP[x,1], \
+##                           azNP[x], elNP[x], \
+##                          NchanArray[x], Tsys[x], Ssrc[x], vAnt))
+
+##                 TBL.putcol('Row', int(x), int(x), 1, 1)
+##                 TBL.putcol('Ant', vAnt, int(x), 1, 1)
+##                 ROWs.append(int(x))
+##                 IDs.append(int(ID))
+##                 self.Row2ID[vAnt][int(x)] = int(ID)
+            ID += 1
+
+        self.vAnt += 1
+
