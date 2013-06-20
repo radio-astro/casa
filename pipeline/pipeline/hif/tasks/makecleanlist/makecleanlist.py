f1fb70d1aa996b12b2e18f86192f1ff954254354
john lightfoot <john.lightfoot@stfc.ac.uk>
2013-06-20 20:16:40 +0000

    parameters added
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@25061 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hif/tasks/makecleanlist/makecleanlist.py
+++ pipeline/pipeline/hif/tasks/makecleanlist/makecleanlist.py
@@ -15,7 +15,10 @@ class MakeCleanListInputs(basetask.StandardInputs):
 
     def __init__(self, context, output_dir=None, vis=None, spw=None,
       intent=None, field=None, imagename=None, mode=None, outframe=None,
-      imsize=None, cell=None, phasecenter=None):
+      imsize=None, cell=None, phasecenter=None, start=None, width=None,
+      nchan=None, restfreq=None, weighting=None, robust=None, noise=None,
+      npixels=None, restoringbeam=None, nterms=None, uvrange=None,
+      maxthreshiter=None):
 
         self._init_properties(vars())
 
@@ -65,7 +68,10 @@ class MakeCleanListInputs(basetask.StandardInputs):
     @property
     def mode(self):
         if self._mode is None:
-            return ''
+            if 'TARGET' in self.intent:
+                return 'frequency'
+            else:
+                return 'mfs'
         return self._mode
 
     @mode.setter
@@ -112,6 +118,128 @@ class MakeCleanListInputs(basetask.StandardInputs):
     def phasecenter(self, value):
         self._phasecenter = value
 
+    # optional added parameters start here
+
+    @property
+    def start(self):
+        if self._start is None:
+            return 'default'
+        return self._start
+
+    @start.setter
+    def start(self, value):
+        self._start = value
+
+    @property
+    def width(self):
+        if self._width is None:
+            return 'default'
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        self._width = value
+
+    @property
+    def nchan(self):
+        if self._nchan is None:
+            return 'default'
+        return self._nchan
+
+    @nchan.setter
+    def nchan(self, value):
+        self._nchan = value
+
+    @property
+    def restfreq(self):
+        if self._restfreq is None:
+            return 'default'
+        return self._restfreq
+
+    @restfreq.setter
+    def restfreq(self, value):
+        self._restfreq = value
+
+    @property
+    def weighting(self):
+        if self._weighting is None:
+            return 'default'
+        return self._weighting
+
+    @weighting.setter
+    def weighting(self, value):
+        self._weighting = value
+
+    @property
+    def robust(self):
+        if self._robust is None:
+            return 'default'
+        return self._robust
+
+    @robust.setter
+    def robust(self, value):
+        self._robust = value
+
+    @property
+    def noise(self):
+        if self._noise is None:
+            return 'default'
+        return self._noise
+
+    @noise.setter
+    def noise(self, value):
+        self._noise = value
+
+    @property
+    def npixels(self):
+        if self._npixels is None:
+            return 'default'
+        return self._npixels
+
+    @npixels.setter
+    def npixels(self, value):
+        self._npixels = value
+
+    @property
+    def restoringbeam(self):
+        if self._restoringbeam is None:
+            return 'default'
+        return self._restoringbeam
+
+    @restoringbeam.setter
+    def restoringbeam(self, value):
+        self._restoringbeam = value
+
+    @property
+    def nterms(self):
+        if self._nterms is None:
+            return 'default'
+        return self._nterms
+
+    @nterms.setter
+    def nterms(self, value):
+        self._nterms = value
+
+    @property
+    def uvrange(self):
+        if self._uvrange is None:
+            return 'default'
+        return self._uvrange
+
+    @uvrange.setter
+    def uvrange(self, value):
+        self._uvrange = value
+
+    @property
+    def maxthreshiter(self):
+        if self._maxthreshiter is None:
+            return 'default'
+        return self._maxthreshiter
+
+    @maxthreshiter.setter
+    def maxthreshiter(self, value):
+        self._maxthreshiter = value
+
 
 class MakeCleanList(basetask.StandardTaskTemplate):
     Inputs = MakeCleanListInputs
@@ -128,44 +256,58 @@ class MakeCleanList(basetask.StandardTaskTemplate):
         if type(inputs.vis) is not types.ListType:
             inputs.vis = [inputs.vis]
 
-        # read the spw, if none given then select all science spws
+        # read the spw, if none then set default depending on mode
         spw = inputs.spw
+   
+        #print 'before', spw
         if spw == '':
             ms = inputs.context.observing_run.get_ms(name=inputs.vis[0])
             spws = ms.get_spectral_windows(science_windows_only=True)
-            spwids = [spw.id for spw in spws]
-            spw = ','.join(str(spwid) for spwid in spwids)
-        else:
-            spwids = map(int, spw.split(','))
+            if inputs.mode == 'mfs':
+                spwids = [spw.id for spw in spws]
+                spw = ','.join(str(spwid) for spwid in spwids)
+                spw = "['%s']" % spw
+            else:
+                spwids = [spw.id for spw in spws]
+                spw = ','.join("'%s'" % (spwid) for spwid in spwids)
+                spw = '[%s]' % spw
+        #print 'after', spw, inputs.spw
+
+        spwlist = spw.replace('[','').replace(']','')
+        spwlist = spwlist[1:-1].split("','")
+        #print 'main', spwlist
 
         # instantiate the heuristics classes needed, some sorting out needed
         # here to remove duplicated code
         self.heuristics = makecleanlist.MakeCleanListHeuristics(
           context=inputs.context, vislist=inputs.vis, spw=spw)
-        self.clean_heuristics = {}
-        for spwid in spwids:
-            self.clean_heuristics[spwid] = clean.CleanHeuristics(
-              context=inputs.context, vislist=inputs.vis, spw=str(spwid))
 
         # get list of field_ids/intents to be cleaned
         field_intent_list = self.heuristics.field_intent_list(
           intent=inputs.intent, field=inputs.field)
+
+#        self.clean_heuristics = {}
+#        for spwid in spwids:
+#            self.clean_heuristics[spwid] = clean.CleanHeuristics(
+#              context=inputs.context, vislist=inputs.vis, spw=str(spwid))
 
         # cell is a list of form [cellx, celly]. If the list has form [cell]
         # then that means the cell is the same size in x and y. If cell is
         # empty then fill it with a heuristic result
         cell = inputs.cell
         cells = {}
+        beams = {}
         if cell == []:
-            for spwid in spwids:
+            for spwspec in spwlist:
                 # the heuristic cell is always the same for x and y as
                 # the value derives from a single value returned by
                 # imager.advise
-                cells[spwid] = [self.heuristics.cell(
-                  field_intent_list=field_intent_list, spwid=spwid)]
+                cells[spwspec], beams[spwspec] = self.heuristics.cell(
+                  field_intent_list=field_intent_list, spwspec=spwspec)
         else:
-            for spwid in spwids:
-                cells[spwid] = cell
+            for spwspec in spwlist:
+                cells[spwspec] = cell
+        #print 'cells', cells
 
         # if phase center not set then use heuristic code to calculate the
         # centers for each field
@@ -174,28 +316,29 @@ class MakeCleanList(basetask.StandardTaskTemplate):
             phasecenters = {}
             for field_intent in field_intent_list:
                 try:
-                    field_ids = self.clean_heuristics[spwids[0]].field(
+                    field_ids = self.heuristics.field(
                       field_intent[1], field_intent[0])
                     phasecenters[field_intent[0]] = \
-                      self.clean_heuristics[spwids[0]].phasecenter(field_ids)
+                      self.heuristics.phasecenter(field_ids)
                 except Exception, e:
                     # problem defining center
                     LOG.warn(e)
                     pass
+        #print 'phasecenters', phasecenters
 
         # if imsize not set then use heuristic code to calculate the
-        # centers for each field/spw
+        # centers for each field/spwspec
         imsize = inputs.imsize
         imsizes = {}
         if imsize == []:
             for field_intent in field_intent_list:
-                for spwid in spwids:
+                for spwspec in spwlist:
                     try:
-                        field_ids = self.clean_heuristics[spwid].field(
+                        field_ids = self.heuristics.field(
                           field_intent[1], field_intent[0])
-                        imsizes[(field_intent[0],spwid)] = \
-                          self.clean_heuristics[spwid].imsize(fields=field_ids,
-                          cell=cells[spwid])
+                        imsizes[(field_intent[0],spwspec)] = \
+                          self.heuristics.imsize(fields=field_ids,
+                          cell=cells[spwspec], beam=beams[spwspec])
                     except Exception, e:
                         # problem defining imsize
                         LOG.warn(e)
@@ -203,49 +346,79 @@ class MakeCleanList(basetask.StandardTaskTemplate):
  
         else:
             for field_intent in field_intent_list:
-                for spwid in spwids:
-                    imsizes[(field_intent[0],spwid)] = imsize
+                for spwspec in spwlist:
+                    imsizes[(field_intent[0],spwspec)] = imsize
+        #print 'imsize', imsizes
 
         # construct imagename
         imagename = inputs.imagename
         imagenames = {}
         for field_intent in field_intent_list:
-            for spwid in spwids:
+            for spwspec in spwlist:
                 if inputs.imagename == '':
-                    imagenames[(field_intent,spwid)] = \
-                      self.clean_heuristics[spwid].imagename(
+                    imagenames[(field_intent,spwspec)] = \
+                      self.heuristics.imagename(
                       output_dir=inputs.output_dir, intent=field_intent[1],
-                      field=field_intent[0], spw=str(spwid))
+                      field=field_intent[0], spwspec=spwspec)
                 else:
-                    imagenames[(field_intent,spwid)] = inputs.imagename
+                    imagenames[(field_intent,spwspec)] = inputs.imagename
+        #print 'imagenames', imagenames
 
         # now construct the list of imaging command parameter lists that must
         # be run to obtain the required images
         result = MakeCleanListResult()
         for field_intent in field_intent_list:
-            for spw in spwids:
-                if imsizes.has_key((field_intent[0],spwid)):
+            for spwspec in spwlist:
+                if imsizes.has_key((field_intent[0],spwspec)):
                     LOG.debug (
                       'field:%s intent:%s spw:%s cell:%s imsize:%s phasecenter:%s'
-                      % (field_intent[0], field_intent[1], spw, cells[spw], 
-                      imsizes[(field_intent[0],spwid)],
+                      % (field_intent[0], field_intent[1], spwspec,
+                      cells[spwspec], imsizes[(field_intent[0],spwspec)],
                       phasecenters[field_intent[0]]))
 
                     target = {'field':field_intent[0],
                               'intent':field_intent[1],
-                              'spw':str(spw),
-                              'cell':cells[spw],
-                              'imsize':imsizes[(field_intent[0],spwid)],
+                              'spw': spwspec,
+                              'cell':cells[spwspec],
+                              'imsize':imsizes[(field_intent[0],spwspec)],
                               'phasecenter':phasecenters[field_intent[0]],
-                              'imagename':imagenames[(field_intent,spw)]}
-                    if inputs.mode != '':
-                        target['mode'] = inputs.mode
+                              'mode':inputs.mode,
+                              'imagename':imagenames[(field_intent,spwspec)]}
+
+                    # optional parameters that will override the defaults of
+                    # the clean task if set
+                    if inputs.start != 'default':
+                        target['start'] = inputs.start
+                    if inputs.width != 'default':
+                        target['width'] = inputs.width
+                    if inputs.nchan != 'default':
+                        target['nchan'] = inputs.nchan
+                    if inputs.restfreq != 'default':
+                        target['restfreq'] = inputs.restfreq
+                    if inputs.weighting != 'default':
+                        target['weighting'] = inputs.weighting
+                    if inputs.robust != 'default':
+                        target['robust'] = inputs.robust
+                    if inputs.noise != 'default':
+                        target['noise'] = inputs.noise
+                    if inputs.npixels != 'default':
+                        target['npixels'] = inputs.npixels
+                    if inputs.restoringbeam != 'default':
+                        target['restoringbeam'] = inputs.restoringbeam
+                    if inputs.nterms != 'default':
+                        target['nterms'] = inputs.nterms
+                    if inputs.uvrange != 'default':
+                        target['uvrange'] = inputs.uvrange
+                    if inputs.maxthreshiter != 'default':
+                        target['maxthreshiter'] = inputs.maxthreshiter
+
+                    #print target
                     result.add_target(target)
 
                 else:
                     LOG.warn (
                       'failed to define image for field:%s intent:%s spw:%s'
-                      % (field_intent[0], field_intent[1], spw))
+                      % (field_intent[0], field_intent[1], spwspec))
 
         return result
 
