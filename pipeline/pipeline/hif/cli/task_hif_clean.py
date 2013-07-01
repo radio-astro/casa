45c28b06860189ba7e0f6b34bc0c7efe48ace997
john lightfoot <john.lightfoot@stfc.ac.uk>
2013-07-01 21:37:22 +0000

    minor changes
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@25254 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hif/cli/task_hif_clean.py
+++ pipeline/pipeline/hif/cli/task_hif_clean.py
@@ -8,8 +8,9 @@ import pipeline.h.cli.utils as utils
 def hif_clean(vis=None, imagename=None, intent=None, field=None, spw=None,
               uvrange=None, mode=None, imagermode=None, outframe=None, 
               nchan=None, start=None, width=None, imsize=None, cell=None,
-              phasecenter=None, weighting=None, robust=None, noise=None,
-	      npixels=None, hm_clean=None, mask=None, threshold=None,
+              phasecenter=None, weighting=None, robust=None, 
+              restoringbeam=None, noise=None, npixels=None,
+              hm_cleanboxing=None, mask=None, threshold=None,
 	      maxthreshiter=None, pipelinemode=None, dryrun=None, acceptresults=None):
 
     # create a dictionary containing all the arguments given in the
