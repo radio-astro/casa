990de6ba555900f412ad2f418dc593ee2e3318b3
john lightfoot <john.lightfoot@stfc.ac.uk>
2013-07-01 20:04:34 +0000

    cleaning parameters added
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@25242 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hif/tasks/cleanlist/cleanlist.py
+++ pipeline/pipeline/hif/tasks/cleanlist/cleanlist.py
@@ -14,8 +14,9 @@ LOG = infrastructure.get_logger(__name__)
 
 class CleanListInputs(basetask.StandardInputs):
 
-    def __init__(self, context, output_dir=None, 
-     vis=None, target_list=None):
+    def __init__(self, context, output_dir=None, vis=None, target_list=None,
+      weighting=None, robust=None, noise=None, npixels=None,
+      hm_cleanboxing=None, mask=None, threshold=None, maxthreshiter=None):
         self._init_properties(vars())
 
     @property
@@ -27,6 +28,86 @@ class CleanListInputs(basetask.StandardInputs):
     @target_list.setter
     def target_list(self, value):
         self._target_list = value
+
+    @property
+    def weighting(self):
+        if self._weighting is None:
+            return 'natural'
+        return self._weighting
+
+    @weighting.setter
+    def weighting(self, value):
+        self._weighting = value
+
+    @property
+    def robust(self):
+        if self._robust is None:
+            return 0.0
+        return self._robust
+
+    @robust.setter
+    def robust(self, value):
+        self._robust = value
+
+    @property
+    def noise(self):
+        if self._noise is None:
+            return '1.0Jy'
+        return self._noise
+
+    @noise.setter
+    def noise(self, value):
+        self._noise = value
+
+    @property
+    def npixels(self):
+        if self._npixels is None:
+            return 0
+        return self._npixels
+
+    @npixels.setter
+    def npixels(self, value):
+        self._npixels = value
+
+    @property
+    def hm_cleanboxing(self):
+        if self._hm_cleanboxing is None:
+            return 'autobox'
+        return self._hm_cleanboxing
+
+    @hm_cleanboxing.setter
+    def hm_cleanboxing(self, value):
+        self._hm_cleanboxing = value
+
+    @property
+    def mask(self):
+        if self._mask is None:
+            return ''
+        return self._mask
+
+    @mask.setter
+    def mask(self, value):
+        self._mask = value
+
+    @property
+    def threshold(self):
+        if self._target_list is None:
+            return 0.0
+        return self._threshold
+
+    @threshold.setter
+    def threshold(self, value):
+        self._threshold = value
+
+    @property
+    def maxthreshiter(self):
+        if self._maxthreshiter is None:
+            return 10
+        return self._maxthreshiter
+
+    @maxthreshiter.setter
+    def maxthreshiter(self, value):
+        self._maxthreshiter = value
 
 
 class CleanList(basetask.StandardTaskTemplate):
@@ -62,6 +143,23 @@ class CleanList(basetask.StandardTaskTemplate):
             full_image_target = dict(image_target)
             full_image_target['output_dir'] = inputs.output_dir
             full_image_target['vis'] = inputs.vis
+
+            full_image_target['weighting'] = inputs.weighting
+            if inputs.weighting == 'briggs':
+                full_image_target['robust'] = inputs.robust
+                full_image_target['npixels'] = inputs.npixels
+            elif inputs.weighting == 'briggsabs':
+                full_image_target['robust'] = inputs.robust
+                full_image_target['noise'] = inputs.noise
+            elif inputs.weighting == 'superuniform':
+                full_image_target['npixels'] = inputs.npixels
+
+            full_image_target['hm_cleanboxing'] = inputs.hm_cleanboxing
+            if inputs.hm_cleanboxing == 'autobox':
+                full_image_target['maxthreshiter'] = inputs.maxthreshiter
+            elif inputs.hm_cleanboxing == 'manual':
+                full_image_target['mask'] = inputs.mask
+                full_image_target['threshold'] = inputs.threshold
 
             # build the task to do the cleaning and execute it
             datainputs = Clean.Inputs(context=inputs.context,
