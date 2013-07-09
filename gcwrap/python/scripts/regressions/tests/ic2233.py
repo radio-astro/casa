a86c7e4badec433d8e59166f1285ada288b248a4
kumar golap <kgolap@nrao.edu>
2013-07-09 17:08:41 +0000

    New Development: No             JIRA Issue: No List JIRA ticket.         Ready for Test: Yes      Interface Changes: Yes/No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: Describe your changes here...
    	Make it use in memory images as it is painfully slow with on disk images
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@25388 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/regressions/tests/ic2233.py
+++ gcwrap/python/scripts/regressions/tests/ic2233.py
@@ -20,6 +20,7 @@ def run(fetch=False) :
     im.selectvis(spw='0', nchan=[6], start=[0], step=[1])
     im.defineimage(nx=npix, ny=npix, cellx='3.0arcsec', celly='3.0arcsec', stokes="IV", spw=[0])
     im.weight(type='briggs', robust=0.7)
+    im.setoptions(imagetilevol=-1000000);
     im.setvp(dovp=bool(1), usedefaultvp=bool(1), dosquint=bool(1),
              parangleinc='5.0deg')
     im.make('squint_corr')
