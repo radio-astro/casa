0fe891d07e8ca9880ac45c4b22612ff986774294
stewart williams <sjw@roe.ac.uk>
2013-07-18 11:51:59 +0000

    legacy thumbnail viewer blanked out all plots when multiple buttons in the same button group were selected.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@25547 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/infrastructure/renderer/templates/resources/plotgroup.js
+++ pipeline/pipeline/infrastructure/renderer/templates/resources/plotgroup.js
@@ -1,138 +1,81 @@
-$(document).ready(function(){
-    /**
-     * Adds 0 left margin to the first thumbnail on each row that don't get it via CSS rules.
-     * Recall the function when the floating of the elements changed.
-     */
-    function fixThumbnailMargins() {
-        $('ul.thumbnails').each(function() {
-            var allThumbnails = $(this).children(),
-            	visibleThumbnails = $(allThumbnails).filter(":visible"),
-                previousOffsetLeft;
-            
-            if (visibleThumbnails.length === 0) {
-        	return;
-            }            
-            previousOffsetLength = $(visibleThumbnails).first().offset().left;
-            
-            $(allThumbnails).removeClass('first-in-row');
-            $(visibleThumbnails).first().addClass('first-in-row');
-            visibleThumbnails.each(function() {
-                var thumbnail = $(this),
-                    offsetLeft = $(thumbnail).offset().left;
-                if (offsetLeft < previousOffsetLeft) {
-                    thumbnail.addClass('first-in-row');
-                }
-                previousOffsetLeft = offsetLeft;
+$(document).ready(function () {
+    // Show/hide images depending on selected spectral window
+    $(".plotfilter button").click(function () {
+        var toFilter = {},
+            clicked = this,
+            parentIndex;
+
+        // create a list of the active buttons for each button group
+        $(".plotfilter").each(function (index) {
+            var activeValues = [];
+            $(this).children(".active").each(function () {
+                activeValues.push($(this).data("value"));
+            });
+
+            toFilter[index] = activeValues;
+
+            if ($.contains(this, clicked)) {
+                parentIndex = index;
+            }
+        });
+
+        // active class is added or removed *after* this event handler has
+        // completed, so we need to add this button if it's not already active
+        // or remove it if it's just been deselected
+        if ($(this).hasClass("active")) {
+            toFilter[parentIndex].splice(
+                toFilter[parentIndex].indexOf($(this).data("value"))
+                , 1)
+        } else {
+            toFilter[parentIndex].push($(this).data("value"));
+        }
+
+        $.each(toFilter, function (key, value) {
+            $.each(value, function (index, selector) {
+                toFilter[key][index] = selector.toLowerCase();
             });
         });
-    }
 
-    
-    // Show/hide images depending on selected spectral window
-    $(".plotfilter button").click(function() {
-	var toFilter = [],
-	    index;
-	
-	$(".plotfilter button.active").each(function() {
-	    var filterClass = $(this).data("value");
-	    toFilter.push(filterClass);
-	});
+        function disjoin(a, b) {
+            return $.grep(a, function ($e) {
+                return $.inArray($e, b) == -1;
+            });
+        };
 
-	// active class is added or removed *after* this event handler, so we
-	// need to add this button if it's not already active.
-	if ($(this).hasClass("active")) {	    
-	    index = toFilter.indexOf($(this).data("value"));
-	    toFilter.splice(index, 1)
-	} else {
-	    toFilter.push($(this).data("value"));
-	}
+        $("ul.thumbnails li").each(function () {
+            var imgAttrs = $(this).data("value").toLowerCase().split(" ");
+            var isVisible = true;
 
-	for (index=0; index<toFilter.length; index += 1) {
-	    toFilter[index] = toFilter[index].toLowerCase();
-	}
+            $.each(toFilter, function (index, activeSelectors) {
+                var disjoint = disjoin(activeSelectors, imgAttrs);
 
-	function disjoin(a, b) {
-	    return $.grep(a, function($e) { return $.inArray($e, b) == -1; });
-	};
-	
-	$("ul.thumbnails li").each(function() {
-	    var imgAttrs = $(this).data("value").toLowerCase().split(" "),
-	    	i;
-	    // get the intersection of the image attributes and active selectors
-	    // lists
-	    var disjoint = disjoin(toFilter, imgAttrs);
-	    
-	    if (disjoint.length === 0) {
-		$(this).show();
-	    } else {
-		if (toFilter.length === 0) {
-		    $(this).show();
-		} else {
-		    $(this).hide();
-		}
-	    }
-	});
-	
-	fixThumbnailMargins();	
+                isVisible = isVisible && (activeSelectors.length > disjoint.length
+                                          || activeSelectors.length === 0);
+            });
+
+            if (isVisible) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+
+        UTILS.fixThumbnailMargins();
     });
 
-    $("#clearbutton").click(function() {
+    $("#clearbutton").click(function () {
         // clear the state of all toggle buttons
-	$(".plotfilter button.active").each(function() {
-	    $(this).click();
-	});
+        $(".plotfilter button.active").each(function () {
+            $(this).click();
+        });
     });
 
-    $("ul.thumbnails li div a").click(function(evt) {
-	evt.preventDefault();
-	var target = this.href;
-	launchFancyboxInParent(target);
-	return false;
+    $("ul.thumbnails li div a").click(function (evt) {
+        evt.preventDefault();
+        var target = this.href;
+        UTILS.launchFancybox(target);
+        return false;
     });
 
-    function launchFancyboxInParent(target){
-	var fullsize = [];
-	var thumbnail;
-	var thumbnailImg;
-	var fullsizeToThumbs = {};
-	var index = 0;
-	
-
-	$("ul.thumbnails li:visible div a").each(function() {        
-	    var mainImage = this.href; // Find Image href
-	    var title = this.title; // Find Image title
-	    if (mainImage == target) {
-        	index = fullsize.length;
-	    }
-	    fullsize.push({
-        	href  : mainImage,
-        	title : title                     
-	    });
-	    thumbnailImg = $(this).children("img:first")[0].src;
-	    fullsizeToThumbs[mainImage] = thumbnailImg;
-	});
-
-	$.fancybox(fullsize, {
-	    loop : false,
-	    type    : 'image',
-	    index   : index,
-	    prevEffect : 'none',
-	    nextEffect : 'none',
-	    helpers	: {
-		title	: {
-		    type: 'outside'
-		},
-		thumbs	: {
-		    width	: 50,
-		    height	: 50,
-		    source  : function(current) {
-			var href = current.href;
-			return fullsizeToThumbs[href];
-	            }
-		}
-	    }
-	});
-    }
-    
-    fixThumbnailMargins();
+    UTILS.fixThumbnailMargins();
 });
