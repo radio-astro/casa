9dfe6c163237ed3f9a37048dbc7868d7a9534620
scott rankin <srankin@nrao.edu>
2013-07-25 19:43:52 +0000

    Committing a hack fix to allow progress while Urvashi designs a real fix.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@25661 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/tools/synthesis/synthesisimstore_private.h
+++ gcwrap/tools/synthesis/synthesisimstore_private.h
@@ -2,16 +2,18 @@
 friend class synthesisparsync;
 friend class synthesisimager;
 
+public:
+
+synthesisimstore(casa::SIImageStore* imstore);
+casa::SIImageStore* getImStore();
+
 protected:
 
-casa::SIImageStore* getImStore();
 
 private:
 
 //std::auto_ptr<casa::SIImageStore> itsImStore;
 
 casa::SIImageStore* itsImStore;
-
-synthesisimstore(casa::SIImageStore* imstore);
 
 bool containsimage;
