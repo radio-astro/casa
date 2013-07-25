fc425897484263a0e0bd40e01c456ef93c39f0a5
takeshi nakazato <takeshi.nakazato@nao.ac.jp>
2013-07-25 06:34:40 +0000

    New Development: No             JIRA Issue: No         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: No              Module(s): pipeline            Description: Describe your changes here...
    Updated caltsys plots. Time variation of averaged Tsys is plotted against 
    time instead of elevation. Also made some changes on axes management.
    
    	
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@25644 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/infrastructure/displays/singledish/tsys.py
+++ pipeline/pipeline/infrastructure/displays/singledish/tsys.py
@@ -1,11 +1,15 @@
 import os
 import numpy
 import pylab as pl
+import string
 
+import pipeline.infrastructure.logging as logging
 import pipeline.infrastructure.casatools as casatools
 import pipeline.infrastructure.renderer.logger as logger
 from . import common
 from . import utils
+
+LOG = logging.get_logger(__name__)
 
 def get_ylim(tsys):
     tshape = tsys.shape
@@ -24,8 +28,75 @@ def get_ylim(tsys):
     ymin = med - fmin * cstd
     ymax = med + fmax * cstd
     return (ymin,ymax)
-    
 
+
+class TsysFreqAxesManager(object):
+    def __init__(self, figure_id=8908, baseframe=None):
+        self._axes = None
+        self._baseframe = baseframe
+        self._xlabel = string.Template('${frame} Frequency [MHz]')
+        if common.ShowPlot:
+            pl.ion()
+        else:
+            pl.ioff()
+        pl.figure(figure_id)
+        pl.clf()
+
+    @property
+    def baseframe(self):
+        return self._baseframe
+
+    @baseframe.setter
+    def baseframe(self, value):
+        if self._baseframe != value:
+            self._baseframe = value
+
+            # update axes
+            if self._axes is not None:
+                self.axes.set_xlabel(self._xlabel.safe_substitute(frame=self.baseframe))
+        
+    @property
+    def axes(self):
+        if self._axes is None:
+            self._axes = self.__axes()
+        return self._axes
+
+    def __axes(self):
+        a = pl.subplot(111)
+        a.set_xlabel(self._xlabel.safe_substitute(frame=self.baseframe))
+        a.set_ylabel('Tsys [K]')
+        
+        return a
+
+class TsysTimeAxesManager(common.TimeAxesManager):
+    def __init__(self):
+        super(TsysTimeAxesManager, self).__init__()
+        self._axes = None
+
+    @property
+    def axes(self):
+        if self._axes is None:
+            self._axes = self.__axes()
+        return self._axes
+
+    def __axes(self):
+        a = pl.subplot(111)
+        a.set_xlabel('Time (UT)')
+        a.set_ylabel('Channel Averaged Tsys [K]')
+        a.xaxis.set_major_locator(self.locator)
+        a.xaxis.set_major_formatter(utils.utc_formatter())
+
+        # shift axes upward
+        pos = a.get_position()
+        left = pos.x0
+        bottom = pos.y0 + 0.02
+        width = pos.x1 - pos.x0
+        height = pos.y1 - pos.y0
+        a.set_position([left, bottom, width, height])
+        
+        return a
+    
+        
 class SDTsysDisplay(common.SDCalibrationDisplay):
     MATPLOTLIB_FIGURE_ID = 8908
 
@@ -38,7 +109,7 @@ class SDTsysDisplay(common.SDCalibrationDisplay):
         plot_list = self._plot_tsys_vs_freq(result, stage_dir)
         plots.extend(plot_list)
 
-        plot_list = self._plot_tsys_vs_el(result, stage_dir)
+        plot_list = self._plot_tsys_vs_time(result, stage_dir)
         plots.extend(plot_list)
 
         return plots
@@ -68,10 +139,11 @@ class SDTsysDisplay(common.SDCalibrationDisplay):
         if common.ShowPlot: pl.ion()
         else: pl.ioff()
         pl.figure(self.MATPLOTLIB_FIGURE_ID)
+        pl.clf()
+
+        axes_manager = TsysFreqAxesManager(baseframe=base_frame)
+        axes = axes_manager.axes
         
-        ylabel = 'Tsys [K]'
-        xlabel = '%s Frequency [MHz]'%(base_frame)
-        #print xlabel, ylabel
         parameters = {}
         parameters['intent'] = 'ATMCAL'
         parameters['spw'] = ''
@@ -89,24 +161,23 @@ class SDTsysDisplay(common.SDCalibrationDisplay):
                     time_group.append(row+1)
             time_group.append(nrow)
             for i in xrange(len(time_group)-1):
-                if common.ShowPlot: pl.ioff()
-                pl.clf()
                 start = time_group[i]
                 end = time_group[i+1]
                 title = 'Tsys vs Frequency (%s)'%(utils.mjd_to_datestring(timestamps[start],fmt='%b %d %H:%M:%S %Y UT'))
                 #print title
                 #print 'range=',range(start,end)
+                lines = []
                 for row in xrange(start,end):
                     spw = tsel.getcell('IFNO',row)
                     pol = tsel.getcell('POLNO',row)
                     tsys = tsel.getcell('TSYS',row)
                     freq_id = tsel.getcell('FREQ_ID',row)
                     freq = base_freqs[freq_id] * 1.0e-6
-                    pl.plot(freq, tsys, '.-', label='spw%s, pol%s'%(spw,pol))
+                    lines.extend(
+                        pl.plot(freq, tsys, '.-', label='spw%s, pol%s'%(spw,pol))
+                    )
                 pl.title(title)
-                pl.xlabel(xlabel)
-                pl.ylabel(ylabel)
-                pl.legend(loc='best', numpoints=1, prop={'size':'small'})
+                axes.legend(loc='best', numpoints=1, prop={'size':'small'})
                 plotfile='tsys_vs_freq_%s_time%s.png'%(os.path.basename(table),i)
                 plotfile=os.path.join(stage_dir, plotfile)
                 plot = logger.Plot(plotfile,
@@ -116,10 +187,13 @@ class SDTsysDisplay(common.SDCalibrationDisplay):
                 pl.savefig(plotfile)
                 if common.ShowPlot: pl.draw()
                 plots.append(plot)
+
+                for line in lines:
+                    line.remove()
             tsel.close()
         return plots 
 
-    def _plot_tsys_vs_el(self, result, stage_dir):
+    def _plot_tsys_vs_time(self, result, stage_dir):
         table = result.outcome.applytable
         calto = result.outcome.calto
         antenna_name = calto.antenna
@@ -128,75 +202,94 @@ class SDTsysDisplay(common.SDCalibrationDisplay):
         if common.ShowPlot: pl.ion()
         else: pl.ioff()
         pl.figure(self.MATPLOTLIB_FIGURE_ID)
-        ylabel = 'Channel-Averaged Tsys [K]'
-        xlabel = 'Elevation [deg]'
-        #print xlabel, ylabel
         parameters_base = {}
         parameters_base['intent'] = 'ATMCAL'
         parameters_base['spw'] = ''
         parameters_base['pol'] = ''
         parameters_base['ant'] = antenna_name
         parameters_base['file'] = os.path.basename(table)
+
+        # setup axes
+        axes_manager = TsysTimeAxesManager()
+        
         with casatools.TableReader(table) as tb:
             spwlist = numpy.unique(tb.getcol('IFNO'))
             pollist = numpy.unique(tb.getcol('POLNO'))
+            
+            timelist = numpy.unique(tb.getcol('TIME'))
+            if len(timelist) < 2:
+                axes_manager.init()
+            else:
+                interval = timelist[1:]-timelist[:-1]
+                mean_interval = numpy.abs(timelist[1:] - timelist[:-1]).mean()
+                start_time = min(timelist) - 0.5 * mean_interval
+                end_time = max(timelist) + 0.5 * mean_interval
+                axes_manager.init(start_time=start_time, end_time=end_time)
+                
             if common.ShowPlot: pl.ioff()
             pl.clf()
+            axes = axes_manager.axes
             title = 'Tsys vs Elevation (Averaged Full Channel)'
+            lines = []
+            noedge_data = {}
             for spw in spwlist:
+                noedge_data[spw] = {}
                 for pol in pollist:
                     tsel = tb.query('IFNO==%s && POLNO==%s'%(spw,pol), sortlist='ELEVATION')
                     tsys = tsel.getcol('TSYS')
-                    el = tsel.getcol('ELEVATION') * 180.0 / numpy.pi
+                    time = tsel.getcol('TIME')
                     tsel.close()
                     atsys = tsys.mean(axis=0)
-                    pl.plot(el, atsys, '.-', label='spw=%s, pol=%s'%(spw,pol))
+                    etsys = common.drop_edge(tsys)
+                    if etsys is not None:
+                        noedge_data[spw][pol] = [time, etsys.mean(axis=0)]
+                    lines.extend(
+                        pl.plot(time, atsys, '.-', label='spw=%s, pol=%s'%(spw,pol))
+                    )
             pl.title(title)
-            pl.xlabel(xlabel)
-            pl.ylabel(ylabel)
-            pl.legend(loc='best', numpoints=1, prop={'size':'small'})
-            plotfile='tsys_vs_el_%s.png'%(os.path.basename(table))
+            axes.legend(loc='best', numpoints=1, prop={'size':'small'})
+            plotfile='tsys_vs_time_%s.png'%(os.path.basename(table))
             plotfile=os.path.join(stage_dir, plotfile)
             if common.ShowPlot: pl.draw()
             pl.savefig(plotfile)
             parameters = parameters_base.copy()
             parameters['type'] = 'full channel'
             plot = logger.Plot(plotfile,
-                               x_axis='Elevation', y_axis='Tsys',
+                               x_axis='Time', y_axis='Tsys',
                                field=parent_ms.fields[0].name,
                                parameters=parameters)
             plots.append(plot)
 
-            if common.ShowPlot: pl.ioff()
-            pl.clf()
+            # reset lines
+            for line in lines:
+                line.remove()
+
+            # reset colormap
+            axes.set_color_cycle(pl.rcParams['axes.color_cycle'])
+
             title = 'Tsys vs Elevation (Average Excluding Edge)'
             lines = []
-            for spw in spwlist:
-                for pol in pollist:
-                    tsel = tb.query('IFNO==%s && POLNO==%s'%(spw,pol), sortlist='ELEVATION')
-                    tsys = tsel.getcol('TSYS')
-                    el = tsel.getcol('ELEVATION') * 180.0 / numpy.pi
-                    tsel.close()
-                    etsys = common.drop_edge(tsys)
-                    if etsys is not None:
-                        atsys = etsys.mean(axis=0)
-                        lines.append(pl.plot(el, atsys, '.-', label='spw=%s, pol=%s'%(spw,pol)))
+            for (spw, values) in noedge_data.items():
+                for (pol, data) in values.items():
+                    lines.extend(
+                        pl.plot(data[0], data[1], '.-', label='spw=%s, pol=%s'%(spw,pol))
+                    )
             if len(lines) > 0:
                 pl.title(title)
-                pl.xlabel(xlabel)
-                pl.ylabel(ylabel)
-                pl.legend(loc='best', numpoints=1, prop={'size':'small'})
-                plotfile='tsys_vs_el_%s_noedge.png'%(os.path.basename(table))
+                axes.legend(loc='best', numpoints=1, prop={'size':'small'})
+                plotfile='tsys_vs_time_%s_noedge.png'%(os.path.basename(table))
                 plotfile=os.path.join(stage_dir, plotfile)
                 if common.ShowPlot: pl.draw()
                 pl.savefig(plotfile)
                 parameters = parameters_base.copy()
                 parameters['type'] = 'without edge'
                 plot = logger.Plot(plotfile,
-                                   x_axis='Elevation', y_axis='Tsys',
+                                   x_axis='Time', y_axis='Tsys',
                                    field=parent_ms.fields[0].name,
                                    parameters=parameters)
                 plots.append(plot)
+                for line in lines:
+                    line.remove()
         return plots
                 
 
