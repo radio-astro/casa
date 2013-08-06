ba5a6bdf2ecf7d7ee7904e0bb71c66598a936608
sanjay bhatnagar <sbhatnag@nrao.edu>
2013-08-06 00:15:55 +0000

    New Development: No             JIRA Issue: No         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes              Module(s): Module Names change impacts.            Description: Describe your changes here...
    Typo errors fixed.	
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@25878 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/recipes/checkpimagersetup.py
+++ gcwrap/python/scripts/recipes/checkpimagersetup.py
@@ -1,11 +1,11 @@
 from parallel.pimager import pimager
 
-def flagratio(msname=,spw='*',timerange='',field='*',obsid=''):
+def flagratio(msname,spw='*',timerange='',field='*',obsid=''):
     flag_summary=flagdata(vis=msname,mode='summary',field=field,timerange=timerange,spw=spw,observation=obsid)
     print "percentage data flagged in time range"+timerange+"is",((100*flag_summary['flagged'])/flag_summary['total']);
 
 def checkpimagersetup(msname, npartition, spw='*', timerange='', field='*', obsid=''):
-    tlist=pimager.findtimerange(msname=msname, field=field,spw=spw,numpartition=npartition);
+    tlist=pimager.findtimerange(msname=msname, field=field,spwids=spw,numpartition=npartition);
     print "Time range list: ";
     print tlist;
     n=len(tlist)
