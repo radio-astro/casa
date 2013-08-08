b699340cf0173090dfc0084e6372cc246de6ecea
lindsey davis <ldavis@nrao.edu>
2013-08-08 22:18:06 +0000

    Added a tsys_spwmap to the attribute list.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@25956 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/domain/measurementset.py
+++ pipeline/pipeline/domain/measurementset.py
@@ -215,6 +215,7 @@ class MeasurementSet(object):
         self.states = []
         self.reference_antenna = None
         self.reference_spwmap= None
+        self.tsys_spwmap= None
         self.flagcmds = []
         self.session = session
         self.filesize = self._calc_filesize() 
