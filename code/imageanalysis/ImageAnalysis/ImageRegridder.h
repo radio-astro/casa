cb0225dd5ff145608c1fa412511345ecbb63fd12
scott rankin <srankin@nrao.edu>
2013-08-27 16:34:15 +0000

    Merged revisions 26263,26292 via svnmerge from https://svn.cv.nrao.edu/svn/casa/trunk/code
    ........
      r26263 | dave.mehringer | 2013-08-26 15:24:47 -0600 (Mon, 26 Aug 2013) | 14 lines
      
      
             New Development: Yes
                  JIRA Issue: CAS-5524 
              Ready for Test: Yes
           Interface Changes: No
      What Interface Changed: Please list interface changes
               Test Programs: List test programs
        Put in Release Notes: Yes/No
                   Module(s): Module Names change impacts.
                 Description: better implementation of image overlap check
      
      	
    ........
      r26292 | justo.gonzalez | 2013-08-27 09:21:58 -0600 (Tue, 27 Aug 2013) | 11 lines
      
      
             New Development: no
                  JIRA Issue: CAS-4900
              Ready for Test: Yes
           Interface Changes: No
      What Interface Changed: 
               Test Programs: test_mstransform
        Put in Release Notes: No
                   Module(s): mstransform
                 Description: Fixed issue reported from science testing that was causing the DATA column to be zero when the last channel has to be dropped due to non-uniform width
    ........
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/test@26296 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/imageanalysis/ImageAnalysis/ImageRegridder.h
+++ code/imageanalysis/ImageAnalysis/ImageRegridder.h
@@ -143,8 +143,27 @@ private:
 
 	TempImage<Float> _regridByVelocity() const;
 
+	static Bool _doImagesOverlap(
+		const ImageInterface<Float>& image0,
+		const ImageInterface<Float>& image1
+	);
 
+	static Bool _doLineSegmentsIntersect(
+		const Vector<Double>& line0point0,
+		const Vector<Double>& line0point1,
+		const Vector<Double>& line1point0,
+		const Vector<Double>& line1point1
+	);
 
+	static Double _2DCrossProduct(
+		const Vector<Double> v0,
+		const Vector<Double> v1
+	);
+
+	static Vector<std::pair<Double, Double> > _getDirectionCorners(
+		const DirectionCoordinate& dc,
+		const IPosition& directionShape
+	);
 
 };
 }
