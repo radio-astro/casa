7ce2206a2cfc07c67f95e0355fa7c5b568686858
susan loveland <susan.loveland@nrao.edu>
2013-09-11 17:06:59 +0000

    New Development: No             JIRA Issue: No         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: No              Module(s): Module Names change impacts.            Description: Clean up of test class for plotms.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@26510 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/tests/test_plotms.py
+++ gcwrap/python/scripts/tests/test_plotms.py
@@ -27,6 +27,7 @@ class test_base(unittest.TestCase):
     datapath = os.environ.get('CASAPATH').split()[0] + '/data/regression/unittest/plotms/'
     if not testmms:
         ms = datapath + ms
+    outputDir='/tmp/'
     plotfile_jpg = "/tmp/myplot.jpg"
     display = os.environ.get("DISPLAY")
     plotfile_hash = ""
@@ -62,11 +63,7 @@ class test_base(unittest.TestCase):
             # store to check against following test results
             self.plotfile_hash = sha.new(open(self.plotfile_jpg, 'r').read()).hexdigest()
             
-    def _waitForFile(self, file, seconds):
-        for i in range(0,seconds):
-            if (os.path.isfile(file)):
-                return
-            time.sleep(1)
+    
         
 class plotms_test1(test_base):
 
@@ -95,7 +92,8 @@ class plotms_test1(test_base):
         
     def test001(self):
         '''Plotms 1: Write a jpg file using the plotms task'''
-        '''self.plotfile_jpg = "/tmp/testPlot001.jpg"
+        self.plotfile_jpg = self.outputDir + "testPlot001.jpg"
+        print 'Writing to ', self.plotfile_jpg
         if os.path.exists( self.plotfile_jpg):
             os.remove( self.plotfile_jpg)
         self.assertTrue(self.display.startswith(':'),'DISPLAY not set, cannot run test')
@@ -103,15 +101,15 @@ class plotms_test1(test_base):
         self.res = plotms(vis=self.ms, plotfile=self.plotfile_jpg, expformat="jpg", 
                           scriptclient=True, overwrite=True, showgui=False)   
         self.assertTrue(self.res)
-        #self._waitForFile(self.plotfile_jpg, 10)
         self.assertTrue(os.path.exists(self.plotfile_jpg), 'Plot was not created')
         print 'Plot file size is ', os.path.getsize(self.plotfile_jpg)
-        self._checkPlotFile(60000)'''
+        self._checkPlotFile(60000)
         
         
     def test002(self):
         '''Plotms 2: Check overwrite=False functionality works'''
-        '''self.plotfile_jpg = "/tmp/testPlot002.jpg"
+        self.plotfile_jpg = self.outputDir + "testPlot002.jpg"
+        print 'Writing to ', self.plotfile_jpg
         if os.path.exists( self.plotfile_jpg):
             os.remove( self.plotfile_jpg)
         self.assertTrue(self.display.startswith(':'),'DISPLAY not set, cannot run test')
@@ -121,16 +119,16 @@ class plotms_test1(test_base):
                           overwrite=True, scriptclient=True, showgui=False)
         self.assertTrue(self.res)
         self.assertTrue( os.path.exists(self.plotfile_jpg), 'Plot was not created')
-        #self._waitForFile(self.plotfile_jpg, 10)
         self._checkPlotFile(60000)
         #Next, overwrite is turned off so the save should fail.
         self.res = plotms(vis=self.ms, plotfile=self.plotfile_jpg, expformat='jpg', scriptclient=True)
-        self.assertFalse(self.res)'''
+        self.assertFalse(self.res)
         
 
     def test003(self):
         '''Plotms 3: Plot using data selection'''
-        '''self.plotfile_jpg = "/tmp/testPlot003.jpg"
+        self.plotfile_jpg = self.outputDir + "testPlot003.jpg"
+        print 'Writing to ', self.plotfile_jpg
         if os.path.exists( self.plotfile_jpg):    
             os.remove( self.plotfile_jpg)
         self.assertTrue(self.display.startswith(':'),'DISPLAY not set, cannot run test')
@@ -138,15 +136,15 @@ class plotms_test1(test_base):
         self.res = plotms(vis=self.ms,  plotfile=self.plotfile_jpg, expformat='jpg', 
                           scriptclient=True, showgui=False, overwrite=True, selectdata=True, scan='2,4')
         self.assertTrue(self.res)
-        #self._waitForFile(self.plotfile_jpg, 5)
         self.assertTrue(os.path.isfile(self.plotfile_jpg))
         plotSize = os.path.getsize(self.plotfile_jpg)
         self.assertTrue(plotSize > 60000)
-        self.assertTrue(plotSize < 70000)'''
+        self.assertTrue(plotSize < 70000)
     
     def test004(self):
         '''Plotms 4: Set a custom plotting symbol'''
-        self.plotfile_jpg = "/tmp/testPlot004.jpg"
+        self.plotfile_jpg = self.outputDir + "testPlot004.jpg"
+        print 'Writing to ', self.plotfile_jpg
         if os.path.exists( self.plotfile_jpg):    
             os.remove( self.plotfile_jpg)
         self.assertTrue(self.display.startswith(':'),'DISPLAY not set, cannot run test')
@@ -156,7 +154,6 @@ class plotms_test1(test_base):
                           customsymbol=True, symbolshape='diamond', symbolsize=5,
                           symbolcolor='00ff00', symbolfill='mesh3')   
         self.assertTrue(self.res)
-        #self._waitForFile(self.plotfile_jpg, 10)
         self.assertTrue(os.path.exists(self.plotfile_jpg), 'Plot was not created')
         print 'Plot file size is ', os.path.getsize(self.plotfile_jpg)
         self._checkPlotFile(60000)
@@ -164,7 +161,8 @@ class plotms_test1(test_base):
 
     def test005(self):
         '''Plotms 5: Check overwrite=True functionality works'''
-        '''self.plotfile_jpg = "/tmp/testPlot005.jpg"
+        self.plotfile_jpg = self.outputDir + "testPlot005.jpg"
+        print 'Writing to ', self.plotfile_jpg
         if os.path.exists( self.plotfile_jpg):
             os.remove( self.plotfile_jpg)
         self.assertTrue(self.display.startswith(':'),'DISPLAY not set, cannot run test')
@@ -172,18 +170,17 @@ class plotms_test1(test_base):
         self.res = plotms(vis=self.ms, plotfile=self.plotfile_jpg, expformat='jpg', 
                           scriptclient =True, overwrite=True, showgui=False)
         self.assertTrue(self.res)
-        #self._waitForFile(self.plotfile_jpg, 10)
         self._checkPlotFile(60000)
 
         time.sleep(5)
         self.res = plotms(vis=self.ms, plotfile=self.plotfile_jpg, expformat='jpg', 
                           overwrite=True, showgui=False)
         self.assertTrue(self.res)
-        #self._waitForFile(self.plotfile_jpg, 10)
-        self._checkPlotFile(60000)'''
+        self._checkPlotFile(60000)
 
 
 def suite():
+    print 'Tests may fail due to DBUS timeout if the version of Qt is not at least 4.8.5'
     return [plotms_test1]
   
  
