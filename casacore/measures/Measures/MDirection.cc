30f332a195e046a0672e0e627edec60c5caf38ae
dave mehringer <dmehring@zia.aoc.nrao.edu>
2013-09-11 20:16:12 +0000

    New Development: Yes             JIRA Issue: CAS-5266         Ready for Test: Yes      Interface Changes: Yes What Interface Changed: Added MDirection::toString()          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: as above
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@26511 ac9c0da5-131f-0410-9009-a9c195f183e6

--- casacore/measures/Measures/MDirection.cc
+++ casacore/measures/Measures/MDirection.cc
@@ -27,6 +27,9 @@
 
 //# Includes
 #include <casa/Exceptions.h>
+#include <casa/Quanta/MVAngle.h>
+#include <casa/Quanta/MVTime.h>
+#include <casa/Quanta/QMath.h>
 #include <casa/Utilities/Assert.h>
 #include <casa/Arrays/Vector.h>
 #include <casa/Utilities/Register.h>
@@ -456,6 +459,34 @@ void MDirection::shiftAngle(Double off, Double pa) {
 
 Measure *MDirection::clone() const {
     return (new MDirection(*this));
+}
+
+String MDirection::toString() const {
+	Quantity longitude = getValue().getLong("deg");
+	Quantity lat = getValue().getLat("deg");
+	Types type = castType(ref.getType());
+	String output;
+	if (
+		type == J2000  || type == JMEAN
+		|| type == JTRUE || type == APP
+		|| type == B1950 || type == B1950_VLA
+		|| type == BMEAN || type == BTRUE
+	) {
+		String ra = MVTime(longitude).string(MVTime::TIME, 12);
+		String dec = MVAngle(abs(lat)).string(MVAngle::ANGLE_CLEAN, 11);
+		dec.trim();
+		if (lat.getValue() < 0) {
+			dec = "-" + dec;
+		}
+		output = ra + " " + dec;
+	}
+	else {
+		String longStr = MVAngle(longitude).string(MVAngle::ANGLE, 11);
+		String latStr = MVAngle(lat).string(MVAngle::ANGLE, 11);
+		output = longStr + " " + latStr;
+	}
+	output += " " + showType(type);
+	return output;
 }
 
 } //# NAMESPACE CASA - END
