782417daa45f768b6bcf64ffedd96603e483112a
john lightfoot <john.lightfoot@stfc.ac.uk>
2013-10-24 12:45:46 +0000

    can handle missing TARGET data
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@26981 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hif/heuristics/wvrgcal.py
+++ pipeline/pipeline/hif/heuristics/wvrgcal.py
@@ -16,88 +16,10 @@ class WvrgcalHeuristics(object):
 
         ms = context.observing_run.get_ms(name=vis)
 
-        # get info on all spectral windows
-        self.spws = ms.spectral_windows
-        for spw in self.spws:
-            LOG.debug('SpW id:%s nchan:%s intents:%s' % (spw.id,
-              spw.num_channels, spw.intents))
-
-        # Get the data_desc_ids of the wvr data.
-        wvr_spw = [spw for spw in self.spws
-          if spw.num_channels==4 and 'WVR' in spw.intents]
-
-        if wvr_spw == []:
-            wvr_spw = [spw for spw in self.spws if spw.id==0]
-            if wvr_spw[0].num_channels != 4:
-                # EARLY RETURN HERE
-                self._wvr_available = False
-                return
-            LOG.info('assuming WVR spw.id=0')
-
-        wvr_dd_ids = [ms.get_data_description(spw).id for spw in wvr_spw]
-        LOG.info('WVR data_desc_id is %s' % wvr_dd_ids)
-
-        # now get the science spws, those used for scientific intent
-        science_spws = [spw for spw in self.spws if 
-          spw.num_channels not in [1,4] and not spw.intents.isdisjoint(
-          ['BANDPASS', 'AMPLITUDE', 'PHASE', 'TARGET'])]
-        LOG.info('science spws are: %s' % [spw.id for spw in science_spws])
-
-        # looking at just PHASE and TARGET intents, find the integration times
-        # associated with wvr data and with science data for each data_desc
-        science_field_ids = [field.id for field in ms.fields
-          if not set(field.intents).isdisjoint(['PHASE', 'TARGET'])]
-        science_state_ids = [state.id for state in ms.states
-          if not set(state.intents).isdisjoint(['PHASE', 'TARGET'])]
-
-        with casatools.TableReader(ms.name) as table:
-            taql = '''(STATE_ID IN %s AND FIELD_ID IN %s AND
-              DATA_DESC_ID in %s)''' % (science_state_ids, science_field_ids,
-              wvr_dd_ids)
-            subtable = table.query(taql)
-            integration = subtable.getcol('INTERVAL')
-            self.wvr_integration = np.median(integration)
-            wvr_max_integration = np.max(integration)
-            wvr_min_integration = np.min(integration)
-            subtable.done()
-
-        LOG.info('median WVR integration time: %s' % self.wvr_integration)
-        if wvr_max_integration != self.wvr_integration:
-            LOG.warning('max WVR integration time is different: %s' %
-              wvr_max_integration)
-        if wvr_min_integration != self.wvr_integration:
-            LOG.warning('min WVR integration time is different: %s' %
-              wvr_min_integration)
-
-        self.science_integration = {}
-        science_max_integration = {}
-        science_min_integration = {}
-        with casatools.TableReader(ms.name) as table:
-            for spw in science_spws:
-                dd_id = ms.get_data_description(spw).id
-                taql = '''(STATE_ID IN %s AND FIELD_ID IN %s AND
-                  DATA_DESC_ID in %s)''' % (science_state_ids,
-                  science_field_ids, dd_id)
-                subtable = table.query(taql)
-                integration = subtable.getcol('INTERVAL')
-
-                self.science_integration[spw.id] = np.median(integration)
-                science_max_integration[spw.id] = np.max(integration)
-                science_min_integration[spw.id] = np.min(integration)
-                LOG.info('SpW %s median integration time: %s' %
-                  (spw.id, self.science_integration[spw.id]))
-
-                if self.science_integration[spw.id] != \
-                  science_max_integration[spw.id]:
-                    LOG.warning('max integration time is different: %s' %
-                      science_max_integration[spw.id])
-                if self.science_integration[spw.id] != \
-                  science_min_integration[spw.id]:
-                    LOG.warning('min integration time is different: %s' %
-                      science_max_integration[spw.id])
-
-                # free the resources held by the sub-table
-                subtable.done()
+        if hm_smooth == 'manual':
+            self._smooth = smooth
+        else:
+            self._smooth = self._calculate_smooth(ms)
 
         if hm_tie == 'manual':
             self._tie = tie
@@ -129,6 +51,93 @@ class WvrgcalHeuristics(object):
         if self._nsol != 1 and self._segsource:
             raise Exception, \
               'WvrgHeuristics: nsol <>1 incompatible with segsource True'  
+
+    def _calculate_smooth(self, ms):
+        # get info on all spectral windows
+        self.spws = ms.spectral_windows
+        for spw in self.spws:
+            LOG.debug('SpW id:%s nchan:%s intents:%s' % (spw.id,
+              spw.num_channels, spw.intents))
+
+        # Get the data_desc_ids of the wvr data.
+        wvr_spw = [spw for spw in self.spws
+          if spw.num_channels==4 and 'WVR' in spw.intents]
+
+        # Check that wvr data are available
+        if wvr_spw == []:
+            # EARLY RETURN HERE
+            self._wvr_available = False
+            return
+
+        wvr_dd_ids = [ms.get_data_description(spw).id for spw in wvr_spw]
+        LOG.info('WVR data_desc_id is %s' % wvr_dd_ids)
+
+        # now get the science spws, those used for scientific intent
+        science_spws = [spw for spw in self.spws if 
+          spw.num_channels not in [1,4] and not spw.intents.isdisjoint(
+          ['BANDPASS', 'AMPLITUDE', 'PHASE', 'TARGET'])]
+        LOG.info('science spws are: %s' % [spw.id for spw in science_spws])
+
+        # and the science fields/states
+        science_field_ids = [field.id for field in ms.fields
+          if not set(field.intents).isdisjoint(
+            ['BANDPASS', 'AMPLITUDE', 'PHASE', 'TARGET'])]
+        science_state_ids = [state.id for state in ms.states
+          if not set(state.intents).isdisjoint(
+            ['BANDPASS', 'AMPLITUDE', 'PHASE', 'TARGET'])]
+
+        # get the median integration time for wvr data associated with
+        # science targets, warn if there is a range of values
+        with casatools.TableReader(ms.name) as table:
+            taql = '''(STATE_ID IN %s AND FIELD_ID IN %s AND
+              DATA_DESC_ID in %s)''' % (science_state_ids, science_field_ids,
+              wvr_dd_ids)
+            subtable = table.query(taql)
+            integration = subtable.getcol('INTERVAL')
+            self.wvr_integration = np.median(integration)
+            wvr_max_integration = np.max(integration)
+            wvr_min_integration = np.min(integration)
+            subtable.done()
+
+        LOG.info('median WVR integration time: %s' % self.wvr_integration)
+        if wvr_max_integration != self.wvr_integration:
+            LOG.warning('max WVR integration time is different: %s' %
+              wvr_max_integration)
+        if wvr_min_integration != self.wvr_integration:
+            LOG.warning('min WVR integration time is different: %s' %
+              wvr_min_integration)
+
+        # get the median integration time for science data associated with
+        # science targets, warn if there is a range of values
+        self.science_integration = {}
+        science_max_integration = {}
+        science_min_integration = {}
+        with casatools.TableReader(ms.name) as table:
+            for spw in science_spws:
+                dd_id = ms.get_data_description(spw).id
+                taql = '''(STATE_ID IN %s AND FIELD_ID IN %s AND
+                  DATA_DESC_ID in %s)''' % (science_state_ids,
+                  science_field_ids, dd_id)
+                subtable = table.query(taql)
+                integration = subtable.getcol('INTERVAL')
+
+                self.science_integration[spw.id] = np.median(integration)
+                science_max_integration[spw.id] = np.max(integration)
+                science_min_integration[spw.id] = np.min(integration)
+                LOG.info('SpW %s median science integration time: %s' %
+                  (spw.id, self.science_integration[spw.id]))
+
+                if self.science_integration[spw.id] != \
+                  science_max_integration[spw.id]:
+                    LOG.warning('max science integration time is different: %s' %
+                      science_max_integration[spw.id])
+                if self.science_integration[spw.id] != \
+                  science_min_integration[spw.id]:
+                    LOG.warning('min science integration time is different: %s' %
+                      science_max_integration[spw.id])
+
+                # free the resources held by the sub-table
+                subtable.done()
 
     def _calculate_tie(self, ms):
         # get target names and directions
