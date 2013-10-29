3d7e2efd8c43ac2609b89c477047a311bc17a578
brian kent <bkent@nrao.edu>
2013-10-29 13:53:50 +0000

    flagging set to True in testBPdcals
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@27069 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/vla/tasks/testBPdcals/testBPdcals.py
+++ pipeline/pipeline/vla/tasks/testBPdcals/testBPdcals.py
@@ -62,7 +62,7 @@ class testBPdcals(basetask.StandardTaskTemplate):
 
         context = self.inputs.context
         refantfield = context.evla['msinfo'][m.name].calibrator_field_select_string
-        refantobj = findrefant.RefAntHeuristics(vis=self.inputs.vis,field=refantfield,geometry=True,flagging=False, intent='', spw='')
+        refantobj = findrefant.RefAntHeuristics(vis=self.inputs.vis,field=refantfield,geometry=True,flagging=True, intent='', spw='')
         
         RefAntOutput=refantobj.calculate()
         
