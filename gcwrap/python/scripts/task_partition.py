9da35b6c3d7f3385b030fa9919252852eb4970fd
sandra castro <sbhatnag@nrao.edu>
2013-11-04 13:10:57 +0000

    New Development: No             JIRA Issue: CAS-4899         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: test_partition   Put in Release Notes: No              Module(s): mstransform            Description: Removed the 'experimental' status of partition and listpartition. Re-formated the inline help            of tasks partition and mstransform, now that the XML Description text is actually showed.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@27159 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/task_partition.py
+++ gcwrap/python/scripts/task_partition.py
@@ -146,7 +146,7 @@ def partition(vis,
 
         # Configure the tool and all the parameters
         
-        casalog.post('%s'%config, 'DEBUG')
+        casalog.post('%s'%config, 'DEBUG1')
         mtlocal.config(config)
         
         # Open the MS, select the data and configure the output
@@ -168,7 +168,7 @@ def partition(vis,
     try:
         param_names = partition.func_code.co_varnames[:partition.func_code.co_argcount]
         param_vals = [eval(p) for p in param_names]
-        casalog.post('Updating the history of the output MS','DEBUG1')
+        casalog.post('Updating the history in the output', 'DEBUG1')
         write_history(mslocal, outputvis, 'partition', param_names,
                       param_vals, casalog)
     except Exception, instance:
