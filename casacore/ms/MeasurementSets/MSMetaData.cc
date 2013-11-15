5cdcba47e2cdbf320505c083d6d5ad5f5f0e6371
dave mehringer <dmehring@zia.aoc.nrao.edu>
2013-11-15 18:12:06 +0000

    New Development: Yes/No             JIRA Issue: CAS-5701         Ready for Test: Yes      Interface Changes: Yes What Interface Changed: implmented getExposureMap()          Test Programs: tMSMetaData   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: as above
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/release-4_2_0@27355 ac9c0da5-131f-0410-9009-a9c195f183e6

--- casacore/ms/MeasurementSets/MSMetaData.cc
+++ casacore/ms/MeasurementSets/MSMetaData.cc
@@ -305,6 +305,14 @@ Vector<Double> MSMetaData::_getTimes(const MeasurementSet& ms) {
 	return ScalarColumn<Double>(ms, timeColName).getColumn();
 }
 
+
+Quantum<Vector<Double> > MSMetaData::_getExposures(const MeasurementSet& ms) {
+	String colName = MeasurementSet::columnName(MSMainEnums::EXPOSURE);
+	ScalarColumn<Double> exposure (ms, colName);
+	String unit = *exposure.keywordSet().asArrayString("QuantumUnits").begin();
+	return Quantum<Vector<Double> >(exposure.getColumn(), unit);
+}
+
 Vector<Double> MSMetaData::_getTimeCentroids(const MeasurementSet& ms) {
 	String timeCentroidColName = MeasurementSet::columnName(MSMainEnums::TIME_CENTROID);
 	return ScalarColumn<Double>(ms, timeCentroidColName).getColumn();
@@ -589,6 +597,37 @@ std::map<Int, vector<Double> > MSMetaData::_getScanToTimeRangeMap(
 		}
 	}
 	return out;
+}
+
+vector<std::map<Int, Quantity> > MSMetaData::_getFirstExposureTimeMap(
+	uInt nDataDescIDs, const Vector<Int>& scans,
+	const Vector<Int>& dataDescIDs, const Vector<Double>& times,
+	const Quantum<Vector<Double> >& exposureTimes
+) {
+	vector<std::map<Int, Quantity> > ret(nDataDescIDs);
+	vector<std::map<Int, Double> > tmap(nDataDescIDs);
+	Vector<Int>::const_iterator siter = scans.begin();
+	Vector<Int>::const_iterator send = scans.end();
+	Vector<Int>::const_iterator diter = dataDescIDs.begin();
+	Vector<Double>::const_iterator titer = times.begin();
+	Vector<Double> eTimes = exposureTimes.getValue();
+	String unit = exposureTimes.getUnit();
+	Vector<Double>::const_iterator eiter = eTimes.begin();
+	while (siter != send) {
+		std::map<Int, Quantity> mymap = ret[*diter];
+		if (
+			mymap.find(*siter) == mymap.end()
+			|| *titer < tmap[*diter][*siter]
+		) {
+			ret[*diter][*siter] = Quantity(*eiter, unit);
+			tmap[*diter][*siter] = *titer;
+		}
+		siter++;
+		diter++;
+		titer++;
+		eiter++;
+	}
+	return ret;
 }
 
 void MSMetaData::_getAntennas(
--- casacore/ms/MeasurementSets/MSMetaDataOnDemand.h
+++ casacore/ms/MeasurementSets/MSMetaDataOnDemand.h
@@ -126,6 +126,9 @@ public:
 	// get the number of arrays (from the ARRAY table) in the dataset
 	uInt nArrays();
 
+	// get the number of data description IDs (from the DATA_DESCRIPTION table)
+	uInt nDataDescriptions();
+
 	// get the set of spectral windows for the specified scan number.
 	std::set<uInt> getSpwsForScan(const Int scan);
 
@@ -299,13 +302,16 @@ public:
 
 	uInt nPol();
 
+	// get a map of data desc ID, scan number pair to exposure time for the first time
+	// for that data desc ID, scan number pair
+	vector<std::map<Int, Quantity	> > getFirstExposureTimeMap();
 
 private:
 	const MeasurementSet* _ms;
 	Float _cacheMB;
 	const Float _maxCacheMB;
 	uInt _nStates, _nACRows, _nXCRows, _nSpw, _nFields, _nAntennas,
-		_nObservations, _nScans, _nArrays, _nrows, _nPol;
+		_nObservations, _nScans, _nArrays, _nrows, _nPol, _nDataDescIDs;
 	std::set<String> _uniqueIntents;
 	std::map<Int, std::set<uInt> > _scanToSpwsMap;
 	std::set<Int> _uniqueScanNumbers, _uniqueFieldIDs, _uniqueStateIDs;
@@ -326,6 +332,7 @@ private:
 	vector<String> _fieldNames, _antennaNames, _observatoryNames, _stationNames;
 	std::map<String, uInt> _antennaNameToIDMap;
 	std::tr1::shared_ptr<Vector<Double> > _times;
+	std::tr1::shared_ptr<Quantum<Vector<Double> > > _exposures;
 	std::tr1::shared_ptr<std::map<Int, std::set<Double> > > _scanToTimesMap;
 	std::map<String, std::set<Int> > _intentToFieldIDMap, _intentToScansMap;
 	std::map<String, std::set<uInt> > _intentToSpwsMap;
@@ -347,6 +354,7 @@ private:
 	std::map<Int, vector<Double> > _scanToTimeRangeMap;
 	std::map<Int, std::map<uInt, Double> > _scanSpwToIntervalMap;
 	Bool _spwInfoStored;
+	vector<std::map<Int, Quantity> > _firstExposureTimeMap;
 
 	// disallow copy constructor and = operator
 	MSMetaDataOnDemand(const MSMetaDataOnDemand&);
@@ -399,6 +407,8 @@ private:
 
 	std::tr1::shared_ptr<Vector<Double> > _getTimes();
 
+	std::tr1::shared_ptr<Quantum<Vector<Double> > > _getExposureTimes();
+
 	std::tr1::shared_ptr<ArrayColumn<Bool> > _getFlags();
 
 	std::set<Int> _getUniqueFiedIDs();
@@ -426,6 +436,7 @@ private:
 
 	static uInt _sizeof(const std::map<String, std::set<uInt> >& map);
 
+	static uInt _sizeof(const vector<std::map<Int, Quantity> >& map);
 
 	void _getFieldsAndSpwMaps(
 		std::map<Int, std::set<uInt> >& fieldToSpwMap,
