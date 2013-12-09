82a7f45291280afc5c090503341fa9d022d0ac0a
wataru kawasaki <wataru.kawasaki@nao.ac.jp>
2013-12-09 10:04:03 +0000

    New Development: Yes             JIRA Issue: Yes CAS-5842         Ready for Test: Yes      Interface Changes: Yes What Interface Changed: task parameters (1 added, 1 moved to subparameter)          Test Programs:   Put in Release Notes: No              Module(s): sd            Description: (1) added a new boolean parameter 'splitant' to sdsave. if True, the dataset are splitted and saved as several scantables for each antenna. default is False. the input data must be in MS format. (2) a parameter 'antenna' is now a subparameter of 'splitant'.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@27645 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/task_sdsave.py
+++ gcwrap/python/scripts/task_sdsave.py
@@ -1,11 +1,23 @@
+
+       New Development: Yes/No
+            JIRA Issue: No/Yes List JIRA ticket.
+        Ready for Test: Yes/No
+     Interface Changes: Yes/No
+What Interface Changed: Please list interface changes
+         Test Programs: List test programs
+  Put in Release Notes: Yes/No
+             Module(s): Module Names change impacts.
+           Description: Describe your changes here...
+
+	
 from taskinit import casalog
 
 import asap as sd
-from asap.scantable import is_scantable
+from asap.scantable import is_scantable, is_ms
 import sdutil
 
 @sdutil.sdtask_decorator
-def sdsave(infile, antenna, getpt, rowlist, scanlist, field, iflist, pollist, scanaverage, timeaverage, tweight, polaverage, pweight, restfreq, outfile, outform, overwrite):
+def sdsave(infile, splitant, antenna, getpt, rowlist, scanlist, field, iflist, pollist, scanaverage, timeaverage, tweight, polaverage, pweight, restfreq, outfile, outform, overwrite):
     with sdutil.sdtask_manager(sdsave_worker, locals()) as worker:
         worker.initialize()
         worker.execute()
@@ -24,39 +36,102 @@ class sdsave_worker(sdutil.sdtask_template):
         self.rfset = (self.restfreq != '') and (self.restfreq != [])
         
     def initialize_scan(self):
-        self.scan = sd.scantable(self.infile,
-                                 average=self.scanaverage,
-                                 antenna=self.antenna,
-                                 getpt=self.getpt)
+        if self.splitant:
+            if not is_ms(self.infile):
+                msg = 'input data must be in MS format'
+                raise Exception, msg
+            
+            import datetime
+            dt = datetime.datetime.now()
+            self.temp_prefix = "temp-sdsave" + dt.strftime("%Y%m%d%H%M%S")
+            self.split_infiles = sd.splitant(filename=self.infile,
+                                             outprefix=self.temp_prefix,
+                                             overwrite=self.overwrite,
+                                             getpt=self.getpt)
+            self.scans = []
+            self.antenna_names = []
+            for split_infile in self.split_infiles:
+                work_scan = sd.scantable(split_infile,
+                                         average=self.scanaverage)
+                # scantable selection
+                work_scan.set_selection(self.get_selector_by_list())
+                self.scans.append(work_scan)
 
-        # scantable selection
-        #self.scan.set_selection(self.get_selector())
-        self.scan.set_selection(self.get_selector_by_list())
+                #retrieve antenna names
+                self.antenna_names.append(split_infile.split('.')[1])
+            
+        else:
+            self.scan = sd.scantable(self.infile,
+                                     average=self.scanaverage,
+                                     antenna=self.antenna,
+                                     getpt=self.getpt)
+
+            # scantable selection
+            #self.scan.set_selection(self.get_selector())
+            self.scan.set_selection(self.get_selector_by_list())
 
     def execute(self):
-        # Apply averaging
-        self.original_scan = self.scan
-        self.scan = sdutil.doaverage(self.original_scan, self.scanaverage,
-                                     self.timeaverage, self.tweight,
-                                     self.polaverage, self.pweight)
-
-        if self.original_scan == self.scan and self.rfset \
-               and is_scantable(self.infile) \
-               and self.is_disk_storage:
-            self.molids = self.original_scan._getmolidcol_list()
-            self.restore = True
+        if self.splitant:
+            for work_scan in self.scans:
+                #Apply averaging
+                self.original_scan = work_scan
+                work_scan = sdutil.doaverage(self.original_scan, self.scanaverage,
+                                             self.timeaverage, self.tweight,
+                                             self.polaverage, self.pweight)
+                # set rest frequency
+                casalog.post('restore=%s'%(self.restore))
+                if self.rfset:
+                    work_scan.set_restfreqs(sdutil.normalise_restfreq(self.restfreq))
         
-        # set rest frequency
-        casalog.post('restore=%s'%(self.restore))
-        if self.rfset:
-            self.scan.set_restfreqs(sdutil.normalise_restfreq(self.restfreq))
+        else:
+            # Apply averaging
+            self.original_scan = self.scan
+            self.scan = sdutil.doaverage(self.original_scan, self.scanaverage,
+                                         self.timeaverage, self.tweight,
+                                         self.polaverage, self.pweight)
+
+            if self.original_scan == self.scan and self.rfset \
+                   and is_scantable(self.infile) \
+                   and self.is_disk_storage:
+                self.molids = self.original_scan._getmolidcol_list()
+                self.restore = True
+        
+            # set rest frequency
+            casalog.post('restore=%s'%(self.restore))
+            if self.rfset:
+                self.scan.set_restfreqs(sdutil.normalise_restfreq(self.restfreq))
 
     def save(self):
-        # save
-        sdutil.save(self.scan, self.outfile, self.outform, self.overwrite)
+        if self.splitant:
+            outfile_ext = ''
+            elem_outfilename = self.outfile.split('.')
+            len_elem = len(elem_outfilename)
+            if (len_elem > 1):
+                outfile_ext = elem_outfilename.pop().lower()
+                if (outfile_ext == 'asap'):
+                    outfile_ext = '.asap'
+                else:
+                    elem_outfilename.append(outfile_ext)
+                    outfile_ext = ''
+
+            outfile_prefix = '.'.join(elem_outfilename) + '_'
+
+            i = 0
+            for work_scan in self.scans:
+                split_outfile = outfile_prefix + self.antenna_names[i] + outfile_ext
+                sdutil.save(work_scan, split_outfile, self.outform, self.overwrite)
+                i += 1
+
+            for tempfile in self.split_infiles:
+                import os
+                os.system('rm -rf %s' % tempfile)
+            
+        else:
+            # save
+            sdutil.save(self.scan, self.outfile, self.outform, self.overwrite)
         
     def cleanup(self):
         if hasattr(self,'restore') and self.restore:
-            casalog.post( "Restoreing MOLECULE_ID column in %s "%self.infile )
+            casalog.post( "Restoring MOLECULE_ID column in %s "%self.infile )
             self.original_scan._setmolidcol_list(self.molids)
             
