a0e5faf8bda58e37024c2d07d979328b811ce34b
kana sugimoto <kana.sugi@nao.ac.jp>
2013-12-12 07:27:46 +0000

    New Development: Yes             JIRA Issue: Yes (CAS-5620)         Ready for Test: Yes      Interface Changes: Yes What Interface Changed: A new baseline task, hsd_baseline2(). Input parameters are identical to hsd_baseline() except fitfunc only supports 'cspline'.          Test Programs:   Put in Release Notes: Yes              Module(s): hsd_baseline2            Description: Duplicated hsd_baseline task and generated a new task hsd_baseline2 to work on CAS-5620.  Task hsd_baseline2 is aimed to update SD baseline task so that it generates baseline table for future use in restore process. Note hsd_baseline2 only supports cubic-spline fitting. Support of polynomial fitting is dropped at the beginning of the development.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@27686 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hsd/tasks/baseline/__init__.py
+++ pipeline/pipeline/hsd/tasks/baseline/__init__.py
@@ -1,3 +1,4 @@
 from __future__ import absolute_import
 
 from .baseline import SDBaseline
+from .baseline2 import SDBaseline as SDBaseline2
---
+++ pipeline/pipeline/hsd/tasks/baseline/baseline2.py
@@ -0,0 +1,185 @@
+from __future__ import absolute_import
+
+import numpy
+
+import pipeline.infrastructure as infrastructure
+import pipeline.infrastructure.sdfilenamer as filenamer
+
+from .. import common
+from . import maskline
+#from .fitting import FittingFactory
+from . import fitting2 as fitting
+
+LOG = infrastructure.get_logger(__name__)
+
+class SDBaselineInputs(common.SingleDishInputs):
+    """
+    Inputs for imaging
+    """
+    def __init__(self, context, infiles=None, iflist=None, pollist=None,
+                 linewindow=None, edge=None, broadline=None, fitorder=None,
+                 fitfunc=None):
+        self._init_properties(vars())
+        self._to_list(['infiles', 'iflist', 'pollist', 'edge', 'linewindow'])
+        self._to_bool('broadline')
+        self._to_numeric('fitorder')
+        if isinstance(self.fitorder, float):
+            self.fitorder = int(self.fitorder)
+
+    @property
+    def antennalist(self):
+        if type(self.infiles) == list:
+            antennas = [self.context.observing_run.get_scantable(f).antenna.name 
+                        for f in self.infiles]
+            return list(set(antennas))
+        else:
+            return [self.context.observing_run.get_scantable(self.infiles).antenna.name]
+
+class SDBaselineResults(common.SingleDishResults):
+    def __init__(self, task=None, success=None, outcome=None):
+        super(SDBaselineResults, self).__init__(task, success, outcome)
+
+    def merge_with_context(self, context):
+        super(SDBaselineResults, self).merge_with_context(context)
+
+        # replace and export datatable to merge updated data with context
+        datatable = self.outcome.pop('datatable')
+        datatable.exportdata(minimal=True)
+
+        context.observing_run.datatable_instance = datatable
+        
+        # increment iteration counter
+        # register detected lines to reduction group member
+        reduction_group = context.observing_run.reduction_group
+        for b in self.outcome['baselined']:
+            spw = b['spw']
+            antenna = b['index']
+            pols = b['pols']
+            lines = b['lines']
+            for _ant in antenna:
+                group_id = -1
+                for (idx,desc) in reduction_group.items():
+                    if desc[0].spw == spw:
+                        group_id = idx
+                        break
+                if group_id >= 0:
+                    reduction_group[group_id].iter_countup(_ant, spw, pols)
+                    reduction_group[group_id].add_linelist(lines, _ant, spw, pols)
+                st = context.observing_run[_ant]
+                st.work_data = st.baselined_name
+
+    def _outcome_name(self):
+        return ['%s: %s (spw=%s, pol=%s)'%(idx, name, b['spw'], b['pols'])
+                for b in self.outcome['baselined']
+                for (idx,name) in zip(b['index'], b['name'])]
+
+class SDBaseline(common.SingleDishTaskTemplate):
+    Inputs = SDBaselineInputs
+
+    def prepare(self):
+        inputs = self.inputs
+        context = inputs.context
+        datatable = context.observing_run.datatable_instance
+        reduction_group = context.observing_run.reduction_group
+        infiles = inputs.infiles
+        iflist = inputs.iflist
+        #antennalist = inputs.antennalist
+        pollist = inputs.pollist
+        st_names = context.observing_run.st_names
+        file_index = [st_names.index(infile) for infile in infiles]
+
+        window = [] if inputs.linewindow is None else inputs.linewindow
+        edge = (0,0) if inputs.edge is None else inputs.edge
+        broadline = False if inputs.broadline is None else inputs.broadline
+        fitorder = 'automatic' if inputs.fitorder is None or inputs.fitorder < 0 else inputs.fitorder
+        fitfunc = 'spline' if inputs.fitfunc is None else inputs.fitfunc
+        
+        baselined = []
+
+        ifnos = numpy.array(datatable.getcol('IF'))
+        polnos = numpy.array(datatable.getcol('POL'))
+        srctypes = numpy.array(datatable.getcol('SRCTYPE'))
+        antennas = numpy.array(datatable.getcol('ANTENNA'))
+
+        # loop over reduction group
+        files = set()
+        for (group_id,group_desc) in reduction_group.items():            
+            # assume all members have same spw and pollist
+            first_member = group_desc[0]
+            spwid = first_member.spw
+            LOG.debug('spwid=%s'%(spwid))
+            pols = first_member.pols
+            iteration = first_member.iteration[0]
+            if pollist is not None:
+                pols = list(set(pollist) & set(pols))
+
+            # skip spw not included in iflist
+            if iflist is not None and spwid not in iflist:
+                LOG.debug('Skip spw %s'%(spwid))
+                continue
+
+            # reference data is first scantable 
+            st = context.observing_run[first_member.antenna]
+
+            # skip channel averaged spw
+            nchan = group_desc.nchan
+            if nchan == 1:
+                LOG.info('Skip channel averaged spw %s.'%(spwid))
+                continue
+
+                
+            srctype = st.calibration_strategy['srctype']
+            _file_index = set(file_index) & set([m.antenna for m in group_desc])
+            files = files | _file_index
+            index_list = numpy.where(numpy.logical_and(ifnos == spwid, srctypes==srctype))[0]
+            maskline_inputs = maskline.MaskLine.Inputs(context, list(_file_index), spwid, iteration, 
+                                                       index_list, window, edge, broadline)
+            maskline_task = maskline.MaskLine(maskline_inputs)
+            maskline_result = self._executor.execute(maskline_task, merge=True)
+            detected_lines = maskline_result.outcome['detected_lines']
+            cluster_info = maskline_result.outcome['cluster_info']
+
+            #LOG.info('detected_lines=%s'%(detected_lines))
+            #LOG.info('cluster_info=%s'%(cluster_info))
+
+            # filenamer
+            namer = filenamer.BaselineSubtractedTable()
+            namer.spectral_window(spwid)
+
+            # fit order determination and fitting
+            fitter_cls = fitting.FittingFactory.get_fitting_class(fitfunc)
+
+            # loop over file
+            for idx in _file_index:
+                iteration = group_desc.get_iteration(idx, spwid)
+                fitter_inputs = fitter_cls.Inputs(context, idx, spwid, pols, iteration, 
+                                                  fitorder, edge)
+                fitter = fitter_cls(fitter_inputs)
+                fitter_result = self._executor.execute(fitter, merge=True)
+                
+            name_list = [context.observing_run[f].baselined_name
+                         for f in _file_index]
+            baselined.append({'name': name_list, 'index': list(_file_index),
+                              'spw': spwid, 'pols': pols,
+                              'lines': detected_lines,
+                              'clusters': cluster_info})
+            LOG.debug('cluster_info=%s'%(cluster_info))
+
+        outcome = {'datatable': datatable,
+                   'baselined': baselined,
+                   'edge': edge}
+        results = SDBaselineResults(task=self.__class__,
+                                    success=True,
+                                    outcome=outcome)
+                
+        if self.inputs.context.subtask_counter is 0: 
+            results.stage_number = self.inputs.context.task_counter - 1
+        else:
+            results.stage_number = self.inputs.context.task_counter 
+                
+        return results
+
+    def analyse(self, result):
+        return result
+
+
