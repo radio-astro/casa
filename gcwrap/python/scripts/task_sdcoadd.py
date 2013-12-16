ed35c4a391e6416ee1e18377db62d252ea97f02d
takeshi nakazato <takeshi.nakazato@nao.ac.jp>
2013-12-16 08:20:06 +0000

    New Development: No             JIRA Issue: Yes CAS-5865         Ready for Test: Yes      Interface Changes: Yes What Interface Changed: Removed some parameters required by the ticket          Test Programs: test_sdcoadd   Put in Release Notes: Yes              Module(s): sd            Description: Describe your changes here...
    Removed fluxunit, telescopeparm, specunit, frame, doppler, 
    scanaverage, timeaverage, polaverage, tweight, and pweight 
    parameters from the task.
    
    	
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@27731 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/task_sdcoadd.py
+++ gcwrap/python/scripts/task_sdcoadd.py
@@ -4,7 +4,7 @@ import asap as sd
 import sdutil
 
 @sdutil.sdtask_decorator
-def sdcoadd(infiles, antenna, fluxunit, telescopeparm, specunit, frame, doppler, scanaverage, timeaverage, tweight, polaverage, pweight, outfile, outform, overwrite):
+def sdcoadd(infiles, antenna, outfile, outform, overwrite):
     with sdutil.sdtask_manager(sdcoadd_worker, locals()) as worker:
         worker.initialize()
         worker.execute()
@@ -23,51 +23,16 @@ class sdcoadd_worker(sdutil.sdtask_template):
         super(sdcoadd_worker,self).initialize()
             
     def initialize_scan(self):
-        self.scanlist = [None] * self.nfile
-        self.restorer = [None] * self.nfile
-
-        for i in xrange(self.nfile):
-            sorg = sd.scantable(self.infiles[i],average=self.scanaverage,antenna=self.antenna)
-            
-            # prepare restorer object
-            restorer = sdutil.scantable_restore_factory(sorg,
-                                                        self.infiles[i],
-                                                        self.fluxunit,
-                                                        self.specunit,
-                                                        self.frame,
-                                                        self.doppler)
-            # convert flux
-            stmp = sdutil.set_fluxunit(sorg, self.fluxunit, self.telescopeparm, False)
-            if stmp:
-                # Restore header in original table before deleting
-                if restorer is not None:
-                    restorer.restore()
-                self.scanlist[i] = stmp
-            else:
-                self.scanlist[i] = sorg
-                self.restorer[i] = restorer
-
-        # this is bit tricky
-        # set fluxunit here instead of self.set_to_scan
-        # and remove fluxunit attribute to disable additional
-        # call of set_fluxunit in self.set_to_scan
-        self.fluxunit_saved = self.fluxunit
-        del self.fluxunit
+        self.scanlist = [sd.scantable(f,average=False,antenna=self.antenna)
+                         for f in self.infiles]
 
     def execute(self):
         self.set_to_scan()
 
         self.merge()
 
-        # Average in time if desired
-        self.scan = sdutil.doaverage(self.scan, self.scanaverage, self.timeaverage,
-                                     self.tweight, self.polaverage, self.pweight)
-
     def set_to_scan(self):
-        for scan in self.scanlist:
-            self.scan = scan
-            super(sdcoadd_worker,self).set_to_scan()
-        del self.scan
+        pass
 
     def merge(self):
         self.scan = sd.merge(self.scanlist)
@@ -85,8 +50,4 @@ class sdcoadd_worker(sdutil.sdtask_template):
         sdutil.save(self.scan, self.project, self.outform, self.overwrite)
         
     def cleanup(self):
-        if hasattr(self,'restorer'):
-            for r in self.restorer:
-                if r:
-                    r.restore()
-
+        pass
