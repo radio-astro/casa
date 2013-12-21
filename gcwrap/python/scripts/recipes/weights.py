34302b21db0c3c4bfefdfa74f3f39c0e5079de3e
scott rankin <srankin@nrao.edu>
2013-12-21 01:02:53 +0000

    Merged revisions 27758,27764,27771,27819,27842,27848 via svnmerge from https://svn.cv.nrao.edu/svn/casa/branches/release-4_2_0/gcwrap
    ........
      r27758 | dave.mehringer | 2013-12-17 11:31:40 -0700 (Tue, 17 Dec 2013) | 14 lines
      
      
             New Development: Yes/No
                  JIRA Issue: CAS-5466 
              Ready for Test: Yes
           Interface Changes: No
      What Interface Changed: Please list interface changes
               Test Programs: List test programs
        Put in Release Notes: Yes/No
                   Module(s): Module Names change impacts.
                 Description: doc updates that were committed on trunk r27000 but for whatever reason were not merged to the pre-release branch
      
      	
    ........
      r27764 | takeshi.nakazato | 2013-12-17 19:01:47 -0700 (Tue, 17 Dec 2013) | 16 lines
      
      
             New Development: No
                  JIRA Issue: No
              Ready for Test: Yes
           Interface Changes: No
      What Interface Changed: Please list interface changes
               Test Programs: List test programs
        Put in Release Notes: No
                   Module(s): Module Names change impacts.
                 Description: Describe your changes here...
      
      Fix for the data that doesn't have channel averaged spw.
      
      	
    ........
      r27771 | sandra.castro | 2013-12-18 02:31:02 -0700 (Wed, 18 Dec 2013) | 12 lines
      
      
             New Development: No
                  JIRA Issue: CAS-4899
              Ready for Test: Yes
           Interface Changes: No
      What Interface Changed: 
               Test Programs: test_mstransform
        Put in Release Notes: No
                   Module(s): mstransform
                 Description: Updated the documentation on the use of WEIGHT_SPECTRUM.
      	
    ........
      r27819 | takeshi.nakazato | 2013-12-20 00:11:22 -0700 (Fri, 20 Dec 2013) | 16 lines
      
      
             New Development: No
                  JIRA Issue: Yes CAS-5845, CAS-4772
              Ready for Test: Yes
           Interface Changes: Yes
      What Interface Changed: task description updated
               Test Programs: help(sdcal)/help(sdcal2)
        Put in Release Notes: Yes/No
                   Module(s): Module Names change impacts.
                 Description: Describe your changes here...
      
      Updated task description.
      
      	
    ........
      r27842 | dave.mehringer | 2013-12-20 11:23:48 -0700 (Fri, 20 Dec 2013) | 14 lines
      
      
             New Development: Yes/No
                  JIRA Issue: CAS-5735 
              Ready for Test: Yes
           Interface Changes: No
      What Interface Changed: Please list interface changes
               Test Programs: test_msmd
        Put in Release Notes: Yes/No
                   Module(s): Module Names change impacts.
                 Description: bug fix
      
      	
    ........
      r27848 | george.moellenbrock | 2013-12-20 16:43:57 -0700 (Fri, 20 Dec 2013) | 22 lines
      
      
             New Development: No
                  JIRA Issue: CAS-5929
              Ready for Test: Yes
           Interface Changes: No
      What Interface Changed: 
               Test Programs: 
        Put in Release Notes: No
                   Module(s): recipes
                 Description: Added average-weightless version of adjustweights
      			that scales weights by 2*dt*df/nchan to enable
      			continuum imaging of mixed-mode spws with optimum
      			sensitivity in the spw combination.  adjustweights2
      			improves on adjustweights by generating the formally
      			exact correction, rather than relying on relaive
      			average weights (which could be skewed by relative
      			scale in the prior calibration of the weights).
      
      			See CAS-5929 for more details.
    ........
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@27852 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/recipes/weights.py
+++ gcwrap/python/scripts/recipes/weights.py
@@ -11,14 +11,17 @@
 # help scaleweights
 # help getavweight
 # help abschanwidth
+# help getnch
 # help adjustweights
+# help adjustweights2
 #
 # DP, Dec 2013
+# GM, 2013Dec19  added chbw/nchan version (adjustweights2)
 
 import taskinit
 import numpy
 
-def scaleweights(vis="", field=[], spw="", scale=1.):
+def scaleweights(vis="", field=[], spw="", scale=1., dotime=False):
 
     """
     Usage: scaleweights(vis, field, spw, scale)
@@ -41,6 +44,9 @@ def scaleweights(vis="", field=[], spw="", scale=1.):
     w = mytb.getcol("WEIGHT")
     dd = mytb.getcol("DATA_DESC_ID")
     ff = mytb.getcol("FIELD_ID")
+    tt =[]
+    if (dotime):
+        tt=mytb.getcol('EXPOSURE')
     mytb.close()
 
     mytb.open(myvis+"/DATA_DESCRIPTION")
@@ -64,7 +70,9 @@ def scaleweights(vis="", field=[], spw="", scale=1.):
                 changes += 1
                 for i in range(0, len(w)):
                     w[i][row] *= myscale
-
+                    if dotime:
+                        w[i][row] *= (2*tt[row])  # include 2 factor, too
+            
         mytb.open(myvis, nomodify=False)
         mytb.putcol("WEIGHT", w)
         mytb.close()
@@ -161,6 +169,32 @@ def abschanwidth(vis="", spw=""):
 
     return numpy.fabs(mychw)
 
+def getnch(vis="", spw=""):
+
+    """
+    Usage: getnch(vis, spw)
+           Get the nchan for the given spw.
+           Returns 0 upon error.
+    """
+
+    if(vis=="" or spw==""):
+        print "Usage: abschanwidth(vis, spw)"
+        return 0
+
+    myvis = vis
+    myspw = spw
+    mytb=taskinit.tbtool()
+    
+    mytb.open(myvis+"/SPECTRAL_WINDOW")
+    if(spw>=mytb.nrows() or spw<0):
+        print "Error: spw out of range. Min is 0. Max is ", mytb.nrows()-1
+        return 0
+        
+    mynch = mytb.getcell("NUM_CHAN",spw)
+    mytb.close()
+
+    return numpy.abs(mynch)
+
 
 def adjustweights(vis="", field="", refspws=[], spws=[]):
 
@@ -241,6 +275,63 @@ def adjustweights(vis="", field="", refspws=[], spws=[]):
         myscale = ravweight/avweights[i]*cws[i]/refcw
         print "Scale factor for weights in spw ", spws[i], " is ", myscale
         scaleweights(myvis, [myfield], spws[i], myscale)
+
+    print "Done."
+
+    return True
+
+
+def adjustweights2(vis="", field="", spws=[]):
+
+    """
+       Usage: adjustweights2(vis, field, spws)
+              Scale the weights in specified spws by a factor
+              2*df*dt/nchan, where df is the channel bandwidth,
+              dt is the integration time, and nchan is the number
+              of channels in the spw.  This enables imaging of
+              mixed mode spws in CASA v4.2 and earlier.  (Note
+              that there will be no net effect if the spws
+              share the same df, dt, and nchan.
+
+              spws are of type list,
+              field should be given as field id.
+    """
+
+    myvis = vis
+    myfield = int(field)
+    mytb=taskinit.tbtool()
+    
+    if (vis=="" or myfield=="" or spws==[] or not type(spws)==list):
+        print "Usage: adjustweights2(vis, field, spws)"
+        print "       spws are of type list,"
+        print "       field should be given as field id"
+        return False
+
+    # get avweight and chanwidth from spws
+    chw = []
+    nch = []
+    for spw in spws:
+        cw = abschanwidth(myvis, spw)
+        if cw==0:
+            print "Error reading channel width of spw ", spw
+            return False
+        chw.append(cw)
+
+
+        nc = getnch(myvis, spw)
+        if(nc==0):
+            print "Error: nch of spw ", spw, " is zero (could also mean no data)."
+            return False    
+        nch.append(nc)
+
+        print "Spw ", spw, ", channelwidth", cw, ", nchan ", nc
+
+    # calculate scale factor and apply scaling to the spws
+
+    for i in range(0,len(spws)):
+        myscale = chw[i]/nch[i]
+        print "Scale factor for weights in spw ", spws[i], " is ", myscale
+        scaleweights(myvis, [myfield], spws[i], myscale, True) # include integ time
 
     print "Done."
 
