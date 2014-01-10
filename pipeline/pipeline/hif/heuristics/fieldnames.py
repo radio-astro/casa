872c99f2799af27059367daae8eef01f60cc6f83
stewart williams <sjw@roe.ac.uk>
2014-01-10 11:48:21 +0000

    Field identifier was returned as string rather than numeric ID when identically-named fields with intents other than that desired were present in MS
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@27991 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hif/heuristics/fieldnames.py
+++ pipeline/pipeline/hif/heuristics/fieldnames.py
@@ -2,13 +2,23 @@ import pipeline.infrastructure.api as api
 
 class IntentFieldnames(api.Heuristic):
     def calculate(self, ms, intent):
-        fields = ms.get_fields(intent=intent)        
-        names = set([f.name for f in fields])
+        # get all fields that have the required intent
+        fields = ms.get_fields(intent=intent)
+
+        identifiers = []
         
-        # return names if no identically-named field exists with other
-        # intents, otherwise return fields IDs. 
-        fields_with_name = ms.get_fields(name = names)
-        if len(fields_with_name) is len(fields) or len(fields) is not len(names):
-            return ','.join([str(f.id) for f in fields])
-        else:
-            return ','.join(names)
+        # Check whether each field can be uniquely identified by its name 
+        # alone, or whether it has also been observed with other intents and
+        # must therefore be referred to by numeric ID. This sometimes occurs
+        # when a field is (mistakenly) duplicated in the MS, once with 
+        # POINTING intent and again with say BANDPASS intent.
+        for field in fields:
+            with_intent = ms.get_fields(name=[field.name], intent=intent)
+            any_intent = ms.get_fields(name=[field.name])
+
+            if len(with_intent) == len(any_intent):
+                identifiers.append(field.name)
+            else:
+                identifiers.append(str(field.id))
+                
+        return ','.join(identifiers)
