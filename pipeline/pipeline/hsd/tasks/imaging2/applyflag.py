62972b183417685e9fc1293bf86a4027ddce098b
takeshi nakazato <takeshi.nakazato@nao.ac.jp>
2014-01-13 15:14:33 +0000

    Defined function to replace with apply_baseline_flag based on baseline table.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@28029 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hsd/tasks/imaging2/applyflag.py
+++ pipeline/pipeline/hsd/tasks/imaging2/applyflag.py
@@ -61,13 +61,16 @@ class SDApplyFlag(common.SingleDishTaskTemplate):
                     continue
 
                 # apply baseline flags to the data
+                data = context.observing_run[index]
                 spwid = member.spw
                 namer.spectral_window(spwid)
                 namer.asdm(common.asdm_name(data))
                 namer.antenna_name(data.antenna.name)
                 bltable_name = namer.get_filename()
                 filename = data.name
+                srctype = data.calibration_strategy['srctype']
                 self._apply_baseline_flags(filename, bltable_name)
+                #self._apply_baseline_flag2(filename, index, spwid, srctype)
 
             
         result = SDApplyFlagResults(task=self.__class__,
@@ -100,6 +103,25 @@ class SDApplyFlag(common.SingleDishTaskTemplate):
         job = casa_tasks.sdflag2(**args)
         self._executor.execute(job, merge=False)
 
+    def _apply_apriori_flags2(self, filename, antenna, spwid, on_source):
+        context = self.inputs.context
+        datatable = context.observing_run.datatable_instance
+        datatable_name = datatable.plaintable
+        tb = datatable.tb1
+        taqlstring = 'USING STYLE PYTHON SELECT RO.ROW FROM "%s" RO, "%s" RW WHERE ANTENNA==%s && IF==%s && SRCTYPE==%s && FLAG_SUMMARY==0'\
+            % (os.path.join(datatable_name, 'RO'), os.path.join(datatable_name, 'RW'), antenna, spwid, on_source)
+        tx = tb.taql(taqlstring)
+        rows = tx.getcol('ROW')
+        tx.close()
+        
+        if len(rows) == 0:
+            return
+        
+        args = {'infile': filename,
+                'mode': 'rowid',
+                'rows': list(rows)}
+        job = casa_tasks.sdflag2(**args)
+        self._executor.execute(job, merge=False)
 
     def _apply_baseline_flags(self, filename, bltable_name):
         if not os.path.exists(bltable_name):
