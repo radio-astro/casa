107d5cf0f804ce62a9ec49600f70633b0a42f119
stewart williams <sjw@roe.ac.uk>
2014-01-14 14:22:09 +0000

    Fix contextname vs filename inconsistency and typo in example call.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@28045 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/h/cli/task_h_resume.py
+++ pipeline/pipeline/h/cli/task_h_resume.py
@@ -3,8 +3,8 @@ from taskinit import casalog
 import pipeline.h.cli.cli as cli
 import pipeline.infrastructure.launcher as launcher
 
-def h_resume(contextname=None):
-    pipeline = launcher.Pipeline(context=contextname)
+def h_resume(filename=None):
+    pipeline = launcher.Pipeline(context=filename)
     
     cli.stack[cli.PIPELINE_NAME] = pipeline
 
