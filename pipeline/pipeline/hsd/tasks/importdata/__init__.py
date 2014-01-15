2f5b31bcef98e872bfcd182a897ed7617691c0dd
takeshi nakazato <takeshi.nakazato@nao.ac.jp>
2014-01-15 15:11:24 +0000

    Defined qa analysis for hsd_importdata, which is essentially same as interferomery side.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@28080 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hsd/tasks/importdata/__init__.py
+++ pipeline/pipeline/hsd/tasks/importdata/__init__.py
@@ -1,3 +1,8 @@
 from __future__ import absolute_import
+import pipeline.infrastructure.pipelineqa as pipelineqa
 
 from .importdata import SDImportData
+from . import qa
+
+pipelineqa.registry.add_handler(qa.SDImportDataQAHandler())
+pipelineqa.registry.add_handler(qa.SDImportDataListQAHandler())
