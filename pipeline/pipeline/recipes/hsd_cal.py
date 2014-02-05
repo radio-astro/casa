ba9e8c77f944470bea73d169e19d9f1ecc92b8c7
takeshi nakazato <takeshi.nakazato@nao.ac.jp>
2014-02-05 01:53:41 +0000

    Added Python version of standard procedure for hsd science (hsd.py) and hsd calibrator (hsd_cal.py).
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@28418 ac9c0da5-131f-0410-9009-a9c195f183e6

---
+++ pipeline/pipeline/recipes/hsd_cal.py
@@ -0,0 +1,91 @@
+# General imports
+
+import os
+import sys
+import traceback
+import inspect
+
+# Make sure CASA exceptions are rethrown
+try:
+    if not  __rethrow_casa_exceptions:
+        def_rethrow = False
+    else:
+        def_rethrow = __rethrow_casa_exceptions
+except:
+    def_rethrow = False
+
+__rethrow_casa_exceptions = True
+
+#myf=sys._getframe(len(inspect.stack())-1).f_globals
+#try:
+    #default__rethrow_casa_exceptions = myf['__rethrow_casa_exceptions']
+#except Exception, e:
+    #default__rethrow_casa_exceptions = False
+    #myf['__rethrow_casa_exceptions'] = True
+
+# Setup paths
+sys.path.insert (0, os.path.expandvars("$SCIPIPE_HEURISTICS"))
+execfile(os.path.join( os.path.expandvars("$SCIPIPE_HEURISTICS"), "pipeline/h/cli/mytasks.py"))
+execfile(os.path.join( os.path.expandvars("$SCIPIPE_HEURISTICS"), "pipeline/hif/cli/mytasks.py"))
+execfile(os.path.join( os.path.expandvars("$SCIPIPE_HEURISTICS"), "pipeline/hifa/cli/mytasks.py"))
+execfile(os.path.join( os.path.expandvars("$SCIPIPE_HEURISTICS"), "pipeline/hsd/cli/mytasks.py"))
+
+# Pipeline imports
+import pipeline.infrastructure.casatools as casatools
+
+IMPORT_ONLY = 'Import only'
+
+# Run the procedure
+def hsd_cal (vislist, importonly=False, pipelinemode='automatic', interactive=True):
+
+    echo_to_screen = interactive
+    casatools.post_to_log ("Beginning pipeline run ...")
+
+    try:
+        # Initialize the pipeline
+        h_init()
+
+        # Load the data
+        hsd_importdata (infiles=vislist, pipelinemode=pipelinemode)
+        if importonly:
+           raise Exception(IMPORT_ONLY)
+    
+        # Inspect data
+        hsd_inspectdata (pipelinemode=pipelinemode)
+    
+        # Compute the system temperature calibration
+        hsd_caltsys (pipelinemode=pipelinemode)
+
+        # Compute the sky calibration
+        hsd_calsky (pipelinemode=pipelinemode)
+        
+        # Apply the calibrations
+        hsd_applycal (pipelinemode=pipelinemode)
+    
+        # Imaging
+        hsd_imaging (pipelinemode=pipelinemode)
+
+        # Export the data
+        hsd_exportdata(pipelinemode=pipelinemode)
+    
+    except Exception, e:
+        if str(e) == IMPORT_ONLY:
+            casatools.post_to_log ("Exiting after import step ...",
+	        echo_to_screen=echo_to_screen)
+        else:
+            casatools.post_to_log ("Error in procedure execution ...",
+	        echo_to_screen=echo_to_screen)
+            errstr = traceback.format_exc()
+            casatools.post_to_log (errstr, echo_to_screen=echo_to_screen)
+
+    finally:
+
+        # Save the results to the context
+        h_save()
+
+        casatools.post_to_log ("Terminating procedure execution ...",
+                               echo_to_screen=echo_to_screen)
+
+        # Restore previous state
+        __rethrow_casa_exceptions = def_rethrow
+
