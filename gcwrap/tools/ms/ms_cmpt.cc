558c048b145f93d0e143ddf775ecf215ba802908
dave mehringer <dmehring@zia.aoc.nrao.edu>
2014-03-05 15:51:06 +0000

    New Development: Yes/No             JIRA Issue: CAS-3476         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: updates for corresponding casacore interface changes
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@28766 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/tools/ms/ms_cmpt.cc
+++ gcwrap/tools/ms/ms_cmpt.cc
@@ -34,6 +34,7 @@
 #include <sys/wait.h>
 #include <casa/BasicSL/String.h>
 #include <casa/Exceptions/Error.h>
+#include <casa/IO/STLIO.h>
 #include <ms_cmpt.h>
 #include <msmetadata_cmpt.h>
 #include <tools/ms/Statistics.h>
