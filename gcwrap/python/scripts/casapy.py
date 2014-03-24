f724288e3fb1a339fdc8474f1129f72ffc9cf996
darrell schiebel <ds@nrao.edu>
2014-03-24 18:55:42 +0000

    New Development: no             JIRA Issue: no       Ready to Release: yes      Interface Changes: none What Interface Changed: none          Test Programs: none   Put in Release Notes: no
               Description: my previous fix allows xvfb processes to hang around (because they are in an orphaned process group)
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@28979 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/casapy.py
+++ gcwrap/python/scripts/casapy.py
@@ -57,7 +57,8 @@ if os.fork( ) == 0 :
         except:
             break
         time.sleep(3)
-    time.sleep(6)
+    os.killpg(ppid, signal.SIGTERM)
+    time.sleep(120)
     os.killpg(ppid, signal.SIGKILL)
     sys.exit(1)
 
