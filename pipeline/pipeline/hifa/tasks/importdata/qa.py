9c1cc2587376d153501e5bf4d83908dc14025e7c
lindsey davis <ldavis@nrao.edu>
2014-04-01 21:54:49 +0000

    Added support for polarizatioin intent checking.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@29099 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifa/tasks/importdata/qa.py
+++ pipeline/pipeline/hifa/tasks/importdata/qa.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 import pipeline.hif.tasks.importdata.importdata as importdata
 import pipeline.infrastructure.logging as logging
+import pipeline.infrastructure.utils as utils
 import pipeline.infrastructure.pipelineqa as pqa
 import pipeline.qa.scorecalculator as qacalc
 from . import almaimportdata
@@ -16,7 +17,17 @@ class ALMAImportDataQAHandler(pqa.QAResultHandler):
     
     def handle(self, context, result):
         # replace this with results of calls to ALMA-specific functions in qacalc
-        score = pqa.QAScore(0.1, longmsg='Hello from ALMA-specific QA', shortmsg='ALMA QA') 
-        scores = [score]
+        #score = pqa.QAScore(0.1, longmsg='Hello from ALMA-specific QA', shortmsg='ALMA QA') 
+        #scores = [score]
+	score1 = self._check_polintents(result.mses)
+
+	scores = [score1]
 
         result.qa.pool.extend(scores)
+
+    def _check_polintents(self, mses):
+        '''
+	Check each measurement set for polarization inents
+	'''
+	return qacalc.score_polintents(mses)
+
