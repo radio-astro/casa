1ee8585c54b0480cf5bb912253505f5921f42cb6
dave mehringer <dmehring@zia.aoc.nrao.edu>
2014-04-08 12:56:40 +0000

    New Development: Yes/No             JIRA Issue: CAS-6134         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: test to verify CAS-6134 fix
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@29198 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/tests/test_specfit.py
+++ gcwrap/python/scripts/tests/test_specfit.py
@@ -404,6 +404,46 @@ class specfit_test(unittest.TestCase):
             for im in solims:
                 self.checkImage(im, datapath + im)
                 shutil.rmtree(im)
+                
+    def test_4_5(self):
+        """writing solution images for multipixel, two gaussian fit with mask - CAS-6134"""
+        imagename = twogauss
+        outfile = "CAS6134_in.im"
+        myia = iatool()
+        myia.open(imagename)
+        subim = myia.subimage(outfile=outfile)
+        myia.done()
+        cc = subim.getchunk()
+        cc[5,5,:,:] = 1e9
+        subim.putchunk(cc)
+        # so we have a mask in the output
+        subim.calcmask(outfile + "<1e8")
+        subim.done()
+        imagename = outfile
+        box = ""
+        region = ""
+        chans = ""
+        stokes = ""
+        axis = 2
+        mask = ""
+        ngauss = 2
+        poly = -1
+        multifit = True
+        model = ""
+        residual = ""
+        [
+            amp, amperr, center, centererr,
+            fwhm, fwhmerr, integral, integralerr
+        ] = solims
+        for code in [run_fitprofile, run_specfit]:
+            res = code(
+                imagename, box, region, chans,
+                stokes, axis, mask, ngauss, poly,
+                multifit, model, residual, amp,
+                amperr, center, centererr, fwhm, fwhmerr,
+                integral, integralerr
+            )
+            # running successfully validates that the fix worked
             
     def test_5(self):
         """test results of multi-pixel one gaussian fit with estimates file"""
