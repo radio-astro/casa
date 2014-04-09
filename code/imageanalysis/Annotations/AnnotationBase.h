1baaad31ceff5263dd413755011b4fbbc7cc1e1c
dave mehringer <dmehring@zia.aoc.nrao.edu>
2014-04-09 18:40:07 +0000

    New Development: Yes/No             JIRA Issue: CAS-5725         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: bug fix for rotating ellipse from one frame to another
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@29225 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/imageanalysis/Annotations/AnnotationBase.h
+++ code/imageanalysis/Annotations/AnnotationBase.h
@@ -390,6 +390,8 @@ protected:
 	// return a string representing a pixel value, precision 1.
 	static String _printPixel(const Double& d);
 
+	MDirection::Types _getDirectionRefFrame() const { return _directionRefFrame; }
+
 private:
 	Type _type;
 	MDirection::Types _directionRefFrame;
