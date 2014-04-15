62f9a09d3a8c6d4936cd70ce502cc6034ec29e52
john lightfoot <john.lightfoot@stfc.ac.uk>
2014-04-15 10:20:59 +0000

    bug fix
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@29318 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifa/tasks/tsysflagspectra/__init__.py
+++ pipeline/pipeline/hifa/tasks/tsysflagspectra/__init__.py
@@ -1,9 +1,9 @@
 from __future__ import absolute_import
 import pipeline.infrastructure.pipelineqa as pipelineqa
 
-from .tsysflag import Tsysflag
-from .tsysflagchans import Tsysflagchans
-from . import qa
+from .tsysflagspectra import Tsysflagspectra
+from pipeline.hifa.tasks.tsysflag import qa
+from pipeline.hifa.tasks.tsysflag import resultobjects
 
 pipelineqa.registry.add_handler(qa.TsysflagQAHandler())
 pipelineqa.registry.add_handler(qa.TsysflagListQAHandler())
