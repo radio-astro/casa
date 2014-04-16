8b0e1a338945f62f4ca794e8efc77cabd492dcf0
kana sugimoto <kana.sugi@nao.ac.jp>
2014-04-16 08:07:50 +0000

    New Development: Yes             JIRA Issue: No (work for new SD module)         Ready for Test: Yes      Interface Changes: No What Interface Changed:          Test Programs: cmake code with definition of -DLIBSAKURA_ROOT   Put in Release Notes: No              Module(s): casa-sakura            Description: Enabled specifying ROOT directory to search LIBSAKURA package by variable LIBSAKURA_ROOT. example: cmake -DLIBSAKURA_ROOT=/root/path/to/libsakura ..
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project-singledish-ms@29335 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/install/FindLIBSAKURA.cmake
+++ code/install/FindLIBSAKURA.cmake
@@ -1,4 +1,18 @@
-set(libsakura_install_prefix "$ENV{HOME}/workspace/libsakura/build/installed")
+# - Find libsakura
+#
+# This script sets the following variables:
+# LIBSAKURA_FOUND - true if libsakura is found
+# LIBSAKURA_LIBRARIES - list of libraries to use libsakura package
+# LIBSAKURA_INCLUDE_DIRS - a list of include directories for header files. 
+# LIBSAKURA_EXE_LINKER_FLAGS - linker flags used to create executables.
+#
+IF(DEFINED LIBSAKURA_ROOT)
+  set(libsakura_install_prefix "${LIBSAKURA_ROOT}")
+ELSE(DEFINED LIBSAKURA_ROOT)
+  # default for NAOJ servers
+  set(libsakura_install_prefix "$ENV{HOME}/workspace/libsakura/build/installed")
+ENDIF(DEFINED LIBSAKURA_ROOT)
+
 find_path(LIBSAKURA_INCLUDE_DIR libsakura/sakura.h PATHS ${libsakura_install_prefix}/include)
 find_library(LIBSAKURA_LIBRARY NAMES sakura PATHS ${libsakura_install_prefix}/lib)
 
