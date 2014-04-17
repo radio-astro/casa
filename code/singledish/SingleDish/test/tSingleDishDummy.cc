74f027528c1819339e265abdcc9e914a6f1fe676
wataru kawasaki <wataru.kawasaki@nao.ac.jp>
2014-04-17 11:40:21 +0000

    New Development: Yes/No             JIRA Issue: No/Yes List JIRA ticket.         Ready for Test: Yes/No      Interface Changes: Yes/No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: add a dummy class SingleDishDummy to be compiled to libsingledish.so.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project-singledish-ms@29359 ac9c0da5-131f-0410-9009-a9c195f183e6

---
+++ code/singledish/SingleDish/test/tSingleDishDummy.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <singledish/SingleDish/SingleDishDummy.cc>
+
+int main(int argc, char *argv[]) {
+
+  // Just a dummy method
+  casa::SingleDishDummy::DummyMethod();
+  
+  return 0;
+}
