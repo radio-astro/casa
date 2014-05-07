8e9179c98abc26614ea463355152d1734a4e7348
brian kent <bkent@nrao.edu>
2014-05-07 13:45:09 +0000

    Added QA for VLA Setjy
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@29600 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifv/tasks/setmodel/__init__.py
+++ pipeline/pipeline/hifv/tasks/setmodel/__init__.py
@@ -1,5 +1,14 @@
 from __future__ import absolute_import
+import pipeline.infrastructure.pipelineqa as pipelineqa
+import pipeline.infrastructure.renderer.qaadapter as qaadapter
+from pipeline.hif.tasks.common import commonfluxresults
+
 
 from .vlasetjy import VLASetjy
 from .setmodel import SetModel
 from .fluxgains import Fluxgains
+from . import qa
+
+pipelineqa.registry.add_handler(qa.VLASetjyQAHandler())
+pipelineqa.registry.add_handler(qa.VLASetjyListQAHandler())
+qaadapter.registry.register_to_calibration_topic(commonfluxresults.FluxCalibrationResults)
