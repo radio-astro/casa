e644a37627b2c9f5a7fdcb42a863b97c6e16d73d
takeshi nakazato <takeshi.nakazato@nao.ac.jp>
2014-05-13 00:24:03 +0000

    New Development: No             JIRA Issue: No         Ready for Test: No      Interface Changes: Yes/No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: Describe your changes here...
    Added ssdreduce for interface discussion. 
    Its implementation is to print locals() to stdout.
    
    	
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project-singledish-ms@29668 ac9c0da5-131f-0410-9009-a9c195f183e6

---
+++ gcwrap/python/scripts/task_ssdreduce.py
@@ -0,0 +1,16 @@
+from taskinit import casalog
+
+
+def ssdreduce(vis,
+             field, spw,
+             selectdata, timerange, antenna, scan, pol, observaiton, msselect,
+             gaintable, interp, spwmap,
+             maskmode, thresh, avg_limit, edge, blmask,
+             blfunc, order, npiece, applyfft, fftmethod, fftthresh, addwn,
+             rejwn, clipthresh, clipniter,
+             bloutput, blformat,
+             clipminmax, clipoutside,
+             kernel, kwidth, usefft, interpflag,
+             statmask, stoutput, stformat):
+             
+    print locals()
