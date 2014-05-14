418c0eda378a05f87c29f193447e857e410b6a86
john lightfoot <john.lightfoot@stfc.ac.uk>
2014-05-14 12:22:42 +0000

    sharps,sharps2 becomes sharps
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@29703 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hif/cli/task_hif_bpflagchans.py
+++ pipeline/pipeline/hif/cli/task_hif_bpflagchans.py
@@ -8,7 +8,7 @@ import pipeline.h.cli.utils as utils
 def hif_bpflagchans(caltable=None,
   flag_edges=None, edge_limit=None,
   flag_sharps=None, sharps_limit=None,
-  flag_sharps2=None, sharps2_limit=None,
+  flag_hilo=None, fhl_limit=None, fhl_minsample=None,
   flag_diffmad=None, diffmad_limit=None, diffmad_nchan_limit=None,
   flag_tmf=None, tmf_frac_limit=None, tmf_nchan_limit=None,
   pipelinemode=None, dryrun=None, acceptresults=None):
