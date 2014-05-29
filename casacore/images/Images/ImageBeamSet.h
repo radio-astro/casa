1e1909d3cdcdf618081aa57e14a85e07dc8808e1
dave mehringer <dmehring@zia.aoc.nrao.edu>
2014-05-29 18:05:21 +0000

    New Development: Yes/No             JIRA Issue: CAS-6569         Ready for Test: Yes      Interface Changes: Yes What Interface Changed: implemented ImageBeamSet::summary()          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: as above by moving private method from ImageSummary to ImageBeamSet::summary()
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@29903 ac9c0da5-131f-0410-9009-a9c195f183e6

--- casacore/images/Images/ImageBeamSet.h
+++ casacore/images/Images/ImageBeamSet.h
@@ -34,6 +34,8 @@
 
 namespace casa {
 
+class SpectralCoordinate;
+
 class CoordinateSystem;
 
 // <summary>
@@ -263,6 +265,9 @@ public:
 	Record toRecord() const;
 	//</group>
 
+	// If verbose, log all beams, if not just summarize beam stats.
+	void summarize(LogIO& log, Bool verbose, const CoordinateSystem& csys) const;
+
 private:
 
 	static const String _DEFAULT_AREA_UNIT;
@@ -281,6 +286,16 @@ private:
 		Double pa, Double xScaleFactor, Double yScaleFactor
 	);
 
+	static void _chanInfoToStream(
+		ostream& os, const SpectralCoordinate *const &spCoord,
+		const uInt chan, const uInt chanWidth, const uInt freqPrec,
+		const uInt velWidth, const uInt velPrec
+	);
+
+	static void _beamToStream(
+		ostream& os, const GaussianBeam& beam,
+		const Unit& unit
+	);
 };
 
 ostream &operator<<(ostream &os, const ImageBeamSet& beamSet);
