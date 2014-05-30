c50781cc1ecc61da92cec3196d37357113d16a41
brian kent <bkent@nrao.edu>
2014-05-30 19:09:16 +0000

    Changed linear to linearflag
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@29921 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifv/tasks/finalcals/finalcals.py
+++ pipeline/pipeline/hifv/tasks/finalcals/finalcals.py
@@ -103,7 +103,7 @@ class Finalcals(basetask.StandardTaskTemplate):
         
         #Force calwt for the bp table to be False
         calto = callibrary.CalTo(self.inputs.vis)
-        calfrom = callibrary.CalFrom(bpcaltable, interp='linearperobs,linear', calwt=True)
+        calfrom = callibrary.CalFrom(bpcaltable, interp='linearperobs,linearflag', calwt=True)
         context.callibrary._remove(calto, calfrom, context.callibrary._active)
         
         calto = callibrary.CalTo(self.inputs.vis)
