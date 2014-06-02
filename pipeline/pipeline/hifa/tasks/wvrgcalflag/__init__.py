f999613f7853a35e5d38fa115c040d7d464bebfd
lindsey davis <ldavis@nrao.edu>
2014-06-02 21:58:54 +0000

    Removed the Flagging topics handler
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@29938 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifa/tasks/wvrgcalflag/__init__.py
+++ pipeline/pipeline/hifa/tasks/wvrgcalflag/__init__.py
@@ -9,5 +9,5 @@ from . import resultobjects
 pipelineqa.registry.add_handler(qa.WvrgcalflagQAHandler())
 pipelineqa.registry.add_handler(qa.WvrgcalflagListQAHandler())
 # wvrcalflag does both calibration and flagging?
-qaadapter.registry.register_to_flagging_topic(resultobjects.WvrgcalflagResult)
+#qaadapter.registry.register_to_flagging_topic(resultobjects.WvrgcalflagResult)
 qaadapter.registry.register_to_calibration_topic(resultobjects.WvrgcalflagResult)
