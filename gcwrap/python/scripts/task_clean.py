d8ec4eb0ce617b07c418951aea00257de0c42ce1
sanjay bhatnagar <sbhatnag@nrao.edu>
2014-06-06 15:59:10 +0000

    New Development: No             JIRA Issue: No         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes              Module(s): Module Names change impacts.            Description: Describe your changes here...
    Allow advancedaprojection option from clean task.
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@30007 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/task_clean.py
+++ gcwrap/python/scripts/task_clean.py
@@ -21,8 +21,8 @@ def clean(vis, imagename,outlierfile, field, spw, selectdata, timerange,
     casalog.origin('clean')
     casalog.post('nchan='+str(nchan)+' start='+str(start)+' width='+str(width))  
     #If using new FT-Machines, do not use the on-the-fly model_data columns.
-    if (gridmode == 'advancedaprojection'):
-        raise Exception, 'This mode is not yet ready for use'
+    # if (gridmode == 'advancedaprojection'):
+    #     raise Exception, 'This mode is not yet ready for use'
 
         
     if gridmode == 'advancedaprojection' and usescratch==False:
