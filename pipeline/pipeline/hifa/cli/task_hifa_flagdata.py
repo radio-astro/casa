720baaf69462af837c36f327be980e51e8852082
lindsey davis <ldavis@nrao.edu>
2014-06-11 17:16:25 +0000

    Added hm_tbuff and tbuff to the ALMA flagging task interface.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@30076 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifa/cli/task_hifa_flagdata.py
+++ pipeline/pipeline/hifa/cli/task_hifa_flagdata.py
@@ -6,8 +6,8 @@ import pipeline.h.cli.utils as utils
 def hifa_flagdata(vis=None, autocorr=None, shadow=None, scan=None,
                  scannumber=None, intents=None, edgespw=None, fracspw=None,
                  fracspwfps=None, online=None, fileonline=None, template=None,
-                 filetemplate=None, pipelinemode=None, flagbackup=None, 
-                 dryrun=None, acceptresults=None):
+                 filetemplate=None, hm_tbuff=None, tbuff=None, pipelinemode=None,
+		 flagbackup=None, dryrun=None, acceptresults=None):
 
     # create a dictionary containing all the arguments given in the
     # constructor
