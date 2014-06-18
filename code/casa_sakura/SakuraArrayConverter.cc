88f8b0c024a21d70b3276d0b0c170ec368139cb0
shinnosuke kawakami <shinnosuke.kawakami@nao.ac.jp>
2014-06-18 00:40:09 +0000

    New Development: Yes             JIRA Issue: No         Ready for Test: Yes      Interface Changes: No What Interface Changed:          Test Programs:   Put in Release Notes: No              Module(s):            Description: modified template specialization of CASAtoSakura/SakuraToCASA
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project-singledish-ms@30174 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/casa_sakura/SakuraArrayConverter.cc
+++ code/casa_sakura/SakuraArrayConverter.cc
@@ -14,16 +14,17 @@
 #include <casa/Arrays/Cube.h>
 namespace casa {
 
-template<> void SakuraArrayConverter::ConvertCASAToSakura<Complex, float>(uInt const index_in,
-		uInt const index_out, const Complex* pin,float *output_data)
-		{
-	        output_data[index_out] = casa::real(pin[index_in]);
-		}
-
-template<> void SakuraArrayConverter::ConvertSakuraToCASA<float,Complex>(uInt const index_in,
-			uInt const index_out, float const *input_data,Complex* pout){
-	        casa::real(pout[index_out]) = input_data[index_in];
-	        casa::imag(pout[index_out]) = 0.0;
-	}
+template<> void SakuraArrayConverter::CASAToSakura<Complex, float>(
+		uInt const index_in, uInt const index_out, const Complex* pin,
+		float *output_data) {
+	output_data[index_out] = casa::real(pin[index_in]);
 }
- // End of casa namespace.
+
+template<> void SakuraArrayConverter::SakuraToCASA<float, Complex>(
+		uInt const index_in, uInt const index_out, float const *input_data,
+		Complex* pout) {
+	casa::real(pout[index_out]) = input_data[index_in];
+	casa::imag(pout[index_out]) = 0.0;
+}
+}
+// End of casa namespace.
--- code/casa_sakura/SakuraArrayConverter.h
+++ code/casa_sakura/SakuraArrayConverter.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <string>
+#include <stdexcept>
 //#include <libsakura/sakura.h>
 #include <casa/aipstype.h>
 
@@ -21,21 +22,62 @@ class SakuraArrayConverter {
 protected:
 
 public:
-	template <class T, class S> static void ConvertCASAToSakura(uInt const index_in,
-			uInt const index_out, const T* pin,S *output_data){
-	        output_data[index_out] = pin[index_in];
+	template<class T, class S> static void ConvertCASAToSakura(
+			uInt const pol_index, uInt const num_data,
+			const Matrix<T> &input_matrix, S *output_data) {
+		uInt const max_pol = input_matrix.nrow();
+		if (!(pol_index < max_pol)) {
+			throw std::invalid_argument(
+					"pol_index must less than input_matrix.nrow()");
+		}
+		if (!(num_data == input_matrix.ncolumn())) {
+			throw std::invalid_argument(
+					"num_data must equal to input_matrix.ncolumn()");
+		}
+		Bool deleteIn;
+		const T* pin = input_matrix.getStorage(deleteIn);
+		for (uInt i = 0; i < num_data; ++i) {
+			CASAToSakura(max_pol * i + pol_index, i, pin, output_data);
+		}
+		input_matrix.freeStorage(pin, deleteIn);
 	}
-	template <class S, class T> static void ConvertSakuraToCASA(uInt const index_in,
-			uInt const index_out, S const *input_data,T* pout){
-		    pout[index_out] = input_data[index_in];
+	template<class S, class T> static void ConvertSakuraToCASA(
+			uInt const pol_index, uInt const num_data, S const *input_data,
+			Matrix<T> &output_matrix) {
+		uInt const max_pol = output_matrix.nrow();
+		if (!(pol_index < max_pol)) {
+			throw std::invalid_argument(
+					"pol_index must less than input_matrix.nrow()");
+		}
+		if (!(num_data == output_matrix.ncolumn())) {
+			throw std::invalid_argument(
+					"num_data must equal to output_matrix.ncolumn()");
+		}
+		Bool deleteOut;
+		T* pout = output_matrix.getStorage(deleteOut);
+		for (uInt i = 0; i < num_data; ++i) {
+			SakuraToCASA(i, max_pol * i + pol_index, input_data, pout);
+		}
+		output_matrix.putStorage(pout, deleteOut);
 	}
+
 private:
+	template<class T, class S> static void CASAToSakura(uInt const index_in,
+			uInt const index_out, const T* pin, S *output_data) {
+		output_data[index_out] = pin[index_in];
+	}
+	template<class S, class T> static void SakuraToCASA(uInt const index_in,
+			uInt const index_out, S const *input_data, T* pout) {
+		pout[index_out] = input_data[index_in];
+	}
 };
 
-template<> void SakuraArrayConverter::ConvertCASAToSakura<Complex, float>(uInt const index_in,
-		uInt const index_out, const Complex* pin,float *output_data);
-template<> void SakuraArrayConverter::ConvertSakuraToCASA<float,Complex>(uInt const index_in,
-		uInt const index_out, float const *input_data,Complex* pout);
+template<> void SakuraArrayConverter::CASAToSakura<Complex, float>(
+		uInt const index_in, uInt const index_out, const Complex* pin,
+		float *output_data);
+template<> void SakuraArrayConverter::SakuraToCASA<float, Complex>(
+		uInt const index_in, uInt const index_out, float const *input_data,
+		Complex* pout);
 
 } //# NAMESPACE CASA - END
 
