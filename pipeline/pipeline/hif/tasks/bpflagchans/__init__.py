a41720f004a4ecf5fb7adfa0ae6c483acd8c8172
dirk muders <dmuders@mpifr-bonn.mpg.de>
2014-07-09 10:52:11 +0000

    Added QA scoring for bpflagchans.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@30418 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hif/tasks/bpflagchans/__init__.py
+++ pipeline/pipeline/hif/tasks/bpflagchans/__init__.py
@@ -1,7 +1,13 @@
 from __future__ import absolute_import
+import pipeline.infrastructure.pipelineqa as pipelineqa
 import pipeline.infrastructure.renderer.qaadapter as qaadapter
 
 from .bpflagchans import Bandpassflagchans
 from . import resultobjects
 
-qaadapter.registry.register_to_flagging_topic(resultobjects.BandpassflagResults)
+from . import qa
+
+pipelineqa.registry.add_handler(qa.BandpassQAHandler())
+pipelineqa.registry.add_handler(qa.BandpassListQAHandler())
+
+qaadapter.registry.register_to_calibration_topic(resultobjects.BandpassflagResults)
