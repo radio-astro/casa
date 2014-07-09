65d8b6206a713b5e6d8799838a926cbd9e22f0ac
jim jacobs <jjacobs@nrao.edu>
2014-07-09 23:09:06 +0000

    New Development: Yes             JIRA Issue: cas-6118         Ready for Test: Yes      Interface Changes: No What Interface Changed:          Test Programs:   Put in Release Notes: No              Module(s):            Description: Small file error which borke build.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@30433 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/msvis/MSVis/VisBufferUtil.cc
+++ code/msvis/MSVis/VisBufferUtil.cc
@@ -39,11 +39,11 @@
 #include <measures/Measures/UVWMachine.h>
 #include <measures/Measures/MeasTable.h>
 
-#include <synthesis/MSVis/VisBufferUtil.h>
-#include <synthesis/MSVis/StokesVector.h>
-#include <synthesis/MSVis/VisibilityIterator.h>
-#include <synthesis/MSVis/VisibilityIterator2.h>
-#include <synthesis/MSVis/VisBuffer.h>
+#include <msvis/MSVis/VisBufferUtil.h>
+#include <msvis/MSVis/StokesVector.h>
+#include <msvis/MSVis/VisibilityIterator.h>
+#include <msvis/MSVis/VisibilityIterator2.h>
+#include <msvis/MSVis/VisBuffer.h>
 #include <ms/MeasurementSets/MSColumns.h>
 #include <casa/iostream.h>
 
