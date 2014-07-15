3e966cbcf638ec957ed76a24a4c474471c1d54f4
wataru kawasaki <wataru.kawasaki@nao.ac.jp>
2014-07-15 12:49:43 +0000

    New Development: No             JIRA Issue: No         Ready for Test: Yes      Interface Changes: No What Interface Changed:          Test Programs:   Put in Release Notes:              Module(s): sd            Description: again commit a fix for get_taql() since the previous commit actually did no fix to suppress warning messages.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project-singledish-ms@30475 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/reductionhelper.py
+++ gcwrap/python/scripts/reductionhelper.py
@@ -167,11 +167,9 @@ class GenerateQueryHelper(object):
         self._append_taql(elem, 'ANTENNA1', '==', antenna_id)
         self._append_taql(elem, 'ANTENNA2', '==', antenna_id)
         self._append_taql(elem, 'FIELD_ID', 'IN', 'field', True)
-        if timerange is not None:
+        if timerange != '':
             taql_timerange = rhutil.select_by_timerange(self.vis, timerange)
             self._append_taql(elem, '', '', taql_timerange, True)
-        #taql_timerange = rhutil.select_by_timerange(self.vis, timerange) if timerange is not None else ''
-        #self._append_taql(elem, '', '', taql_timerange, True)
         self._append_taql(elem, 'SCAN_NUMBER', 'IN', 'scan', True)
         self._append_taql(elem, 'OBSERVATION_ID', 'IN', 'obsids', True)
         self._append_taql(elem, '', '', msselect, True)
