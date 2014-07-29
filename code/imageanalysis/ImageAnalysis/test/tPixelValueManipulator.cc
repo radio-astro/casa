40d647e087a031d4440df9f9e8dc7d59f1ad6c26
susan loveland <susan.loveland@nrao.edu>
2014-07-29 17:36:08 +0000

    New Development: No             JIRA Issue: CAS-6793         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: No              Module(s): imageanalysis            Description: Rewrote tPixelValueManipulator to reproduce problem.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@30646 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/imageanalysis/ImageAnalysis/test/tPixelValueManipulator.cc
+++ code/imageanalysis/ImageAnalysis/test/tPixelValueManipulator.cc
@@ -27,9 +27,10 @@
 
 
 #include <imageanalysis/ImageAnalysis/PixelValueManipulator.h>
-
+#include <imageanalysis/ImageAnalysis/ImageCollapserData.h>
 #include <coordinates/Coordinates/CoordinateSystem.h>
 #include <images/Images/TempImage.h>
+#include <images/Images/FITSImage.h>
 #include <casa/namespace.h>
 
 void writeTestString(const String& test) {
@@ -40,16 +41,36 @@ void writeTestString(const String& test) {
 int main() {
 	Bool ok = True;
 	try {
-		CoordinateSystem csys = CoordinateUtil::defaultCoords3D();
-		TiledShape shape(IPosition(3, 20));
-        SPCIIF image(new TempImage<Float>(shape, csys));
-        PixelValueManipulator<Float> pvm(image, NULL, "");
+	  std::tr1::shared_ptr<ImageInterface<Float> > im( new FITSImage("/home/uniblab/casa/trunk/test/Orion.methanol.cbc.contsub.image.fits"));
+	  CoordinateSystem cSys = im->coordinates();
+	  const String radUnits( "rad" );
+	  const String absStr( "abs");
+	  Record regionRecord;
+	  Int directionIndex = cSys.findCoordinate( Coordinate::DIRECTION );
+	  Vector<Int> dirPixelAxis = cSys.pixelAxes( directionIndex );
+	  RegionManager regMan;
+	  Vector<Quantity> blc(2);
+	  Vector<Quantity> trc(2);
+	  blc(0) = Quantity( 1.46277,radUnits);
+	  blc(1) = Quantity( -0.0938656, radUnits);
+	  trc(0) = Quantity( 1.46272,radUnits);
+	  trc(1) = Quantity( -0.0938005, radUnits);
+	  Vector<Int> pixax( 2);
+	  pixax(0) = 0;
+	  pixax(1) = 1;
+	  Record* imageregRecord = regMan.wbox( blc, trc, pixax, cSys, absStr, radUnits);
+
+
+	
+        PixelValueManipulator<Float> pvm(im, imageregRecord, "");
         Record ret = pvm.getProfile(
-        	2, "mean", "Hz", PixelValueManipulatorData::DEFAULT, NULL, ""
+				    //	2, "mean", "Hz", PixelValueManipulatorData::DEFAULT, NULL, ""
+				    2, ImageCollapserData::MEAN, "Hz", PixelValueManipulatorData::DEFAULT, NULL, "LSRK"
         );
         cout << ret << endl;
         ret = pvm.getProfile(
-        	2, "mean", "GHz", PixelValueManipulatorData::DEFAULT, NULL, ""
+			     //2, "mean", "GHz", PixelValueManipulatorData::DEFAULT, NULL, ""
+			     2,ImageCollapserData::MEAN,"GHz", PixelValueManipulatorData::DEFAULT, NULL, "LSRK"
         );
         cout << ret << endl;
         cout << "ok" << endl;
