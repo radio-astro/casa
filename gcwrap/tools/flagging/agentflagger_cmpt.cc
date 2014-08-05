af601c909aef0157e16aae74ae9875cb4e43c6e2
sandra castro <sbhatnag@nrao.edu>
2014-08-05 07:26:05 +0000

    New Development: No             JIRA Issue: CAS-6285         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: test_flagdata   Put in Release Notes: No              Module(s): flagging            Description: Added a MeasIERS::closeTables() to the agentflagger destructor to close the measures tables, particularly, IERSeop97.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/release-4_2_2@30714 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/tools/flagging/agentflagger_cmpt.cc
+++ gcwrap/tools/flagging/agentflagger_cmpt.cc
@@ -23,6 +23,7 @@
 #include <unistd.h>
 #include <stdcasa/StdCasa/CasacSupport.h>
 #include <tables/Tables/Table.h>
+#include <measures/Measures/MeasIERS.h>
 
 
 using namespace std;
@@ -56,6 +57,7 @@ agentflagger::~agentflagger()
 		RETHROW(x);
 	}
     Table::relinquishAutoLocks(True);
+    MeasIERS::closeTables();
 
 }
 
