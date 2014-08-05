a2a88f64d822dd9316512941d7cc62b7817cf74a
sandra castro <sbhatnag@nrao.edu>
2014-08-05 09:20:22 +0000

    New Development: No             JIRA Issue: CAS-6285         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: test_mstransform   Put in Release Notes: No              Module(s): mstransform            Description: Added a MeasIERS::closeTables() to the mstransformer destructor to close the measures tables, particularly, IERSeop97.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/release-4_2_2@30719 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/tools/mstransformer/mstransformer_cmpt.cc
+++ gcwrap/tools/mstransformer/mstransformer_cmpt.cc
@@ -15,6 +15,7 @@
 #include <unistd.h>
 #include <stdcasa/StdCasa/CasacSupport.h>
 #include <tables/Tables/Table.h>
+#include <measures/Measures/MeasIERS.h>
 
 
 using namespace std;
@@ -48,6 +49,7 @@ mstransformer::~mstransformer()
 		RETHROW(x);
 	}
     Table::relinquishAutoLocks(True);
+    MeasIERS::closeTables();
 
 }
 
