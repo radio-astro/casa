87467e3947e9783ba6917ecca4763e93f2236c10
john lightfoot <john.lightfoot@stfc.ac.uk>
2014-08-11 09:14:15 +0000

    replace getcal by _readvis in caltable getter
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@30754 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifa/tasks/tsysflag/tsysflag.py
+++ pipeline/pipeline/hifa/tasks/tsysflag/tsysflag.py
@@ -36,9 +36,9 @@ class TsysflagInputs(basetask.StandardInputs):
             result = None
             for name in caltables:
                 # Get the tsys table name
-                tsystable = caltableaccess.CalibrationTableDataFiller.getcal(
-                  name)
-                if tsystable.vis in self.vis:
+                tsystable_vis = \
+                  caltableaccess.CalibrationTableDataFiller._readvis(name)
+                if tsystable_vis in self.vis:
                     result = name
                     break
 
