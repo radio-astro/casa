de37f3b5a820779513401d92703e6e4b57617797
john lightfoot <john.lightfoot@stfc.ac.uk>
2014-08-13 12:25:17 +0000

    first version
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@30782 ac9c0da5-131f-0410-9009-a9c195f183e6

---
+++ pipeline/pipeline/hif/tasks/rawflagchans/resultobjects.py
@@ -0,0 +1,52 @@
+from __future__ import absolute_import
+import collections
+import copy
+
+import pipeline.infrastructure as infrastructure
+import pipeline.infrastructure.basetask as basetask
+
+LOG = infrastructure.get_logger(__name__)
+
+
+class RawflagchansResults(basetask.Results):
+    def __init__(self):
+        """
+        Construct and return a new RawflagchansResults.
+        """
+        super(RawflagchansResults, self).__init__()
+        # views and associated results
+        self.flagging = []
+        # following are used instead of standard dictionaries so that
+        # missing keys are created automatically as needed
+        self.view = collections.defaultdict(list)
+
+    def merge_with_context(self, context):
+        # do nothing, the tsys cal files should already be in the context
+        # and we don't want to insert them twice.
+        print 'MERGE'
+        pass
+
+    def addview(self, description, viewresult):
+        self.view[description].append(viewresult)
+
+    def addflags(self, flags):
+        self.flagging += flags
+
+    def descriptions(self):
+        return self.view.keys()
+
+    def first(self, description):
+        return copy.deepcopy(self.view[description][0])
+
+    def flagcmds(self):
+        return copy.deepcopy(self.flagging)
+
+    def flagged(self):
+        return len(self.flagging) > 0
+
+    def last(self, description):
+        return copy.deepcopy(self.view[description][-1])
+
+    def __repr__(self):
+        s = 'RawflagchansResults'
+        return s
