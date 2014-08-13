abb46331c7c960aad1f4dbdb1ea0e06e79792695
john lightfoot <john.lightfoot@stfc.ac.uk>
2014-08-13 11:32:51 +0000

    delete
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@30778 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hif/tasks/flagchans/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-import pipeline.infrastructure.renderer.qaadapter as qaadapter
-
-from .rawflagchans import Rawflagchans
-from . import resultobjects
-
-qaadapter.registry.register_to_flagging_topic(resultobjects.RawflagchansResults)
