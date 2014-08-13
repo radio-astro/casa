8183f6e779bf340a9be32897bffa9509e5c37c49
john lightfoot <john.lightfoot@stfc.ac.uk>
2014-08-13 11:21:55 +0000

    Flagchans renamed Rawflagchans
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@30772 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hif/tasks/flagchans/__init__.py
+++ pipeline/pipeline/hif/tasks/flagchans/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 import pipeline.infrastructure.renderer.qaadapter as qaadapter
 
-from .flagchans import Flagchans
+from .rawflagchans import Rawflagchans
 from . import resultobjects
 
-qaadapter.registry.register_to_flagging_topic(resultobjects.FlagchansResults)
+qaadapter.registry.register_to_flagging_topic(resultobjects.RawflagchansResults)
