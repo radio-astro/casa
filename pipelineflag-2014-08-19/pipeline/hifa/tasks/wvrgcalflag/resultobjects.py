4389a8152578834435bcc05f862323c6a268cb9a
john lightfoot <john.lightfoot@stfc.ac.uk>
2014-08-29 14:03:52 +0000

    changes for rawflagchans
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@30956 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipelineflag-2014-08-19/pipeline/hifa/tasks/wvrgcalflag/resultobjects.py
+++ pipelineflag-2014-08-19/pipeline/hifa/tasks/wvrgcalflag/resultobjects.py
@@ -9,6 +9,13 @@ class WvrgcalflagResult(wvrgcalresults.WvrgcalResult):
     def addflags(self, flags):
         self.flagging += flags
 
+    def add_flag_reason_plane(self, flag_reason_plane, flag_reason_key):
+        for description in self.descriptions():
+            self.view[description][-1].flag_reason_plane = \
+              flag_reason_plane[description]
+            self.view[description][-1].flag_reason_key = \
+              flag_reason_key
+
     def addview(self, description, viewresult):
         self.view[description].append(viewresult)
 
