4155b0364b21eeb095b4532e41c88dcb4d945533
john lightfoot <john.lightfoot@stfc.ac.uk>
2014-08-29 14:04:57 +0000

    changes for rawflagchans
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@30958 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipelineflag-2014-08-19/pipeline/hif/tasks/rawflagchans/__init__.py
+++ pipelineflag-2014-08-19/pipeline/hif/tasks/rawflagchans/__init__.py
@@ -1,7 +1,18 @@
 from __future__ import absolute_import
+import pipeline.infrastructure.pipelineqa as pipelineqa
 import pipeline.infrastructure.renderer.qaadapter as qaadapter
 
 from .rawflagchans import Rawflagchans
 from . import resultobjects
 
+from pipeline.hif.tasks.rawflagchans import qa
+
+pipelineqa.registry.add_handler(qa.RawflagchansQAHandler())
+pipelineqa.registry.add_handler(qa.RawflagchansListQAHandler())
 qaadapter.registry.register_to_flagging_topic(resultobjects.RawflagchansResults)
+
+
+
+
+
+
