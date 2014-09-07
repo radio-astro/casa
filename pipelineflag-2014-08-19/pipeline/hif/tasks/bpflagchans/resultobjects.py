6e89b6e37c8cac08aef58e42c9f0a38b111216e3
john lightfoot <john.lightfoot@stfc.ac.uk>
2014-09-07 09:08:13 +0000

    add_flag_reason_plane added
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@31012 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipelineflag-2014-08-19/pipeline/hif/tasks/bpflagchans/resultobjects.py
+++ pipelineflag-2014-08-19/pipeline/hif/tasks/bpflagchans/resultobjects.py
@@ -35,6 +35,13 @@ class BandpassflagResults(basetask.Results):
     def addflags(self, flags):
         self.flagging += flags
 
+    def add_flag_reason_plane(self, flag_reason_plane, flag_reason_key):
+        for description in self.descriptions():
+            self.view[description][-1].flag_reason_plane = \
+              flag_reason_plane[description]
+            self.view[description][-1].flag_reason_key = \
+              flag_reason_key
+
     def descriptions(self):
         return self.view.keys()
 
