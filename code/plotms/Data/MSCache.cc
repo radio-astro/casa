060631b7bd2c5d98e57ead61a502f9082ec13e9c
susan loveland <susan.loveland@nrao.edu>
2014-09-10 16:36:12 +0000

    New Development: No             JIRA Issue: CAS-6753         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: No              Module(s): plotms            Description: Plotms not closing table when there is insufficient memory to generate a plot.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@31047 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/plotms/Data/MSCache.cc
+++ code/plotms/Data/MSCache.cc
@@ -424,6 +424,14 @@ void MSCache::trapExcessVolume(map<PMS::Axis,Bool> pendingLoadAxes) {
 		// catch detected volume excess, clear the existing cache, and rethrow
 		logLoad(log.getMesg());
 		clear();
+
+		//Close any open tables.
+		if (rvi_p)
+				delete rvi_p;
+		wvi_p=NULL;
+		rvi_p=NULL;
+
+
 		stringstream ss;
 		ss << "Please try selecting less data or averaging and/or" << endl
 				<< " 'force reload' (to clear unneeded cache items) and/or" << endl
