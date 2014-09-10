5ba93417d2a35b2ef0b860f7b40560cc34565597
lindsey davis <ldavis@nrao.edu>
2014-09-10 21:24:28 +0000

    Added the QA scoring routines for the spwphaseup task.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@31051 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifa/tasks/spwphaseup/__init__.py
+++ pipeline/pipeline/hifa/tasks/spwphaseup/__init__.py
@@ -1,5 +1,12 @@
 from __future__ import absolute_import
+import pipeline.infrastructure.pipelineqa as pipelineqa
+import pipeline.infrastructure.renderer.qaadapter as qaadapter
 
 from .spwphaseup import SpwPhaseup
+from . import qa
+
+pipelineqa.registry.add_handler(qa.SpwPhaseupQAHandler())
+pipelineqa.registry.add_handler(qa.SpwPhaseupListQAHandler())
+qaadapter.registry.register_to_calibration_topic(spwphaseup.SpwPhaseupResults)
 
 
---
+++ pipeline/pipeline/hifa/tasks/spwphaseup/qa.py
@@ -0,0 +1,57 @@
+from __future__ import absolute_import
+
+import pipeline.infrastructure.logging as logging
+import pipeline.infrastructure.utils as utils
+import pipeline.infrastructure.pipelineqa as pqa
+import pipeline.qa.scorecalculator as qacalc
+
+LOG = logging.get_logger(__name__)
+
+from . import spwphaseup
+
+
+class SpwPhaseupQAHandler(pqa.QAResultHandler):
+    result_cls = spwphaseup.SpwPhaseupResults
+    child_cls = None
+    generating_task = spwphaseup.SpwPhaseup
+
+    def handle(self, context, result):
+
+        vis= result.inputs['vis']
+	ms = context.observing_run.get_ms(vis)
+
+	# Check for the frequency of narrow to wide mapping vs one to 
+	# one mapping
+        score1 = self._phaseup_mapping_fraction(ms, result.inputs['field'],
+	    result.inputs['intent'], result.phaseup_spwmap)
+        scores = [score1]
+	    
+        result.qa.pool.extend(scores)
+    
+    def _phaseup_mapping_fraction(self, ms, field, intent, phaseup_swpmap):
+        '''
+        Check whether or not there has been spw phaseup mapping . 
+        '''
+        return qacalc.score_phaseup_mapping_fraction(ms, field, intent, phaseup_spwmap)
+
+class SpwPhaseupListQAHandler(pqa.QAResultHandler):
+    """
+    QA handler for a list containing SpwPhaseupResults.
+    """
+    result_cls = list
+    child_cls = spwphaseup.SpwPhaseup
+    generating_task = spwphaseup.SpwPhaseup
+
+    def handle(self, context, result):
+        # collate the QAScores from each child result, pulling them into our
+        # own QAscore list
+        collated = utils.flatten([r.qa.pool for r in result])
+        result.qa.pool[:] = collated
+
+        mses = [r.inputs['vis'] for r in result]
+        longmsg = 'No mapped narrow spws in %s' % utils.commafy(mses,
+                                                                    quotes=False,
+                                                                    conjunction='or')
+        result.qa.all_unity_longmsg = longmsg
+
+
