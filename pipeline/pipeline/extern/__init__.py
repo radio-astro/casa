20f623928404bb7e13e3eabb32d0bc2be0126fd8
stewart williams <sjw@roe.ac.uk>
2014-09-11 15:55:25 +0000

    Update to Mako 1.0.0
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@31062 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/extern/__init__.py
+++ pipeline/pipeline/extern/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__),'Mako-0.7.0-py2.6.egg'))
+sys.path.append(os.path.join(os.path.dirname(__file__),'Mako-1.0.0-py2.7.egg'))
 import mako
 
 from . import pyparsing
