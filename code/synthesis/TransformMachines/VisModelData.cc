9f0c558984c3b58aa8269ee01ebe14117ea0fdd9
kumar golap <kgolap@nrao.edu>
2014-10-23 17:23:04 +0000

    New Development: No             JIRA Issue: CAS-6490         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: Describe your changes here...
    Forced a removal of dangling keywords
    	
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@31412 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/synthesis/TransformMachines/VisModelData.cc
+++ code/synthesis/TransformMachines/VisModelData.cc
@@ -204,6 +204,18 @@ void VisModelData::clearModel(const MeasurementSet& thems){
 	if(newTab.rwKeywordSet().isDefined("definedmodel_field_"+String::toString(fields[k])))
 	  newTab.rwKeywordSet().removeField("definedmodel_field_"+String::toString(fields[k]));
       }
+      else{
+	///remove keys that point to nowehere
+	////cerr << "The key " << elkey << endl;
+	if(newTab.source().rwKeywordSet().isDefined(elkey))
+	  newTab.source().rwKeywordSet().removeField(elkey);
+	if(newTab.rwKeywordSet().isDefined(elkey))
+	  newTab.rwKeywordSet().removeField(elkey);
+	if(newTab.source().rwKeywordSet().isDefined("definedmodel_field_"+String::toString(fields[k])))
+	  newTab.source().rwKeywordSet().removeField("definedmodel_field_"+String::toString(fields[k]));
+	if(newTab.rwKeywordSet().isDefined("definedmodel_field_"+String::toString(fields[k])))
+	   newTab.rwKeywordSet().removeField("definedmodel_field_"+String::toString(fields[k]));
+      }
   }
   ////Cleaning out orphaned image disk models
   String srctable=thems.source().tableName();
