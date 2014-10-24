b250925f90a631a41e5f1bf8268014f76858acd9
brian kent <bkent@nrao.edu>
2014-10-24 13:42:57 +0000

    VLA Hanning weblog refactor
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@31417 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifv/tasks/hanning/__init__.py
+++ pipeline/pipeline/hifv/tasks/hanning/__init__.py
@@ -1,3 +1,11 @@
 from __future__ import absolute_import
+import pipeline.infrastructure.renderer.basetemplates as basetemplates
+import pipeline.infrastructure.renderer.weblog as weblog
+
 
 from .hanning import Hanning
+from . import hanning
+
+
+weblog.add_renderer(Hanning, basetemplates.T2_4MDetailsDefaultRenderer(uri='hanning.mako',
+                                                                      description='VLA Hanning Smoothing', always_rerender=True))
