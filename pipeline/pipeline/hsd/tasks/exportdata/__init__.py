60c254a5ed5374b2a12e8ac3b60101ed4a6e1c9d
takeshi nakazato <takeshi.nakazato@nao.ac.jp>
2014-10-27 13:58:38 +0000

    Attach default renderer to exportdata task.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@31426 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hsd/tasks/exportdata/__init__.py
+++ pipeline/pipeline/hsd/tasks/exportdata/__init__.py
@@ -1,3 +1,10 @@
 from __future__ import absolute_import
 
+import pipeline.infrastructure.renderer.weblog as weblog
+import pipeline.infrastructure.renderer.basetemplates as super_renderer
+
 from .exportdata import SDExportData
+
+weblog.add_renderer(SDExportData, 
+                    super_renderer.T2_4MDetailsDefaultRenderer(description='Export Pipeline Products', 
+                                                               always_rerender=False))
