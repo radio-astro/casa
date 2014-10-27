823e544b945ba23940d6c35f251ea32e4617bfa0
takeshi nakazato <takeshi.nakazato@nao.ac.jp>
2014-10-27 12:59:50 +0000

    Attach renderer to mstoscantable task. The renderer is a default one at the moment. It will be implemented later.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@31425 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hsd/tasks/mstoscantable/__init__.py
+++ pipeline/pipeline/hsd/tasks/mstoscantable/__init__.py
@@ -1,3 +1,10 @@
 from __future__ import absolute_import
 
+import pipeline.infrastructure.renderer.weblog as weblog
+import pipeline.infrastructure.renderer.basetemplates as super_renderer
+
 from .mstoscantable import SDMsToScantable as SDMsToScantable
+
+weblog.add_renderer(SDMsToScantable, 
+                    super_renderer.T2_4MDetailsDefaultRenderer(description='Convert MS to Scantable', 
+                                                               always_rerender=True))
