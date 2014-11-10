e8d67c527ddd7b98e1386753a93a2cd83c8f9332
john lightfoot <john.lightfoot@stfc.ac.uk>
2014-11-10 16:41:12 +0000

    resultobjects refactor
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@31575 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hif/tasks/lowgainflag/resultobjects.py
+++ pipeline/pipeline/hif/tasks/lowgainflag/resultobjects.py
@@ -4,48 +4,24 @@ import copy
 
 import pipeline.infrastructure as infrastructure
 import pipeline.infrastructure.basetask as basetask
+from pipeline.hif.tasks.common import flaggableviewresults
 
 LOG = infrastructure.get_logger(__name__)
 
 
-class LowgainflagResults(basetask.Results):
+class LowgainflagResults(basetask.Results,
+  flaggableviewresults.FlaggableViewResults):
     def __init__(self):
         """
         Construct and return a new LowgainflagResults.
         """
-        super(LowgainflagResults, self).__init__()
-        # views and associated results
-        self.flagging = []
-        # following are used instead of standard dictionaries so that
-        # missing keys are created automatically as needed
-        self.view = collections.defaultdict(list)
-        self.children = collections.defaultdict(dict)
+        basetask.Results.__init__(self)
+        flaggableviewresults.FlaggableViewResults.__init__(self)
 
     def merge_with_context(self, context):
         # do nothing, none of the gain cals used for the flagging
         # views should be used elsewhere
         pass
-
-    def addview(self, description, viewresult):
-        self.view[description].append(viewresult)
-
-    def addflags(self, flags):
-        self.flagging += flags
-
-    def descriptions(self):
-        return self.view.keys()
-
-    def first(self, description):
-        return copy.deepcopy(self.view[description][0])
-
-    def flagcmds(self):
-        return copy.deepcopy(self.flagging)
-
-#    def flagged(self):
-#        return len(self.flagging) > 0
-
-    def last(self, description):
-        return copy.deepcopy(self.view[description][-1])
 
     def __repr__(self):
         s = 'LowgainflagResults'
