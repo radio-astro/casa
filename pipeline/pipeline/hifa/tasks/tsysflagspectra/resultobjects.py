9617efd8db21b849bb34c61614844db8154bcf1a
john lightfoot <john.lightfoot@stfc.ac.uk>
2014-11-10 16:46:12 +0000

    resultobjects refactor
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@31580 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifa/tasks/tsysflagspectra/resultobjects.py
+++ pipeline/pipeline/hifa/tasks/tsysflagspectra/resultobjects.py
@@ -4,52 +4,27 @@ import collections
 import copy
 
 from pipeline.hifa.tasks.tsyscal import resultobjects 
+from pipeline.hif.tasks.common import flaggableviewresults
 
 import pipeline.infrastructure as infrastructure
 
 LOG = infrastructure.get_logger(__name__)
 
 
-class TsysflagspectraResults(resultobjects.TsyscalResults):
+class TsysflagspectraResults(resultobjects.TsyscalResults,
+  flaggableviewresults.FlaggableViewResults):
     def __init__(self, final=[], pool=[], preceding=[]):
         """
         Construct and return a new TsysflagspectraResults.
         """
         # the results to be merged back into the context
-        super(TsysflagspectraResults, self).__init__(final, pool, preceding)
-
-        # views and associated results
-        self.flagging = []
-        # following are used instead of standard dictionaries so that
-        # missing keys are created automatically as needed
-        self.view = collections.defaultdict(list)
-        self.children = collections.defaultdict(dict)
+        resultobjects.TsyscalResults.__init__(self, final, pool, preceding)
+        flaggableviewresults.FlaggableViewResults.__init__(self)
 
     def merge_with_context(self, context):
         # do nothing, the tsys cal files should already be in the context
         # and we don't want to insert them twice.
         pass
-
-    def addview(self, description, viewresult):
-        self.view[description].append(viewresult)
-
-    def addflags(self, flags):
-        self.flagging += flags
-
-    def descriptions(self):
-        return self.view.keys()
-
-    def first(self, description):
-        return copy.deepcopy(self.view[description][0])
-
-    def flagcmds(self):
-        return copy.deepcopy(self.flagging)
-
-    def flagged(self):
-        return len(self.flagging) > 0
-
-    def last(self, description):
-        return copy.deepcopy(self.view[description][-1])
 
     def __repr__(self):
         s = super(TsysflagspectraResults, self).__repr__()
