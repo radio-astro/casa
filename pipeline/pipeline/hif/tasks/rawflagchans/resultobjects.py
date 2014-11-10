d7a7cffac6f281b729bf2f919ff6ed419d57939e
john lightfoot <john.lightfoot@stfc.ac.uk>
2014-11-10 16:40:36 +0000

    resultobjects refactor
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@31574 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hif/tasks/rawflagchans/resultobjects.py
+++ pipeline/pipeline/hif/tasks/rawflagchans/resultobjects.py
@@ -4,47 +4,24 @@ import copy
 
 import pipeline.infrastructure as infrastructure
 import pipeline.infrastructure.basetask as basetask
+from pipeline.hif.tasks.common import flaggableviewresults
 
 LOG = infrastructure.get_logger(__name__)
 
 
-class RawflagchansResults(basetask.Results):
+class RawflagchansResults(basetask.Results,
+  flaggableviewresults.FlaggableViewResults):
     def __init__(self):
         """
         Construct and return a new RawflagchansResults.
         """
-        super(RawflagchansResults, self).__init__()
-        # views and associated results
-        self.flagging = []
-        # following are used instead of standard dictionaries so that
-        # missing keys are created automatically as needed
-        self.view = collections.defaultdict(list)
+        basetask.Results.__init__(self)
+        flaggableviewresults.FlaggableViewResults.__init__(self)
 
     def merge_with_context(self, context):
         # do nothing, the tsys cal files should already be in the context
         # and we don't want to insert them twice.
         pass
-
-    def addview(self, description, viewresult):
-        self.view[description].append(viewresult)
-
-    def addflags(self, flags):
-        self.flagging += flags
-
-    def descriptions(self):
-        return self.view.keys()
-
-    def first(self, description):
-        return copy.deepcopy(self.view[description][0])
-
-    def flagcmds(self):
-        return copy.deepcopy(self.flagging)
-
-    def flagged(self):
-        return len(self.flagging) > 0
-
-    def last(self, description):
-        return copy.deepcopy(self.view[description][-1])
 
     def __repr__(self):
         s = 'RawflagchansResults'
