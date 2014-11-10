af656ec3f6120e377f03eb4468da071838dc5700
john lightfoot <john.lightfoot@stfc.ac.uk>
2014-11-10 16:40:12 +0000

    resultobjects refactor
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@31573 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hif/tasks/rawflagchans/__init__.py
+++ pipeline/pipeline/hif/tasks/rawflagchans/__init__.py
@@ -1,7 +1,19 @@
 from __future__ import absolute_import
+import pipeline.infrastructure.pipelineqa as pipelineqa
 import pipeline.infrastructure.renderer.qaadapter as qaadapter
 
 from .rawflagchans import Rawflagchans
 from . import resultobjects
 
-qaadapter.registry.register_to_flagging_topic(resultobjects.RawflagchansResults)
+from pipeline.hif.tasks.rawflagchans import qa
+
+pipelineqa.registry.add_handler(qa.RawflagchansQAHandler())
+pipelineqa.registry.add_handler(qa.RawflagchansListQAHandler())
+qaadapter.registry.register_to_flagging_topic(
+  resultobjects.RawflagchansResults)
+
+
+
+
+
+
