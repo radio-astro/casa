ad69392b350c7600ce9b0a092d336238b11312f4
john lightfoot <john.lightfoot@stfc.ac.uk>
2014-11-10 16:44:58 +0000

    fist version
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@31579 ac9c0da5-131f-0410-9009-a9c195f183e6

---
+++ pipeline/pipeline/hif/tasks/common/flaggableviewresults.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import
+import collections
+import copy
+
+import pipeline.infrastructure as infrastructure
+
+LOG = infrastructure.get_logger(__name__)
+
+
+class FlaggableViewResults:
+    def __init__(self):
+        """
+        Construct and return a new FlaggableViewResults.
+        """
+        # views and associated results
+        self.flagging = []
+        # following are used instead of standard dictionaries so that
+        # missing keys are created automatically as needed
+        self.view = collections.defaultdict(list)
+
+    def addview(self, description, viewresult):
+        self.view[description].append(viewresult)
+
+    def addflags(self, flags):
+        self.flagging += flags
+
+    def add_flag_reason_plane(self, flag_reason_plane, flag_reason_key):
+        for description in self.descriptions():
+            self.view[description][-1].flag_reason_plane = \
+              flag_reason_plane[description]
+            self.view[description][-1].flag_reason_key = \
+              flag_reason_key
+
+    def descriptions(self):
+        return self.view.keys()
+
+    def first(self, description):
+        return copy.deepcopy(self.view[description][0])
+
+    def flagcmds(self):
+        return copy.deepcopy(self.flagging)
+
+    def flagged(self):
+        return len(self.flagging) > 0
+
+    def last(self, description):
+        return copy.deepcopy(self.view[description][-1])
+
