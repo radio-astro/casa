25811829f19ad0b4b2a89c25781bcd8326d2488f
kana sugimoto <kana.sugi@nao.ac.jp>
2014-11-21 08:44:03 +0000

    New Development: Yes             JIRA Issue: Yes (CAS-6880)         Ready for Test: Yes      Interface Changes: No What Interface Changed:                  Test Programs:   Put in Release Notes: No              Module(s): pipeline.recipes            Description: Introduced non-linearity correction step (hsd_simplescale) in singledish recipe. The step is inserted after calibration before baseline subtraction (science target) / imaging (amp cal). The exact place to insert the step is still wating for EOC for verification.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@31692 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/recipes/hsd.py
+++ pipeline/pipeline/recipes/hsd.py
@@ -37,6 +37,7 @@ from hsd_inspectdata_cli import hsd_inspectdata_cli as hsd_inspectdata
 from hsd_caltsys_cli import hsd_caltsys_cli as hsd_caltsys
 from hsd_calsky_cli import hsd_calsky_cli as hsd_calsky
 from hsd_applycal_cli import hsd_applycal_cli as hsd_applycal
+from hsd_simplescale_cli import hsd_simplescale_cli as hsd_simplescale
 from hsd_baseline_cli import hsd_baseline_cli as hsd_baseline
 from hsd_blflag_cli import hsd_blflag_cli as hsd_blflag
 from hsd_imaging_cli import hsd_imaging_cli as hsd_imaging
@@ -77,6 +78,9 @@ def hsd (vislist, importonly=False, pipelinemode='automatic', interactive=True):
         
         # Apply the calibrations
         hsd_applycal (pipelinemode=pipelinemode)
+    
+        # Apply non-linearity correction
+        hsd_simplescale (pipelinemode=pipelinemode)
     
         # Improve line mask for baseline subtraction by executing 
         # hsd_baseline and hsd_blflag iteratively
--- pipeline/pipeline/recipes/hsd_cal.py
+++ pipeline/pipeline/recipes/hsd_cal.py
@@ -38,6 +38,7 @@ from hsd_inspectdata_cli import hsd_inspectdata_cli as hsd_inspectdata
 from hsd_caltsys_cli import hsd_caltsys_cli as hsd_caltsys
 from hsd_calsky_cli import hsd_calsky_cli as hsd_calsky
 from hsd_applycal_cli import hsd_applycal_cli as hsd_applycal
+from hsd_simplescale_cli import hsd_simplescale_cli as hsd_simplescale
 from hsd_imaging_cli import hsd_imaging_cli as hsd_imaging
 from hsd_exportdata_cli import hsd_exportdata_cli as hsd_exportdata
 from h_save_cli import h_save_cli as h_save
@@ -73,6 +74,9 @@ def hsd_cal (vislist, importonly=False, pipelinemode='automatic', interactive=Tr
         
         # Apply the calibrations
         hsd_applycal (pipelinemode=pipelinemode)
+    
+        # Apply non-linearity correction
+        hsd_simplescale (pipelinemode=pipelinemode)
     
         # Imaging
         hsd_imaging (pipelinemode=pipelinemode)
