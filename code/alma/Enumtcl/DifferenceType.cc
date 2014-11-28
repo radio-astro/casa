5e05691a1e496be5febd384d2f80b479011d6918
michel caillat <michel.caillat@nrao.edu>
2014-11-28 16:57:01 +0000

    New Development: Yes             JIRA Issue: No         Ready for Test: Yes      Interface Changes: No What Interface Changed:          Test Programs:   Put in Release Notes: Yes              Module(s): alma            Description: Added two Enumerations :            * DifferenceType. It's used to qualify some columns of the (new) table DelayModelVariableParameters.            * SynthProf. It has been requested by Manabu Watanabe to qualify the spectral windows in the BDFs produced by the ACA correlator (cf ICT-4023 and ICT-4022)
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@31754 ac9c0da5-131f-0410-9009-a9c195f183e6

---
+++ code/alma/Enumtcl/DifferenceType.cc
@@ -0,0 +1,10 @@
+#include "DifferenceType.h"
+
+string enum_map_traits<DifferenceTypeMod::DifferenceType,void>::typeName_="DifferenceType";
+string enum_map_traits<DifferenceTypeMod::DifferenceType,void>::enumerationDesc_="";
+string enum_map_traits<DifferenceTypeMod::DifferenceType,void>::order_="";
+string enum_map_traits<DifferenceTypeMod::DifferenceType,void>::xsdBaseType_="void";
+map<DifferenceTypeMod::DifferenceType,EnumPar<void> > enum_map_traits<DifferenceTypeMod::DifferenceType,void>::m_;
+bool   enum_map_traits<DifferenceTypeMod::DifferenceType,void>::init_=init();
+
+
