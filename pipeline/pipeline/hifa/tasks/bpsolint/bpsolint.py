Fixed a type in phaseup solint computation.
git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@31930 ac9c0da5-131f-0410-9009-a9c195f183e6

pipeline/pipeline/hifa/tasks/bpsolint/bpsolint.py
@@ -989,7 +989,7 @@ class BpSolint(basetask.StandardTaskTemplate):
 	        solint_dict[spwid]['nint_phaseup_solint'] = 1
 	    else:
 	        solint_dict[spwid]['phaseup_solint'] = '%fs' % \
-	            (solint_dict[spwid]['integrationtime'] * \
+	            (solint_dict[spwid]['integration_minutes'] * \
 		    requiredIntegrations * 60.0)
 	        solint_dict[spwid]['nint_phaseup_solint'] = \
 		    int(np.ceil(requiredIntegrations))