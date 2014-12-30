76bf94e044d4a81ae10d923206f3423b96cc7f5a
lindsey davis <ldavis@nrao.edu>
2014-12-30 23:29:18 +0000

    Added template registration for normflux and fluxscale tasks.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@31987 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hif/tasks/fluxscale/__init__.py
+++ pipeline/pipeline/hif/tasks/fluxscale/__init__.py
@@ -3,4 +3,12 @@ from __future__ import absolute_import
 from .fluxcal import Fluxcal
 from .fluxscale import Fluxscale
 from .normflux import NormaliseFlux
-#from .gcorfluxscale import GcorFluxscale
+
+import pipeline.infrastructure.renderer.weblog as weblog
+import pipeline.infrastructure.renderer.basetemplates as basetemplates
+
+weblog.add_renderer(NormaliseFlux, basetemplates.T2_4MDetailsDefaultRenderer(uri='normflux.mako',
+               description='Normalise flux scales accross ASDMs'))
+
+weblog.add_renderer(Fluxscale, basetemplates.T2_4MDetailsDefaultRenderer(uri='fluxscale.mako',
+               description='Compute calibrator source fluxes'))
