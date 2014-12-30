9ae307da32cce68d47868bcbe137fae1e74ad19b
lindsey davis <ldavis@nrao.edu>
2014-12-30 20:16:54 +0000

    Hooked up new template.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@31983 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hif/tasks/exportdata/__init__.py
+++ pipeline/pipeline/hif/tasks/exportdata/__init__.py
@@ -1,2 +1,7 @@
 from __future__ import absolute_import
 from .exportdata import ExportData
+import pipeline.infrastructure.renderer.weblog as weblog
+import pipeline.infrastructure.renderer.basetemplates as basetemplates
+
+weblog.add_renderer(ExportData, basetemplates.T2_4MDetailsDefaultRenderer(uri='exportdata.mako',
+               description='Prepare pipeline data products for export'))
