a17aa448725baf6cee5323c34564309ced579467
stewart williams <sjw@roe.ac.uk>
2015-01-06 13:54:12 +0000

    Remove unused QA rendering class
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@32022 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/infrastructure/renderer/sharedrenderer.py
+++ pipeline/pipeline/infrastructure/renderer/sharedrenderer.py
@@ -12,14 +12,14 @@ import pipeline.infrastructure.renderer.basetemplates as basetemplates
 class PlotGroupRenderer(basetemplates.CommonRenderer):
     def __init__(self, context, result, plot_group, prefix='',
                  uri='plotgroup.mako'):
-        super(PlotGroupRenderer, self).__init__(context=context, 
+        super(PlotGroupRenderer, self).__init__(context=context,
                                                 result=result,
                                                 uri=uri)
         self.plot_group = plot_group
 
         if prefix != '':
             prefix = '%s-' % prefix
-        basename = '%s%s-%s-thumbnails.html' % (plot_group.x_axis, 
+        basename = '%s%s-%s-thumbnails.html' % (plot_group.x_axis,
                                                 plot_group.y_axis,
                                                 prefix)
         self.basename = filenamer.sanitize(basename)
@@ -27,22 +27,3 @@ class PlotGroupRenderer(basetemplates.CommonRenderer):
 
     def update_mako_context(self, mako_context):
         mako_context.update({'plot_group':self.plot_group})
-
-
-class QAPlotRenderer(PlotGroupRenderer):
-    def __init__(self, context, result, plot_group, prefix='', uri='qa.mako'):
-        super(QAPlotRenderer, self).__init__(context, result, plot_group, 
-                                             prefix=prefix, uri=uri)
-
-        json_path = os.path.join(context.report_dir, 
-                                 'stage%s' % result.stage_number, 
-                                 'qa.json')        
-        if os.path.exists(json_path):
-            with open(json_path, 'r') as json_file:
-                self.json = json_file.readlines()[0]
-        else:
-            self.json = '{}'
-
-    def update_mako_context(self, mako_context):
-        super(QAPlotRenderer, self).update_mako_context(mako_context)
-        mako_context.update({'json' : self.json})
