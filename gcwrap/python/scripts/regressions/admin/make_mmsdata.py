eac0dc3e0954bde1d62f4d806d3d97fa7aa82bb0
sandra castro <sbhatnag@nrao.edu>
2015-01-20 15:00:04 +0000

    New Development: No             JIRA Issue: CAS-7277         Ready for Test: Yes      Interface Changes: No What Interface Changed:          Test Programs: test_uvcontsub   Put in Release Notes: No              Module(s): HPC            Description: Fixed a bug in the ignore option. Updated the usage message
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@32136 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/regressions/admin/make_mmsdata.py
+++ gcwrap/python/scripts/regressions/admin/make_mmsdata.py
@@ -24,7 +24,7 @@ TASKLIST = [
             'bandpass',
             'concat', # shared with virtualconcat
             'cvel',
-            'fixvis'
+            'fixvis',
             'flagdata',
             'fluxscale',
             'gaincal',
