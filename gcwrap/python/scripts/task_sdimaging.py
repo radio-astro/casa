715a4f9121fd35c3a2d580ec1d7c52cd88d850aa
takeshi nakazato <takeshi.nakazato@nao.ac.jp>
2015-01-30 01:23:19 +0000

    New Development: No             JIRA Issue: No         Ready for Test: Yes      Interface Changes: Yes/No What Interface Changed: Please list interface changes          Test Programs: test_sdimaging   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: Describe your changes here...
    Improvement of fix for sdimaging unit test failure.
    
    	
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@32226 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/task_sdimaging.py
+++ gcwrap/python/scripts/task_sdimaging.py
@@ -451,17 +451,18 @@ class sdimaging_worker(sdutil.sdtask_template_imaging):
         ###weight_val = my_ia.getchunk()
         ###valid_pixels = numpy.where(weight_val > 0.0)
         ####Never do the above in a task...it can swap out easily
-        #stat=my_ia.statistics(mask="'"+weightfile+"' > 0.0", robust=True)
-        #valid_pixels=stat['npts']
-        #if len(valid_pixels) == 0:
-        # Avoid throwing exception when all weights are zero
-        stat = my_ia.statistics(robust=True)
-        if all(stat['max'] == 0.0) and all(stat['min'] == 0.0):
+        try:
+            stat=my_ia.statistics(mask="'"+weightfile+"' > 0.0", robust=True)
+            valid_pixels=stat['npts']
+        except RuntimeError, e:
+            if e.message.find('No valid data found.') >= 0:
+                valid_pixels = [0]
+            else:
+                raise e
+        if len(valid_pixels) == 0 or valid_pixels[0] == 0:
             my_ia.close()
             casalog.post("All pixels weight zero. This indicates no data in MS is in image area. Mask will not be set. Please check your image parameters.","WARN")
             return
-        stat=my_ia.statistics(mask="'"+weightfile+"' > 0.0", robust=True)
-        valid_pixels=stat['npts']
         median_weight = stat['median'][0]
         casalog.post("Median of weight in the map is %f" % median_weight, \
                      "INFO")
