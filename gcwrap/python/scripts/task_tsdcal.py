0e5ee62a1c4800b18a0bd04089a8ede8677d3117
takeshi nakazato <takeshi.nakazato@nao.ac.jp>
2015-02-03 08:35:02 +0000

    New Development: No             JIRA Issue: Yes CAS-6991         Ready for Test: Yes      Interface Changes: Yes/No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: Describe your changes here...
    Update according to changes made in cb tool.
    
    	
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@32264 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/task_tsdcal.py
+++ gcwrap/python/scripts/task_tsdcal.py
@@ -1,22 +1,23 @@
 import sys
 import os
 from taskinit import *
-from applycal import applycal
+#from applycal import applycal
 
 (cb,) = gentools(['cb'])
-def tsdcal(infile=None, calmode='tsys', fraction='10%', noff=-1, width=0.5, elongated=False, applytable='',interp='', spwmap=[],
-		field='', spw='', scan='', pol='', outfile='', overwrite=False): 
+def tsdcal(infile=None, calmode='tsys', fraction='10%', noff=-1,
+           width=0.5, elongated=False, applytable='',interp='', spwmap=[],
+           field='', spw='', scan='', pol='', outfile='', overwrite=False): 
+       
+        """ Externally specify calibration solutions af various types
+        """
 
-       """ Externally specify calibration solutions af various types
-       """
-
-       #Python script
-       try:
+        #Python script
+        try:
 
 
-		if ((type(infile)==str) and (os.path.exists(infile))):
-                     # don't need scr col for this
-                     cb.open(filename=infile,compress=False,addcorr=False,addmodel=False)
+                if ((type(infile)==str) and (os.path.exists(infile))):
+                       # don't need scr col for this
+                       cb.open(filename=infile,compress=False,addcorr=False,addmodel=False)
 		else:
 			raise Exception, 'Infile data set not found - please verify the name'
 
@@ -26,7 +27,8 @@ def tsdcal(infile=None, calmode='tsys', fraction='10%', noff=-1, width=0.5, elon
 		if ((type(calmode)==str) and (calmode=='apply') and (applytable =='')):
                         raise Exception, 'A applytable name must be specified'
 
-
+                if (not overwrite) and (os.path.exists(outfile)):
+                        raise RuntimeError, 'Output file \'%s\' exists.'%(outfile)
 
 
 		if ((type(calmode)==str) and (calmode=='apply') and (applytable !='') and (os.path.exists(applytable))):
@@ -35,26 +37,26 @@ def tsdcal(infile=None, calmode='tsys', fraction='10%', noff=-1, width=0.5, elon
                                 raise UserWarning, 'An outfile is not generated but is added to MS as a new table, namely corrected data'
 
 
-		calmodemap = {'tsys': 'tsys',
+                calmodemap = {'tsys': 'tsys',
                               'ps': 'sdsky_ps'}
-		
 
-		if(calmode!='apply'):	     
-			cb.specifycal(caltable=outfile,time="",spw=spw,pol=pol,
-                            caltype=calmodemap[calmode])
+                if(calmode!='apply'):
+                        if len(outfile) == 0:
+                                raise RuntimeError, 'Output file name must be specified.'
+                        if calmode=='tsys':
+                                cb.specifycal(caltable=outfile,time="",spw=spw,pol=pol,
+                                              caltype=calmodemap[calmode])
+                        else:
+                                cb.selectvis(spw=spw, scan=scan, field=field)
+                                cb.setsolve(type=calmodemap[calmode], table=outfile)
+                                cb.solve()
 
+        except UserWarning, instance:
+                print '*** Warning ***',instance
 
+        except Exception, instance:
+                print '*** Error ***',instance
+                raise Exception, instance
 
-              #cb.close()
-       
-       except UserWarning, instance:
-              print '*** Warning ***',instance
-              #cb.close()
-
-       except Exception, instance:
-              print '*** Error ***',instance
-              #cb.close()
-              raise Exception, instance
-
-       finally:
-              cb.close()
+        finally:
+                cb.close()
