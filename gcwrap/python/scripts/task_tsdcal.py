78c34b1a038a0951665c4762383ee420e3a0fa2f
takeshi nakazato <takeshi.nakazato@nao.ac.jp>
2015-02-06 07:48:11 +0000

    New Development: No             JIRA Issue: No         Ready for Test: Yes      Interface Changes: Yes/No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: Describe your changes here...
    Replaced TAB with 4 spaces.
    
    	
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@32309 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/task_tsdcal.py
+++ gcwrap/python/scripts/task_tsdcal.py
@@ -18,20 +18,20 @@ def tsdcal(infile=None, calmode='tsys', fraction='10%', noff=-1,
                 if ((type(infile)==str) and (os.path.exists(infile))):
                        # don't need scr col for this
                        cb.open(filename=infile,compress=False,addcorr=False,addmodel=False)
-		else:
-			raise Exception, 'Infile data set not found - please verify the name'
+                else:
+                        raise Exception, 'Infile data set not found - please verify the name'
 
                 if((type(calmode)==str) and calmode.lower() not in ['tsys', 'ps', 'apply']): 
-			raise Exception, 'A calmode must be either \'ps\' or \'tsys\' or \'apply\''
+                        raise Exception, 'A calmode must be either \'ps\' or \'tsys\' or \'apply\''
 
-		if ((type(calmode)==str) and (calmode=='apply') and (applytable =='')):
+                if ((type(calmode)==str) and (calmode=='apply') and (applytable =='')):
                         raise Exception, 'A applytable name must be specified'
 
                 if (not overwrite) and (os.path.exists(outfile)):
                         raise RuntimeError, 'Output file \'%s\' exists.'%(outfile)
 
 
-		if ((type(calmode)==str) and (calmode=='apply') and (applytable !='') and (os.path.exists(applytable))):
+                if ((type(calmode)==str) and (calmode=='apply') and (applytable !='') and (os.path.exists(applytable))):
                         applycal(vis=infile, docallib=False, gaintable=applytable, applymode='calonly')
                         if (outfile != ''):
                                 raise UserWarning, 'An outfile is not generated but is added to MS as a new table, namely corrected data'
