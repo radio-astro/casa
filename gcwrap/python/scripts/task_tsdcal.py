f152b8e96e8b41ad04c2bef26aa7e7097565cf92
masaya kuniyoshi <mkuniyos@aoc.nrao.edu>
2015-02-06 09:20:15 +0000

    New Development: No             JIRA Issue: No         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: test_tsdcal   Put in Release Notes: No              Module(s): No            Description: modified task_tsdcal.py (indent=2)
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@32312 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/task_tsdcal.py
+++ gcwrap/python/scripts/task_tsdcal.py
@@ -8,55 +8,50 @@ def tsdcal(infile=None, calmode='tsys', fraction='10%', noff=-1,
            width=0.5, elongated=False, applytable='',interp='', spwmap=[],
            field='', spw='', scan='', pol='', outfile='', overwrite=False): 
        
-        """ Externally specify calibration solutions af various types
-        """
+	""" Externally specify calibration solutions af various types
+	"""
 
-        #Python script
-        try:
+	#Python script
+	try:
 
+		if ((type(infile)==str) and (os.path.exists(infile))):
+			# don't need scr col for this
+			cb.open(filename=infile,compress=False,addcorr=False,addmodel=False)
+		else:
+			raise Exception, 'Infile data set not found - please verify the name'
 
-                if ((type(infile)==str) and (os.path.exists(infile))):
-                       # don't need scr col for this
-                       cb.open(filename=infile,compress=False,addcorr=False,addmodel=False)
-                else:
-                        raise Exception, 'Infile data set not found - please verify the name'
+		if((type(calmode)==str) and calmode.lower() not in ['tsys', 'ps', 'apply']): 
+			raise Exception, 'A calmode must be either \'ps\' or \'tsys\' or \'apply\''
 
-                if((type(calmode)==str) and calmode.lower() not in ['tsys', 'ps', 'apply']): 
-                        raise Exception, 'A calmode must be either \'ps\' or \'tsys\' or \'apply\''
+		if ((type(calmode)==str) and (calmode=='apply') and (applytable =='')):
+			raise Exception, 'A applytable name must be specified'
 
-                if ((type(calmode)==str) and (calmode=='apply') and (applytable =='')):
-                        raise Exception, 'A applytable name must be specified'
+		if (not overwrite) and (os.path.exists(outfile)):
+			raise RuntimeError, 'Output file \'%s\' exists.'%(outfile)
 
-                if (not overwrite) and (os.path.exists(outfile)):
-                        raise RuntimeError, 'Output file \'%s\' exists.'%(outfile)
+		if ((type(calmode)==str) and (calmode=='apply') and (applytable !='') and (os.path.exists(applytable))):
+			applycal(vis=infile, docallib=False, gaintable=applytable, applymode='calonly')
+			if (outfile != ''):
+				raise UserWarning, 'An outfile is not generated but is added to MS as a new table, namely corrected data'
 
+		calmodemap = {'tsys': 'tsys','ps': 'sdsky_ps'}
 
-                if ((type(calmode)==str) and (calmode=='apply') and (applytable !='') and (os.path.exists(applytable))):
-                        applycal(vis=infile, docallib=False, gaintable=applytable, applymode='calonly')
-                        if (outfile != ''):
-                                raise UserWarning, 'An outfile is not generated but is added to MS as a new table, namely corrected data'
+		if(calmode!='apply'):
+			if len(outfile) == 0:
+				raise RuntimeError, 'Output file name must be specified.'
+			if calmode=='tsys':
+				cb.specifycal(caltable=outfile,time="",spw=spw,pol=pol,caltype=calmodemap[calmode])
+			else:
+				cb.selectvis(spw=spw, scan=scan, field=field)
+				cb.setsolve(type=calmodemap[calmode], table=outfile)
+				cb.solve()
 
+	except UserWarning, instance:
+		print '*** Warning ***',instance
 
-                calmodemap = {'tsys': 'tsys',
-                              'ps': 'sdsky_ps'}
+	except Exception, instance:
+		print '*** Error ***',instance
+		raise Exception, instance
 
-                if(calmode!='apply'):
-                        if len(outfile) == 0:
-                                raise RuntimeError, 'Output file name must be specified.'
-                        if calmode=='tsys':
-                                cb.specifycal(caltable=outfile,time="",spw=spw,pol=pol,
-                                              caltype=calmodemap[calmode])
-                        else:
-                                cb.selectvis(spw=spw, scan=scan, field=field)
-                                cb.setsolve(type=calmodemap[calmode], table=outfile)
-                                cb.solve()
-
-        except UserWarning, instance:
-                print '*** Warning ***',instance
-
-        except Exception, instance:
-                print '*** Error ***',instance
-                raise Exception, instance
-
-        finally:
-                cb.close()
+	finally:
+		cb.close()
