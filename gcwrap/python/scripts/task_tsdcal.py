201d63591bdac5dd6cb41b4bd772128e888f09af
masaya kuniyoshi <mkuniyos@aoc.nrao.edu>
2015-02-10 07:34:34 +0000

    New Development: No             JIRA Issue: Yes  related to JIRA-6995         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: test_tsdcal   Put in Release Notes: No              Module(s): Module Names change impacts.            Description: implementation of calmode='apply' in tsdcal. spwmap can cope with 'list' and 'dict'.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@32339 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/task_tsdcal.py
+++ gcwrap/python/scripts/task_tsdcal.py
@@ -1,11 +1,12 @@
 import sys
 import os
 from taskinit import *
-#from applycal import applycal
+from applycal import applycal
+import types
 
 (cb,) = gentools(['cb'])
 def tsdcal(infile=None, calmode='tsys', fraction='10%', noff=-1,
-           width=0.5, elongated=False, applytable='',interp='', spwmap=[],
+           width=0.5, elongated=False, applytable='',interp='', spwmap={},
            field='', spw='', scan='', pol='', outfile='', overwrite=False): 
        
 	""" Externally specify calibration solutions af various types
@@ -17,6 +18,9 @@ def tsdcal(infile=None, calmode='tsys', fraction='10%', noff=-1,
 		if ((type(infile)==str) and (os.path.exists(infile))):
 			# don't need scr col for this
 			cb.open(filename=infile,compress=False,addcorr=False,addmodel=False)
+			cb.selectvis(spw=spw, scan=scan, field=field)
+			#cb.setsolve(type=calmodemap[calmode], table=outfile)
+			#cb.solve()
 		else:
 			raise Exception, 'Infile data set not found - please verify the name'
 
@@ -24,15 +28,40 @@ def tsdcal(infile=None, calmode='tsys', fraction='10%', noff=-1,
 			raise Exception, 'A calmode must be either \'ps\' or \'tsys\' or \'apply\''
 
 		if ((type(calmode)==str) and (calmode=='apply') and (applytable =='')):
-			raise Exception, 'A applytable name must be specified'
+			raise Exception, 'A applytable name must be specified.'
 
 		if (not overwrite) and (os.path.exists(outfile)):
 			raise RuntimeError, 'Output file \'%s\' exists.'%(outfile)
 
 		if ((type(calmode)==str) and (calmode=='apply') and (applytable !='') and (os.path.exists(applytable))):
-			applycal(vis=infile, docallib=False, gaintable=applytable, applymode='calonly')
+			#applycal(vis=infile, docallib=False, gaintable=applytable, applymode='calonly')
 			if (outfile != ''):
-				raise UserWarning, 'An outfile is not generated but is added to MS as a new table, namely corrected data'
+				raise UserWarning, 'Outfile is not generated but is added to MS as a new table, namely corrected data'
+				
+			if(type(spwmap)!=types.ListType and (type(spwmap)!=types.DictType)):
+				raise Exception, 'type of the spwmap must be list or dictionary.'
+			
+			if (type(spwmap)==types.ListType):
+				applycal(vis=infile, docallib=False, gaintable=applytable, applymode='calonly')
+				#raise UserWarning, 'spwmap is list'
+			elif (type(spwmap)==types.DictType):
+
+				MS = infile
+				tb.open(MS+'/SPECTRAL_WINDOW')
+				total_spwID=tb.nrows()
+				spwmap_dict = spwmap
+				spwmap_list=[]
+				for num in range(total_spwID):
+					spwmap_list.append(num)
+
+				for key, value in spwmap_dict.items():
+					for v in value:
+						if v in spwmap_list:
+							index = spwmap_list.index(v)
+							spwmap_list[index]=int(key)
+
+				spwmap = spwmap_list	
+				applycal(vis=infile, docallib=False, gaintable=applytable, applymode='calonly')	
 
 		calmodemap = {'tsys': 'tsys','ps': 'sdsky_ps'}
 
@@ -40,6 +69,8 @@ def tsdcal(infile=None, calmode='tsys', fraction='10%', noff=-1,
 			if len(outfile) == 0:
 				raise RuntimeError, 'Output file name must be specified.'
 			if calmode=='tsys':
+				if spw!='':
+					raise UserWarning,'The input of spw should be \'\'(empty) in this mode.'
 				cb.specifycal(caltable=outfile,time="",spw=spw,pol=pol,caltype=calmodemap[calmode])
 			else:
 				cb.selectvis(spw=spw, scan=scan, field=field)
