908dd050ea9287d9cbe8b07881a1d134a950fba0
masaya kuniyoshi <mkuniyos@aoc.nrao.edu>
2015-02-23 02:29:34 +0000

    New Development: No             JIRA Issue: Yes JIRA-6992/6995 realted ticket.         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: test_tsdcal   Put in Release Notes: No              Module(s): No            Description: tab=>4spaces
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@32441 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/tests/test_tsdcal.py
+++ gcwrap/python/scripts/tests/test_tsdcal.py
@@ -15,100 +15,101 @@ from tsdcal import tsdcal
 
 class tsdcal_test(unittest.TestCase):
 
-	"""
-	Unit test for task tsdcal.
-	
-	The list of tests:
-	test00	--- default parameters (raises an error)
-	test01	--- spwmap comprising list
-	test02	--- spwmap comprising dictionary
-	test03	--- spwmap comprising others
-	test04	--- there is no infile
-	test05
-	"""
+    """
+    Unit test for task tsdcal.
 
-	# Data path of input
-	datapath=os.environ.get('CASAPATH').split()[0]+ '/data/regression/unittest/tsdcal/'
+    The list of tests:
+    test00	--- default parameters (raises an error)
+    test01	--- spwmap comprising list
+    test02	--- spwmap comprising dictionary
+    test03	--- spwmap comprising others
+    test04	--- there is no infile
+    test05
+    """
 
-	# Input 
-	infile1 = 'uid___A002_X6218fb_X264.ms'
-	infiles = [infile1]
+    # Data path of input
+    datapath=os.environ.get('CASAPATH').split()[0]+ '/data/regression/unittest/tsdcal/'
 
-	def setUp(self):
-		for infile in self.infiles:
-			if os.path.exists(infile):
-				shutil.rmtree(infile)
-			shutil.copytree(self.datapath+infile, infile)		
-		default(tsdcal)
+    # Input 
+    infile1 = 'uid___A002_X6218fb_X264.ms'
+    infiles = [infile1]
 
-	def tearDown(self):
-		for infile in self.infiles:
-			if (os.path.exists(infile)):
-				shutil.rmtree(infile)
+    def setUp(self):
+        for infile in self.infiles:
+            if os.path.exists(infile):
+                shutil.rmtree(infile)
+            shutil.copytree(self.datapath+infile, infile)		
+        default(tsdcal)
 
-	def _compareOutFile(self,out,reference):
-		self.assertTrue(os.path.exists(out))
-		self.assertTrue(os.path.exists(reference),msg="Reference file doesn't exist: "+reference)
-		self.assertTrue(listing.compare(out,reference),'New and reference files are different. %s != %s. '%(out,reference))
+    def tearDown(self):
+        for infile in self.infiles:
+            if (os.path.exists(infile)):
+                shutil.rmtree(infile)
 
-	def test00(self):
-		"""Test00:Check the identification of TSYS_SPECTRuM and FPARAM"""
+    def _compareOutFile(self,out,reference):
+        self.assertTrue(os.path.exists(out))
+        self.assertTrue(os.path.exists(reference),msg="Reference file doesn't exist: "+reference)
+        self.assertTrue(listing.compare(out,reference),'New and reference files are different. %s != %s. '%(out,reference))
 
-		tid = "00"
-		infile = self.infile1
-		tsdcal(infile=infile, calmode='tsys', outfile='out.cal')
-		compfile1=infile+'/SYSCAL'
-		compfile2='out.cal'
+    def test00(self):
+        """Test00:Check the identification of TSYS_SPECTRuM and FPARAM"""
 
-		tb.open(compfile1)
-		subt1=tb.query('', sortlist='ANTENNA_ID, TIME, SPECTRAL_WINDOW_ID', columns='TSYS_SPECTRUM')
-		tsys1=subt1.getcol('TSYS_SPECTRUM')
-		tb.close()
-		subt1.close()
+        tid = "00"
+        infile = self.infile1
+        tsdcal(infile=infile, calmode='tsys', outfile='out.cal')
+        compfile1=infile+'/SYSCAL'
+        compfile2='out.cal'
 
-		tb.open(compfile2)
-		subt2=tb.query('', sortlist='ANTENNA1, TIME, SPECTRAL_WINDOW_ID', columns='FPARAM, FLAG')
-		tsys2=subt2.getcol('FPARAM')
-		flag=subt2.getcol('FLAG')
+        tb.open(compfile1)
+        subt1=tb.query('', sortlist='ANTENNA_ID, TIME, SPECTRAL_WINDOW_ID', columns='TSYS_SPECTRUM')
+        tsys1=subt1.getcol('TSYS_SPECTRUM')
+        tb.close()
+        subt1.close()
 
-		tb.close()
-		subt2.close()
+        tb.open(compfile2)
+        subt2=tb.query('', sortlist='ANTENNA1, TIME, SPECTRAL_WINDOW_ID', columns='FPARAM, FLAG')
+        tsys2=subt2.getcol('FPARAM')
+        flag=subt2.getcol('FLAG')
 
-		if tsys1.all() == tsys2.all():
-			print ''
-			print 'The shape of the MS/SYSCAL/TSYS_SPECTRUM', tsys1.shape
-			print 'The shape of the FPARAM extracted with tsdcal', tsys2.shape  
-			print 'Both tables are identical.'
-		else:
-			print ''
-			print 'The shape of the MS/SYSCAL/TSYS_SPECTRUM', tsys1.shape
-			print 'The shape of the FPARAM of the extraction with tsdcal', tsys2.shape
-			print 'Both tables are not identical.'
+        tb.close()
+        subt2.close()
 
-		if flag.all()==0:
-			print 'ALL FLAGs are set to zero.'
+        if tsys1.all() == tsys2.all():
+            print ''
+            print 'The shape of the MS/SYSCAL/TSYS_SPECTRUM', tsys1.shape
+            print 'The shape of the FPARAM extracted with tsdcal', tsys2.shape  
+            print 'Both tables are identical.'
+        else:
+            print ''
+            print 'The shape of the MS/SYSCAL/TSYS_SPECTRUM', tsys1.shape
+            print 'The shape of the FPARAM of the extraction with tsdcal', tsys2.shape
+            print 'Both tables are not identical.'
 
-
-	def test01(self):
-		"""Test01: Validateion when spwmap comprising list"""
-		
-		tid = "01"
-		infile = self.infile1
-		tsdcal(infile=infile, calmode='tsys', outfile='tsys.cal')
-		spwmap=[0,1,2,3,4,5,6,7,8,1,10,3,12,5,14,7,16]
-		tsdcal(infile=infile, calmode='apply', spwmap=spwmap, applytable='tsys.cal', outfile='')
+        if flag.all()==0:
+            print 'ALL FLAGs are set to zero.'
 
 
-#	def test02(self):
-#		"""Test01: Validateion when spwmap comprising dictionary"""
-#		
-#		tid ="02"
-#		infile=self.infile1
-#		tsdcal(infile=infile, calmode='tsys', outfile='tsys.cal')
-#		spwmap={1:[9],3:[11],5:[13],7:[15]}
-#		tsdcal(infile=infile, calmode='apply', spwmap=spwmap, applytable='tsys.cal', outfile='')
+    def test01(self):
+        """Test01: Validateion when spwmap comprising list"""
+
+        tid = "01"
+        infile = self.infile1
+        tsdcal(infile=infile, calmode='tsys', outfile='tsys.cal')
+        spwmap=[0,1,2,3,4,5,6,7,8,1,10,3,12,5,14,7,16]
+        #spwmap={1:[9],3:[11],5:[13],7:[15]}
+        tsdcal(infile=infile, calmode='apply', spwmap=spwmap, applytable='tsys.cal', outfile='')
 
 
+    def test02(self):
+        """Test02: Validateion when spwmap comprising dictionary
+
+        tid ="02"
+        infile=self.infile1
+        tsdcal(infile=infile, calmode='tsys', outfile='tsys_dic.cal')
+        #spwmap=[0,1,2,3,4,5,6,7,8,1,10,3,12,5,14,7,16]
+        spwmap={1:[9],3:[11],5:[13],7:[15]}
+        tsdcal(infile=infile, calmode='apply', spwmap=spwmap, applytable='tsys_dic.cal', outfile='')
+        """
 
 
 class tsdcal_test_skycal(unittest.TestCase):
