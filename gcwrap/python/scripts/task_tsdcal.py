44be2bc177cdbe6c22c9400782fb2ac18385cd48
masaya kuniyoshi <mkuniyos@aoc.nrao.edu>
2015-02-23 02:02:02 +0000

    New Development: No             JIRA Issue: Yes JIRA-6995/6992 related issue.         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: test_tsdcal   Put in Release Notes: No              Module(s): No            Description: tab=>4 spaces
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@32440 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/task_tsdcal.py
+++ gcwrap/python/scripts/task_tsdcal.py
@@ -9,81 +9,85 @@ def tsdcal(infile=None, calmode='tsys', fraction='10%', noff=-1,
            width=0.5, elongated=False, applytable='',interp='', spwmap={},
            field='', spw='', scan='', pol='', outfile='', overwrite=False): 
        
-	""" Externally specify calibration solutions af various types
-	"""
+    """ Externally specify calibration solutions af various types
+    """
 
-	#Python script
-	try:
+    #Python script
+    try:
 
-		if ((type(infile)==str) and (os.path.exists(infile))):
-			# don't need scr col for this
-			cb.open(filename=infile,compress=False,addcorr=False,addmodel=False)
-			cb.selectvis(spw=spw, scan=scan, field=field)
-			#cb.setsolve(type=calmodemap[calmode], table=outfile)
-			#cb.solve()
-		else:
-			raise Exception, 'Infile data set not found - please verify the name'
+        if ((type(infile)==str) and (os.path.exists(infile))):
+            # don't need scr col for this
+            cb.open(filename=infile,compress=False,addcorr=False,addmodel=False)
+            cb.selectvis(spw=spw, scan=scan, field=field)
+            #cb.setsolve(type=calmodemap[calmode], table=outfile)
+            #cb.solve()
+        else:
+            raise Exception, 'Infile data set not found - please verify the name'
 
-		if((type(calmode)==str) and calmode.lower() not in ['tsys', 'ps', 'apply']): 
-			raise Exception, 'A calmode must be either \'ps\' or \'tsys\' or \'apply\''
+        if((type(calmode)==str) and calmode.lower() not in ['tsys', 'ps', 'apply']): 
+            raise Exception, 'Calmode must be either \'ps\' or \'tsys\' or \'apply\''
 
-		if ((type(calmode)==str) and (calmode=='apply') and (applytable =='')):
-			raise Exception, 'A applytable name must be specified.'
+        if ((type(calmode)==str) and (calmode=='apply') and (applytable =='')):
+            raise Exception, 'Applytable name must be specified.'
 
-		if (not overwrite) and (os.path.exists(outfile)):
-			raise RuntimeError, 'Output file \'%s\' exists.'%(outfile)
+        if (not overwrite) and (os.path.exists(outfile)):
+            raise RuntimeError, 'Output file \'%s\' exists.'%(outfile)
 
-		if ((type(calmode)==str) and (calmode=='apply') and (applytable !='') and (os.path.exists(applytable))):
-			#applycal(vis=infile, docallib=False, gaintable=applytable, applymode='calonly')
-			if (outfile != ''):
-				raise UserWarning, 'Outfile is not generated but is added to MS as a new table, namely corrected data'
-				
-			if(type(spwmap)!=types.ListType and (type(spwmap)!=types.DictType)):
-				raise Exception, 'type of the spwmap must be list or dictionary.'
+
+        if ((type(calmode)==str) and (calmode=='apply') and (os.path.exists(applytable))):
+            #applycal(vis=infile, docallib=False, gaintable=applytable, applymode='calonly')
+            if (outfile != ''):
+                raise UserWarning, 'Outfile is not generated but is added to MS as a new table, namely corrected data'
+
+            if(type(spwmap)!=types.ListType and (type(spwmap)!=types.DictType)):
+                raise Exception, 'Spwmap type must be list or dictionary.'
+
+            if(len(spwmap)==0):
+                raise Exception, 'Spwmap must be specified.'
 			
-			#if (type(spwmap)==types.ListType):
-			#	applycal(vis=infile, docallib=False, gaintable=applytable, applymode='calonly')
-				#raise UserWarning, 'spwmap is list'
-			if (type(spwmap)==types.DictType):
+            #if (type(spwmap)==types.ListType):
+            #	applycal(vis=infile, docallib=False, gaintable=applytable, applymode='calonly')
+                #raise UserWarning, 'spwmap is list'
+            if (type(spwmap)==types.DictType):
 
-				MS = infile
-				tb.open(MS+'/SPECTRAL_WINDOW')
-				total_spwID=tb.nrows()
-				spwmap_dict = spwmap
-				spwmap_list=[]
-				for num in range(total_spwID):
-					spwmap_list.append(num)
+                MS = infile
+                tb.open(MS+'/SPECTRAL_WINDOW')
+                total_spwID=tb.nrows()
+                spwmap_dict = spwmap
+                spwmap_list=[]
+                for num in range(total_spwID):
+                    spwmap_list.append(num)
 
-				for key, value in spwmap_dict.items():
-					for v in value:
-						if v in spwmap_list:
-							index = spwmap_list.index(v)
-							spwmap_list[index]=int(key)
+                for key, value in spwmap_dict.items():
+                    for v in value:
+                        if v in spwmap_list:
+                            index = spwmap_list.index(v)
+                            spwmap_list[index]=int(key)
 
-				spwmap = spwmap_list
-					
-			applycal(vis=infile, spwmap=spwmap, docallib=False, gaintable=applytable, applymode='calonly')	
+                spwmap = spwmap_list
 
-		calmodemap = {'tsys': 'tsys','ps': 'sdsky_ps'}
+            applycal(vis=infile, spwmap=spwmap, docallib=False, gaintable=applytable, applymode='calonly')	
 
-		if(calmode!='apply'):
-			if len(outfile) == 0:
-				raise RuntimeError, 'Output file name must be specified.'
-			if calmode=='tsys':
-				if spw!='':
-					raise UserWarning,'The input of spw should be \'\'(empty) in this mode.'
-				cb.specifycal(caltable=outfile,time="",spw=spw,pol=pol,caltype=calmodemap[calmode])
-			else:
-				cb.selectvis(spw=spw, scan=scan, field=field)
-				cb.setsolve(type=calmodemap[calmode], table=outfile)
-				cb.solve()
+        calmodemap = {'tsys': 'tsys','ps': 'sdsky_ps'}
 
-	except UserWarning, instance:
-		print '*** Warning ***',instance
+        if(calmode!='apply'):
+            if len(outfile) == 0:
+                raise RuntimeError, 'Output file name must be specified.'
+            if calmode=='tsys':
+                if spw!='':
+                    raise UserWarning,'Input of spw should be \'\'(empty list) in this mode.'
+                cb.specifycal(caltable=outfile,time="",spw=spw,pol=pol,caltype=calmodemap[calmode])
+            else:
+                cb.selectvis(spw=spw, scan=scan, field=field)
+                cb.setsolve(type=calmodemap[calmode], table=outfile)
+                cb.solve()
 
-	except Exception, instance:
-		print '*** Error ***',instance
-		raise Exception, instance
+    except UserWarning, instance:
+        print '*** Warning ***',instance
 
-	finally:
-		cb.close()
+    except Exception, instance:
+        print '*** Error ***',instance
+        raise Exception, instance
+
+    finally:
+        cb.close()
