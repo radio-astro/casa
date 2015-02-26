0346ec7c24a61676d28ee242b6e61762e334119d
darrell schiebel <ds@nrao.edu>
2015-02-26 22:20:59 +0000

    New Development: no             JIRA Issue: no       Ready to Release: yes      Interface Changes: none What Interface Changed: none          Test Programs: none   Put in Release Notes: no
               Description: ouch, checked in build tree...
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@32495 ac9c0da5-131f-0410-9009-a9c195f183e6

--- asap/build/CMakeFiles/inst.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/inst"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/inst.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
