c5c2abf14afecafcc2a64d7e7b46da973fc1c9e5
kumar golap <kgolap@nrao.edu>
2015-03-04 00:49:36 +0000

    New Development: No             JIRA Issue: CAS-7109         Ready for Test: Yes/No      Interface Changes: Yes/No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: Describe your changes here...
    	Fixed some bugs
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@32550 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/tools/synthesis/linearmosaic_cmpt.cc
+++ gcwrap/tools/synthesis/linearmosaic_cmpt.cc
@@ -12,6 +12,7 @@
 #include <iostream>
 #include <linearmosaic_cmpt.h>
 #include <images/Images/PagedImage.h>
+#include <images/Images/TempImage.h>
 #include <synthesis/MeasurementEquations/LinearMosaic.h>
 #include <casa/Logging/LogIO.h>
 #include <casa/Utilities/Assert.h>
@@ -36,12 +37,14 @@ linearmosaic::linearmosaic()
 linearmosaic::~linearmosaic()
 {
   delete itsMos;
+  itsMos=NULL;
   delete itsLog;
+  itsLog=NULL;
 }
 
 
 bool 
-linearmosaic::defineimage(const int nx,
+linearmosaic::defineoutputimage(const int nx,
 		      const int ny, 
 		      const ::casac::variant& cellx,
 		      const ::casac::variant& celly,
@@ -51,6 +54,7 @@ linearmosaic::defineimage(const int nx,
   Bool rstat(False);
   try {
     if(itsMos) delete itsMos;
+    itsMos=NULL;
     casa::MDirection imcen;
     if(!casaMDirection(imagecenter, imcen)){
     	    throw(AipsError("Could not interprete phasecenter parameter"));
@@ -72,17 +76,54 @@ linearmosaic::defineimage(const int nx,
   }
   return rstat;
 }
+//-------------------------------------------------------------------------
+bool linearmosaic::setoutputimage(const  string& outputimage, const  string& outputweight,  int  imageweight){
+	 Bool rstat(False);
+	  try {
+		  	  itsMos->setOutImages(outputimage, outputweight, imageweight);
+
+		  	  rstat = True;
+		  } catch  (AipsError x) {
+
+		    *itsLog << LogIO::SEVERE << "Exception Reported: " << x.getMesg() << LogIO::POST;
+		    RETHROW(x);
+		  }
+		  return rstat;
+
+}
+
   // ---------------------------------------------------------------------
-  bool linearmosaic::makemosaic( const std::vector<std::string>& images,  const std::vector<std::string>& weightimages){
+  bool linearmosaic::makemosaic(const casac::variant& inimages,  const casac::variant& inweightimages, const int wgttype){
 
  Bool rstat(True);
   try {
 
-	  Vector<CountedPtr<ImageInterface<Float> > > im(1), wgt(1);
-	  for (int k=0; images.size(); ++k){
+	  casa::Vector<String> images, weightimages;
+	  images=toVectorString(toVectorString(inimages, "images"));
+	  weightimages=toVectorString(toVectorString(inweightimages, "weightimages"));
 
-		  im[0]=new casa::PagedImage<Float>(images[k]);
+	  if(images.nelements() != weightimages.nelements())
+		  throw(AipsError("number of images and weightimages not equal"));
+	  Vector<CountedPtr<ImageInterface<Float> > > im(1), wgt(1);
+	  for (uInt k=0; k < images.nelements(); ++k){
+
+
 		  wgt[0]=new casa::PagedImage<Float>(weightimages[k]);
+		  if(wgttype==1)
+			  im[0]=new casa::PagedImage<Float>(images[k]);
+		  else if(wgttype==0){
+			  PagedImage<Float>tmp(images[k]);
+			  im[0]=new casa::TempImage<Float>(tmp.shape(), tmp.coordinates());
+			  im[0]->copyData((LatticeExpr<Float>)(tmp*(*wgt[0])));
+		  }
+		  else if(wgttype==2){
+			  PagedImage<Float>tmp(images[k]);
+			  im[0]=new casa::TempImage<Float>(tmp.shape(), tmp.coordinates());
+			  im[0]->copyData((LatticeExpr<Float>)(iif(*(wgt[0]) > (0.0),
+			  						       (tmp/(*wgt[0])), 0)));
+		  }
+		  else
+			  throw(AipsError("image is weighted in an unknown fashion"));
 		  rstat=rstat && itsMos->makeMosaic(im, wgt);
 	  }
     
