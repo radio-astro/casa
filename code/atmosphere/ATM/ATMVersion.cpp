f062e561ae8cfaa96a6112504a6d8c106227e229
kana sugimoto <kana.sugi@nao.ac.jp>
2015-03-06 10:40:40 +0000

    New Development: No             JIRA Issue: Yes (CAS-7220)         Ready for Test: Yes      Interface Changes: No What Interface Changed:          Test Programs: at.getAtmVersion()   Put in Release Notes: No              Module(s): atmosphere            Description: Introduced Dominiques fix to getTag() ATM codes are now in sync with https://almajsvn.mtk.nao.ac.jp/p2/branches/ALMA-RELEASE-B/TELCAL/TelCalResults/Libraries/ATM@215336
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@32595 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/atmosphere/ATM/ATMVersion.cpp
+++ code/atmosphere/ATM/ATMVersion.cpp
@@ -12,7 +12,7 @@ ATM_NAMESPACE_BEGIN
 
 std::string getVersion() {return "0.7.0";}
 
-std::string getTag() {return "";}
+std::string getTag() {return "ATM-0_7_0";}
 
 std::string getALMARelease() {return "ALMA-2015.2";}
 
