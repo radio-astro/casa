b7651c43f90e020c822652bbe709880a277f09a8
john lightfoot <john.lightfoot@stfc.ac.uk>
2015-03-11 13:01:07 +0000

    first version
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@32650 ac9c0da5-131f-0410-9009-a9c195f183e6

---
+++ pipeline/pipeline/hif/tasks/flagcorrected/qa.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import
+import os
+
+import pipeline.infrastructure.logging as logging
+import pipeline.infrastructure.pipelineqa as pqa
+import pipeline.infrastructure.utils as utils
+import pipeline.qa.scorecalculator as qacalc
+
+from . import resultobjects
+
+LOG = logging.get_logger(__name__)
+
+
+class FlagcorrectedQAHandler(pqa.QAResultHandler):    
+    """
+    QA handler for an uncontained FlagcorrectedResult.
+    """
+    result_cls = resultobjects.FlagcorrectedResults
+    child_cls = None
+
+    def handle(self, context, result):
+        vis = result.inputs['vis']
+        ms = context.observing_run.get_ms(vis)
+    
+        scores = [qacalc.linear_score_fraction_newly_flagged(ms.basename, result.summaries)]
+        result.qa.pool[:] = scores
+
+        result.qa.all_unity_longmsg = 'No extra data was flagged in %s' % vis
+
+
+class FlagcorrectedListQAHandler(pqa.QAResultHandler):
+    """
+    QA handler for a list containing FlagcorrectedResults.
+    """
+    result_cls = list
+    child_cls = resultobjects.FlagcorrectedResults
+
+    def handle(self, context, result):
+        # collate the QAScores from each child result, pulling them into our
+        # own QAscore list
+        collated = utils.flatten([r.qa.pool for r in result]) 
+        result.qa.pool[:] = collated
+
+        vises = [r.inputs['vis'] for r in result]
+        longmsg = 'No extra data was flagged in %s' % utils.commafy(vises, 
+                                                                    quotes=False, 
+                                                                    conjunction='or')
+        result.qa.all_unity_longmsg = longmsg
