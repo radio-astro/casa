915b19b7fbc5530985fd5c93e4250014ce918c10
brian kent <bkent@nrao.edu>
2015-03-19 15:17:02 +0000

    Added math lib to flagdetervla
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@32748 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifv/tasks/flagging/flagdetervla.py
+++ pipeline/pipeline/hifv/tasks/flagging/flagdetervla.py
@@ -66,6 +66,7 @@ from __future__ import absolute_import
 import os
 import types
 import string
+import math
 
 import flaghelper
 
--- pipeline/pipeline/hifv/tasks/flagging/vlaagentflagger.py
+++ pipeline/pipeline/hifv/tasks/flagging/vlaagentflagger.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 import os
 import re
 import types
+import math
 
 import flaghelper
 
