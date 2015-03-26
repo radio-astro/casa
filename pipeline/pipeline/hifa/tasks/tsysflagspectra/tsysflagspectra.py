Fix of nmedian and derivative spectral Tsys flagging metrics. Proper handling of single Tsys measurement (i.e., one antenna and one Tsys measurement in the MS).
git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@32812 ac9c0da5-131f-0410-9009-a9c195f183e6

pipeline/pipeline/hifa/tasks/tsysflagspectra/tsysflagspectra.py
@@ -820,8 +820,8 @@ class TsysflagspectraWorker(basetask.StandardTaskTemplate):
                 tsysspectrum = tsysspectra[pol].last(description)
                 if tsysspectrum.pol==corr_type[pol][0]:
                     if spectrumstack is None:
-                        spectrumstack = tsysspectrum.data
-                        flagstack = tsysspectrum.flag
+                        spectrumstack = np.vstack((tsysspectrum.data,))
+                        flagstack = np.vstack((tsysspectrum.flag,))
                     else:
                         spectrumstack = np.vstack((tsysspectrum.data,
                           spectrumstack))
@@ -941,8 +941,8 @@ class TsysflagspectraWorker(basetask.StandardTaskTemplate):
                 tsysspectrum = tsysspectra[pol].last(description)
                 if tsysspectrum.pol==corr_type[pol][0]:
                     if spectrumstack is None:
-                        spectrumstack = tsysspectrum.data
-                        flagstack = tsysspectrum.flag
+                        spectrumstack = np.vstack((tsysspectrum.data,))
+                        flagstack = np.vstack((tsysspectrum.flag,))
                     else:
                         spectrumstack = np.vstack((tsysspectrum.data,
                           spectrumstack))