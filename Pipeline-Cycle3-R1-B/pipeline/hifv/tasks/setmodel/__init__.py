b73249dea9dcdaa3ebe5d9a1b2ea4fd1699ec22e
brian kent <bkent@nrao.edu>
2015-04-15 13:37:32 +0000

    Added rendering classes for correlation in the VLA pipeline setjy and applycal stages
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@33007 ac9c0da5-131f-0410-9009-a9c195f183e6

--- Pipeline-Cycle3-R1-B/pipeline/hifv/tasks/setmodel/__init__.py
+++ Pipeline-Cycle3-R1-B/pipeline/hifv/tasks/setmodel/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 from pipeline.hif.tasks.common import commonfluxresults
-import pipeline.hif.tasks.setmodel.renderer as super_renderer
+#import pipeline.hif.tasks.setmodel.renderer as super_renderer
 import pipeline.infrastructure.pipelineqa as pipelineqa
 import pipeline.infrastructure.renderer.qaadapter as qaadapter
 import pipeline.infrastructure.renderer.weblog as weblog
@@ -9,9 +9,11 @@ from .vlasetjy import VLASetjy
 from .setmodel import SetModel
 from .fluxgains import Fluxgains
 from . import qa
+from . import renderer
 
 pipelineqa.registry.add_handler(qa.VLASetjyQAHandler())
 pipelineqa.registry.add_handler(qa.VLASetjyListQAHandler())
 qaadapter.registry.register_to_calibration_topic(commonfluxresults.FluxCalibrationResults)
 
-weblog.add_renderer(VLASetjy, super_renderer.T2_4MDetailsSetjyRenderer(uri="vlasetjy.mako"))
+#weblog.add_renderer(VLASetjy, super_renderer.T2_4MDetailsSetjyRenderer(uri="vlasetjy.mako"))
+weblog.add_renderer(VLASetjy, renderer.T2_4MDetailsVLASetjyRenderer(uri="vlasetjy.mako"))
