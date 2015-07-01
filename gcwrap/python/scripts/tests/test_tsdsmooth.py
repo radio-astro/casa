f8ac9fda2bb22534ba0eb52cae5f29bda02722a8
takeshi nakazato <takeshi.nakazato@nao.ac.jp>
2015-07-01 06:45:50 +0000

    New Development: No             JIRA Issue: Yes CAS-7441         Ready for Test: Yes      Interface Changes: Yes/No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: Describe your changes here...
    Add more tests to tsdsmooth unit test.
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@33776 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/tests/test_tsdsmooth.py
+++ gcwrap/python/scripts/tests/test_tsdsmooth.py
@@ -4,6 +4,7 @@ import shutil
 import re
 import numpy
 import math
+from scipy import signal
 
 from __main__ import default
 from tasks import *
@@ -19,6 +20,15 @@ try:
     from testutils import copytree_ignore_subversion
 except:
     from tests.testutils import copytree_ignore_subversion
+
+def gaussian_kernel(nchan, kwidth):
+    sigma = kwidth / (2.0 * math.sqrt(2.0 * math.log(2.0)))
+    g = signal.gaussian(nchan, sigma, False)
+    g /= g.sum()
+    g0 = g[0]
+    g[:-1] = g[1:]
+    g[-1] = g0
+    return g
 
 class tsdsmooth_test_base(unittest.TestCase):
     """
@@ -38,6 +48,10 @@ class tsdsmooth_test_base(unittest.TestCase):
     # task execution result
     result = None
     
+    @property
+    def outfile(self):
+        return self.infile.rstrip('/') + '_out'
+
     # decorators
     @staticmethod
     def invalid_argument_case(func):
@@ -75,7 +89,79 @@ class tsdsmooth_test_base(unittest.TestCase):
                 self.assertIsNotNone(re.search(exception_pattern, message), msg='error message \'%s\' is not expected.'%(message))
             return _wrapper
         return wrapper
+    
+    def run_test(self, *args, **kwargs):
+        datacol_name = self.datacolumn.upper()
+        
+        if kwargs.has_key('kwidth'):
+            kwidth = kwargs['kwidth']
+        else:
+            kwidth = 5
+        
+        self.result = tsdsmooth(infile=self.infile, outfile=self.outfile, kernel='gaussian', datacolumn=self.datacolumn, **kwargs)
 
+        # sanity check
+        self.assertIsNone(self.result, msg='The task must complete without error')
+        self.assertTrue(os.path.exists(self.outfile), msg='Output file is not properly created.')
+
+        if kwargs.has_key('spw'):
+            spw = kwargs['spw']
+        else:
+            spw = ''
+        dd_selection = None
+        if len(spw) == 0:
+            expected_nrow = 2
+            with sdutil.tbmanager(self.infile) as tb:
+                data_in = tb.getvarcol(datacol_name)
+                flag_in = tb.getvarcol('FLAG')
+        else:
+            myms = gentools(['ms'])[0]
+            a = myms.msseltoindex(self.infile, spw=spw)
+            spw_selection = a['spw']
+            dd_selection = a['dd']
+            expected_nrow = len(spw_selection)
+            with sdutil.tbmanager(self.infile) as tb:
+                try:
+                    tsel = tb.query('DATA_DESC_ID IN %s'%(dd_selection.tolist()))
+                    data_in = tsel.getvarcol(datacol_name)
+                    flag_in = tsel.getvarcol('FLAG')
+                finally:
+                    tsel.close()
+
+        with sdutil.tbmanager(self.outfile) as tb:
+            nrow = tb.nrows()
+            data_out = tb.getvarcol(datacol_name)
+            flag_out = tb.getvarcol('FLAG')
+            
+        # verify nrow
+        self.assertEqual(nrow, expected_nrow, msg='Number of rows mismatch (expected %s actual %s)'%(expected_nrow, nrow))
+
+        # verify data
+        eps = 1.0e-6
+        for key in data_out.keys():
+            row_in = data_in[key]
+            flg_in = flag_in[key]
+            row_in[numpy.where(flg_in == True)] = 0.0
+            row_out = data_out[key]
+            self.assertEqual(row_in.shape, row_out.shape, msg='Shape mismatch in row %s'%(key))
+            
+            nchan = row_out.shape[1]
+            kernel_array = gaussian_kernel(nchan, kwidth)
+            expected = numpy.convolve(row_in[0,:,0], kernel_array, mode='same')
+            output = row_out[0,:,0]
+            zero_index = numpy.where(numpy.abs(expected) <= eps)
+            self.assertTrue(all(numpy.abs(output[zero_index]) < eps), msg='Failed to verify zero values: row %s'%(key))
+            nonzero_index= numpy.where(numpy.abs(expected) > eps)
+            diff = numpy.abs((output[nonzero_index] - expected[nonzero_index]) / expected[nonzero_index].max())
+            #print diff
+            #print output[nonzero_index]
+            #print expected[nonzero_index]
+            self.assertTrue(all(diff < eps), msg='Failed to verify nonzero values: row %s'%(key))
+            #print 'row_in', row_in[0,:,0].tolist()
+            #print 'gaussian', kernel_array.tolist()
+            #print 'expected', expected.tolist() 
+            #print 'result', row_out[0,:,0].tolist()
+        
     def _setUp(self, files, task):
         for f in files:
             if os.path.exists(f):
@@ -89,153 +175,59 @@ class tsdsmooth_test_base(unittest.TestCase):
             if os.path.exists(f):
                 shutil.rmtree(f)
                 
+    def setUp(self):
+        self._setUp([self.infile], tsdsmooth)
+
+    def tearDown(self):
+        self._tearDown([self.infile, self.outfile])
+
 class tsdsmooth_test_fail(tsdsmooth_test_base):   
     """
     Unit test for task tsdsmooth.
 
     The list of tests:
-    test_tsdsmooth_fail00 --- default parameters (raises an error)
-    test_tsdsmooth_fail01 --- invalid kernel type
-    test_tsdsmooth_fail02 --- invalid selection (empty selection result)
-    test_tsdsmooth_fail03 --- outfile exists (overwrite=False)
-    test_tsdsmooth_fail04 --- empty outfile
+    test_tsdsmooth_fail01 --- default parameters (raises an error)
+    test_tsdsmooth_fail02 --- invalid kernel type
+    test_tsdsmooth_fail03 --- invalid selection (empty selection result)
+    test_tsdsmooth_fail04 --- outfile exists (overwrite=False)
+    test_tsdsmooth_fail05 --- empty outfile
+    test_tsdsmooth_fail06 --- invalid data column name
     """
     invalid_argument_case = tsdsmooth_test_base.invalid_argument_case
     exception_case = tsdsmooth_test_base.exception_case
     
     infile = tsdsmooth_test_base.infile_data
     
-    @property
-    def outfile(self):
-        return self.infile.rstrip('/') + '_out'
-
-    def setUp(self):
-        self._setUp([self.infile], tsdsmooth)
-
-    def tearDown(self):
-        self._tearDown([self.infile, self.outfile])
-        
     @invalid_argument_case
-    def test_tsdsmooth_fail00(self):
-        """test_tsdsmooth_fail00 --- default parameters (raises an error)"""
+    def test_tsdsmooth_fail01(self):
+        """test_tsdsmooth_fail01 --- default parameters (raises an error)"""
         self.result = tsdsmooth()
         
     @invalid_argument_case
-    def test_tsdsmooth_fail01(self):
-        """test_tsdsmooth_fail01 --- invalid kernel type"""
+    def test_tsdsmooth_fail02(self):
+        """test_tsdsmooth_fail02 --- invalid kernel type"""
         self.result = tsdsmooth(infile=self.infile, kernel='normal', outfile=self.outfile)
         
     @exception_case(RuntimeError, 'No Spw ID\(s\) matched specifications')
-    def test_tsdsmooth_fail02(self):
-        """test_tsdsmooth_fail02 --- invalid selection (empty selection result)"""
+    def test_tsdsmooth_fail03(self):
+        """test_tsdsmooth_fail03 --- invalid selection (empty selection result)"""
         self.result = tsdsmooth(infile=self.infile, kernel='gaussian', outfile=self.outfile, spw='3')
         
     @exception_case(Exception, 'tsdsmooth_test\.ms_out exists\.')
-    def test_tsdsmooth_fail03(self):
-        """test_tsdsmooth_fail03 --- outfile exists (overwrite=False)"""
+    def test_tsdsmooth_fail04(self):
+        """test_tsdsmooth_fail04 --- outfile exists (overwrite=False)"""
         shutil.copytree(self.infile, self.outfile)
         self.result = tsdsmooth(infile=self.infile, kernel='gaussian', outfile=self.outfile, overwrite=False)
 
     @exception_case(Exception, 'outfile is empty\.')
-    def test_tsdsmooth_fail04(self):
-        """test_tsdsmooth_fail04 --- empty outfile"""
+    def test_tsdsmooth_fail05(self):
+        """test_tsdsmooth_fail05 --- empty outfile"""
         self.result = tsdsmooth(infile=self.infile, kernel='gaussian', outfile='')
-
-    def normal_case(**kwargs):
-        """
-        Decorator for the test case that is intended to verify
-        normal execution result.
-
-        selection --- data selection parameter as dictionary
-
-        Here, expected result is as follows:
-            - total number of rows is 12
-            - number of antennas is 2
-            - number of spectral windows is 2
-            - each (antenna,spw) pair has 3 rows
-            - expected sky data is a certain fixed value except completely
-              flagged channels
-              ANT, SPW, SKY
-              0     9   [1.0, 2.0, 3.0]
-              1     9   [7.0, 8.0, 9.0]
-              0    11   [4.0, 5.0, 6.0]
-              1    11   [10.0, 11.0, 12.0]
-            - channels 0~10 are flagged, each integration has sprious
-              ANT, SPW, SKY
-              0     9   [(511,512), (127,128), (383,384)]
-              1     9   [(511,512), (127,128), (383,384)]
-              0    11   [(511,512), (127,128), (383,384)]
-              1    11   [(511,512), (127,128), (383,384)]
-        """
-        def wrapper(func):
-            import functools
-            @functools.wraps(func)
-            def _wrapper(self):
-                func(self)
-
-                # sanity check
-                self.assertIsNone(self.result, msg='The task must complete without error')
-                self.assertTrue(os.path.exists(self.outfile), msg='Output file is not properly created.')
-
-                # verifying nrow
-                if len(kwargs) == 0:
-                    expected_nrow = 12
-                    antenna1_selection = None
-                    spw_selection = None
-                else:
-                    myms = gentools(['ms'])[0]
-                    myargs = kwargs.copy()
-                    if not myargs.has_key('baseline'):
-                        with sdutil.tbmanager(self.infile) as tb:
-                            antenna1 = numpy.unique(tb.getcol('ANTENNA1'))
-                            myargs['baseline'] = '%s&&&'%(','.join(map(str,antenna1)))
-                    a = myms.msseltoindex(self.infile, **myargs)
-                    antenna1_selection = a['antenna1']
-                    spw_selection = a['spw']
-                    expected_nrow = 3 * len(spw_selection) * len(antenna1_selection)
-                with sdutil.tbmanager(self.outfile) as tb:
-                    self.assertEqual(tb.nrows(), expected_nrow, msg='Number of rows mismatch (expected %s actual %s)'%(expected_nrow, tb.nrows()))
-
-                # verifying resulting sky spectra
-                expected_value = {0: {9: [1., 2., 3.],
-                                      11: [4., 5., 6.]},
-                                  1: {9: [7., 8., 9.],
-                                      11: [10., 11., 12.]}}
-                eps = 1.0e-6
-                for (ant,d) in expected_value.items():
-                    if antenna1_selection is not None and ant not in antenna1_selection:
-                        continue
-                    for (spw,val) in d.items():
-                        if spw_selection is not None and spw not in spw_selection:
-                            continue
-                        #print ant, spw, val
-                        construct = lambda x: '%s == %s'%(x)
-                        taql = ' && '.join(map(construct,[('ANTENNA1',ant), ('SPECTRAL_WINDOW_ID',spw)]))
-                        with sdutil.table_selector(self.outfile, taql) as tb:
-                            nrow = tb.nrows()
-                            self.assertEqual(nrow, 3, msg='Number of rows mismatch')
-                            for irow in xrange(tb.nrows()):
-                                expected = val[irow]
-                                self.assertGreater(expected, 0.0, msg='Internal Error')
-                                fparam = tb.getcell('FPARAM', irow)
-                                flag = tb.getcell('FLAG', irow)
-                                message_template = lambda x,y: 'Unexpected %s for antenna %s spw %s row %s (expected %s)'%(x,ant,spw,irow,y)
-                                self.assertTrue(all(flag[:,:10].flatten() == True), msg=message_template('flag status', True))
-                                self.assertTrue(all(flag[:,10:].flatten() == False), msg=message_template('flag status', False))
-                                fparam_valid = fparam[flag == False]
-                                error = abs((fparam_valid - expected) / expected) 
-                                self.assertTrue(all(error < eps), msg=message_template('sky data', expected))
-            return _wrapper
-        return wrapper
-            
-    
-#     @invalid_argument_case
-#     def test_ps00(self):
-#         """
-#         test_ps00 --- default parameters (raises an error)
-#         """
-#         self.result = tsdsmooth()
-
+        
+    @invalid_argument_case
+    def test_tsdsmooth_fail06(self):
+        """test_tsdsmooth_fail06 --- invalid data column name"""
+        self.result = tsdsmooth(infile=self.infile, outfile=self.outfile, kernel='gaussian', datacolumn='spectra')
 
 
 class tsdsmooth_test_complex(tsdsmooth_test_base):   
@@ -249,7 +241,31 @@ class tsdsmooth_test_complex(tsdsmooth_test_base):
     test_tsdsmooth_complex_select --- data selection (spw)
     test_tsdsmooth_complex_overwrite --- overwrite existing outfile (overwrite=True)
     """
+    exception_case = tsdsmooth_test_base.exception_case
     infile = tsdsmooth_test_base.infile_data
+    datacolumn = 'data'
+
+    @exception_case(RuntimeError, 'Desired column \(FLOAT_DATA\) not found in the input MS')
+    def test_tsdsmooth_complex_fail01(self):
+        """test_tsdsmooth_complex_fail01 --- non-existing data column (FLOAT_DATA)"""
+        self.result = tsdsmooth(infile=self.infile, outfile=self.outfile, kernel='gaussian', datacolumn='float_data')
+ 
+    def test_tsdsmooth_complex_gauss01(self):
+        """test_tsdsmooth_complex_gauss01 --- gaussian smoothing (kwidth 5)"""
+        self.run_test(kwidth=5)
+        
+    def test_tsdsmooth_complex_gauss02(self):
+        """test_tsdsmooth_complex_gauss02 --- gaussian smoothing (kwidth 3)"""
+        self.run_test(kwidth=3)
+
+    def test_tsdsmooth_complex_select(self):
+        """test_tsdsmooth_complex_select --- data selection (spw)"""
+        self.run_test(kwidth=5, spw='1')
+
+    def test_tsdsmooth_complex_overwrite(self):
+        """test_tsdsmooth_complex_overwrite --- overwrite existing outfile (overwrite=True)"""
+        shutil.copytree(self.infile, self.outfile)
+        self.run_test(kwidth=5, overwrite=True)
  
 class tsdsmooth_test_float(tsdsmooth_test_base):   
     """
@@ -262,9 +278,32 @@ class tsdsmooth_test_float(tsdsmooth_test_base):
     test_tsdsmooth_float_select --- data selection (spw)
     test_tsdsmooth_float_overwrite --- overwrite existing outfile (overwrite=True)
     """
+    exception_case = tsdsmooth_test_base.exception_case
     infile = tsdsmooth_test_base.infile_float
+    datacolumn = 'float_data'
     
+    @exception_case(RuntimeError, 'Desired column \(DATA\) not found in the input MS')
+    def test_tsdsmooth_float_fail01(self):
+        """test_tsdsmooth_complex_fail01 --- non-existing data column (DATA)"""
+        self.result = tsdsmooth(infile=self.infile, outfile=self.outfile, kernel='gaussian', datacolumn='data')
     
+    def test_tsdsmooth_float_gauss01(self):
+        """test_tsdsmooth_float_gauss01 --- gaussian smoothing (kwidth 5)"""
+        self.run_test(kwidth=5)
+        
+    def test_tsdsmooth_float_gauss02(self):
+        """test_tsdsmooth_float_gauss02 --- gaussian smoothing (kwidth 3)"""
+        self.run_test(kwidth=3)
+
+    def test_tsdsmooth_float_select(self):
+        """test_tsdsmooth_float_select --- data selection (spw)"""
+        self.run_test(kwidth=5, spw='1')
+
+    def test_tsdsmooth_float_overwrite(self):
+        """test_tsdsmooth_float_overwrite --- overwrite existing outfile (overwrite=True)"""
+        shutil.copytree(self.infile, self.outfile)
+        self.run_test(kwidth=5, overwrite=True)
+   
 def suite():
     return [tsdsmooth_test_fail, tsdsmooth_test_complex,
             tsdsmooth_test_float]
