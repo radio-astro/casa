373148e1c9bed4e750b22a601885dc1cfd5771cb
lindsey davis <ldavis@nrao.edu>
2015-07-02 16:18:07 +0000

    Added bdfflags to the CASA task interfaces.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@33814 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hif/cli/task_hif_restoredata.py
+++ pipeline/pipeline/hif/cli/task_hif_restoredata.py
@@ -6,7 +6,7 @@ import pipeline.h.cli.utils as utils
 
 
 def hif_restoredata(vis=None, session=None, products_dir=None, copytoraw=None,
-    rawdata_dir=None, lazy=None, pipelinemode=None, dryrun=None, acceptresults=None):
+    rawdata_dir=None, lazy=None, bdfflags=None, pipelinemode=None, dryrun=None, acceptresults=None):
 
     # create a dictionary containing all the arguments given in the
     # constructor
