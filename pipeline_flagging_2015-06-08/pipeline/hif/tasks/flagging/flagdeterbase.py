radio-astro/casa

commit 4071324a14065c1e6d5fa70578099942de83fab6

Update in ALMA flagging branch in hif and hifa to ensure pipeline-generated flagging commands follow the syntax of putting parameter values between quotes. Fixed mixed tab/space indentation for numerous lines in fluxcalflag.
git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@33842 ac9c0da5-131f-0410-9009-a9c195f183e6

pipeline_flagging_2015-06-08/pipeline/hif/tasks/flagging/flagdeterbase.py
@@ -395,7 +395,7 @@ class FlagDeterBase(basetask.StandardTaskTemplate):
                                                            inputs.ms.basename))
             else:
                 flag_cmds.extend(self._read_flagfile(inputs.fileonline))
-                flag_cmds.append('mode=summary name=online')
+                flag_cmds.append("mode='summary' name='online'")
         
         # flag template?
         if inputs.template:
@@ -405,59 +405,56 @@ class FlagDeterBase(basetask.StandardTaskTemplate):
                                                            inputs.ms.basename))
             else:
                 flag_cmds.extend(self._read_flagfile(inputs.filetemplate))
-                flag_cmds.append('mode=summary name=template')
+                flag_cmds.append("mode='summary' name='template'")
 
         # Flag autocorrelations?
         if inputs.autocorr:
-            flag_cmds.append('mode=manual autocorr=True reason=autocorr')
-            flag_cmds.append('mode=summary name=autocorr')
+            flag_cmds.append("mode='manual' autocorr='True' reason='autocorr'")
+            flag_cmds.append("mode='summary' name='autocorr'")
     
         # Flag shadowed antennas?
         if inputs.shadow:
-            flag_cmds.append('mode=shadow reason=shadow')
-            flag_cmds.append('mode=summary name=shadow')
+            flag_cmds.append("mode='shadow' reason='shadow'")
+            flag_cmds.append("mode='summary' name='shadow'")
         
         # Flag according to scan numbers and intents?
         if inputs.scan and inputs.scannumber != '':
-            flag_cmds.append('mode=manual scan=%s reason=scans' % inputs.scannumber)
-            flag_cmds.append('mode=summary name=scans')
+            flag_cmds.append("mode='manual' scan='%s' reason='scans'" % inputs.scannumber)
+            flag_cmds.append("mode='summary' name='scans'")
 
         # These must be separated due to the way agent flagging works
         if inputs.intents != '':
             #for intent in inputs.intents.split(','):
             #    if '*' not in intent:
             #        intent = '*%s*' % intent
-            #    flag_cmds.append('mode=manual intent=%s reason=intents' % intent)
-            #flag_cmds.append('mode=summary name=intents')
+            #    flag_cmds.append("mode='manual' intent='%s' reason='intents'" % intent)
+            #flag_cmds.append("mode='summary' name='intents'")
             for intent in inputs.intents.split(','):
                 #if '*' not in intent:
                     #intent = '*%s*' % intent
                 intentlist = list(inputs.ms.get_original_intent(intent))
                 for intent_item in intentlist:
-                    flag_cmds.append('mode=manual intent=%s reason=intents' % intent_item)
-            flag_cmds.append('mode=summary name=intents') 
-            
-            
-            
-
+                    flag_cmds.append("mode='manual' intent='%s' reason='intents'" % intent_item)
+            flag_cmds.append("mode='summary' name='intents'") 
+        
         # Flag spectral window edge channels?
         if inputs.edgespw: 
             to_flag = self._get_edgespw_cmds()
             if to_flag:
                 spw_arg = ','.join(to_flag)
-                flag_cmds.append('mode=manual spw=%s reason=edgespw' % spw_arg)
-                flag_cmds.append('mode=summary name=edgespw')
+                flag_cmds.append("mode='manual' spw='%s' reason='edgespw'" % spw_arg)
+                flag_cmds.append("mode='summary' name='edgespw'")
 
         # summarise the state before flagging rather than assuming the initial
         # state is unflagged
         if flag_cmds:
-            flag_cmds.insert(0, 'mode=summary name=before')
+            flag_cmds.insert(0, "mode='summary' name='before'")
 
         return flag_cmds
 
     def _get_autocorr_cmd (self):
-        #return 'mode=manual antenna=*&&&'
-        return 'mode=manual autocorr=True'
+        #return "mode='manual' antenna='*&&&'"
+        return "mode='manual' autocorr='True'"
 
     
     def verify_spw(self, spw):

pipeline_flagging_2015-06-08/pipeline/hif/tasks/importdata/importdata.py
@@ -377,9 +377,9 @@ class ImportData(basetask.StandardTaskTemplate):
                 f.writelines(['# Examples\n'])
                 f.writelines(['# Note: Do not put spaces inside the reason string !\n'])
                 f.writelines(['#\n'])
-                f.writelines(['# mode=manual correlation=YY antenna=DV01;DV08;DA43;DA48&DV23 spw=21:1920~2880  autocorr=False reason=\'bad_channels\'\n'])
-                f.writelines(['# mode=manual spw=\'25:0~3;122~127\' reason=\'stage8_2\'\n'])
-                f.writelines(['# mode=manual antenna=\'DV07\' timerange=\'2013/01/31/08:09:55.248~2013/01/31/08:10:01.296\' reason=\'quack\'\n'])
+                f.writelines(["# mode='manual' correlation='YY' antenna='DV01;DV08;DA43;DA48&DV23' spw='21:1920~2880' autocorr='False' reason='bad_channels'\n"])
+                f.writelines(["# mode='manual' spw='25:0~3;122~127' reason='stage8_2'\n"])
+                f.writelines(["# mode='manual' antenna='DV07' timerange='2013/01/31/08:09:55.248~2013/01/31/08:10:01.296' reason='quack'\n"])
                 f.writelines(['#\n'])
 
 

pipeline_flagging_2015-06-08/pipeline/hif/tasks/flagging/agentflagger.py
@@ -157,7 +157,7 @@ class AgentFlagger(basetask.StandardTaskTemplate):
 
         # parseAgents does not set the summary name, so we have to replace
         # it with the name extracted from the command. This should no
-	# longer be necesssary
+        # longer be necessary
         #for summary in summary_reps:
         #    _, agent_num = summary['name'].split('_')
         #    agent_num = int(agent_num)
@@ -219,7 +219,7 @@ class AgentFlagger(basetask.StandardTaskTemplate):
             # If the twice the number of flagged channels is greater than the
             # number of channels for a given spectral window, skip it.
             #frac_chan = int(round(fracspw * spw.num_channels + 0.5))
-	    # Make flagging less agressive
+            # Make flagging less aggressive
             frac_chan = int(round(fracspw * spw.num_channels))
             if 2*frac_chan >= spw.num_channels:
                 LOG.debug('Too many flagged channels %s for spw %s '
@@ -230,7 +230,7 @@ class AgentFlagger(basetask.StandardTaskTemplate):
             # left minimum as it is always channel 0.
             l_max = frac_chan - 1
             #r_min = spw.num_channels - frac_chan - 1
-	    # Fix high / low assymetry.
+            # Fix high / low asymmetry.
             r_min = spw.num_channels - frac_chan 
             r_max = spw.num_channels - 1
 
@@ -256,7 +256,7 @@ class AgentFlagger(basetask.StandardTaskTemplate):
                                                            inputs.ms.basename))
             else:
                 cmds.extend(self._read_flagfile(inputs.fileonline))
-                cmds.append('mode=summary name=online')
+                cmds.append("mode='summary' name='online'")
 
         # flag template?
         if inputs.template:
@@ -266,43 +266,43 @@ class AgentFlagger(basetask.StandardTaskTemplate):
                                                            inputs.ms.basename))
             else:
                 cmds.extend(self._read_flagfile(inputs.filetemplate))
-                cmds.append('mode=summary name=template')
+                cmds.append("mode='summary' name='template'")
 
         # Flag autocorrelations?
         if inputs.autocorr:
-            cmds.append('mode=manual autocorr=True reason=autocorr')
-            cmds.append('mode=summary name=autocorr')
+            cmds.append("mode='manual' autocorr='True' reason='autocorr'")
+            cmds.append("mode='summary' name='autocorr'")
 
         # Flag shadowed antennas?
         if inputs.shadow:
-            cmds.append('mode=shadow reason=shadow')
-            cmds.append('mode=summary name=shadow')
+            cmds.append("mode='shadow' reason='shadow'")
+            cmds.append("mode='summary' name='shadow'")
 
         # Flag according to scan numbers and intents?
         if inputs.scan and inputs.scannumber != '':
-            cmds.append('mode=manual scan=%s reason=scans' % inputs.scannumber)
-            cmds.append('mode=summary name=scans')
+            cmds.append("mode='manual' scan='%s' reason='scans'" % inputs.scannumber)
+            cmds.append("mode='summary' name='scans'")
 
         # These must be separated due to the way agent flagging works
         if inputs.intents != '':
             for intent in inputs.intents.split(','):
                 if '*' not in intent:
                     intent = '*%s*' % intent
-                cmds.append('mode=manual intent=%s reason=intents' % intent)
-            cmds.append('mode=summary name=intents')
+                cmds.append("mode='manual' intent='%s' reason='intents'" % intent)
+            cmds.append("mode='summary' name='intents'")
 
         # Flag spectral window edge channels?
         if inputs.edgespw: 
             to_flag = self._get_edgespw_cmds()
             if to_flag:
                 spw_arg = ','.join(to_flag)
-                cmds.append('mode=manual spw=%s reason=edgespw' % spw_arg)
-                cmds.append('mode=summary name=edgespw')
+                cmds.append("mode='manual' spw='%s' reason='edgespw'" % spw_arg)
+                cmds.append("mode='summary' name='edgespw'")
 
         # summarise the state before flagging rather than assuming the initial
         # state is unflagged
         if cmds:
-            cmds.insert(0, 'mode=summary name=before')
+            cmds.insert(0, "mode='summary' name='before'")
 
         LOG.trace('Flag commands for %s:\n%s' % (inputs.ms.basename, 
                                                  '\n'.join(cmds)))

commit fd426442e8657b636ffcfebdeddef0acf7ad848f

Fix for problem introduced in r33842 (putting values of flagging command parameters between quotes): Removed quotes from around value of "autocorr" parameter, since flagdata only accepts a boolean for "autocorr", not a string.
git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@33860 ac9c0da5-131f-0410-9009-a9c195f183e6

pipeline_flagging_2015-06-08/pipeline/hif/tasks/flagging/flagdeterbase.py
@@ -409,7 +409,7 @@ class FlagDeterBase(basetask.StandardTaskTemplate):
 
         # Flag autocorrelations?
         if inputs.autocorr:
-            flag_cmds.append("mode='manual' autocorr='True' reason='autocorr'")
+            flag_cmds.append("mode='manual' autocorr=True reason='autocorr'")
             flag_cmds.append("mode='summary' name='autocorr'")
     
         # Flag shadowed antennas?
@@ -454,7 +454,7 @@ class FlagDeterBase(basetask.StandardTaskTemplate):
 
     def _get_autocorr_cmd (self):
         #return "mode='manual' antenna='*&&&'"
-        return "mode='manual' autocorr='True'"
+        return "mode='manual' autocorr=True"
 
     
     def verify_spw(self, spw):

pipeline_flagging_2015-06-08/pipeline/hif/tasks/importdata/importdata.py
@@ -377,7 +377,7 @@ class ImportData(basetask.StandardTaskTemplate):
                 f.writelines(['# Examples\n'])
                 f.writelines(['# Note: Do not put spaces inside the reason string !\n'])
                 f.writelines(['#\n'])
-                f.writelines(["# mode='manual' correlation='YY' antenna='DV01;DV08;DA43;DA48&DV23' spw='21:1920~2880' autocorr='False' reason='bad_channels'\n"])
+                f.writelines(["# mode='manual' correlation='YY' antenna='DV01;DV08;DA43;DA48&DV23' spw='21:1920~2880' autocorr=False reason='bad_channels'\n"])
                 f.writelines(["# mode='manual' spw='25:0~3;122~127' reason='stage8_2'\n"])
                 f.writelines(["# mode='manual' antenna='DV07' timerange='2013/01/31/08:09:55.248~2013/01/31/08:10:01.296' reason='quack'\n"])
                 f.writelines(['#\n'])