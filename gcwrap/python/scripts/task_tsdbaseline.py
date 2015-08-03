bc75c1fc68a2a623af30581b16add50876b9eda5
masaya kuniyoshi <mkuniyos@aoc.nrao.edu>
2015-08-03 05:01:48 +0000

    New Development: No             JIRA Issue: Yes  JIRA 7445 ticket.         Ready for Test: No      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: No              Module(s): No            Description: enalbe blformat=text,and csv
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@34143 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/task_tsdbaseline.py
+++ gcwrap/python/scripts/task_tsdbaseline.py
@@ -1,13 +1,14 @@
 import numpy
 import os
+#from taskinit import *
 from taskinit import gentools, casalog
 import sdutil
-ms, sdms, tb = gentools(['ms','sdms','tb'])
+from collections import Counter
+ms,sdms,tb = gentools(['ms','sdms','tb'])
 
 def tsdbaseline(infile=None, datacolumn=None, antenna=None, field=None, spw=None, timerange=None, scan=None, pol=None, maskmode=None, thresh=None, avg_limit=None, edge=None, blmode=None, dosubtract=None, blformat=None, bloutput=None, bltable=None, blfunc=None, order=None, npiece=None, applyfft=None, fftmethod=None, fftthresh=None, addwn=None, rejwn=None, clipthresh=None, clipniter=None, blparam=None, verify=None, verbose=None, showprogress=None, minnrow=None, outfile=None, overwrite=None):
 
     casalog.origin('tsdbaseline')
-
     try:
         if os.path.exists(outfile) and not overwrite:
             raise Exception(outfile + ' exists.')
@@ -73,9 +74,228 @@ def tsdbaseline(infile=None, datacolumn=None, antenna=None, field=None, spw=None
                                antenna=str(antenna), timerange=timerange, 
                                scan=scan)
 
-            #if isinstance(blformat, list)
+            
+            #if(outfile==''):
+            #    outfile=infile
 
-            #if isinstance(bloutput, list)
+
+            
+
+            
+
+#            if isinstance(blformat, str):
+#                if('' == blformat):
+#                    #blformat=',,'
+#                    bloutput=',,'
+#                    print 'blformat'
+#                if('text' == blformat):
+#                    bloutput_tmp = infile + '_blparam.txt'
+#                    bloutput = ',' + bloutput_tmp + ','
+#                    if os.path.exists(bloutput):
+#                        raise Exception(fname + ' exists.') 
+
+
+
+
+
+            new_bloutput=[]
+
+
+            if isinstance(blformat, str):
+                blformat = [blformat]
+        
+            
+
+            if isinstance(bloutput, str):
+                bloutput = [bloutput] 
+
+
+
+
+            if isinstance(blformat, list):
+                if('' in blformat):
+                    blformat = ',,'
+                #elif(len(blformat) > 3 ):
+                #    raise ValueError, "The maximum size of blformat must be three."
+                elif(len(blformat) != len(bloutput)):
+                    raise ValueError, "The size of blfomat must be the same as that of bloutput."
+                elif [key for key, val in Counter(blformat).items() if val>1]:
+                    raise ValueError, "Same multiple blformats are not allowed."
+                else:
+                    if('csv' in blformat):
+                        if(''!= bloutput[blformat.index('csv')]):
+                            fname=bloutput[blformat.index('csv')]
+                            if os.path.exists(fname):
+                                raise Exception(fname + ' exists.') 
+                            new_bloutput.append(bloutput[blformat.index('csv')])
+                        else:
+                            fname= infile + '_blparam.csv'
+                            if os.path.exists(fname):
+                                raise Exception(fname + ' exists. ')######################################################
+                            new_bloutput.append(infile + '_blparam.csv')
+                    else:
+                         new_bloutput.append('')
+
+                    
+
+                    
+
+                    if('text' in blformat):
+                        if(''!= bloutput[blformat.index('text')]):
+                            new_bloutput.append(bloutput[blformat.index('text')])
+                        
+                            fname = bloutput[blformat.index('text')]
+
+                            
+                            if os.path.exists(fname):
+                                raise Exception(fname + ' exists.')
+
+
+                            f = open(fname, "w")
+
+                            blf = blfunc.lower()
+                            mm = maskmode.lower()
+                            if blf == 'poly':
+                                ftitles = ['Fit order']
+                            elif blf == 'chebyshev':
+                                ftitles = ['Fit order']
+                            elif blf == 'cspline':
+                                ftitles = ['nPiece']
+                                print 'task 2'
+                            else: # sinusoid
+                                ftitles = ['applyFFT', 'fftMethod', 'fftThresh', 'addWaveN', 'rejWaveN']
+                            if mm == 'auto':
+                                mtitles = ['Threshold', 'avg_limit', 'Edge']
+                            elif mm == 'list':
+                                mtitles = []
+                            else: # interact
+                                mtitles = []
+                            ctitles = ['clipThresh', 'clipNIter']
+
+                            #fkeys = getattr(self, '%s_keys'%(blfunc))
+                            #mkeys = getattr(self, '%s_keys'%(maskmode))
+                            
+                            outfile2=''
+                            if(outfile==''): 
+                                outfile2 = infile
+
+                            info = [['Source Table', infile],
+                                    ['Output File', outfile2]]
+                                    #['Function', blfunc],
+                                    #['Fit order', order]] 
+                            #for i in xrange(len(ftitles)):
+                            #    info.append([ftitles[i],getattr(self,fkeys[i])])
+                            #if blf != 'poly':
+                            #    for i in xrange(len(ctitles)):
+                            #        info.append([ctitles[i],clip_keys[i]])
+                            info.append(['Mask mode', maskmode])
+                            for i in xrange(len(mtitles)):
+                                info.append([mtitles[i],getattr(mkeys[i])])
+
+                            separator = "#"*60 + "\n"
+
+                            f.write(separator)
+                            for i in xrange(len(info)):
+                                f.write('%12s: %s\n'%tuple(info[i]))
+                            f.write(separator)
+                            f.close()
+
+                        
+                        
+                        else:
+                            #new_bloutput.append(infile + '_blparam.txt')
+                            new_bloutput.append(infile + '_blparam.txt')
+                            fname2  = infile + '_blparam.txt'
+                            if os.path.exists(fname2):
+                                raise Exception(fname2 + ' exists.')
+
+                            f = open(fname2, "w")
+
+                            blf = blfunc.lower()
+                            mm = maskmode.lower()
+                            if blf == 'poly':
+                                ftitles = ['Fit order']
+                            elif blf == 'chebyshev':
+                                ftitles = ['Fit order']
+                            elif blf == 'cspline':
+                                ftitles = ['nPiece']
+                            else: # sinusoid
+                                ftitles = ['applyFFT', 'fftMethod', 'fftThresh', 'addWaveN', 'rejWaveN']
+                            if mm == 'auto':
+                                mtitles = ['Threshold', 'avg_limit', 'Edge']
+                            elif mm == 'list':
+                                mtitles = []
+                            else: # interact
+                                mtitles = []
+                            ctitles = ['clipThresh', 'clipNIter']
+
+                            #fkeys = getattr(self, '%s_keys'%(blfunc))
+                            #mkeys = getattr(self, '%s_keys'%(maskmode))
+
+                            outfile2=''
+                            if(outfile==''):
+                                outfile2 = infile
+
+                            info = [['Source Table', infile],
+                                    ['Output File', outfile]]
+                                    #['Function', blfunc],
+                                    #['Fit order', order]] 
+                            #for i in xrange(len(ftitles)):
+                            #    info.append([ftitles[i],getattr(self,fkeys[i])])
+                            #if blf != 'poly':
+                            #    for i in xrange(len(ctitles)):
+                            #        info.append([ctitles[i],clip_keys[i]])
+                            info.append(['Mask mode', maskmode])
+                            for i in xrange(len(mtitles)):
+                                info.append([mtitles[i],getattr(mkeys[i])])
+
+                            separator = "#"*60 + "\n"
+
+                            f.write(separator)
+                            for i in xrange(len(info)):
+                                f.write('%12s: %s\n'%tuple(info[i]))
+                            f.write(separator)
+                            f.close()       
+                    
+                   
+                    else:
+                        new_bloutput.append('')
+
+
+
+                    if('table' in blformat):
+                        print 'blformat->',type(blformat), blformat
+                        print "blformat.index('table')", blformat.index('table')
+                        print "bloutput[blformat.index('table')])", bloutput[blformat.index('table')]
+
+                        if(''!= bloutput[blformat.index('table')]):
+                            fname = bloutput[blformat.index('table')]
+                            if os.path.exists(fname):
+                                raise Exception(fname + ' exists.')#############################################
+
+                            new_bloutput.append(bloutput[blformat.index('table')])
+                       
+                        else:
+                            fname = infile + '_blparam.bltable'
+                            if os.path.exists(fname):
+                                raise Exception(fname + ' exists.')#############################################
+                                print ''
+                            new_bloutput.append(fname)
+
+
+
+                    else:
+                        new_bloutput.append('')
+
+                    blformat = ",".join(blformat)
+                    bloutput = ",".join(new_bloutput)
+
+            
+            #print 'task blformat',type(blformat), blformat
+            #print 'task bloutput',type(bloutput), bloutput
+                
+
+
 
             params, func = prepare_for_baselining(blfunc=blfunc,
                                                   datacolumn=datacolumn,
@@ -93,7 +313,10 @@ def tsdbaseline(infile=None, datacolumn=None, antenna=None, field=None, spw=None
                                                   num_fitting_max=clipniter+1)
             if overwrite and os.path.exists(outfile):
                 os.system('rm -rf %s' % outfile)
-            func(**params)
+            
+            #print params
+            if(blformat != ',,'):
+                func(**params)
 
             if (blfunc == 'variable'):
                 restore_sorted_table_keyword(infile, sorttab_info)
@@ -103,12 +326,72 @@ def tsdbaseline(infile=None, datacolumn=None, antenna=None, field=None, spw=None
         raise Exception, instance
 
 
+
+#def __init_blfile(self):
+#    if self.bloutput:
+#        self.blfile = self.bl + "_blparam.txt"
+#
+#        if (self.blformat.lower() != "csv"):
+#            f = open(self.blfile, "w")
+#            
+#            blf = self.blfunc.lower()
+#            mm = self.maskmode.lower()
+#            if blf == 'poly':
+#                ftitles = ['Fit order']
+#            elif blf == 'chebyshev':
+#                ftitles = ['Fit order']
+#            elif blf == 'cspline':
+#                ftitles = ['nPiece']
+#            else: # sinusoid
+#                ftitles = ['applyFFT', 'fftMethod', 'fftThresh', 'addWaveN', 'rejWaveN']
+#            if mm == 'auto':
+#                mtitles = ['Threshold', 'avg_limit', 'Edge']
+#            elif mm == 'list':
+#                mtitles = []
+#            else: # interact
+#                mtitles = []
+#            ctitles = ['clipThresh', 'clipNIter']
+#
+#            fkeys = getattr(self, '%s_keys'%(self.blfunc))
+#            mkeys = getattr(self, '%s_keys'%(self.maskmode))
+#
+#            info = [['Source Table', self.infile],
+#                    ['Output File', self.outfile],
+#                    ['Function', self.blfunc]]
+#            for i in xrange(len(ftitles)):
+#                info.append([ftitles[i],getattr(self,fkeys[i])])
+#            if blf != 'poly':
+#                for i in xrange(len(ctitles)):
+#                    info.append([ctitles[i],self.clip_keys[i]])
+#            info.append(['Mask mode', self.maskmode])
+#            for i in xrange(len(mtitles)):
+#                info.append([mtitles[i],getattr(self,mkeys[i])])
+#
+#            separator = "#"*60 + "\n"
+#
+#            f.write(separator)
+#            for i in xrange(len(info)):
+#                f.write('%12s: %s\n'%tuple(info[i]))
+#            f.write(separator)
+#            f.close()
+#        else:
+#            self.blfile = ""
+
+
+
+
+
+
+
+
+
+
 def prepare_for_baselining(**keywords):
     params = {}
     funcname = 'subtract_baseline'
 
     blfunc = keywords['blfunc']
-    keys = ['datacolumn', 'outfile', 'bltable', 'dosubtract', 'spw', 'pol']
+    keys = ['datacolumn', 'outfile', 'bltable', 'blformat','bloutput', 'dosubtract', 'spw', 'pol']
     if blfunc in ['poly', 'chebyshev']:
         keys += ['blfunc', 'order', 'clip_threshold_sigma', 'num_fitting_max']
     elif blfunc == 'cspline':
