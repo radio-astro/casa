a849b6f4fc06e5d57f7d9e76fc496480f9d04997
pam ford <pam.ford@nrao.edu>
2015-08-11 21:56:33 +0000

    New Development: No             JIRA Issue: CAS-7785         Ready for Test: Yes      Interface Changes: No What Interface Changed:          Test Programs:   Put in Release Notes: No              Module(s): plotms            Description: Fixed bug -- set plotms showgui to False before any other pm calls so that GUI does not flicker on screen
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@34227 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/task_plotms.py
+++ gcwrap/python/scripts/task_plotms.py
@@ -257,16 +257,16 @@ def plotms(vis=None,
             if ( gridrows*gridcols - 1 < plotindex):
                 casalog.post("A nonzero plotindex is not valid when resetting the page grid", "SEVERE")
                 return False'''
-        if pm.isDrawing() and clearplots:
-            casalog.post("Plotms is running in GUI mode and cannot be run again until the current drawing completes.")
-            print "ERROR: Plotms is running in GUI mode and cannot be run again until the current drawing completes."
-            return False
-
         #Determine whether this is going to be a scripting client or a full GUI supporting
         #user interaction.  This must be done before other properties are set because it affects
         #the constructor of plotms.
         pm.setShowGui( showgui )
         
+        if pm.isDrawing() and clearplots:
+            casalog.post("Plotms is running in GUI mode and cannot be run again until the current drawing completes.")
+            print "ERROR: Plotms is running in GUI mode and cannot be run again until the current drawing completes."
+            return False
+
         #Clear any existing plots.
         if clearplots:
             pm.clearPlots()
