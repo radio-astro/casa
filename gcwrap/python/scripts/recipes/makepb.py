7fc33af8dc00aff61c68238977cdb61fd715b577
urvashi rao <rurvashi@nrao.edu>
2015-09-10 05:06:13 +0000

    New Development: Yes/No             JIRA Issue: No/Yes List JIRA ticket.         Ready for Test: Yes/No      Interface Changes: Yes/No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: Added makePB script for cas-7664
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/release-4_5@34627 ac9c0da5-131f-0410-9009-a9c195f183e6

---
+++ gcwrap/python/scripts/recipes/makepb.py
@@ -0,0 +1,77 @@
+###
+###  Make a PB
+###  - MS and selections
+###  - Defineimage (try to just reuse coordinatesystem)
+###  - im.makePB
+
+#!/usr/bin/env python
+
+import os;
+import shutil;
+from  casac import *;
+from tasks import imregrid
+
+def makePB(vis='',field='',spw='',timerange='',uvrange='',antenna='',observation='',intent='',scan='', imtemplate='',outimage='',pblimit=0.2):
+    
+    """ Make a PB image using the imager tool, onto a specified image coordinate system 
+
+         This function can be used along with tclean to make .pb images for gridders that
+         do not already do it (i.e. other than mosaic, awproject)
+
+         This script takes an image to use as a template coordinate system, 
+         attempts to set up an identical coordinate system with the old imager tool, 
+         makes a PB for the telescope listed in the MS observation subtable, and 
+         regrids it (just in case) to the target coordinate system). This can be used for
+         single fields and mosaics.
+
+    """
+
+    tb = casac.table()
+    im = casac.imager()
+    ia = casac.image()
+    me = casac.measures()
+    qa = casac.quanta()
+
+    print 'MAKEPB : Making a PB image using the imager tool'
+
+    tb.open(vis+'/OBSERVATION')
+    tel = tb.getcol('TELESCOPE_NAME')[0]
+    tb.close()
+
+    print 'MAKEPB : Making PB for ', tel
+
+    ia.open(imtemplate)
+    csysa = ia.coordsys()
+    csys = csysa.torecord()
+    shp = ia.shape()
+    ia.close()
+    stokes = 'I'
+    dirs = csys['direction0']
+    phasecenter = me.direction(dirs['system'], qa.quantity(dirs['crval'][0],dirs['units'][0]) , qa.quantity(dirs['crval'][1],dirs['units'][1]) )
+    cellx=qa.quantity(dirs['cdelt'][0],dirs['units'][0])
+    celly=qa.quantity(dirs['cdelt'][1],dirs['units'][1])
+    nchan=shp[3]
+    start=qa.quantity( csysa.referencevalue()['numeric'][3], csysa.units()[3] )  ## assumes refpix is zero
+    step=qa.quantity( csysa.increment()['numeric'][3], csysa.units()[3] )
+
+    print 'MAKEPB : Starting imager tool'
+
+    im.open(vis)
+    im.selectvis(field=field,spw=spw,time=timerange,intent=intent,scan=scan,uvrange=uvrange,baseline=antenna,observation=observation)
+    im.defineimage(nx=shp[0],ny=shp[0],phasecenter=phasecenter,cellx=qa.tos(cellx),celly=qa.tos(celly),nchan=nchan,start=start,step=step)
+    im.setvp(dovp=True,telescope=tel)
+    im.makeimage(type='pb',image=outimage+'.tmp')
+    im.close()
+
+    print 'MAKEPB : Regrid to desired coordinate system'
+    
+    imregrid(imagename=outimage+'.tmp', template=imtemplate,output=outimage,overwrite=True,asvelocity=False)
+
+    shutil.rmtree(outimage+'.tmp')
+
+    print 'MAKEPB : Set mask to pblimit'
+
+    ia.open(outimage)
+    ia.calcmask( outimage+'>'+str(pblimit) )
+    ia.close()
+
