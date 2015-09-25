6e7e707af720398f7ec53c82f329a20a81fdc391
urvashi rao <rurvashi@nrao.edu>
2015-09-25 04:51:46 +0000

    New Development: Yes/No             JIRA Issue: No/Yes List JIRA ticket.         Ready for Test: Yes/No      Interface Changes: Yes/No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: Doc edits. added stopreason for parallel runs
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@34833 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/refimagerhelper.py
+++ gcwrap/python/scripts/refimagerhelper.py
@@ -1008,7 +1008,7 @@ class PyParallelDeconvolver(PySynthesisImager):
         stopflag = self.IBtool.cleanComplete()
         print 'Converged : ', stopflag
         if( stopflag>0 ):
-            stopreasons = ['iteration limit', 'threshold', 'force stop']
+            stopreasons = ['iteration limit', 'threshold', 'force stop','no change in peak residual across two major cycles']
             casalog.post("Reached global stopping criterion : " + stopreasons[stopflag-1], "INFO")
         return (stopflag>0)
 
