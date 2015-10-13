ae038838b285b1ba8ae9a6bf1de2702d13dd2625
darrell schiebel <ds@nrao.edu>
2015-10-13 19:54:28 +0000

    New Development: no             JIRA Issue: no       Ready to Release: yes      Interface Changes: none What Interface Changed: none          Test Programs: none   Put in Release Notes: no
               Description: add potential replacement (for after C++11 conversion of threads)
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@35013 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/stdcasa/thread/Barrier.h
+++ code/stdcasa/thread/Barrier.h
@@ -49,4 +49,38 @@ namespace casa {
     };
 }
 
+#if 0
+//
+// could switch to this (from Jim) which allows for sequential applications of the barrier...
+// after we switch to C++11 threads (from boost threads)...
+//
+class Barrier {
+public:
+    explicit Barrier(std::size_t iCount) : 
+        mThreshold(iCount), 
+        mCount(iCount), 
+        mGeneration(0) {
+    }
+
+    void Wait() {
+        auto lGen = mGeneration;
+        std::unique_lock<std::mutex> lLock{mMutex};
+        if (!--mCount) {
+            mGeneration++;
+            mCount = mThreshold;
+            mCond.notify_all();
+        } else {
+            mCond.wait(lLock, [this, lGen] { return lGen != mGeneration; });
+        }
+    }
+
+private:
+    std::mutex mMutex;
+    std::condition_variable mCond;
+    std::size_t mThreshold;
+    std::size_t mCount;
+    std::size_t mGeneration;
+};
+#endif
+
 #endif
