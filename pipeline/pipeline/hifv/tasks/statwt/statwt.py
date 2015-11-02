737bb227db65277dcb79a26d70965d18a063d3eb
brian kent <bkent@nrao.edu>
2015-11-02 21:34:41 +0000

    Code cleanup of VLA statwt task.  Removed unused import statements.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@35201 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifv/tasks/statwt/statwt.py
+++ pipeline/pipeline/hifv/tasks/statwt/statwt.py
@@ -2,23 +2,19 @@ from __future__ import absolute_import
 
 import pipeline.infrastructure.basetask as basetask
 from pipeline.infrastructure import casa_tasks
-import pipeline.infrastructure.casatools as casatools
-import pipeline.domain.measures as measures
 import pipeline.infrastructure as infrastructure
-import pipeline.infrastructure.callibrary as callibrary
 
 LOG = infrastructure.get_logger(__name__)
 
 # CALCULATE DATA WEIGHTS BASED ON ST. DEV. WITHIN EACH SPW
 # use statwt
 
+
 class StatwtInputs(basetask.StandardInputs):
     @basetask.log_equivalent_CASA_call
     def __init__(self, context, vis=None):
         # set the properties to the values given as input arguments
         self._init_properties(vars())
-    
-    
     
 
 class StatwtResults(basetask.Results):
@@ -33,18 +29,18 @@ class StatwtResults(basetask.Results):
             s += '%s performed. ' % str(job)
         return s 
 
+
 class Statwt(basetask.StandardTaskTemplate):
     Inputs = StatwtInputs
     
     def prepare(self):
-        
 
         statwt_result = self._do_statwt()
         
         return StatwtResults([statwt_result])
     
     def analyse(self, results):
-	return results
+        return results
     
     def _do_statwt(self):
         
@@ -61,8 +57,3 @@ class Statwt(basetask.StandardTaskTemplate):
         job = casa_tasks.statwt(**task_args)
             
         return self._executor.execute(job)
-    
-    
-    
-    
-    
