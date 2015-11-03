39c1c50429b7c0bbd24e1693253ac31a85873a4f
brian kent <bkent@nrao.edu>
2015-11-03 16:03:50 +0000

    Code cleanup of VLA tasks.finalcals.applycals
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@35213 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifv/tasks/finalcals/applycals.py
+++ pipeline/pipeline/hifv/tasks/finalcals/applycals.py
@@ -2,20 +2,17 @@ from __future__ import absolute_import
 
 import os
 import pipeline.infrastructure.basetask as basetask
-from pipeline.infrastructure import casa_tasks
-import pipeline.infrastructure.casatools as casatools
-import pipeline.domain.measures as measures
 import pipeline.infrastructure as infrastructure
 import pipeline.infrastructure.callibrary as callibrary
 
 from pipeline.hifv.tasks.flagging.uncalspw import Uncalspw
 
-import itertools
 
 from pipeline.hif.tasks import applycal
 
 
 LOG = infrastructure.get_logger(__name__)
+
 
 class ApplycalsInputs(basetask.StandardInputs):
     @basetask.log_equivalent_CASA_call
@@ -23,16 +20,17 @@ class ApplycalsInputs(basetask.StandardInputs):
         # set the properties to the values given as input arguments
         self._init_properties(vars())
 
+
 class ApplycalsResults(basetask.Results):
     def __init__(self, applied=[]):
         """
-        Construct and return a new ApplycalResults.
+        Construct and return a new ApplycalsResults.
         
         The resulting object should be initialized with a list of
         CalibrationTables corresponding to the caltables applied by this task.
 
         :param applied: caltables applied by this task
-        :type applied: list of :class:`~pipeline.domain.caltable.CalibrationTable`
+        :type applied: list of :class: ~pipeline.domain.caltable.CalibrationTable
         """
         super(ApplycalResults, self).__init__()
         self.applied = set()
@@ -40,7 +38,7 @@ class ApplycalsResults(basetask.Results):
         
     def merge_with_context(self, context):
         """
-        See :method:`~pipeline.Results.merge_with_context`
+        See :method: ~pipeline.Results.merge_with_context
         """
         if not self.applied:
             LOG.error('No results to merge')
@@ -62,24 +60,25 @@ class ApplycalsResults(basetask.Results):
                     name=caltable.gaintable, spw=caltable.spw, 
                     vis=os.path.basename(caltable.vis))
         return s
-        
+
+
 class Applycals(basetask.StandardTaskTemplate):
     Inputs = ApplycalsInputs
     
     def prepare(self):
         
-        #Run applycal
+        # Run applycal
         applycal_results = self._do_applycal(self.inputs.context)
    
-        ##inputs = Uncalspw.Inputs(self.inputs.context, bpcaltable='finalBPcal.b', delaycaltable='finaldelay.k')
-        ##task = Uncalspw(inputs)
-        ##result = self._executor.execute(task)
+        # inputs = Uncalspw.Inputs(self.inputs.context, bpcaltable='finalBPcal.b', delaycaltable='finaldelay.k')
+        # task = Uncalspw(inputs)
+        # result = self._executor.execute(task)
         
         
         return applycal_results
     
     def analyse(self, results):
-	return results
+        return results
     
     def _do_applycal(self, context=None):
         """Run CASA task applycal"""
@@ -89,7 +88,7 @@ class Applycals(basetask.StandardTaskTemplate):
         
         tablesToAdd = ['finaldelay.k', 'finalBPcal.b', 'averagephasegain.g', 'finalampgaincal.g', 'finalphasegaincal.g']
         
-        tablesToAdd = [basevis +'.' + table for table in tablesToAdd]
+        tablesToAdd = [basevis + '.' + table for table in tablesToAdd]
         
         for addcaltable in tablesToAdd:
             print addcaltable
@@ -99,12 +98,12 @@ class Applycals(basetask.StandardTaskTemplate):
             self.inputs.context.callibrary.add(calto, calfrom)
         
         applycal_inputs = applycal.Applycal.Inputs(self.inputs.context,
-            vis = self.inputs.vis,
-            field = '',
-            spw = '',
-            intent = '',
-            flagbackup = False,
-            calwt = False)
+                                                   vis=self.inputs.vis,
+                                                   field='',
+                                                   spw='',
+                                                   intent='',
+                                                   flagbackup=False,
+                                                   calwt=False)
         
         applycal_task = applycal.Applycal(applycal_inputs)
         
