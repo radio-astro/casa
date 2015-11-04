741b46cdaa26517f886c9a7ba05e18884aa9f171
brian kent <bkent@nrao.edu>
2015-11-04 16:29:33 +0000

    Cleanup of imports and usage of domain ms object in VLA  targetflag.py
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@35236 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifv/tasks/flagging/targetflag.py
+++ pipeline/pipeline/hifv/tasks/flagging/targetflag.py
@@ -2,30 +2,20 @@ from __future__ import absolute_import
 
 import pipeline.infrastructure.basetask as basetask
 from pipeline.infrastructure import casa_tasks
-import pipeline.infrastructure.casatools as casatools
-import pipeline.domain.measures as measures
 import pipeline.infrastructure as infrastructure
-import pipeline.infrastructure.callibrary as callibrary
 
-import itertools
-
-from pipeline.hif.tasks import gaincal
-from pipeline.hif.tasks import bandpass
-from pipeline.hif.tasks import applycal
-from pipeline.hifv.heuristics import getCalFlaggedSoln, getBCalStatistics
 
 LOG = infrastructure.get_logger(__name__)
 
 # CHECKING FLAGGING OF ALL CALIBRATORS
 # use rflag mode of flagdata
 
+
 class TargetflagInputs(basetask.StandardInputs):
     @basetask.log_equivalent_CASA_call
     def __init__(self, context, vis=None):
         # set the properties to the values given as input arguments
         self._init_properties(vars())
-    
-    
     
 
 class TargetflagResults(basetask.Results):
@@ -40,28 +30,27 @@ class TargetflagResults(basetask.Results):
             s += '%s performed. Statistics to follow?' % str(job)
         return s 
 
+
 class Targetflag(basetask.StandardTaskTemplate):
     Inputs = TargetflagInputs
     
     def prepare(self):
         
-        #Default values
-        m = self.inputs.context.observing_run.measurement_sets[0]
-        #corrstring = self.inputs.context.evla['msinfo'][m.name].corrstring
+        # Default values
+        m = self.inputs.context.observing_run.get_ms(self.inputs.vis)
+        # corrstring = self.inputs.context.evla['msinfo'][m.name].corrstring
         corrstring = m.get_vla_corrstring()
         
         method_args = {'field'       : '',
                        'correlation' : 'ABS_' + corrstring,
                        'scan'        : ''}
-        
 
-        
         rflag_result = self._do_rflag(**method_args)
         
         return TargetflagResults([rflag_result])
     
     def analyse(self, results):
-	return results
+        return results
     
     def _do_rflag(self, field=None, correlation=None, scan=None):
         
@@ -85,8 +74,3 @@ class Targetflag(basetask.StandardTaskTemplate):
         job = casa_tasks.flagdata(**task_args)
             
         return self._executor.execute(job)
-    
-    
-    
-    
-    
