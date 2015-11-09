039fa81876fb703c19061bce87a0408880d9a879
dave mehringer <dmehring@zia.aoc.nrao.edu>
2015-11-09 19:47:06 +0000

    New Development: Yes/No             JIRA Issue: No         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: google tests for MSChecker
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@35294 ac9c0da5-131f-0410-9009-a9c195f183e6

---
+++ code/msvis/MSVis/test/MSCheckerTest.h
@@ -0,0 +1,61 @@
+//# MSCheckerFixture.h
+//# Copyright (C) 1998,1999,2000,2001
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+
+#ifndef CODE_MSVIS_MSVIS_TEST_MSCHECKERFIXTURE_H_
+#define CODE_MSVIS_MSVIS_TEST_MSCHECKERFIXTURE_H_
+
+#include <gtest/gtest.h>
+
+#include <casacore/casa/BasicSL/String.h>
+#include <casacore/ms/MeasurementSets/MeasurementSet.h>
+
+#include <memory>
+
+using namespace std;
+using namespace casacore;
+
+namespace test {
+
+class MSCheckerTest : public ::testing::Test {
+
+public:
+	MSCheckerTest();
+	virtual ~MSCheckerTest();
+
+	virtual void SetUp();
+
+	virtual void TearDown();
+
+	unique_ptr<MeasurementSet> testMS;
+
+private:
+	String _dataPath;
+
+};
+
+#endif /* CODE_MSVIS_MSVIS_TEST_MSCHECKERFIXTURE_H_ */
+
+}
---
+++ code/msvis/MSVis/test/MSChecker_Gtest.cc
@@ -0,0 +1,60 @@
+#include <gtest/gtest.h>
+
+#include <msvis/MSVis/MSChecker.h>
+
+#include <casacore/casa/Containers/ValueHolder.h>
+#include <casacore/tables/Tables/TableProxy.h>
+#include <iostream>
+#include <msvis/MSVis/test/MSCheckerTest.h>
+
+using namespace std;
+
+using namespace casacore;
+
+using namespace casa;
+
+MeasurementSet _testMS;
+
+namespace test {
+
+
+TEST_F(MSCheckerTest, CheckIntegrityTestClean) {
+	MSChecker checker(*testMS);
+	// not throwing an exception indicates this passes
+	checker.checkReferentialIntegrity();
+}
+
+TEST_F(MSCheckerTest, CheckIntegrityTestBadDDID) {
+	TableProxy proxy(*testMS);
+	proxy.putCell("DATA_DESC_ID", Vector<Int>(1, 20), ValueHolder(9));
+	MSChecker checker(*testMS);
+	EXPECT_THROW(checker.checkReferentialIntegrity(), AipsError);
+}
+
+TEST_F(MSCheckerTest, CheckIntegrityTestBadFieldID) {
+	TableProxy proxy(*testMS);
+	proxy.putCell("FIELD_ID", Vector<Int>(1, 20), ValueHolder(9));
+	MSChecker checker(*testMS);
+	EXPECT_THROW(checker.checkReferentialIntegrity(), AipsError);
+}
+
+TEST_F(MSCheckerTest, CheckIntegrityTestBadAntenna1) {
+	TableProxy proxy(*testMS);
+	proxy.putCell("ANTENNA1", Vector<Int>(1, 20), ValueHolder(9));
+	MSChecker checker(*testMS);
+	EXPECT_THROW(checker.checkReferentialIntegrity(), AipsError);
+}
+
+TEST_F(MSCheckerTest, CheckIntegrityTestBadAntenna2) {
+	TableProxy proxy(*testMS);
+	proxy.putCell("ANTENNA2", Vector<Int>(1, 20), ValueHolder(9));
+	MSChecker checker(*testMS);
+	EXPECT_THROW(checker.checkReferentialIntegrity(), AipsError);
+}
+}
+
+int main (int nArgs, char * args []) {
+    ::testing::InitGoogleTest(& nArgs, args);
+    cout << "MSChecker test " << endl;
+    return RUN_ALL_TESTS();
+}
