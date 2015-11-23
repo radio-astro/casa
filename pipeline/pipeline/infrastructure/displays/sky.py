a4e7698ed27ff8db0b4ceb067ec2f6c3287c96fd
dirk muders <dmuders@mpifr-bonn.mpg.de>
2015-11-23 16:02:17 +0000

    Avoid spurious edge spikes in MOM8 displays.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@35462 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/infrastructure/displays/sky.py
+++ pipeline/pipeline/infrastructure/displays/sky.py
@@ -99,6 +99,13 @@ class SkyDisplay(object):
         with casatools.ImageReader(result) as image:
             if (collapseFunction == 'center'):
                 collapsed = image.collapse(function='mean', chans=str(image.summary()['shape'][3]/2), axes=[2,3])
+            elif (collapseFunction == 'max'):
+                shape = image.shape()
+                centerMask = ''.join(['T' if item==True else 'F' for item in image.getchunk([shape[0]/2, shape[1]/2, 0, 0], [shape[0]/2, shape[1]/2, 0, shape[3]], getmask=True).flatten()])
+                # Set channel range to avoid spurious edge spikes
+                c1 = centerMask.find('T') + 1
+                c2 = centerMask.rfind('T') - 1
+                collapsed = image.collapse(function='max', chans='%d~%d' % (c1, c2), axes=[2,3])
             else:
                 collapsed = image.collapse(function=collapseFunction, axes=[2,3])
             name = image.name(strippath=True)
