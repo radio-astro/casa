cf5767e32eb8c6e332c3cde435bb5063af0ccd15
lindsey davis <ldavis@nrao.edu>
2015-12-02 15:56:56 +0000

    Added the refant parameter to the CASA task interface and changed the default value of smooth from '1s' to  ''.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@35531 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifa/cli/task_hifa_wvrgcalflag.py
+++ pipeline/pipeline/hifa/cli/task_hifa_wvrgcalflag.py
@@ -8,7 +8,7 @@ import pipeline.h.cli.utils as utils
 def hifa_wvrgcalflag(vis=None, caltable=None, hm_toffset=None, toffset=None,
   segsource=None, sourceflag=None, hm_tie=None, tie=None, nsol=None, disperse=None,
   wvrflag=None, hm_smooth=None, smooth=None, scale=None, maxdistm=None,
-  minnumants=None, mingoodfrac=None, flag_intent=None, qa_intent=None,
+  minnumants=None, mingoodfrac=None, refant=None, flag_intent=None, qa_intent=None,
   qa_bandpass_intent=None, accept_threshold=None, flag_hi=None,
   fhi_limit=None, fhi_minsample=None, pipelinemode=None, dryrun=None,
   acceptresults=None):
