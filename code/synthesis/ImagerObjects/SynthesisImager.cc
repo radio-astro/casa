55d9a24ff01aeb817173f3c54351f9a94f716ec5
urvashi rao <rurvashi@nrao.edu>
2015-12-09 23:25:17 +0000

    another table cache leftover fix
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@35654 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/synthesis/ImagerObjects/SynthesisImager.cc
+++ code/synthesis/ImagerObjects/SynthesisImager.cc
@@ -1840,12 +1840,16 @@ namespace casa { //# NAMESPACE CASA - BEGIN
     //vs_p= new VisSet(blockMSSel_p, sort, noChanSel, useModelCol_p);
     if(!writeAccess){
 
-    	rvi_p=new ROVisibilityIterator(mss4vi_p, sort);
+      if(rvi_p) delete rvi_p;
+      rvi_p = NULL;
+      rvi_p=new ROVisibilityIterator(mss4vi_p, sort);
 
     }
     else{
-    	wvi_p=new VisibilityIterator(mss4vi_p, sort);
-    	rvi_p=wvi_p;
+      if(wvi_p) delete wvi_p;
+      wvi_p = NULL;
+      wvi_p=new VisibilityIterator(mss4vi_p, sort);
+      rvi_p=wvi_p;
     }
     Block<Vector<Int> > blockGroup(msblock.nelements());
     for (uInt k=0; k < msblock.nelements(); ++k){
