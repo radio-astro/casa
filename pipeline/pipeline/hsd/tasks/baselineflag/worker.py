e42ac6f737d59ca1f2d3a89c40b6b696fee777b3
takeshi nakazato <takeshi.nakazato@nao.ac.jp>
2015-12-21 05:00:52 +0000

    Take into account deviation mask properly when baseline rms is evaluated.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@35758 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hsd/tasks/baselineflag/worker.py
+++ pipeline/pipeline/hsd/tasks/baselineflag/worker.py
@@ -248,7 +248,7 @@ class SDBLFlagWorker(object):
                 stats = DataTable.tb2.getcell('STATISTICS',idx)
                 # Calculate Standard Deviation (NOT RMS)
                 ### 2011/05/26 shrink the size of data on memory
-                mask_in = self._get_mask_array(masklist, (edgeL, edgeR), FlIn[index])
+                mask_in = self._get_mask_array(masklist, (edgeL, edgeR), FlIn[index], deviation_mask=deviation_mask)
                 mask_out = numpy.zeros(NCHAN)
                 if isvalid:
                     #mask_in = self._get_mask_array(masklist, (edgeL, edgeR), FlIn[index])
@@ -294,10 +294,10 @@ class SDBLFlagWorker(object):
                             NR += 1
                             RdataOld0 += SpIn[x]
                             masklist = DataTable.tb2.getcell('MASKLIST',chunks[1][x])
-                            mask0 = self._get_mask_array(masklist, (edgeL, edgeR), FlIn[x])
+                            mask0 = self._get_mask_array(masklist, (edgeL, edgeR), FlIn[x], deviation_mask=deviation_mask)
                             RmaskOld += mask0
                             RdataNew0 += SpOut[x]
-                            mask0 = self._get_mask_array(masklist, (edgeL, edgeR), FlOut[x]) if is_baselined else numpy.zeros(NCHAN)
+                            mask0 = self._get_mask_array(masklist, (edgeL, edgeR), FlOut[x], deviation_mask=deviation_mask) if is_baselined else numpy.zeros(NCHAN)
                             RmaskNew += mask0
                     elif START > (valid_nrow - Nmean):
                         NR -= 1
@@ -309,10 +309,10 @@ class SDBLFlagWorker(object):
                         box_edge = valid_indices[START + Nmean - 1]
                         masklist = DataTable.tb2.getcell('MASKLIST',chunks[1][box_edge])
                         RdataOld0 -= (SpIn[index] - SpIn[box_edge])
-                        mask0 = self._get_mask_array(masklist, (edgeL, edgeR), FlIn[box_edge])
+                        mask0 = self._get_mask_array(masklist, (edgeL, edgeR), FlIn[box_edge], deviation_mask=deviation_mask)
                         RmaskOld += (mask0 - mask_in)
                         RdataNew0 -= (SpOut[index] - SpOut[box_edge])
-                        mask0 = self._get_mask_array(masklist, (edgeL, edgeR), FlOut[box_edge]) if is_baselined else numpy.zeros(NCHAN)
+                        mask0 = self._get_mask_array(masklist, (edgeL, edgeR), FlOut[box_edge], deviation_mask=deviation_mask) if is_baselined else numpy.zeros(NCHAN)
                         RmaskNew += (mask0 - mask_out)
                     # Mean spectra of row = row-Nmean ~ row-1
                     if START == 1:
@@ -326,25 +326,25 @@ class SDBLFlagWorker(object):
                         box_edge = valid_indices[START - 2]
                         masklist = DataTable.tb2.getcell('MASKLIST',chunks[1][box_edge])
                         LdataOld0 += SpIn[box_edge]
-                        mask0 = self._get_mask_array(masklist, (edgeL, edgeR), FlIn[box_edge])
+                        mask0 = self._get_mask_array(masklist, (edgeL, edgeR), FlIn[box_edge], deviation_mask=deviation_mask)
                         LmaskOld += mask0
                         LdataNew0 += SpOut[box_edge]
-                        mask0 = self._get_mask_array(masklist, (edgeL, edgeR), FlOut[box_edge]) if is_baselined else numpy.zeros(NCHAN)
+                        mask0 = self._get_mask_array(masklist, (edgeL, edgeR), FlOut[box_edge], deviation_mask=deviation_mask) if is_baselined else numpy.zeros(NCHAN)
                         LmaskNew += mask0
                     else:
                         box_edge_right = valid_indices[START - 2]
                         box_edge_left = valid_indices[START - 2 - Nmean]
                         masklist = DataTable.tb2.getcell('MASKLIST',chunks[1][box_edge_right])
                         LdataOld0 += (SpIn[box_edge_right] - SpIn[box_edge_left])
-                        mask0 = self._get_mask_array(masklist, (edgeL, edgeR), FlIn[box_edge_right])
+                        mask0 = self._get_mask_array(masklist, (edgeL, edgeR), FlIn[box_edge_right], deviation_mask=deviation_mask)
                         LmaskOld += mask0
                         LdataNew0 += (SpOut[box_edge_right] - SpOut[box_edge_left])
-                        mask0 = self._get_mask_array(masklist, (edgeL, edgeR), FlOut[box_edge_right]) if is_baselined else numpy.zeros(NCHAN)
+                        mask0 = self._get_mask_array(masklist, (edgeL, edgeR), FlOut[box_edge_right], deviation_mask=deviation_mask) if is_baselined else numpy.zeros(NCHAN)
                         LmaskNew += mask0
                         masklist = DataTable.tb2.getcell('MASKLIST',chunks[1][box_edge_left])
-                        mask0 = self._get_mask_array(masklist, (edgeL, edgeR), FlIn[box_edge_left])
+                        mask0 = self._get_mask_array(masklist, (edgeL, edgeR), FlIn[box_edge_left], deviation_mask=deviation_mask)
                         LmaskOld -= mask0
-                        mask0 = self._get_mask_array(masklist, (edgeL, edgeR), FlOut[box_edge_left]) if is_baselined else numpy.zeros(NCHAN)
+                        mask0 = self._get_mask_array(masklist, (edgeL, edgeR), FlOut[box_edge_left], deviation_mask=deviation_mask) if is_baselined else numpy.zeros(NCHAN)
                         LmaskNew -= mask0
 
                     diffOld0 = (LdataOld0 + RdataOld0) / float(NL + NR) - SpIn[index]
