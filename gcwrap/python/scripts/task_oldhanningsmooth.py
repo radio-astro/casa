32c693217ebba86bc1310d34da015ab08f82e9a3
sandra castro <sbhatnag@nrao.edu>
2016-01-18 16:57:43 +0000

    New Development: No             JIRA Issue: CAS-7886         Ready for Test: Yes      Interface Changes: No What Interface Changed:          Test Programs:   Put in Release Notes: Yes              Module(s):            Description: Renaming hanningsmooth2 to hanningsmooth. Create a temporary alias to hanningsmooth2 and oldhanningsmooth.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@35878 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/task_oldhanningsmooth.py
+++ gcwrap/python/scripts/task_oldhanningsmooth.py
@@ -1,127 +1,71 @@
+#
+# This file was generated using xslt from its XML file
+#
+# Copyright 2007, Associated Universities Inc., Washington DC
+#
 import os
-import shutil
-import string
-import copy
-import math
 from taskinit import *
-from parallel.parallel_data_helper import ParallelDataHelper
-import testhelper as th
 
-def hanningsmooth(vis=None, 
-                   outputvis=None,
-                   keepmms=None,
-                   field=None,
-                   spw=None, 
-                   scan=None, 
-                   antenna=None, 
-                   correlation=None,
-                   timerange=None, 
-                   intent=None,
-                   array=None,
-                   uvrange=None,
-                   observation=None,
-                   feed=None,
-                   datacolumn=None, 
-                   ):
+def oldhanningsmooth(vis=None, datacolumn=None, outputvis=None):
 
     """Hanning smooth frequency channel data to remove Gibbs ringing
 
+    oldhanningsmooth(vis='ngc5921.ms')
+
+    This function Hanning smooths the frequency channels with
+    a weighted running average of smoothedData[i] =
+    0.25*correctedData[i-1] + 0.50*correctedData[i] +
+    0.25*correctedData[i+1].  The first and last channels are flagged.
+    Inclusion of a flagged value in an average causes that data value
+    to be flagged.
+
+    Keyword arguments:
+    vis -- Name of input visibility file (MS)
+           default: none; example: vis='ngc5921.ms'
+    datacolumn -- the name of the MS column into which to write the smoothed data
+                  default='corrected'; example: datacolumn='data'
+                  options: 'corrected' or 'data'
+    outputvis -- name of the output visibility file (MS)
+                 default=none (write to the input MS); example: outputvis='ngc5921_src.ms'
+
     """
 
-    casalog.origin('hanningsmooth')
-    
-    
-    # Initiate the helper class    
-    pdh = ParallelDataHelper("hanningsmooth", locals()) 
-
-    # Validate input and output parameters
+    #Python script
+    #
     try:
-        pdh.setupIO()
+        casalog.origin('oldhanningsmooth')
+        casalog.post('vis=\''+vis+'\', datacolumn=\''+datacolumn+'\', outputvis=\''+outputvis+'\'', 'INFO')
+        newvis = vis;
+        
+        if os.path.exists(outputvis):
+            ms.close()
+            raise Exception, "Output MS %s already exists - will not overwrite." % outputvis
+
+        if(type(outputvis)==str and not outputvis==''):
+            newvis = outputvis
+            casalog.post('copying '+vis+' to '+newvis , 'INFO')
+            tb.open(vis)
+            tmptb = tb.copy(newvis, deep=True, valuecopy=True)
+            tmptb.close()
+            # note that the resulting copy is writable even if the original was read-only
+            tb.close()
+        else:
+            newvis = vis
+
+        if ((type(newvis)==str) & (os.path.exists(newvis))):
+            ms.open(thems=newvis,nomodify=False)
+        else:
+            raise Exception, 'Visibility data set not found - please verify the name'
+        
+        ms.hanningsmooth(datacolumn=str.lower(datacolumn))
+        
+        # write history
+        ms.writehistory(message='taskname = oldhanningsmooth',origin='oldhanningsmooth')
+        ms.writehistory(message='vis         = "'+str(vis)+'"',origin='oldhanningsmooth')
+        ms.writehistory(message='datacolumn  = "'+str(datacolumn)+'"',origin='oldhanningsmooth')
+        ms.writehistory(message='outputvis   = "'+str(outputvis)+'"',origin='oldhanningsmooth')
+        ms.close()
+        
     except Exception, instance:
-        casalog.post('%s'%instance,'ERROR')
-        return False
-
-    # Input vis is an MMS
-    if pdh.isParallelMS(vis) and keepmms:
-        
-        if not pdh.validateInputParams():        
-            raise Exception, 'Unable to continue with MMS processing'
-                        
-        pdh.setupCluster('hanningsmooth')
-
-        # Execute the jobs
-        try:
-            pdh.go()
-        except Exception, instance:
-            casalog.post('%s'%instance,'ERROR')
-            return False
-                    
-        return True
-
-
-    # Actual task code starts here
-
-    # Create local copies of the MSTransform and ms tools
-    mtlocal = mttool()
-    mslocal = mstool()
-
-    try:
-                    
-        # Gather all the parameters in a dictionary.        
-        config = {}
-        
-        config = pdh.setupParameters(inputms=vis, outputms=outputvis, field=field, 
-                    spw=spw, array=array, scan=scan, antenna=antenna, correlation=correlation,
-                    uvrange=uvrange,timerange=timerange, intent=intent, observation=observation,
-                    feed=feed)
-        
-        
-        # Check if CORRECTED column exists, when requested
-        datacolumn = datacolumn.upper()
-        if datacolumn == 'CORRECTED':
-            dc = 'CORRECTED_DATA'
-            if th.getColDesc(vis,dc) == {}:
-                casalog.post('Input CORRECTED_DATA does not exist. Will use DATA','WARN')
-                datacolumn = 'DATA'
-             
-        casalog.post('Will use datacolumn = %s'%datacolumn, 'DEBUG')
-        config['datacolumn'] = datacolumn
-        
-        # Call MSTransform framework with hanning=True
-        config['hanning'] = True
-
-        # Configure the tool 
-        casalog.post('%s'%config, 'DEBUG1')
-        mtlocal.config(config)
-        
-        # Open the MS, select the data and configure the output
-        mtlocal.open()
-        
-        # Run the tool
-        casalog.post('Apply Hanning smoothing on data')
-        mtlocal.run()        
-            
-        mtlocal.done()
-                    
-    except Exception, instance:
-        mtlocal.done()
-        casalog.post('%s'%instance,'ERROR')
-        return False
-
-    # Write history to output MS, not the input ms.
-    try:
-        param_names = hanningsmooth.func_code.co_varnames[:hanningsmooth.func_code.co_argcount]
-        param_vals = [eval(p) for p in param_names]
-        casalog.post('Updating the history in the output', 'DEBUG1')
-        write_history(mslocal, outputvis, 'hanningsmooth', param_names,
-                      param_vals, casalog)
-    except Exception, instance:
-        casalog.post("*** Error \'%s\' updating HISTORY" % (instance),
-                     'WARN')
-        return False
-
-    mslocal = None
-    
-    return True
- 
- 
+        print '*** Error ***',instance
+        return
