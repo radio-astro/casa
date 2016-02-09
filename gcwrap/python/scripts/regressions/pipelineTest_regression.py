789221eeacf3d4ad6ae70348ac72a449f976e0a3
brian kent <bkent@nrao.edu>
2016-02-09 20:42:18 +0000

    Updated flux value comparison
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@36114 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/regressions/pipelineTest_regression.py
+++ gcwrap/python/scripts/regressions/pipelineTest_regression.py
@@ -118,7 +118,9 @@ def stats():
         #standard_context = pipeline.Pipeline(context=standard_file+'.context', path_overrides={'name':standard_file, 'output_dir':os.getcwd()}).context
         #standard_fluxlist = standard_context.results[12].read()[0].flux_densities
         #value_compare43 = 0.6934577681171487
-        value_compare = 0.716367318068
+        #value_compare = 0.716367318068  #CASA 4.5
+        value_compare = 0.717857716108  #CASA 4.6
+        
         #result_bool = np.isclose(fluxlist[0][0], standard_fluxlist[0][0], rtol=rtol, atol=atol, equal_nan=False)
         result_bool = np.isclose(fluxlist[0][0], value_compare, rtol=rtol, atol=atol, equal_nan=False)
         
