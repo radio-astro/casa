0edd5285ebc8390a828c452e0824513c00cb427b
brian kent <bkent@nrao.edu>
2016-02-10 15:46:46 +0000

    CAS-7695 - Commited first changes for spectral line heuristics, including adding intent and spw input keywords per Emmanuel's script changes.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@36122 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifv/tasks/flagging/targetflag.py
+++ pipeline/pipeline/hifv/tasks/flagging/targetflag.py
@@ -13,10 +13,26 @@ LOG = infrastructure.get_logger(__name__)
 
 class TargetflagInputs(basetask.StandardInputs):
     @basetask.log_equivalent_CASA_call
-    def __init__(self, context, vis=None):
+    def __init__(self, context, vis=None, intents=None, spw=None):
         # set the properties to the values given as input arguments
         self._init_properties(vars())
-    
+
+    @property
+    def intents(self):
+        if type(self.vis) is types.ListType:
+            return self._handle_multiple_vis('intents')
+
+        if self._intents is not None:
+            return self._intents
+
+        # return just the unwanted intents that are present in the MS
+        # intents_to_flag = set(['POINTING','FOCUS','ATMOSPHERE','SIDEBAND','UNKNOWN', 'SYSTEM_CONFIGURATION'])
+        return ''
+
+    @intents.setter
+    def intents(self, value):
+        self._intents = value
+
 
 class TargetflagResults(basetask.Results):
     def __init__(self, jobs=[]):
@@ -43,7 +59,9 @@ class Targetflag(basetask.StandardTaskTemplate):
         
         method_args = {'field'       : '',
                        'correlation' : 'ABS_' + corrstring,
-                       'scan'        : ''}
+                       'scan'        : '',
+                       'intent'      : '',
+                       'spw'         : ''}
 
         rflag_result = self._do_rflag(**method_args)
         
@@ -52,13 +70,15 @@ class Targetflag(basetask.StandardTaskTemplate):
     def analyse(self, results):
         return results
     
-    def _do_rflag(self, field=None, correlation=None, scan=None):
+    def _do_rflag(self, field=None, correlation=None, scan=None, intent=None, spw=None):
         
         task_args = {'vis'          : self.inputs.vis,
                      'mode'         : 'rflag',
                      'field'        : field,
                      'correlation'  : correlation,
                      'scan'         : scan,
+                     'intent'       : intent,
+                     'spw'          : spw,
                      'ntime'        : 'scan',
                      'combinescans' : False,
                      'datacolumn'   : 'corrected',
