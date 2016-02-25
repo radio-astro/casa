bc0eb3c3743bff72f54e34e5754b929d78e8531e
dave mehringer <dmehring@zia.aoc.nrao.edu>
2016-02-25 18:44:55 +0000

    New Development: Yes/No             JIRA Issue: CAS-8341         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: new unit test
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@36235 ac9c0da5-131f-0410-9009-a9c195f183e6

---
+++ gcwrap/python/scripts/tests/test_ia_fromrecord.py
@@ -0,0 +1,101 @@
+##########################################################################
+# imfit_test.py
+#
+# Copyright (C) 2008, 2009
+# Associated Universities, Inc. Washington DC, USA.
+#
+# This script is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Library General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+# License for more details.
+#
+# You should have received a copy of the GNU Library General Public License
+# along with this library; if not, write to the Free Software Foundation,
+# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+#
+# Correspondence concerning AIPS++ should be adressed as follows:
+#        Internet email: aips2-request@nrao.edu.
+#        Postal address: AIPS++ Project Office
+#                        National Radio Astronomy Observatory
+#                        520 Edgemont Road
+#                        Charlottesville, VA 22903-2475 USA
+#
+# <author>
+# Dave Mehringer
+# </author>
+#
+# <summary>
+# Test suite for the CASA tool method ia.fromrecord()
+# </summary>
+#
+# <reviewed reviwer="" date="" tests="" demos="">
+# </reviewed
+#
+# <prerequisite>
+# <ul>
+# </ul>
+# </prerequisite>
+#
+# <etymology>
+# Test for the ia.fromrecord() tool method
+# </etymology>
+#
+# <synopsis>
+# Test for the ia.fromrecord tool method
+# </synopsis> 
+#
+# <example>
+#
+# This test runs as part of the CASA python unit test suite and can be run from
+# the command line via eg
+# 
+# `echo $CASAPATH/bin/casapy | sed -e 's$ $/$'` --nologger --log2term -c `echo $CASAPATH | awk '{print $1}'`/code/xmlcasa/scripts/regressions/admin/runUnitTest.py test_ia_fromrecord[test1,test2,...]
+#
+# </example>
+#
+# <motivation>
+# To provide a test standard for the ia.fromrecord() tool method to ensure
+# coding changes do not break the associated bits 
+# </motivation>
+#
+
+###########################################################################
+import shutil
+import casac
+from tasks import *
+from taskinit import *
+from __main__ import *
+import unittest
+
+class ia_fromrecord_test(unittest.TestCase):
+    
+    def setUp(self):
+        self._myia = iatool()
+    
+    def tearDown(self):
+        self._myia.done()
+        self.assertTrue(len(tb.showcache()) == 0)
+        
+    def test_fromrecord(self):
+        """Test general functionality"""
+        myia = self._myia
+        shape = [2,3,4]
+        myia.fromshape("", shape)
+        myia.addnoise()
+        chunk = myia.getchunk()
+        rec = myia.torecord()
+        myia.fromrecord(rec)
+        self.assertTrue((myia.shape() == shape).all(), "Shape is incorrect")
+        self.assertTrue((myia.getchunk() == chunk).all(), "Pixel values are incorrect")
+        a = myia.history()
+        self.assertTrue(len(a) == 2, "wrong history length")
+        for aa in a:
+            self.assertTrue("fromrecord" in aa, "Expected string not found in history")       
+ 
+def suite():
+    return [ia_fromrecord_test]
