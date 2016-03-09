b3d389ad2ef6bbbbf2b1b77dbc4d341e024a41a9
vincent geers <vincent.geers@nrao.edu>
2016-03-09 16:20:59 +0000

    Remove pipeline flagging refactor branch (branches/project/pipeline_flagging_2015-06-08), reintegrated with pipeline trunk (branches/project/pipeline) in r36322. (Will lose flagging branch r36346 which happened post-reintegration, but will reapply this to new flagging branch.)
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@36350 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline_flagging_2015-06-08/pipeline/h/__init__.py
+++
@@ -1,4 +0,0 @@
-from __future__ import absolute_import
-
-from . import tasks
-from . import heuristics
--- pipeline_flagging_2015-06-08/pipeline/h/heuristics/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import
-
-from pipeline.h.heuristics.echoheuristic import EchoHeuristic
-from pipeline.h.heuristics.linefinder import HeuristicsLineFinder
-from pipeline.h.heuristics.linefinder import AsapLineFinder
