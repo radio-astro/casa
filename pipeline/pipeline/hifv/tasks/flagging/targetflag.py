ee97028314ea6b61ccf9869ff147235aea6496d8
brian kent <bkent@nrao.edu>
2016-03-17 21:14:32 +0000

    CAS-7695.  Added frequency selection from text for use in hifv_targetflag.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@36450 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifv/tasks/flagging/targetflag.py
+++ pipeline/pipeline/hifv/tasks/flagging/targetflag.py
@@ -1,19 +1,33 @@
 from __future__ import absolute_import
 
+import types
+
 import pipeline.infrastructure.basetask as basetask
 from pipeline.infrastructure import casa_tasks
 import pipeline.infrastructure as infrastructure
-
+import pipeline.infrastructure.contfilehandler as contfilehandler
 
 LOG = infrastructure.get_logger(__name__)
 
 # CHECKING FLAGGING OF ALL CALIBRATORS
 # use rflag mode of flagdata
 
+def read_contfile(contfile):
+    '''
+    Read in the spws/frequencies into a dictionary
+    '''
+
+    #First check to see if the file exists
+
+    contfile_handler = contfilehandler.ContFileHandler(contfile)
+    contdict = contfile_handler.read()
+
+    return contdict
+
 
 class TargetflagInputs(basetask.StandardInputs):
     @basetask.log_equivalent_CASA_call
-    def __init__(self, context, vis=None, intents=None, spw=None):
+    def __init__(self, context, vis=None, intents=None, contfile=None):
         # set the properties to the values given as input arguments
         self._init_properties(vars())
 
@@ -32,6 +46,18 @@ class TargetflagInputs(basetask.StandardInputs):
     @intents.setter
     def intents(self, value):
         self._intents = value
+
+    @property
+    def contfile(self):
+
+        if self._contfile is not None:
+            return self._contfile
+
+        return ''
+
+    @contfile.setter
+    def contfile(self, value):
+        self._contfile = value
 
 
 class TargetflagResults(basetask.Results):
@@ -56,15 +82,54 @@ class Targetflag(basetask.StandardTaskTemplate):
         m = self.inputs.context.observing_run.get_ms(self.inputs.vis)
         # corrstring = self.inputs.context.evla['msinfo'][m.name].corrstring
         corrstring = m.get_vla_corrstring()
-        
-        method_args = {'field'       : '',
-                       'correlation' : 'ABS_' + corrstring,
-                       'scan'        : '',
-                       'intent'      : '',
-                       'spw'         : ''}
 
-        rflag_result = self._do_rflag(**method_args)
-        
+
+        if ('CALIBRATE' in self.inputs.intents):
+            LOG.info("TARGETFLAG INFO: DOING CONTINUUM CALIBRATE")
+            method_args = {'field'       : '',
+                           'correlation' : 'ABS_' + corrstring,
+                           'scan'        : '',
+                           'intent'      : '*CALIBRATE*',
+                           'spw'         : ''}
+
+            rflag_result = self._do_rflag(**method_args)
+
+        if ('TARGET' in self.inputs.intents and self.inputs.contfile == ''):
+            LOG.info("TARGETFLAG INFO:  DOING CONTINUUM TARGET")
+            method_args = {'field'       : '',
+                           'correlation' : 'ABS_' + corrstring,
+                           'scan'        : '',
+                           'intent'      : '*TARGET*',
+                           'spw'         : ''}
+
+            rflag_result = self._do_rflag(**method_args)
+
+        if (self.inputs.intents == '' and self.inputs.contfile == ''):
+            LOG.info("TARGETFLAG INFO:  DOING EVERYTHING")
+            method_args = {'field'       : '',
+                           'correlation' : 'ABS_' + corrstring,
+                           'scan'        : '',
+                           'intent'      : '',
+                           'spw'         : ''}
+
+            rflag_result = self._do_rflag(**method_args)
+
+            return TargetflagResults([rflag_result])
+
+        if ('TARGET' in self.inputs.intents and self.inputs.contfile):
+            LOG.info("TARGETFLAG INFO:  SPECTRAL LINE - CONTINUUM MASKING")
+            fielddict = self._cont_file_to_CASA()
+
+            for field in fielddict.keys():
+                method_args = {'field'       : field,
+                               'correlation' : 'ABS_' + corrstring,
+                               'scan'        : '',
+                               'intent'      : '*TARGET*',
+                               'spw'         : fielddict[field]}
+
+                rflag_result = self._do_rflag(**method_args)
+
+
         return TargetflagResults([rflag_result])
     
     def analyse(self, results):
@@ -94,3 +159,31 @@ class Targetflag(basetask.StandardTaskTemplate):
         job = casa_tasks.flagdata(**task_args)
             
         return self._executor.execute(job)
+
+
+
+    def _cont_file_to_CASA(self):
+        '''
+        Take the dictionary created by _read_cont_file and put it into the format:
+        spw = '0:1.380~1.385GHz;1.390~1.400GHz'
+        '''
+
+        contdict = read_contfile(self.inputs.contfile)
+        if contdict == {}:
+            LOG.error(self.inputs.contfile + " is empty, does not exist or cannot be read.")
+            return {}
+
+        fielddict = {}
+
+        for field in contdict.keys():
+            spwstring = ''
+            for spw in contdict[field].keys():
+                spwstring = spwstring + spw + ':'
+                for freqtuple in contdict[field][spw]:
+                    spwstring = spwstring + str(freqtuple[0]) + '~' + str(freqtuple[1]) + 'GHz;'
+                spwstring = spwstring[:-1]
+                spwstring = spwstring + ','
+            spwstring = spwstring[:-1]
+            fielddict[field] = spwstring
+
+        return fielddict
