25c706451e33b2c64409bfee3f438ddf2455bb8f
brian kent <bkent@nrao.edu>
2016-04-12 18:16:54 +0000

    Resolved numpy type differences in testBPdcals
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@36697 ac9c0da5-131f-0410-9009-a9c195f183e6

--- Pipeline-Cycle3-R4-B/pipeline/hifv/tasks/testBPdcals/testBPdcals.py
+++ Pipeline-Cycle3-R4-B/pipeline/hifv/tasks/testBPdcals/testBPdcals.py
@@ -4,6 +4,8 @@ import pipeline.infrastructure.basetask as basetask
 import pipeline.infrastructure as infrastructure
 import pipeline.infrastructure.callibrary as callibrary
 
+import numpy as np
+
 from pipeline.hif.tasks import gaincal
 from pipeline.hif.tasks import bandpass
 from pipeline.hif.tasks import applycal
