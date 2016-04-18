461a6ec6cd031529cde05074ccd80f06915b8d95
jim jacobs <jjacobs@nrao.edu>
2016-04-18 17:38:52 +0000

    New Development: No             JIRA Issue: cas-8494         Ready for Test: Yes      Interface Changes: No What Interface Changed:          Test Programs:   Put in Release Notes: No              Module(s):            Description:
    Fixed problem in VI2 that was causing a seg fault when the underlying
    MS had empty hypercubes in a column with a tiled storage manager.
    	
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@36743 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/msvis/MSVis/VisibilityIteratorImpl2.h
+++ code/msvis/MSVis/VisibilityIteratorImpl2.h
@@ -725,6 +725,9 @@ protected:
     // optimized for the current input state (e.g., a new data description).
 
     virtual void setTileCache ();
+    void setMsColumnCacheSizes (MeasurementSet & partMs,
+                                const string & column);
+
 
     // Throws exception if there is a pending (i.e., unapplied) change to
     // the VI's properties.  Called when the VI is advanced since the user
