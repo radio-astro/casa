f4a63501d374d90e2cd2c15d91be3934072566b1
brian kent <bkent@nrao.edu>
2016-05-09 13:57:22 +0000

    Added QA code for hifv hanning smoothing.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@36948 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifv/tasks/hanning/__init__.py
+++ pipeline/pipeline/hifv/tasks/hanning/__init__.py
@@ -1,11 +1,19 @@
 from __future__ import absolute_import
 import pipeline.infrastructure.renderer.basetemplates as basetemplates
+import pipeline.infrastructure.pipelineqa as pipelineqa
+import pipeline.infrastructure.renderer.qaadapter as qaadapter
 import pipeline.infrastructure.renderer.weblog as weblog
 
 
 from .hanning import Hanning
 from . import hanning
 
+from . import qa
+
+pipelineqa.registry.add_handler(qa.HanningQAHandler())
+pipelineqa.registry.add_handler(qa.HanningListQAHandler())
+qaadapter.registry.register_to_dataset_topic(hanning.HanningResults)
+#qaadapter.registry.register_to_dataset_topic(applycal.ApplycalResults)
 
 weblog.add_renderer(Hanning,
                     basetemplates.T2_4MDetailsDefaultRenderer(uri='hanning.mako',
---
+++ pipeline/pipeline/hifv/tasks/hanning/qa.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import
+
+import os
+
+import pipeline.infrastructure.logging as logging
+import pipeline.infrastructure.utils as utils
+import pipeline.infrastructure.pipelineqa as pqa
+import pipeline.qa.scorecalculator as qacalc
+
+from . import hanning
+
+LOG = logging.get_logger(__name__)
+
+class HanningQAHandler(pqa.QAResultHandler):
+    result_cls = hanning.HanningResults
+    child_cls = None
+    generating_task = hanning.Hanning
+
+    def handle(self, context, result):
+
+        # Check for existence of the the target MS.
+        score1 = self._ms_exists(os.path.dirname(result.inputs['vis']), os.path.basename(result.inputs['vis']))
+        scores = [score1]
+
+        result.qa.pool.extend(scores)
+
+    def _ms_exists(self, output_dir, ms):
+        '''
+        Check for the existence of the target MS
+        '''
+        return qacalc.score_path_exists(output_dir, ms, 'Hanning smoothed ms')
+
+class HanningListQAHandler(pqa.QAResultHandler):
+    """
+    QA handler for a list containing UVcontFitResults.
+    """
+    result_cls = list
+    child_cls = hanning.HanningResults
+    generating_task = hanning.Hanning
+
+    def handle(self, context, result):
+        # collate the QAScores from each child result, pulling them into our
+        # own QAscore list
+        collated = utils.flatten([r.qa.pool for r in result])
+        result.qa.pool[:] = collated
+        mses = [r.inputs['vis'] for r in result]
+        longmsg = 'No missing target MS(s) for %s' % utils.commafy(mses, quotes=False, conjunction='or')
+        result.qa.all_unity_longmsg = longmsg
