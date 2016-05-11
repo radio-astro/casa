b7bdc0b74a306875764e369d48a6067ab17ac2a0
brian kent <bkent@nrao.edu>
2016-05-11 17:10:50 +0000

    Added VLA importdata QA.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@37007 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifv/tasks/importdata/__init__.py
+++ pipeline/pipeline/hifv/tasks/importdata/__init__.py
@@ -1,15 +1,20 @@
 from __future__ import absolute_import
 import pipeline.infrastructure.pipelineqa as pipelineqa
 import pipeline.infrastructure.renderer.qaadapter as qaadapter
+import pipeline.infrastructure.pipelineqa as pipelineqa
+import pipeline.infrastructure.renderer.qaadapter as qaadapter
 import pipeline.infrastructure.renderer.weblog as weblog
 
 from .importdata import VLAImportData
 from . import importdata
 from . import renderer
-#from . import qa
+
+from . import qa
 
 
-#pipelineqa.registry.add_handler(qa.VLAImportDataQAHandler())
-#qaadapter.registry.register_to_dataset_topic(importdata.VLAImportDataResults)
+pipelineqa.registry.add_handler(qa.VLAImportDataQAHandler())
+pipelineqa.registry.add_handler(qa.VLAImportDataListQAHandler())
+qaadapter.registry.register_to_dataset_topic(importdata.VLAImportDataResults)
+
 
 weblog.add_renderer(VLAImportData, renderer.T2_4MDetailsVLAImportDataRenderer(), group_by='ungrouped')
