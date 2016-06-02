91a9e5c29ef76f30df903409a6c070728a357aab
kana sugimoto <kana.sugi@nao.ac.jp>
2016-06-02 05:12:19 +0000

    New Development: No             JIRA Issue: Yes (CAS-8415)         Ready for Test: Yes      Interface Changes: No What Interface Changed:                  Test Programs:        Put in Release Notes: No              Module(s): sdimaging            Description: changed log level of a message.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@37202 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/task_sdimaging.py
+++ gcwrap/python/scripts/task_sdimaging.py
@@ -81,7 +81,7 @@ class sdimaging_worker(sdutil.sdtask_template_imaging):
                 raise ValueError, "Length of %s != infiles." % (name)
         # set convsupport default
         if self.convsupport >= 0 and self.gridfunction.upper() != 'SF':
-            casalog.post("user defined convsupport is ignored for %s kernel" % self.gridfunction, priority='warn')
+            casalog.post("user defined convsupport is ignored for %s kernel" % self.gridfunction, priority='WARN')
             self.convsupport=-1
 
     def __format_spw_string(self, spw):
