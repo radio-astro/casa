e83edbeb561cabd9140f0294efe7c84209ca6459
kana sugimoto <kana.sugi@nao.ac.jp>
2016-06-21 03:58:22 +0000

    New Development: No             JIRA Issue: Yes (CAS-8038, CAS-8046)         Ready for Test: Yes      Interface Changes: No What Interface Changed:          Test Programs:   Put in Release Notes: No              Module(s): measurementset class            Description: Added a new attribute, work_data, to pipeline.domain.measurement class for the sake of SD PL. The work_data attribute stores the name of working data corresponding to the measurement set. The default value is the name of measurementset itself.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@37382 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/domain/measurementset.py
+++ pipeline/pipeline/domain/measurementset.py
@@ -34,6 +34,7 @@ class MeasurementSet(object):
         self.session = session
         self.filesize = self._calc_filesize() 
         self.is_imaging_ms = False
+        self.work_data = name
 
     def _calc_filesize(self):
         """
