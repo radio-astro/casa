b607ae4291400bcb8097294ca75e8318f6391f3b
takeshi nakazato <takeshi.nakazato@nao.ac.jp>
2016-06-30 09:01:07 +0000

    Trivial fix.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@37492 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hsd/tasks/msexportdata/__init__.py
+++ pipeline/pipeline/hsd/tasks/msexportdata/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 import pipeline.infrastructure.renderer.weblog as weblog
 import pipeline.infrastructure.renderer.basetemplates as super_renderer
 
-from .exportdata import SDMSExportData
+from .msexportdata import SDMSExportData
 
 weblog.add_renderer(SDMSExportData, 
                     super_renderer.T2_4MDetailsDefaultRenderer(uri='hsd_exportdata.mako',
