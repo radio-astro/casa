a8bceba8c9aa03d56ff4d415506db25f107a8ba9
dirk muders <dmuders@mpifr-bonn.mpg.de>
2016-07-06 15:06:09 +0000

    Reject empty cubes.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@37537 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hif/heuristics/findcont.py
+++ pipeline/pipeline/hif/heuristics/findcont.py
@@ -22,7 +22,16 @@ class FindContHeuristics(object):
         self.context = context
 
     def find_continuum(self, dirty_cube):
+
+        with casatools.ImageReader(dirty_cube) as image:
+            stats = image.statistics()
+
+        if (stats['min'][0] == stats['max'][0]):
+            LOG.error('Cube %s is constant at level %s.' % (dirty_cube, stats['max'][0]))
+            return ([{'range': 'NONE', 'refer': 'LSRK'}], 'none')
+
+        # Run continuum finder on cube
         channel_selection, png_name, aggregate_bw = findContinuum(dirty_cube)
         frequency_ranges_GHz = \
             [{'range': item, 'refer': 'LSRK'} for item in utils.chan_selection_to_frequencies(dirty_cube, channel_selection, 'GHz')]
-        return(frequency_ranges_GHz, png_name)
+        return (frequency_ranges_GHz, png_name)
