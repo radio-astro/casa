9d71c35c0584b45abb644791c03604eb432b866e
takeshi nakazato <takeshi.nakazato@nao.ac.jp>
2016-07-13 00:38:54 +0000

    Add comment on QA handling of hsdms_exportdata task.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@37597 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hsd/tasks/msexportdata/__init__.py
+++ pipeline/pipeline/hsd/tasks/msexportdata/__init__.py
@@ -5,6 +5,10 @@ import pipeline.infrastructure.renderer.basetemplates as super_renderer
 
 from .msexportdata import SDMSExportData
 
+# NOTE: QA handling is not necessary since SDMSExportData produces 
+#       same results class as ExportData so that results are handled
+#       in the same way as ExportData.
+
 weblog.add_renderer(SDMSExportData, 
                     super_renderer.T2_4MDetailsDefaultRenderer(uri='exportdata.mako',
                                                                description='Prepare pipeline data products for export'),
