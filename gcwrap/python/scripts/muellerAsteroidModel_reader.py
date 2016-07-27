98f14df284fdab856bd06ee9b92be9b77acd9094
tak tsutsumi <ttsutsum@nrao.edu>
2016-07-27 00:44:41 +0000

    New Development: No             JIRA Issue: Yes CAS-7863         Ready for Test: No      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: No              Module(s): Module Names change impacts.            Description: Fixed some bugs in the reader
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@37769 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/muellerAsteroidModel_reader.py
+++ gcwrap/python/scripts/muellerAsteroidModel_reader.py
@@ -7,7 +7,10 @@ import numpy as np
 class asteroid_TPM:
     # objectname: capitalized name
     # frequency in GHz 
-    def __init__(self, objectname, frequency, mjd, path=''):
+    def __init__(self, objectname, frequency, mjd, path='',casalog=None):
+        if not casalog:
+           casalog = casac.logsink()
+        self._casalog = casalog
         self.knownsrcdict={'Ceres':{'interval':'1hour'},
                            'Lutetia':{'interval':'15min'},
                            'Pallas':{'interval':'15min'},
@@ -16,9 +19,11 @@ class asteroid_TPM:
         self.setfreq(frequency)
         self.setepoch(mjd)
         if path =="":
-            self.setdatapath(os.environ['CASAPATH'].split()[0]+'/data/alma/SolarSystemModels/') 
+            self.setdatapath(os.environ['CASAPATH'].split()[0]+'/data/alma/SolarSystemModels/TPM')
         else:
             self.setdatapath(path)
+        self.openedFile=""
+        self.data=None
 
     def setfreq(self,frequency):
         if type(frequency)!=float and type(frequency)!=np.float32 and type(frequency)!=np.float64:
@@ -37,12 +42,13 @@ class asteroid_TPM:
             self.datapath=path
         else:
             raise IOError("Model data path:"+path+" does not exists")
+        self._casalog.post("Model data path: "+path)
 
 
     def getFluxFromMuellerModel(self):
     
         (reffreq, refflux) =  self._muellerModelDataReader()
-        print "scaling from flux density =",refflux,' Jy @ ',reffreq,'GHz'
+        #print "scaling from flux density =",refflux,' Jy @ ',reffreq,'GHz'
         return (refflux*(self.freq/reffreq)**2)
 
 
@@ -54,45 +60,63 @@ class asteroid_TPM:
         from casac import casac 
         qa = casac.quanta()
 
+        reffreq=None
+        refflux=None
+
         date=qa.time(str(self.mjd)+'d',form='ymd')[0]
         yr=date.split('/')[0] 
+       
+        # ------------------------------ 
+        # Following section should only to read the data initially and keep the data and only scan the new model data file 
+        # when the mjd is outside of the current data...
+        # 
         #if int(yr) < 2015: yr = '2015'
         if self.src in self.knownsrcdict.keys():
             filename = self.datapath+self.src+"_ALMA_TPMprediction_"+yr+"_"+self.knownsrcdict[self.src]['interval']+".txt"
-            #print "filename=",filename
-        import glob
-        flist = glob.glob(filename)
-        if len(flist)==1:
-            #print "Reading model data: ",flist[0]
-            with open(flist[0],"r") as f:
-                data = f.readlines()
-            freqs = self._checkheader(data)
+            if (self.openedFile != filename):
+                # read a new file
+                self.openedFile = filename
+                import glob
+                flist = glob.glob(filename)
+                if len(flist)==1:
+                    self._casalog.post("Reading model data: "+flist[0])
+                    with open(flist[0],"r") as f:
+                        self.data = f.readlines()
+                elif len(flist)==0:
+                    raise IOError("Cannot find the model file for "+self.src+" for date="+date+".")
+           # else:
+           #     self._casalog.post("use the file already read..self.openedFile="+self.openedFile)
+
+            freqs = self._checkheader(self.data)
             if len(freqs)==0:
                 raise IOError("Problem in reading model data file!")
+
             i = np.abs(freqs-self.freq).argmin()
             reffreq = freqs[i]
+            #self._casalog.post("self.freq= %s i=%s reffreq=%s" % (self.freq, i, reffreq))
+            #print "self.freq= %s i=%s reffreq=%s" % (self.freq, i, reffreq)
             
             maindata = []
             mjds = []
             fluxes = []
-            for line in data[8:]:
+            for line in self.data[8:]:
                 if line.find('----')<0:
                     maindata.append(line)
                     mjds.append(float(line.split()[5]))
-                    fluxes.append(float(line.split()[i+5]))
+                    # i th flux (0 based) 
+                    fluxes.append(float(line.split()[(i+1)+5]))
 
             mjdarr = np.array(mjds)
             flxarr = np.array(fluxes)
+            # ---------------------
+
             # find flux density of nearest in time
             t = np.abs(mjdarr-self.mjd).argmin()
             refflux = flxarr[t] 
             #print "flxarr=",flxarr
             #print "freq, mjd=",self.freq,self.mjd
-            print "refflux@reffreq=%s@%s at %s" % (refflux,reffreq,mjdarr[t])
-
-        elif len(flist)==0:
-            raise IOError("Cannot find the model file for "+self.src+" for date="+date+".")
-
+            #print "refflux@reffreq=%s@%s at %s" % (refflux,reffreq,mjdarr[t])
+            #self._casalog.post("refflux@reffreq=%s@%s at %s" % (refflux,reffreq,mjdarr[t]),"DEBUG1")
        
         return (reffreq, refflux)
 
