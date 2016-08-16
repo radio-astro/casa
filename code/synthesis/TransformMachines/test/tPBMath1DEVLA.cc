51857b4a2ca5a30c184890867dcdc597d92eb94f
kumar golap <kgolap@nrao.edu>
2016-08-16 23:10:21 +0000

    a bit more meaningful test
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@37964 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/synthesis/TransformMachines/test/tPBMath1DEVLA.cc
+++ code/synthesis/TransformMachines/test/tPBMath1DEVLA.cc
@@ -33,6 +33,7 @@
 #include <images/Images/PagedImage.h>
 #include <coordinates/Coordinates/CoordinateUtil.h>
 #include <casacore/casa/Logging/LogIO.h>
+#include <iomanip>
 using namespace casa;
 Int main(){
   CoordinateSystem cs;
@@ -43,51 +44,39 @@ Int main(){
   IPosition d1, d2;
   Vector<Double> incr=cs.increment();
   cerr << "incr " << incr << endl;
-  incr[0] /=30.0;
-  incr[1] /=30.0;
+  incr[0] /=60.0;
+  incr[1] /=60.0;
   cs.setIncrement(incr);
   cs.list(os, MDoppler::RADIO, d1, d2);
-  {
-    PagedImage<Complex> im(IPosition(4, 2000, 2000, 1, 1), cs, "GULU");
+  MDirection thedir(Quantity(0.0, "deg"), Quantity(0.0, "deg"), MDirection::J2000);
+  Vector<Double> pixworld=cs.referenceValue();
+  PagedImage<Complex> im(IPosition(4, 1000, 1000, 1, 1), cs, "GULU");
+  PBMath1DEVLA pbm(Quantity(0.7, "deg"), True, 1.425e9);
+  std::vector<double> freqs={1.e9, 2.e9, 4.e9, 8.e9, 16.e9, 32.e9, 48.e9};
+  pbm.summary(10);
+  std::vector<Complex> refval={Complex(0.998942792416,0), Complex(0.995834052563,0), Complex(0.984491288662,0), Complex(0.939067363739,0), Complex(0.770893454552,0), Complex(0.316897362471,0), Complex(0.0408035330474,0)};
+  for (unsigned int k=0; k< freqs.size(); ++k){
     im.set(Complex(1.0));
-    {
-      LatticeExprNode LEN = max( im );
-      LatticeExprNode LEN2 = min( im );
-      cerr << "max " << LEN.getComplex() << " min " << LEN2.getComplex() << endl;
-    }
-    PBMath1DEVLA pbm(Quantity(0.5, "deg"), True, 1.425e9);
+    
+    
    
     /*std::vector<Double> coeff={ 1.000, -1.462e-3, 8.23e-7, -1.83e-10}; 
-    BeamSquint squint(MDirection(Quantity(0.0, "'"),
-				 Quantity(0.0, "'"),
-				 MDirection::Ref(MDirection::AZEL)),
-		      Quantity(1.0, "GHz"));
-		      PBMath1DPoly pbm(coeff, Quantity(1.0, "deg"), Quantity(1.0, "GHz"), False,  squint, True);*/
-    pbm.summary(10);
-    Vector<Float> rr;
-    Vector<Float> pb;
-    MDirection thedir(Quantity(0.0, "deg"), Quantity(0.0, "deg"), MDirection::J2000);
-    pbm.applyPB(im, im, thedir);
-    im.flush();
-    Vector<Double> pixworld=cs.referenceValue();
-
-    pbm.viewPB(rr, pb, 10, pixworld[3]);
-    
-        cerr << "pb at " << pixworld[3] << rr << "    " << pb << endl;
-    pixworld[3]=48e9;
+      BeamSquint squint(MDirection(Quantity(0.0, "'"),
+      Quantity(0.0, "'"),
+      MDirection::Ref(MDirection::AZEL)),
+      Quantity(1.0, "GHz"));
+      PBMath1DPoly pbm(coeff, Quantity(1.0, "deg"), Quantity(1.0, "GHz"), False,  squint, True);*/
+    pixworld[3]=freqs[k];
     cs.setReferenceValue(pixworld);
     im.setCoordinateInfo(cs);
     pbm.applyPB(im, im, thedir);
     im.flush();
-    pbm.viewPB(rr, pb, 10, pixworld[3]);
-    cerr << "pb at " << pixworld[3] << rr << "    " << pb << endl;
+    cerr << std::setprecision(12) << "Value at (0, 50) for " << freqs[k] << "  " << im.getAt(IPosition(4,0,50,0,0)) << endl; 
+    AlwaysAssertExit(near(refval[k], im.getAt(IPosition(4, 0, 50, 0, 0)), 1.0e-5));
    
-    {
-      LatticeExprNode LEN = max( im );
-      LatticeExprNode LEN2 = min( im );
-      cerr << "max " << LEN.getComplex() << " min " << LEN2.getComplex() << endl;
-    }
   }
-  
-   exit(0);
+
+
+cout << "OK" << endl;
+exit(0);
 }
