59c8530e27a26a7ffe904d15a515e7442e1e5a49
brian kent <bkent@nrao.edu>
2016-08-16 19:40:47 +0000

    CAS-8897.  Changed warn_nonexist to False so warnings don't pop up in the VLA pipeline
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@37959 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifv/heuristics/lib_EVLApipeutils.py
+++ pipeline/pipeline/hifv/heuristics/lib_EVLApipeutils.py
@@ -58,7 +58,7 @@ def cont_file_to_CASA(contfile='cont.dat'):
         '''
 
         contfile_handler = contfilehandler.ContFileHandler(contfile)
-        contdict = contfile_handler.read(warn_nonexist=True)
+        contdict = contfile_handler.read(warn_nonexist=False)
 
 
         #if contdict == {}:
