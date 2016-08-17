5c3754185b5db1c8bbacf5119c614ef996af29b4
stewart williams <sjw@roe.ac.uk>
2016-08-17 11:01:11 +0000

    Add plotlevel, loglevel and way to disable weblog to h_resume
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@37970 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/h/cli/task_h_resume.py
+++ pipeline/pipeline/h/cli/task_h_resume.py
@@ -2,10 +2,15 @@ from taskinit import casalog
 
 import pipeline.h.cli.cli as cli
 import pipeline.infrastructure.launcher as launcher
+import pipeline.infrastructure.basetask as basetask
 
-def h_resume(filename=None):
-    pipeline = launcher.Pipeline(context=filename)
+def h_resume(pipelinemode=None, filename=None, loglevel=None,
+             plotlevel=None, weblog=None):
+    pipeline = launcher.Pipeline(context=filename, loglevel=loglevel,
+                                 plotlevel=plotlevel)
     
     cli.stack[cli.PIPELINE_NAME] = pipeline
+
+    basetask.DISABLE_WEBLOG = not weblog
 
     return pipeline.context
