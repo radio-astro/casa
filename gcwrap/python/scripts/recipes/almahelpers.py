d05b29ad67a205db22f6c191375864d71dc148c5
todd hunter <thunter@nrao.edu>
2016-09-09 14:26:15 +0000

    Added editIntents and editIntentscsv (for CAS-8960).
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/release-4_7@38196 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/recipes/almahelpers.py
+++ gcwrap/python/scripts/recipes/almahelpers.py
@@ -32,6 +32,7 @@
 import numpy
 import taskinit
 import os
+import csv # used by editIntentscsv
 
 class SpwMap:
     """
@@ -390,3 +391,224 @@ def calantsub(incaltable,outcaltable='',
         st1.close()
     stsel.close()
     mytb.close()
+
+def editIntentscsv(intentcsv, dryrun=False, verbose=False):
+    """
+    Reads a list of parameters from a csv text file and runs editIntents on them.
+    File format: 
+    # any number of comment lines
+    vis,field,scan,"WVR,BANDPASS"
+    vis,field,,'FLUX,WVR'
+    ...
+
+    * field is a required argument, while scan is optional
+    * scan: can be a single integer, integer list, or comma-delimited string list
+         multiple scans or intents must be quoted, either single or double quote
+    -Todd Hunter
+    """
+    if (not os.path.exists(intentcsv)):
+        print "Could not find intentcsv file: ", intentcsv
+        return
+    f = open(intentcsv,'r')
+    lines = f.readlines()
+    f.close() 
+    for originalLine in lines:
+        if (originalLine[0] == '#'): continue
+        line = originalLine.replace("'",'"').replace(' ','')
+        if verbose:
+            print "Parsing reformatted line: ", line[:-1]
+        if (len(line.split(',')) > 3):
+            token = []
+            for i in csv.reader([line]):
+                token.append(i)
+            token = token[0]
+            if (len(token) < 4):
+                print "Skipping invalid line: ", originalLine
+                continue
+            vis = token[0].strip()
+            field = token[1].strip()
+            scan = token[2].strip()
+            intents = token[3].strip()
+            if (os.path.exists(vis)):
+                cmd = "au.editIntents('%s','%s','%s','%s')" % (vis,field,scan,intents)
+                if dryrun:
+                    print "Would call %s\n" % (cmd)
+                else:
+                    print "Calling %s" % (cmd)
+                    editIntents(vis,field,scan,intents)
+            else:
+                print "Could not find requested measurement set: ", vis
+
+def editIntents(msName='', field='', scan='', newintents='', 
+                append=False, verbose=True):
+    """
+    Change the observation intents for a specified field.  
+    Inputs:
+    * field: required argument (integer or string ID, or name)
+    * scan: optional, can be a single integer, integer list, or comma-delimited string list
+    * append: set to True to add the specified intent to the existing intents
+    * newintents: enter as a python list, or as single comma-delimited string
+    * valid intents: the first 12 are simply a shorthand way to specify the latter 12
+       'AMPLITUDE', 'ATMOSPHERE', 'BANDPASS', 'CHECK_SOURCE', 'WVR',
+       'DELAY', 'FLUX', 'FOCUS', 'PHASE', 'POINTING', 'SIDEBAND_RATIO', 'TARGET',
+       'CALIBRATE_AMPLI', 'CALIBRATE_ATMOSPHERE', 'CALIBRATE_BANDPASS',
+       'CALIBRATE_DELAY', 'CALIBRATE_FLUX', 'CALIBRATE_FOCUS',
+       'CALIBRATE_PHASE', 'CALIBRATE_POINTING', 'CALIBRATE_SIDEBAND_RATIO',
+       'OBSERVE_TARGET', 'CALIBRATE_WVR', 'OBSERVE_CHECK_SOURCE'
+      The modifier #ON_SOURCE will be added to all specified intents.
+    - T. Hunter
+    """
+    validIntents = ['AMPLITUDE','ATMOSPHERE','BANDPASS','CHECK_SOURCE',
+                    'DELAY','FLUX','FOCUS', 'PHASE','POINTING', 
+                    'SIDEBAND_RATIO', 'TARGET','WVR', 'CALIBRATE_AMPLI', 
+                    'CALIBRATE_ATMOSPHERE', 'CALIBRATE_BANDPASS',
+                    'CALIBRATE_DELAY', 'CALIBRATE_FLUX', 'CALIBRATE_FOCUS',
+                    'CALIBRATE_PHASE', 'CALIBRATE_POINTING', 
+                    'CALIBRATE_SIDEBAND_RATIO','OBSERVE_TARGET',
+                    'CALIBRATE_WVR', 'OBSERVE_CHECK_SOURCE'
+                    ]
+    if (msName == ''):
+        print "You must specify a measurement set."
+        return
+    if (field == ''):
+        print "You must specify a field ID or name."
+        return
+    mytb = taskinit.tbtool()
+    mytb.open(msName + '/FIELD')
+    fieldnames = mytb.getcol('NAME')
+    if verbose:
+        print "Found fieldnames = ", fieldnames
+    mytb.close()
+
+    mytb.open(msName + '/STATE')
+    intentcol = mytb.getcol('OBS_MODE')
+    intentcol = intentcol
+    mytb.close()
+
+    mytb.open(msName, nomodify=False)
+    naddedrows = 0
+    if (type(newintents)==list):
+        desiredintents = ''
+        for n in newintents:
+            desiredintents += n
+            if (n != newintents[-1]):
+                desiredintents += ','
+    else:
+        desiredintents = newintents
+    desiredintentsList = desiredintents.split(',')
+        
+    for intent in desiredintentsList:
+        if ((intent in validIntents)==False):
+            print "Invalid intent = %s.  Valid intents = %s" % (intent,validIntents)
+            mytb.close()
+            return
+    foundField = False
+    if (type(scan) != list):
+        scan = str(scan)
+    for id,name in enumerate(fieldnames):
+      if (name == field or str(id) == str(field)):
+        foundField = True
+        if verbose:
+            print 'FIELD_ID %s has name %s' % (id, name)
+        if scan == '': 
+            s = mytb.query('FIELD_ID==%s' % id)
+            if verbose:
+                print "Got %d rows in the ms matching field=%s" % (s.nrows(),id)
+        else:
+            if (type(scan) == str):
+                scans = [int(x) for x in scan.split(',')]
+            elif (type(scan) != list):
+                scans = [scan]
+            else:
+                scans = scan
+            if verbose:
+                print "Querying: 'FIELD_ID==%s AND SCAN_NUMBER in %s'" % (id, str(scans))
+            s = mytb.query('FIELD_ID==%s AND SCAN_NUMBER in %s' % (id, str(scans)))
+            if verbose:
+                print "Got %d rows in the ms matching field=%s and scan in %s" % (s.nrows(),id,str(scans))
+        if (s.nrows() == 0):
+            mytb.close()
+            print "Found zero rows for this field (and/or scan). Stopping."
+            return
+        state_ids = s.getcol('STATE_ID')
+        states = []
+        for state_id in state_ids:
+            if state_id not in states:
+                states.append(state_id)
+        for ij in range(len(states)):
+            states[ij] = intentcol[states[ij]]
+
+        print 'current intents are:'
+        for state in states:
+            print state
+
+        if append == False: states = []
+        for desiredintent in desiredintentsList:
+            if desiredintent.find('TARGET')>=0:
+                states.append('OBSERVE_TARGET#ON_SOURCE')
+            elif desiredintent.find('CHECK_SOURCE')>=0:
+                states.append('OBSERVE_CHECK_SOURCE#ON_SOURCE')
+            elif desiredintent.find('BANDPASS')>=0:
+                states.append('CALIBRATE_BANDPASS#ON_SOURCE')
+            elif desiredintent.find('PHASE')>=0:
+                states.append('CALIBRATE_PHASE#ON_SOURCE')
+            elif desiredintent.find('AMPLI')>=0:
+                states.append('CALIBRATE_AMPLI#ON_SOURCE')
+            elif desiredintent.find('FLUX')>=0:
+                states.append('CALIBRATE_FLUX#ON_SOURCE')
+            elif desiredintent.find('ATMOSPHERE')>=0:
+                states.append('CALIBRATE_ATMOSPHERE#ON_SOURCE')
+            elif desiredintent.find('WVR')>=0:
+                states.append('CALIBRATE_WVR#ON_SOURCE')
+            elif desiredintent.find('SIDEBAND_RATIO')>=0:
+                states.append('CALIBRATE_SIDEBAND_RATIO#ON_SOURCE')
+            elif desiredintent.find('DELAY')>=0:
+                states.append('CALIBRATE_DELAY#ON_SOURCE')
+            elif desiredintent.find('FOCUS')>=0:
+                states.append('CALIBRATE_FOCUS#ON_SOURCE')
+            elif desiredintent.find('POINTING')>=0:
+                states.append('CALIBRATE_POINTING#ON_SOURCE')
+            else:
+                print "Unrecognized intent = %s" % desiredintent
+                continue
+            print 'setting %s' % (states[-1])
+
+        if states != []:
+            state = reduce(lambda x,y: '%s,%s' % (x,y), states)
+            if state not in intentcol:
+                if verbose:
+                    print 'adding intent to state table'
+                intentcol = list(intentcol)
+                intentcol.append(state)
+                intentcol = numpy.array(intentcol)
+                state_id = len(intentcol) - 1
+                naddedrows += 1
+                if verbose:
+                    print 'state_id is', state_id
+                state_ids[:] = state_id
+            else:
+                if verbose:
+                    print 'intent already in state table'
+                state_id = numpy.arange(len(intentcol))[intentcol==state]
+                if verbose:
+                    print 'state_id is', state_id
+                if (type(state_id) == list or type(state_id)==numpy.ndarray):
+                    # ms can have identical combinations of INTENT, so just
+                    # pick the row for the first appearance - T. Hunter
+                    state_ids[:] = state_id[0]
+                else:
+                    state_ids[:] = state_id
+            s.putcol('STATE_ID', state_ids)
+    if (foundField == False):
+        print "Field not found"
+        return
+    mytb.close()
+
+    if verbose:
+        print 'writing new STATE table'
+    mytb.open(msName + '/STATE', nomodify=False)
+    if naddedrows > 0:
+        mytb.addrows(naddedrows)
+    mytb.putcol('OBS_MODE', intentcol)
+    mytb.close()
+
