e9750f7c67aa03cd5bc276d297cc8c04e08f8b35
lindsey davis <ldavis@nrao.edu>
2016-09-13 21:39:41 +0000

    Added support for the clearcals parameter.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@38236 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifa/cli/task_hifa_importdata.py
+++ pipeline/pipeline/hifa/cli/task_hifa_importdata.py
@@ -7,7 +7,8 @@ import pipeline.h.cli.utils as utils
 
 def hifa_importdata(vis=None, session=None, pipelinemode=None, asis=None,
     process_caldevice=None, overwrite=None, bdfflags=None, lazy=None,
-    dbservice=None, ocorr_mode=None, createmms=None, dryrun=None, acceptresults=None):
+    dbservice=None, ocorr_mode=None, createmms=None, clearcals=None,
+    dryrun=None, acceptresults=None):
 
     # create a dictionary containing all the arguments given in the
     # constructor
