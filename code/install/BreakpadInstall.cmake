7e1e2a11d3079a21f4bbd0140a38002d5d61fa09
ville suoranta <ville.suoranta@nrao.edu>
2016-09-27 20:32:36 +0000

    Patching copy-paste error
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@38345 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/install/BreakpadInstall.cmake
+++ code/install/BreakpadInstall.cmake
@@ -70,9 +70,9 @@ macro (configure_breakpad Breakpad_Root)
     # Perform the configuration step by executing "configure"
 
 	message ("Configuring Breakpad using ${CMAKE_CXX_COMPILER}")
-    set (configCommand "./configure CC=${CMAKE_CXX_COMPILER} CXX=${CMAKE_CXX_COMPILER}")
+    set (configCommand "./configure CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER}")
 	message ("configCommand ${configCommand}")
-    execute_process (COMMAND ./configure CC=${CMAKE_CXX_COMPILER}
+    execute_process (COMMAND ./configure CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER}
 	WORKING_DIRECTORY "breakpad/breakpad-distro"
 	RESULT_VARIABLE status)
     if (NOT ${status} EQUAL 0)
