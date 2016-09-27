1ae4a79b3f2a91cf7e22c3f5917955f47591c533
ville suoranta <ville.suoranta@nrao.edu>
2016-09-27 15:17:17 +0000

    Use PROJECT_SOURCE_DIR to call RPATH fix on Mac (instead of relative path)
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@38341 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/install/BreakpadInstall.cmake
+++ code/install/BreakpadInstall.cmake
@@ -114,7 +114,7 @@ macro (configure_breakpad Breakpad_Root)
 	    message ("Extracting full paths for Mac libraries")
 	    message ("Project binary directory: ${PROJECT_BINARY_DIR}")
         execute_process (COMMAND perl fixbreakpadframeworkrefs.pl ${PROJECT_BINARY_DIR} 
-	    WORKING_DIRECTORY "../install"
+	    WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/install"
 	    OUTPUT_VARIABLE error_message
 	    ERROR_VARIABLE error_message
 	    RESULT_VARIABLE status)
