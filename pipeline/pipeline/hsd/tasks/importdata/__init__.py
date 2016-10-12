8989581818ff5805cfae4a401fe48b81012f2fc2
lindsey davis <ldavis@nrao.edu>
2016-10-12 16:31:46 +0000

    Chnaged the hif import to an h import.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@38496 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hsd/tasks/importdata/__init__.py
+++ pipeline/pipeline/hsd/tasks/importdata/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
-import pipeline.hif.tasks.importdata.renderer as super_renderer
+#import pipeline.hif.tasks.importdata.renderer as super_renderer
+import pipeline.h.tasks.importdata.renderer as super_renderer
 import pipeline.infrastructure.renderer.qaadapter as qaadapter
 import pipeline.infrastructure.pipelineqa as pipelineqa
 import pipeline.infrastructure.renderer.weblog as weblog
