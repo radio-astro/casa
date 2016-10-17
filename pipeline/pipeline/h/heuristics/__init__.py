6987bd7e1157917d9fb64aa5d6bebb1aa2f3197b
lindsey davis <ldavis@nrao.edu>
2016-10-17 13:38:20 +0000

    Adding the applycal task to h_
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@38546 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/h/heuristics/__init__.py
+++ pipeline/pipeline/h/heuristics/__init__.py
@@ -2,3 +2,4 @@ from __future__ import absolute_import
 
 from pipeline.h.heuristics.echoheuristic import EchoHeuristic
 from pipeline.h.heuristics.linefinder import HeuristicsLineFinder
+from pipeline.h.heuristics.fieldnames import IntentFieldnames
--- pipeline/pipeline/h/tasks/__init__.py
+++ pipeline/pipeline/h/tasks/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+from .applycal import Applycal
 from .exportdata import ExportData
 from .importdata import ImportData
 
