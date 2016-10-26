7b161a093f1b5c63bc4714af26026ff99e07ee1f
brian kent <bkent@nrao.edu>
2016-10-26 13:21:04 +0000

    CAS-9042.  Added override method _export_casa_restore_script for VLA hifv_exportdata.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@38637 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifv/tasks/exportdata/vlaexportdata.py
+++ pipeline/pipeline/hifv/tasks/exportdata/vlaexportdata.py
@@ -1,4 +1,6 @@
 from __future__ import absolute_import
+import os
+import shutil
 
 import pipeline.infrastructure as infrastructure
 import pipeline.infrastructure.basetask as basetask
@@ -20,3 +22,66 @@ class VLAExportData(exportdata.ExportData):
 
     # link the accompanying inputs to this task
     Inputs = VLAExportDataInputs
+
+
+    def _export_casa_restore_script(self, context, script_name, products_dir, oussid, vislist, session_list):
+
+        """
+        Save the CASA restore script.
+        """
+
+        # Generate the file list
+
+        # Get the output file name
+        ps = context.project_structure
+        if ps is None:
+            script_file = os.path.join(context.report_dir, script_name)
+            out_script_file = os.path.join(products_dir, script_name)
+        elif ps.ousstatus_entity_id == 'unknown':
+            script_file = os.path.join(context.report_dir, script_name)
+            out_script_file = os.path.join(products_dir, script_name)
+        else:
+            # ousid = ps.ousstatus_entity_id.translate(string.maketrans(':/', '__'))
+            script_file = os.path.join(context.report_dir, script_name)
+            out_script_file = os.path.join(products_dir, oussid + '.' + script_name)
+
+        LOG.info('Creating casa restore script %s' % (script_file))
+
+        # This is hardcoded.
+        tmpvislist = []
+
+        # ALMA default
+        ocorr_mode = 'co'
+        #if context.project_summary.telescope in ('VLA', 'EVLA'):
+        #    ocorr_mode = 'co'
+
+        for vis in vislist:
+            filename = os.path.basename(vis)
+            if filename.endswith('.ms'):
+                filename, filext = os.path.splitext(filename)
+            tmpvislist.append(filename)
+        task_string = "    hif_restoredata (vis=%s, session=%s, ocorr_mode='%s')" % (
+        tmpvislist, session_list, ocorr_mode)
+
+        # if context.project_summary.telescope in ('VLA', 'EVLA'):
+        #     task_string += '\n    hifv_statwt()'
+        task_string += '\n    hifv_statwt()'
+
+        template = '''__rethrow_casa_exceptions = True
+h_init()
+try:
+%s
+finally:
+    h_save()
+''' % task_string
+
+        with open(script_file, 'w') as casa_restore_file:
+            casa_restore_file.write(template)
+
+        LOG.info('Copying casa restore script %s to %s' % \
+                 (script_file, out_script_file))
+        if not self._executor._dry_run:
+            shutil.copy(script_file, out_script_file)
+
+        return os.path.basename(out_script_file)
+
