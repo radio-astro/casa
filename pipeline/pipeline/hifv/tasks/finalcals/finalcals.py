cca80ae33696d34c37bc816131ff6d1b96670c66
brian kent <bkent@nrao.edu>
2016-11-08 21:37:03 +0000

    Error/warning updates for finalcals setjy calls.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@38787 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifv/tasks/finalcals/finalcals.py
+++ pipeline/pipeline/hifv/tasks/finalcals/finalcals.py
@@ -597,7 +597,7 @@ class Finalcals(basetask.StandardTaskTemplate):
                     try:
                         self._executor.execute(job)
                     except Exception, e:
-                        LOG.error('Unable to complete flux scaling operation.')
+                        LOG.warn('Unable to complete flux scaling operation.')
                         LOG.exception(e)
                         
         
