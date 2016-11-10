8be4ac875361537c2b4bca1ac0b0c7d932169f87
vincent geers <vincent.geers@nrao.edu>
2016-11-10 16:46:21 +0000

    CAS-9042: update to recipes/hsd.py to use hsd_tsyscal and hsd_tsysflag.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@38812 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/recipes/hsd.py
+++ pipeline/pipeline/recipes/hsd.py
@@ -7,7 +7,7 @@ import inspect
 
 # Make sure CASA exceptions are rethrown
 try:
-    if not  __rethrow_casa_exceptions:
+    if not __rethrow_casa_exceptions:
         def_rethrow = False
     else:
         def_rethrow = __rethrow_casa_exceptions
@@ -20,8 +20,8 @@ __rethrow_casa_exceptions = True
 from h_init_cli import h_init_cli as h_init
 from hsd_importdata_cli import hsd_importdata_cli as hsd_importdata
 from hsd_flagdata_cli import hsd_flagdata_cli as hsd_flagdata
-from hifa_tsyscal_cli import hifa_tsyscal_cli as hifa_tsyscal
-from hifa_tsysflag_cli import hifa_tsysflag_cli as hifa_tsysflag
+from hsd_tsyscal_cli import hsd_tsyscal_cli as hsd_tsyscal
+from hsd_tsysflag_cli import hsd_tsysflag_cli as hsd_tsysflag
 from hsd_skycal_cli import hsd_skycal_cli as hsd_skycal
 from hsd_k2jycal_cli import hsd_k2jycal_cli as hsd_k2jycal
 from hsd_applycal_cli import hsd_applycal_cli as hsd_applycal
@@ -39,8 +39,10 @@ IMPORT_ONLY = 'Import only'
 
 ITERATION = 2
 
+
 # Run the procedure
-def hsdms (vislist, importonly=False, pipelinemode='automatic', interactive=True):
+def hsdms(vislist, importonly=False, pipelinemode='automatic',
+          interactive=True):
 
     echo_to_screen = interactive
     casatools.post_to_log ("Beginning pipeline run ...")
@@ -50,62 +52,61 @@ def hsdms (vislist, importonly=False, pipelinemode='automatic', interactive=True
         h_init()
 
         # Load the data
-        hsd_importdata (vis=vislist, pipelinemode=pipelinemode)
+        hsd_importdata(vis=vislist, pipelinemode=pipelinemode)
         if importonly:
             raise Exception(IMPORT_ONLY)
        
         # Deterministic flagging
-        hsd_flagdata (pipelinemode=pipelinemode)
+        hsd_flagdata(pipelinemode=pipelinemode)
         
         # Tsys calibration
-        hifa_tsyscal (pipelinemode=pipelinemode)
+        hsd_tsyscal(pipelinemode=pipelinemode)
         
         # Flag system temperature calibration
-        hifa_tsysflag (pipelinemode=pipelinemode)
+        hsd_tsysflag(pipelinemode=pipelinemode)
         
         # Compute the sky calibration
-        hsd_skycal (pipelinemode=pipelinemode)
+        hsd_skycal(pipelinemode=pipelinemode)
     
         # Compute the Kelvin to Jansky calibration
-        hsd_k2jycal (pipelinemode=pipelinemode)
+        hsd_k2jycal(pipelinemode=pipelinemode)
     
         # Apply the calibrations
-        hsd_applycal (pipelinemode=pipelinemode)
+        hsd_applycal(pipelinemode=pipelinemode)
         
         # # Improve line mask for baseline subtraction by executing 
         # # hsd_baseline and hsd_blflag iteratively
         for i in xrange(ITERATION):
             
             # Baseline subtraction with automatic line detection
-            hsd_baseline (pipelinemode=pipelinemode)
+            hsd_baseline(pipelinemode=pipelinemode)
             
             # Flag data based on baseline quality
-            hsd_blflag (pipelinemode=pipelinemode)
+            hsd_blflag(pipelinemode=pipelinemode)
             
         # Imaging
-        hsd_imaging (pipelinemode=pipelinemode)
+        hsd_imaging(pipelinemode=pipelinemode)
 
         # Export the data
-        hsd_exportdata (pipelinemode=pipelinemode)
+        hsd_exportdata(pipelinemode=pipelinemode)
     
     except Exception, e:
         if str(e) == IMPORT_ONLY:
-            casatools.post_to_log ("Exiting after import step ...",
-	        echo_to_screen=echo_to_screen)
+            casatools.post_to_log("Exiting after import step ...",
+                                  echo_to_screen=echo_to_screen)
         else:
-            casatools.post_to_log ("Error in procedure execution ...",
-	        echo_to_screen=echo_to_screen)
+            casatools.post_to_log("Error in procedure execution ...",
+                                  echo_to_screen=echo_to_screen)
             errstr = traceback.format_exc()
-            casatools.post_to_log (errstr, echo_to_screen=echo_to_screen)
+            casatools.post_to_log(errstr, echo_to_screen=echo_to_screen)
 
     finally:
 
         # Save the results to the context
         h_save()
 
-        casatools.post_to_log ("Terminating procedure execution ...",
-                               echo_to_screen=echo_to_screen)
+        casatools.post_to_log("Terminating procedure execution ...",
+                              echo_to_screen=echo_to_screen)
 
         # Restore previous state
         __rethrow_casa_exceptions = def_rethrow
-
