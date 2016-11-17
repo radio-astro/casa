24b36fc3f267f1e4d59e7ccca1f2637cbeaf8c55
urvashi rao <rurvashi@nrao.edu>
2016-11-17 23:05:51 +0000

    cas-9244
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@38875 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/synthesis/ImagerObjects/SIIterBot.cc
+++ code/synthesis/ImagerObjects/SIIterBot.cc
@@ -149,9 +149,9 @@ namespace casa { //# NAMESPACE CASA - BEGIN
 		//		cout << "itsMajorDone="<<itsMajorDone<<" itsIterDone="<<itsIterDone<< " itsInitPeakResidual="<<itsInitPeakResidual<<" itsPeakResidual="<<itsPeakResidual <<" itsPrevPeakResidual : " <<  itsPrevPeakResidual << " itsStopFlag="<<itsStopFlag<<endl;
 
 		if( itsPeakResidual>0 && itsPrevPeakResidual>0 && 
-		    itsPeakResidual > itsPrevPeakResidual )
+		    fabs(itsPeakResidual - itsPrevPeakResidual)/itsPrevPeakResidual > 2.0 )
 		  {
-		    os << LogIO::WARN << "Peak residual increased from " << itsPrevPeakResidual << " to " << itsPeakResidual << endl;
+		    os << "[WARN] Peak residual increased from " << itsPrevPeakResidual << " to " << itsPeakResidual << LogIO::POST;
 		  }
 
 		/// This may interfere with some other criterion... check.
@@ -178,9 +178,10 @@ namespace casa { //# NAMESPACE CASA - BEGIN
 		    if( fabs( itsInitPeakResidual - itsPeakResidual )<1e-10) 
 		      {stopCode = 4;}
 
-                    // another non-convergent condition: diverging (if it increases more than by 50%)
+                    // another non-convergent condition: diverging (relative increase is more than 5 times)
+                    else if ( itsIterDone > 0 && fabs(itsPeakResidual-itsPrevPeakResidual)/itsPrevPeakResidual  > 5.0) 
 //                    else if ( itsIterDone > 0 &&  itsPeakResidual/itsPrevPeakResidual  >1.5) 
-//                      {stopCode = 5;}
+                      {stopCode = 5;}
 
 		  }
 	        
