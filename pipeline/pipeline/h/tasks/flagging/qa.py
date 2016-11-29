e92a3097be796ba78e8405ca5fb0ca221be96845
lindsey davis <ldavis@nrao.edu>
2016-11-29 21:16:42 +0000

    Updated the name of the score caluclator to included the template designation.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@38989 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/h/tasks/flagging/qa.py
+++ pipeline/pipeline/h/tasks/flagging/qa.py
@@ -19,7 +19,7 @@ class FlagDeterBaseQAHandler(pqa.QAResultHandler):
     
         # CAS-7059 base the metric (and warnings) on Shadowing+Online, instead
         # of on the Total.
-        scores = [qacalc.score_online_shadow_agents(ms, result.summaries)]
+        scores = [qacalc.score_online_shadow_template_agents(ms, result.summaries, name='%OnlineShadowTemplateFlags')]
                   
         result.qa.pool[:] = scores
 
