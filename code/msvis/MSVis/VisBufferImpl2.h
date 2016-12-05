b6a5ad0fa74d2fbe195966bd8685528fc7107128
george moellenbrock <gmoellen@nrao.edu>
2016-12-05 22:37:41 +0000

    New Development: No             JIRA Issue: No         Ready for Test: Yes      Interface Changes: Not really What Interface Changed:          Test Programs:   Put in Release Notes: No              Module(s): msvis            Description: Removed spurious (for now) 'override' from one virtual override 	   		to avoid large number of warnings on OSX.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@39058 ac9c0da5-131f-0410-9009-a9c195f183e6

--- code/msvis/MSVis/VisBufferImpl2.h
+++ code/msvis/MSVis/VisBufferImpl2.h
@@ -421,7 +421,7 @@ protected:
     virtual void stateCopy (const VisBufferImpl2 & other); // copy relevant noncached members
     virtual void setFillable (casacore::Bool isFillable);
     virtual void setRekeyable (casacore::Bool isRekeyable);
-    virtual bool setWritability (bool newWritability) override;
+    virtual bool setWritability (bool newWritability); //  override;
     virtual void setupValidShapes ();
     template <typename Coord>
     void updateCoord (Coord & item, const Coord & otherItem);
