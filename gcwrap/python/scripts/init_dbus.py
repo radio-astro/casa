291b788503338f10190a8f963ea839a510b18625
ville suoranta <ville.suoranta@nrao.edu>
2016-12-16 14:23:39 +0000

    Start dbus
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@39152 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/init_dbus.py
+++ gcwrap/python/scripts/init_dbus.py
@@ -6,23 +6,32 @@
 ##
 ##   note:  hosts which have dbus-daemon-1 but not dbus-daemon seem to have a broken dbus-daemon-1...
 ##
+import signal
+
 for info in [ (['dbus-daemon'],'dbus'),
               (['CrashReportPoster'],'crashPoster'),
               (['ipcontroller','ipcontroller-2.6'], 'ipcontroller'),
               (['ipengine','ipengine-2.6'], 'ipengine') ]:
+    print "Looping info: " + str(info)
     exelist = info[0]
     entry = info[1]
     for exe in exelist:
+        print "EXE:" + exe
         if casa['helpers']['info']:
+            print "AA"
             casa['helpers'][entry] = (lambda fd: fd.readline().strip('\n'))(os.popen(casa['helpers']['info'] + " --exec 'which " + exe + "'"))
         if casa['helpers'][entry] and os.path.exists(casa['helpers'][entry]):
+            print "BB: " + casa['helpers'][entry]
             break
         else:
+            print "CC"
             casa['helpers'][entry] = None
 
         ### first look in known locations relative to top (of binary distros) or known casa developer areas
         for srchdir in [ __casapath__ + '/MacOS', __casapath__ + '/lib/casa/bin', '/usr/lib64/casa/01/bin', '/opt/casa/01/bin' ] :
+            print "srchdir: " + srchdir
             dd = srchdir + os.sep + exe
+            print "DD: " + dd
             if os.path.exists(dd) and os.access(dd,os.X_OK) :
                 casa['helpers'][entry] = dd
                 break
@@ -41,3 +50,36 @@ for info in [ (['dbus-daemon'],'dbus'),
                     break
             if casa['helpers'][entry] is not None:
                 break
+
+if casa['helpers']['dbus'] is not None :
+    dbus_conf= __casapath__ + "/etc/dbus/casa.conf"
+    if not os.path.exists(dbus_conf):
+        dbus_conf = __casapath__ + "/Resources/dbus/casa.conf"
+    argv_0_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+    dbus_path = os.path.dirname(os.path.abspath(casa['helpers']['dbus']))
+
+    (r,w) = os.pipe( )
+
+    if os.fork( ) == 0 :
+        os.close(r)
+        signal.signal(signal.SIGINT, signal.SIG_IGN)
+        signal.signal(signal.SIGHUP, signal.SIG_IGN)
+        ## close standard input to avoid terminal interrupts
+        sys.stdin.close( )
+        os.close(0)
+        args = [ 'casa-dbus-daemon' ]
+        args = args + ['--print-address', str(w)]
+        if dbus_conf is not None and os.path.exists(dbus_conf) :
+            args = args + ['--config-file',dbus_conf]
+        else:
+            args = args + ['--session']
+        os.execvp(casa['helpers']['dbus'],args)
+        sys.exit
+
+    os.close(w)
+    dbus_address = os.read(r,200)
+    dbus_address = dbus_address.strip( )
+    os.close(r)
+    if len(dbus_address) > 0 :
+        os.putenv('DBUS_SESSION_BUS_ADDRESS',dbus_address)
+        os.environ['DBUS_SESSION_BUS_ADDRESS'] = dbus_address
