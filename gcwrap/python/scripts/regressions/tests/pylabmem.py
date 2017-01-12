b6513f9c60611c9b8774c5d71a30272543020984
ville suoranta <ville.suoranta@nrao.edu>
2017-01-12 16:17:11 +0000

    Fixing pylabmem.py
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@39259 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/regressions/tests/pylabmem.py
+++ gcwrap/python/scripts/regressions/tests/pylabmem.py
@@ -2,10 +2,10 @@ import sys
 import os
 import string
 import inspect
+import pylab as pl
 
 gl=sys._getframe(len(inspect.stack())-1).f_globals
 def run(fetch=False):
-    pl=gl['pl']
     x=range(10000000)
     pl.ion()
     for k in range(10):
