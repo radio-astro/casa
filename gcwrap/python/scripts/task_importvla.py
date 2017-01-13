129adadcc8443e55a00b996995035e58362a4e48
ville suoranta <ville.suoranta@nrao.edu>
2017-01-13 19:58:35 +0000

    Initialize vlafiller
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@39279 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/task_importvla.py
+++ gcwrap/python/scripts/task_importvla.py
@@ -1,6 +1,6 @@
 import os
 from taskinit import *
-
+from casa_stack_manip import stack_frame_find
 def importvla(archivefiles,vis,
 	      bandname,frequencytol,
 	      project,
@@ -9,14 +9,17 @@ def importvla(archivefiles,vis,
 	      autocorr,antnamescheme,keepblanks,evlabands):
 	i=0
 	overwrite=True
-        ok = True
+	ok = True
 	try:
 		casalog.origin('importvla')
-		if ((type(vis)!=str) | (vis=='') | (os.path.exists(vis))): 
+		if ((type(vis)!=str) | (vis=='') | (os.path.exists(vis))):
 			raise Exception, 'Need valid visibility file name (bad name or already exists)'
 		if (os.path.exists(vis)): raise Exception, 'Visibility file already exists - remove or rename'
 		for archivefile in archivefiles:
 			if i>0: overwrite=False
+			myf = stack_frame_find( )
+			vftask = myf['vftask']
+			vlafiller = vftask.fill
 			if ((type(archivefile)==str) & (os.path.exists(archivefile))):
 				vlafiller(msname=vis,inputfile=archivefile,
 					  overwrite=overwrite,
@@ -30,30 +33,28 @@ def importvla(archivefiles,vis,
 				i += 1
 			else:
 				raise Exception, 'Archive file not found - please verify the name'
-        except Exception, instance:
+	except Exception, instance:
 		print '*** Error importing %s to %s:' % (archivefiles, vis)
 		raise Exception, instance
 
-        # Write history
-        try:
-                param_names = importvla.func_code.co_varnames[:importvla.func_code.co_argcount]
-                param_vals = [eval(p) for p in param_names]
-                ok &= write_history(mstool(), vis, 'importvla', param_names,
+    # Write history
+	try:
+		param_names = importvla.func_code.co_varnames[:importvla.func_code.co_argcount]
+		param_vals = [eval(p) for p in param_names]
+		ok &= write_history(mstool(), vis, 'importvla', param_names,
                                     param_vals, casalog)
-        except Exception, instance:
-                casalog.post("*** Error \'%s\' updating HISTORY" % (instance),
+	except Exception, instance:
+		casalog.post("*** Error \'%s\' updating HISTORY" % (instance),
                              'WARN')
 
-        # write initial flag version
-        try:
-                aflocal = casac.agentflagger()
-                ok &= aflocal.open(vis);
-                ok &= aflocal.saveflagversion('Original',
-                                           comment='Original flags at import into CASA',
-                                           merge='replace')
-                ok &= aflocal.done();
-        except Exception, instance:
+    # write initial flag version
+	try:
+		aflocal = casac.agentflagger()
+		ok &= aflocal.open(vis);
+		ok &= aflocal.saveflagversion('Original',
+							comment='Original flags at import into CASA',
+							merge='replace')
+		ok &= aflocal.done();
+	except Exception, instance:
 		print '*** Error writing initial flag version of %s:' % vis
 		raise Exception, instance
-
-
