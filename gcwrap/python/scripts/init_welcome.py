7eeedb58abd0a1225718218521309bc52ed76ceb
Federica Pouzols <fpouzols@esonospam.example.org>
2017-01-25 16:42:32 +0000

    CAS-9569: run atexit hooks before doing os._exit when exiting the CASA/IPython interpreter (using atexit._exithandlers). This is required for proper shutdown of MPI services and there are also other areas using atexit hooks.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@39385 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/init_welcome.py
+++ gcwrap/python/scripts/init_welcome.py
@@ -1,62 +1,34 @@
 import os
-import sys
 import traceback
+
+from init_welcome_helpers import redirect_argv, immediate_exit_with_handlers
+
 
 if casa['flags'].execute:
     import os.path
+
     if os.path.isfile(casa['flags'].execute[0]):
-        import contextlib
-
-        ###
-        ### redirect_argv(...) is used to present a consistent, clean sys.argv to
-        ### the file being executed (via "-c somescript.py arg1, arg2..."). The
-        ### redirect_exit(...) is used to substitue in the hard exit that comes
-        ### with "os._exit(status)". Otherwise, ipython prevents "sys.exit(...)"
-        ### exit and dumps us back to the ipython command line.
-        ###
-        def redirect_exit(msg=None):
-            if isinstance(msg,int):
-                os._exit(msg)
-            elif msg is not None:
-                sys.stderr.write(str(msg))
-            os._exit(0)
-
-        @contextlib.contextmanager
-        def redirect_argv(args):
-            sys._argv = sys.argv[:]
-            ###
-            ### this could be cleaned up to be just:
-            ###
-            ###    sys.argv = ['somescript.py', arg1, arg2...]
-            ###
-            ### but casa scripts seem to have been
-            ### expecting the 'casa' arg as well as the '-c' arg
-            sys.argv=['casa','-c'] + args
-            sys._save_exit = sys.exit
-            sys.exit=redirect_exit
-            yield
-            sys.argv = sys._argv
-            sys.exit = sys._save_exit
-
+        # Run file with filename given in the command line
         try:
             with redirect_argv(casa['flags'].execute):
                 execfile(casa['flags'].execute[0])
         except Exception, err:
             traceback.print_exc()
-            os._exit(1)
+            immediate_exit_with_handlers(1)
 
-        os._exit(0)
+        immediate_exit_with_handlers()
+
     else:
+        # Run code given in the command line
         eval(casa['flags'].execute[0])
-        os._exit(0)
+        immediate_exit_with_handlers()
+
 else:
-    from casa_builtin import enable_builtin_protection,register_builtin
+    from casa_builtin import enable_builtin_protection, register_builtin
 
     register_builtin("casa")
     register_builtin("cu")
-    register_builtin(["viewer","imview","msview"])
+    register_builtin(["viewer", "imview", "msview"])
 
-    enable_builtin_protection( )
+    enable_builtin_protection()
     print "CASA Version " + casa['build']['version'] + "\n  Compiled on: " + casa['build']['time']
-
-
---
+++ gcwrap/python/scripts/init_welcome_helpers.py
@@ -0,0 +1,57 @@
+import contextlib
+import os
+import sys
+
+
+def immediate_exit_with_handlers(exit_status=0):
+    """
+    Exits using os._exit(exit_status), to terminate when running in
+    IPython.start_ipython() (which captures SystemExit and all exceptions).
+    But before that abrupt exit it ensures that all the exit handlers
+    registerd with atexit are run.
+    """
+    def run_atexit_handlers():
+        # Note: the drawback of this approach is that atexit._exithandlers is
+        # NOT available in Python 3
+        import atexit
+        # This could be done with atexit._run_exitfuncs(), but let's avoid
+        # using one more protected function:
+        for handler_tuple in reversed(atexit._exithandlers):
+            handler_tuple[0]()
+
+    run_atexit_handlers()
+    import os
+    os._exit(exit_status)
+
+
+###
+### redirect_argv(...) is used to present a consistent, clean sys.argv to
+### the file being executed (via "-c somescript.py arg1, arg2..."). The
+### redirect_exit(...) is used to substitue in the hard exit that comes
+### with "os._exit(status)". Otherwise, ipython prevents "sys.exit(...)"
+### exit and dumps us back to the ipython command line.
+###
+def redirect_exit(msg=None):
+    if isinstance(msg, int):
+        immediate_exit_with_handlers(msg)
+    elif msg is not None:
+        sys.stderr.write(str(msg))
+    immediate_exit_with_handlers(0)
+
+
+@contextlib.contextmanager
+def redirect_argv(args):
+    sys._argv = sys.argv[:]
+    ###
+    ### this could be cleaned up to be just:
+    ###
+    ###    sys.argv = ['somescript.py', arg1, arg2...]
+    ###
+    ### but casa scripts seem to have been
+    ### expecting the 'casa' arg as well as the '-c' arg
+    sys.argv = ['casa', '-c'] + args
+    sys._save_exit = sys.exit
+    sys.exit = redirect_exit
+    yield
+    sys.argv = sys._argv
+    sys.exit = sys._save_exit
