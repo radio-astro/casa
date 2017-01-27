6a416225ed711c3210057483a1df84c336976bab
remy indebetouw <remy@virginia.edu>
2017-01-27 14:39:50 +0000

    fix regression test
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@39417 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/regressions/m51_3sim_regression.py
+++ gcwrap/python/scripts/regressions/m51_3sim_regression.py
@@ -1,12 +1,12 @@
 ###############################################
 # Regression Script for simdata of a 2d image #
-# (single dish only simulation)               #
 ###############################################
  
 import os, time
 
 #modelname="M51HA.MODEL"
 modelname="m51ha.model"
+import shutil
 if os.path.exists(modelname):
     shutil.rmtree(modelname)
 
@@ -269,160 +269,152 @@ endProc = time.clock()
 
 # Regression
 
-test_name = """simdata observation of M51 (ALMA-12m INT + ACA-7m INT + 12m TP)"""
 
-ia.open(project+"/"+project + '.alma_0.5arcsec.image')
-m51both_stats=ia.statistics(verbose=False,list=False)
-ia.close()
+regstate=True
+verbose=True
+import testhelper as th
+
+# test SD first so that we can check that indep. from INT
+newMS=project+"/"+project+".aca.tp.sd.ms"
+if verbose: print newMS
+templateMS=th.findTemplate("sim_m51c",newMS)
+regstate=regstate and th.compMS(newMS,templateMS,verbose=verbose)
+
+newImage=project+"/"+project + '.sd.image'
+if verbose: print newImage
+templateImage=th.findTemplate("sim_m51c",newImage)
+regstate=regstate and th.compImages(newImage,templateImage,verbose=verbose)
+
+# then INT
+newMS=project+"/"+project+".alma_0.5arcsec.ms"
+if verbose: print newMS
+templateMS=th.findTemplate("sim_m51c",newMS)
+regstate=regstate and th.compMS(newMS,templateMS,verbose=verbose)
+
+newImage=project+"/"+project + '.alma_0.5arcsec.image'
+if verbose: print newImage
+templateImage=th.findTemplate("sim_m51c",newImage)
+regstate=regstate and th.compImages(newImage,templateImage,verbose=verbose)
+
+newImage=project+"/"+project + '.alma_0.5arcsec.diff'
+if verbose: print newImage
+templateImage=th.findTemplate("sim_m51c",newImage)
+regstate=regstate and th.compImages(newImage,templateImage,verbose=verbose)
 
 
-ia.open(project+"/"+project + '.alma_0.5arcsec.diff')
-m51both_diffstats=ia.statistics(verbose=False,list=False)
-ia.close()
 
-# # reference statistic values for simulated image
-# 20150323: update after image parameter change in HetArrayConvFunc (r32748)
-#refstats = {'max': 0.1385,
-#             'min': -0.025011,
-#             'rms': 0.019215,
-#             'sigma': 0.016102,
-#             'sum':  1257.2}
-# 20160516: update after image parameter change after Sinc correction in MosaicFT 
-#refstats = {'max': 0.14125,
-#             'min': -0.025361,
-#             'rms': 0.019264,
-#             'sigma': 0.01622,
-#             'sum':  1246.1}
-# 20160615: after fixing component corruption in B1950 frame
-# refstats = {'max': 0.14578,
-#              'min': -0.02492,
-#              'rms': 0.019677,
-#              'sigma': 0.016366,
-#              'sum':  1310.0}
+
+
+#test_name = """simdata observation of M51 (ALMA-12m INT + ACA-7m INT + 12m TP)"""
+#
+#ia.open(project+"/"+project + '.alma_0.5arcsec.image')
+#m51both_stats=ia.statistics(verbose=False,list=False)
+#ia.close()
+#
+#
+#ia.open(project+"/"+project + '.alma_0.5arcsec.diff')
+#m51both_diffstats=ia.statistics(verbose=False,list=False)
+#ia.close()
+#
 # 20160708: After fixing minweight bug in sdimaging
-refstats = {'max': 0.13952,
-             'min': -0.025238,
-             'rms': 0.018789,
-             'sigma': 0.016189,
-             'sum':  1143.4}
-# # reference statistic values for diff image
-# 20150308: update after image parameter change in simanalyze (r32603)
-#diffstats = {'max':  0.03221679,
-#             'min': -0.07377915,
-#             'rms': 0.00990815,
-#             'sigma': 0.00941917,
-#             'sum':  -367.45107187}
-# 20150308: update after image parameter change in HetArrayConvFunc.cc (r32748)
-#diffstats = {'max':  0.03277,
-#             'min': -0.073614,
-#             'rms': 0.010518,
-#             'sigma': 0.010015,
-#             'sum':  -385.33}
-# 20160516: update after sinc correction in MosaicFT
-#diffstats = {'max':  0.032796,
-#             'min': -0.067938,
-#             'rms': 0.010429,
-#             'sigma': 0.0099509,
-#             'sum':  -374.23}
-# 20160615: After fixing B1950 component corruption
-# diffstats = {'max':  0.031154,
-#              'min': -0.067642,
-#              'rms': 0.010623,
-#              'sigma': 0.0099747,
-#              'sum':  -438.1}
-# 20160708: After fixing minweight bug in sdimaging
-diffstats = {'max':  0.030522,
-             'min': -0.06007,
-             'rms': 0.0092802,
-             'sigma': 0.0089998,
-             'sum':  -271.5}
-# relative tolerances to reference values
-reftol   = {'sum':  1e-2,
-            'max':  1e-2,
-            'min':  1e-2,
-            'rms':  1e-2,
-            'sigma': 1e-2}
-
-import datetime
-datestring = datetime.datetime.isoformat(datetime.datetime.today())
-outfile    = project+"/"+project + '.' + datestring + '.log'
-logfile    = open(outfile, 'w')
-
-print 'Writing regression output to ' + outfile + "\n"
-print >> logfile,casa['build']
-
-loghdr = """
-********** Regression *****************
-"""
-
-print >> logfile, loghdr
+# refstats = {'max': 0.13952,
+#              'min': -0.025238,
+#              'rms': 0.018789,
+#              'sigma': 0.016189,
+#              'sum':  1143.4}
+# # 20160708: After fixing minweight bug in sdimaging
+# diffstats = {'max':  0.030522,
+#              'min': -0.06007,
+#              'rms': 0.0092802,
+#              'sigma': 0.0089998,
+#              'sum':  -271.5}
+# # relative tolerances to reference values
+# reftol   = {'sum':  1e-2,
+#             'max':  1e-2,
+#             'min':  1e-2,
+#             'rms':  1e-2,
+#             'sigma': 1e-2}
+# 
+# import datetime
+# datestring = datetime.datetime.isoformat(datetime.datetime.today())
+# outfile    = project+"/"+project + '.' + datestring + '.log'
+# logfile    = open(outfile, 'w')
+# 
+# print 'Writing regression output to ' + outfile + "\n"
+# print >> logfile,casa['build']
+# 
+# loghdr = """
+# ********** Regression *****************
+# """
+# 
+# print >> logfile, loghdr
 
 # more info
-ms.open(project+"/"+project+".alma_0.5arcsec.ms")
-print >> logfile, "Noiseless MS, amp stats:"
-print >> logfile, ms.statistics('DATA','amp')
-print >> logfile, "Noiseless MS, phase stats:"
-print >> logfile, ms.statistics('DATA','phase')
-ms.close()
-#ms.open(project+".noisy.ms")
-#print >> logfile, "Noisy MS, amp stats:"
-#print >> logfile, ms.statistics('DATA','amp')
-#print >> logfile, "Noisy MS, phase stats:"
-#print >> logfile, ms.statistics('DATA','phase')
-#ms.close()
-
-
-regstate = True
-rskes = refstats.keys()
-rskes.sort()
-for ke in rskes:
-    adiff=abs(m51both_stats[ke][0] - refstats[ke])/abs(refstats[ke])
-    if adiff < reftol[ke]:
-        print >> logfile, "* Passed %-5s image test, got % -11.5g expected % -11.5g." % (ke, m51both_stats[ke][0], refstats[ke])
-    else:
-        print >> logfile, "* FAILED %-5s image test, got % -11.5g instead of % -11.5g." % (ke, m51both_stats[ke][0], refstats[ke])
-        regstate = False
-
-rskes = diffstats.keys()
-rskes.sort()
-for ke in rskes:
-    adiff=abs(m51both_diffstats[ke][0] - diffstats[ke])/abs(diffstats[ke])
-    if adiff < reftol[ke]:
-        print >> logfile, "* Passed %-5s  diff test, got % -11.5g expected % -11.5g." % (ke, m51both_diffstats[ke][0], diffstats[ke])
-    else:
-        print >> logfile, "* FAILED %-5s  diff test, got % -11.5g instead of % -11.5g." % (ke, m51both_diffstats[ke][0], diffstats[ke])
-        regstate = False
-
-print >> logfile,'---'
+# ms.open(project+"/"+project+".alma_0.5arcsec.ms")
+# print >> logfile, "Noiseless MS, amp stats:"
+# print >> logfile, ms.statistics('DATA','amp')
+# print >> logfile, "Noiseless MS, phase stats:"
+# print >> logfile, ms.statistics('DATA','phase')
+# ms.close()
+# #ms.open(project+".noisy.ms")
+# #print >> logfile, "Noisy MS, amp stats:"
+# #print >> logfile, ms.statistics('DATA','amp')
+# #print >> logfile, "Noisy MS, phase stats:"
+# #print >> logfile, ms.statistics('DATA','phase')
+# #ms.close()
+# 
+# 
+# regstate = True
+# rskes = refstats.keys()
+# rskes.sort()
+# for ke in rskes:
+#     adiff=abs(m51both_stats[ke][0] - refstats[ke])/abs(refstats[ke])
+#     if adiff < reftol[ke]:
+#         print >> logfile, "* Passed %-5s image test, got % -11.5g expected % -11.5g." % (ke, m51both_stats[ke][0], refstats[ke])
+#     else:
+#         print >> logfile, "* FAILED %-5s image test, got % -11.5g instead of % -11.5g." % (ke, m51both_stats[ke][0], refstats[ke])
+#         regstate = False
+# 
+# rskes = diffstats.keys()
+# rskes.sort()
+# for ke in rskes:
+#     adiff=abs(m51both_diffstats[ke][0] - diffstats[ke])/abs(diffstats[ke])
+#     if adiff < reftol[ke]:
+#         print >> logfile, "* Passed %-5s  diff test, got % -11.5g expected % -11.5g." % (ke, m51both_diffstats[ke][0], diffstats[ke])
+#     else:
+#         print >> logfile, "* FAILED %-5s  diff test, got % -11.5g instead of % -11.5g." % (ke, m51both_diffstats[ke][0], diffstats[ke])
+#         regstate = False
+# 
+# print >> logfile,'---'
 if regstate:
-    print >> logfile, 'Passed',
+#    print >> logfile, 'Passed',
     print ''
     print 'Regression PASSED'
     print ''
 else:
-    print >> logfile, 'FAILED',
+#    print >> logfile, 'FAILED',
     print ''
     print 'Regression FAILED'
     print ''
-
-print >> logfile, 'regression test for simdata of M51 (ALMA-12m INT + ACA-7m INT + 12m TP).'
-print >>logfile,'---'
-print >>logfile,'*********************************'
-    
-print >>logfile,''
-print >>logfile,'********** Benchmarking **************'
-print >>logfile,''
-print >>logfile,'Total wall clock time was: %8.3f s.' % (endTime - startTime)
-print >>logfile,'Total CPU        time was: %8.3f s.' % (endProc - startProc)
-print >>logfile,'Wall processing  rate was: %8.3f MB/s.' % (17896.0 /
-                                                            (endTime - startTime))
-
-### Get last modification time of .ms.
-msfstat = os.stat(project+"/"+project+'.alma_0.5arcsec.ms')
-print >>logfile,'* Breakdown:                           *'
-print >>logfile,'*  generating visibilities took %8.3fs,' % (msfstat[8] - startTime)
-print >>logfile,'*************************************'
-    
-logfile.close()
-						    
-print '--Finished simdata of M51 (total power+interferometric) regression--'
+#
+#print >> logfile, 'regression test for simdata of M51 (ALMA-12m INT + ACA-7m INT + 12m TP).'
+#print >>logfile,'---'
+#print >>logfile,'*********************************'
+#    
+#print >>logfile,''
+#print >>logfile,'********** Benchmarking **************'
+#print >>logfile,''
+#print >>logfile,'Total wall clock time was: %8.3f s.' % (endTime - startTime)
+#print >>logfile,'Total CPU        time was: %8.3f s.' % (endProc - startProc)
+#print >>logfile,'Wall processing  rate was: %8.3f MB/s.' % (17896.0 /
+#                                                            (endTime - startTime))
+#
+#### Get last modification time of .ms.
+#msfstat = os.stat(project+"/"+project+'.alma_0.5arcsec.ms')
+#print >>logfile,'* Breakdown:                           *'
+#print >>logfile,'*  generating visibilities took %8.3fs,' % (msfstat[8] - startTime)
+#print >>logfile,'*************************************'
+#    
+#logfile.close()
+#						    
+#print '--Finished simdata of M51 (total power+interferometric) regression--'
