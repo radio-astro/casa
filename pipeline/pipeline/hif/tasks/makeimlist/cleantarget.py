0042582fcf0ff3c167a96dd79eae9e7475019955
dirk muders <dmuders@mpifr-bonn.mpg.de>
2017-02-02 17:17:49 +0000

    Added clean target template class.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@39469 ac9c0da5-131f-0410-9009-a9c195f183e6

---
+++ pipeline/pipeline/hif/tasks/makeimlist/cleantarget.py
@@ -0,0 +1,23 @@
+class CleanTarget(dict):
+
+    '''Clean target template definition.'''
+
+    def __init__(self):
+         self['field'] = ''
+         self['intent'] = ''
+         self['spw'] = ''
+         self['spwsel_lsrk'] = ''
+         self['spwsel_topo'] = ''
+         self['cell'] = ''
+         self['imsize'] = ''
+         self['phasecenter'] = ''
+         self['specmode'] = ''
+         self['gridder'] = ''
+         self['imagename'] = ''
+         self['start'] = ''
+         self['width'] = ''
+         self['nbin'] = ''
+         self['nchan'] = ''
+         self['uvrange'] = ''
+         self['stokes'] = ''
+         self['nterms'] = None
