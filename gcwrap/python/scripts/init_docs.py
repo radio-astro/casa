beb51c2ad186f4d6b4fa13f009b8e3b3b412bc99
darrell schiebel <ds@nrao.edu>
2017-02-23 03:41:12 +0000

    New Development: no             JIRA Issue: no       Ready to Release: yes      Interface Changes: none What Interface Changed: none          Test Programs: none   Put in Release Notes: no
               Description: add local table of contents functionality
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@39662 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/init_docs.py
+++ gcwrap/python/scripts/init_docs.py
@@ -12,42 +12,59 @@ class __doc(object):
     def __init__( self ):
         self.local_toc = None
         self.remote_toc = None
-
+        self.remote_toc_url = 'https://casa.nrao.edu/PloneResource/stable/toc.xml'
+        self.local_toc_url = casa['dirs']['doc'] + '/casa.nrao.edu/casadocs/toc.xml'
 
     def __call__( self, sec=None, remote=False ):
+        def show_toc( toc_dict ):
+            width = max(len(key) for key in toc_dict.keys( ))+3
+            for i in sorted(toc_dict.iterkeys( )):
+                if "external" in toc_dict[i]['visibility']:
+                    print "".join([i.ljust(width),toc_dict[i]['desc'].replace('\n','')])
+
+        def entry_to_dict(acc,e):
+            if e.tag == 'entry':
+                acc[e.find('key').text] = {
+                    'desc': e.find('desc').text,
+                    'type': e.find('type').text,
+                    'visibility': e.find('visibility').text,
+                    'path': e.find('path').text }
+            return acc
+
         if remote:
             if sec is None:
                 return webbrowser.open("https://casa.nrao.edu/casadocs/")
             else:
-                def show_toc( ):
-                    width = max(len(key) for key in self.remote_toc.keys( ))+3
-                    for i in self.remote_toc.keys( ):
-                        print "".join([i.ljust(width),self.remote_toc[i]['desc']])
-
-                def entry_to_dict(acc,e):
-                    if e.tag == 'entry':
-                        acc[e.find('key').text] = {
-                            'desc': e.find('desc').text,
-                            'type': e.find('type').text,
-                            'path': e.find('path').text }
-                    return acc
                 if self.remote_toc is None:
-                    self.remote_toc =  reduce( entry_to_dict, ET.ElementTree(file=urllib2.urlopen('https://casa.nrao.edu/PloneResource/stable/toc.xml')).getroot( ).getchildren( ), { } )
+                    self.remote_toc =  reduce( entry_to_dict, ET.ElementTree(file=urllib2.urlopen(self.remote_toc_url)).getroot( ).getchildren( ), { } )
                 if sec == 'toc':
-                    show_toc( )
+                    show_toc(self.remote_toc)
                 elif self.remote_toc.has_key(sec):
                     return webbrowser.open("https://casa.nrao.edu/casadocs/stable/" + self.remote_toc[sec]['path'])
                 else:
                     print "Sorry '%s' is not a recognized section..." % sec
                     print "------------------------------------------------------------------------------"
-                    show_toc( )
+                    show_toc(self.remote_toc)
         else:
-            path = casa['dirs']['doc'] + "/casa.nrao.edu/casadocs/stable.html"
-            if os.path.exists(path):
-                return webbrowser.open("file://" + path)
+            if sec is None:
+                path = casa['dirs']['doc'] + "/casa.nrao.edu/casadocs"
+                homepage = path + "/stable.html"
+                if os.path.exists(path):
+                    return webbrowser.open("file://" + homepage)
+                else:
+                    print "local documentation tree not found..."
+                    return False
             else:
-                print "local documentation tree not found..."
-                return False
+                if self.local_toc is None:
+                    self.local_toc = reduce( entry_to_dict, ET.ElementTree(file=urllib2.urlopen("file://" + self.local_toc_url)).getroot( ).getchildren( ), { } )
+                if sec == 'toc':
+                    show_toc(self.local_toc)
+                elif self.local_toc.has_key(sec):
+                    return webbrowser.open(path + "/stable/" + self.remote_toc[sec]['path'])
+                else:
+                    print "Sorry '%s' is not a recognized section..." % sec
+                    print "------------------------------------------------------------------------------"
+                    show_toc(self.local_toc)
 
 
     def fetch( self ):
@@ -75,11 +92,13 @@ class __doc(object):
         if sys.platform == "darwin":
             wgetcmd = casa['dirs']['root'] + "/Resources/wget"
 
-        return subprocess.call( [ wgetcmd, "--no-parent", "--no-check-certificate", "--html-extension", "--convert-links", "--recursive",
+        tree = subprocess.call( [ wgetcmd, "--no-parent", "--no-check-certificate", "--html-extension", "--convert-links", "--recursive",
                                   "--level=inf", "--page-requisites", "-e", "robots=off", "--wait=0", "--quota=inf", "--reject",
                                   '*_form,RSS,*login*,logged_in,*logout*,logged_out,createObject*,select_default_page,selectViewTemplate*,object_cut,object_copy,object_rename,delete_confirmation,content_status_*,addtoFavorites,pdf.html,print.html',
                                   "--exclude-directories='search,*com_mailto*'", "--directory-prefix=" + casa['dirs']['doc'],
                                   "--convert-links", url], stderr=subprocess.STDOUT, stdout=open(os.devnull,"w") )
+        toc = subprocess.call( [ wgetcmd, self.remote_toc_url, "-O", self.local_toc_url], stderr=subprocess.STDOUT, stdout=open(os.devnull,"w") )
+        return [tree, toc]
 
 
 doc = __doc( )
