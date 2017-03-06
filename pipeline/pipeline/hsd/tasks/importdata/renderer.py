6606102d8443ceb957e2863d2d36872ef36f442a
kana sugimoto <kana.sugi@nao.ac.jp>
2017-03-06 03:57:48 +0000

    New Development: Yes             JIRA Issue: Yes (CAS-8705)         Ready for Test: Yes      Interface Changes: No What Interface Changed:   Put in Release Notes: Yes              Module(s): hsd_importdata            Description: Use of appropriate (single dish) renderer in hsd_importdata.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@39751 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hsd/tasks/importdata/renderer.py
+++ pipeline/pipeline/hsd/tasks/importdata/renderer.py
@@ -9,10 +9,10 @@ LOG = logging.get_logger(__name__)
 DeductionGroupTR = collections.namedtuple('ReductionGroupTR', 'id fmin fmax field msname antenna spw nchan')
 
 class T2_4MDetailsSingleDishImportDataRenderer(super_renderer.T2_4MDetailsImportDataRenderer):
-    def __init__(self, template='hsd_importdata.mako',
+    def __init__(self, uri='hsd_importdata.mako',
                  description='Register measurement sets with the pipeline', 
                  always_rerender=False):
-        super(T2_4MDetailsSingleDishImportDataRenderer, self).__init__(template, description, always_rerender)
+        super(T2_4MDetailsSingleDishImportDataRenderer, self).__init__(uri, description, always_rerender)
         
     def update_mako_context(self, mako_context, pipeline_context, result):
         super(T2_4MDetailsSingleDishImportDataRenderer, self).update_mako_context(mako_context, pipeline_context, result)
@@ -36,7 +36,4 @@ class T2_4MDetailsSingleDishImportDataRenderer(super_renderer.T2_4MDetailsImport
                     tr = DeductionGroupTR(group_id, min_freq, max_freq, group_desc.field_name, msname, ants, spwid, num_chan)
                     row_values.append(tr)
 
-        #print("eduction_group = "+str(reduction_group))
-        print("ctx before="+str(mako_context))
         mako_context.update({'reduction_group_rows': utils.merge_td_columns(row_values)})
-        print("ctx after="+str(mako_context))
