c2f7c76251732c4336e8dde412d483c0193e4cb9
lindsey davis <ldavis@nrao.edu>
2017-03-20 13:40:42 +0000

    Adding new mssplit task.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@39849 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/h/tasks/__init__.py
+++ pipeline/pipeline/h/tasks/__init__.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 from .applycal import Applycal
 from .exportdata import ExportData
 from .importdata import ImportData
+from .mstransform import MsSplit
 from .restoredata import RestoreData
 from .tsyscal import Tsyscal
 from .tsysflag import Tsysflag
