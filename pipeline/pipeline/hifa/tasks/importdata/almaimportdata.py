5689e9d1ce8c2d493a951dd4fe1898eba814a572
lindsey davis <ldavis@nrao.edu>
2017-03-24 15:10:39 +0000

    Moved the ALMA data base handling code out of the h/tasks/importdata directory and into the hifa/tasks/importdata directory.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@39906 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifa/tasks/importdata/almaimportdata.py
+++ pipeline/pipeline/hifa/tasks/importdata/almaimportdata.py
@@ -4,6 +4,8 @@ import pipeline.infrastructure as infrastructure
 import pipeline.infrastructure.basetask as basetask
 
 import pipeline.h.tasks.importdata.importdata as importdata
+import pipeline.h.tasks.importdata.fluxes as fluxes
+from . import dbfluxes
 
 LOG = infrastructure.get_logger(__name__)
 
@@ -22,5 +24,31 @@ class ALMAImportDataInputs(importdata.ImportDataInputs):
 
 class ALMAImportData(importdata.ImportData):
     Inputs = ALMAImportDataInputs
+
+    def _get_fluxes(self, context, observing_run):
+
+        # get the flux measurements from Source.xml for each MS
+        if self.inputs.dbservice:
+            xml_results = dbfluxes.get_setjy_results(observing_run.measurement_sets)
+        else:
+            xml_results = fluxes.get_setjy_results(observing_run.measurement_sets)
+        # write/append them to flux.csv
+
+        # Cycle 1 hack for exporting the field intents to the CSV file:
+        # export_flux_from_result queries the context, so we pseudo-register
+        # the mses with the context by replacing the original observing run
+        orig_observing_run = context.observing_run
+        context.observing_run = observing_run
+        try:
+            fluxes.export_flux_from_result(xml_results, context)
+        finally:
+            context.observing_run = orig_observing_run
+
+        # re-read from flux.csv, which will include any user-coded values
+        combined_results = fluxes.import_flux(context.output_dir, observing_run)
+
+        return combined_results
+
+
 
 
