radio-astro/casa

commit 7744cda3c3e2c1afee5f34717b5fa4180f75b969

New Development: No             JIRA Issue: CAS-9096         Ready for Test: Yes      Interface Changes: What Interface Changed:          Test Programs: test_flagdata, tChannelAverageTVI   Put in Release Notes: No              Module(s): synthesis,mstransform,msvis            Description: Restore use of chanbin=1 as full channel-averaging 	   		trigger because flagdata (and mstransform) depend on 			this.  On an interim basis (for CASA 5.0, at most), 			made chanbin=0 a special value that means "no channel 			averaging" so that the bandpass calibration context 			will work. 			Also removed a stray cout from VisibilityIteratorImpl2.cc
git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@39958 ac9c0da5-131f-0410-9009-a9c195f183e6

code/synthesis/MeasurementComponents/SolvableVisCal.cc
@@ -2617,14 +2617,12 @@ void SolvableVisCal::convertHzToCh() {
     // Calculate channel increment from Hz
     if (fintervalCh()<0.0 && fintervalHz()>0.0) {
       // Assumes constant chan width in each spw!
-      Double datawidth=chanwidths[ispw][0].get("Hz").getValue();   //abs(spwcol.chanWidth()(ispw)(IPosition(1,0)));
-      //cout << "ispw=" << ispw << " datawidth=" << datawidth << flush;
-      fintervalCh()=floor(fintervalHz()/datawidth);
-      if (fintervalCh()<1.0) fintervalCh()=1.0;
-      //cout << " dHz=" << fintervalHz() << " --> " << fintervalCh() << " channels." << endl;
+      Double datawidthHz=abs(chanwidths[ispw][0].get("Hz").getValue()); 
+      fintervalCh()=floor(fintervalHz()/datawidthHz);
+      if (fintervalCh()<1.0) fintervalCh()=1.0;  // nothing fractional <1.0
 
       logSink() << ".  Spw " << ispw << ": "
-		<< " (freq solint: " << fintervalHz() << " Hz) / (data width: " << datawidth << " Hz)"
+		<< " (freq solint: " << fintervalHz() << " Hz) / (data width: " << datawidthHz << " Hz)"
 		<< " = " << fintervalCh() << " data channels per solution channel."
 		<< LogIO::POST;
     }

code/mstransform/TVI/ChannelAverageTVI.cc
@@ -90,6 +90,10 @@ Bool ChannelAverageTVI::parseConfiguration(const Record &configuration)
 
 		logger_p << LogIO::NORMAL << LogOrigin("ChannelAverageTVI", __FUNCTION__)
 				<< "Channel bin is " << chanbin_p << LogIO::POST;
+		if (anyEQ(chanbin_p,0)) {
+		  logger_p << LogIO::NORMAL << LogOrigin("ChannelAverageTVI", __FUNCTION__)
+			   << "  NB: Channel bin '0' means no channel averaging for the corresponding spw." << LogIO::POST;
+		}
 	}
 	else
 	{
@@ -124,10 +128,19 @@ void ChannelAverageTVI::initialize()
 	{
 		spw = iter->first;
 
-		// Make sure that chanbin is greater than or equal to 1
-		if ((uInt)chanbin_p(spw_idx) < 1)
+		// No averaging when user specifies 0
+		if (chanbin_p(spw_idx)==0) {
+		  logger_p << LogIO::DEBUG1 << LogOrigin("ChannelAverageTVI", __FUNCTION__)
+			   << "Specified chanbin for spw " << spw
+			   << " of 0 means no averaging."
+			   << LogIO::POST;
+		  
+		  spwChanbinMap_p[spw] = 1;
+		}
+		// Make sure that chanbin is greater than 1
+		else if ((uInt)chanbin_p(spw_idx) <= 1)
 		{
-			logger_p << LogIO::DEBUG1 << LogOrigin("MSTransformManager", __FUNCTION__)
+			logger_p << LogIO::DEBUG1 << LogOrigin("ChannelAverageTVI", __FUNCTION__)
 					<< "Specified chanbin for spw " << spw
 					<< " less than 1 falls back to the default number of"
 					<< " existing/selected channels: " << iter->second.size()
@@ -138,7 +151,7 @@ void ChannelAverageTVI::initialize()
 		// Make sure that chanbin does not exceed number of selected channels
 		else if ((uInt)chanbin_p(spw_idx) > iter->second.size())
 		{
-			logger_p << LogIO::WARN << LogOrigin("MSTransformManager", __FUNCTION__)
+			logger_p << LogIO::WARN << LogOrigin("ChannelAverageTVI", __FUNCTION__)
 					<< "Number of selected channels " << iter->second.size()
 					<< " for SPW " << spw
 					<< " is smaller than specified chanbin " << chanbin_p(spw_idx) << endl

code/msvis/MSVis/VisibilityIteratorImpl2.cc
@@ -1120,7 +1120,7 @@ VisibilityIteratorImpl2::initialize (const Block<const MeasurementSet *> &mss,
 
     if (useMSIter2) {
 
-      cout << "Using MSIter2......................................." << endl;
+      //cout << "Using MSIter2......................................." << endl;
 
       // This version uses the MSSmartInterval for time comparisons
       //   in the Table sort/iteration

code/synthesis/MeasurementComponents/CalSolVi2Organizer.cc
@@ -194,6 +194,10 @@ void CalSolVi2Organizer::addChanAve(Vector<Int> chanbin) {
   //  Must be at least one other layer already...
   AlwaysAssert(factories_.nelements()>0, AipsError);
 
+  // Force no averaging with chanbin[i]=0 in ChannelAverageTVI
+  //  (NB: chanbin[i]=1 means nchan averaging)
+  chanbin(chanbin==1)=0;
+
   Record config;
   config.define("chanbin",chanbin);
 

commit 8e0d07a768aeaa571581173f9c8ad8fafa390e07

New Development: No             JIRA Issue: CAS-9096         Ready for Test: Yes      Interface Changes: No What Interface Changed:          Test Programs: test_gaincal   Put in Release Notes: No              Module(s): synthesis            Description: Fixed special value used to trigger full nchan 	   		averaging for unchannelized calibration (e.g., 			gaincal), which broke in previous commit (r39958).
git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@39959 ac9c0da5-131f-0410-9009-a9c195f183e6

code/synthesis/MeasurementComponents/SolvableVisCal.cc
@@ -2297,7 +2297,7 @@ void SolvableVisCal::reParseSolintForVI2() {
   if (upcase(fsolint()).contains("NONE") ||   // unspecified  OR  
       !freqDepMat()) {                        // cal is entirely unchannelizedb 
     fsolint()="none";
-    fintervalCh_.set(0.0);    // signals full averaging (this is different from old way)
+    fintervalCh_.set(-1.0);    // signals full averaging (this is different from old way)
     fintervalHz_=-1.0;      // don't care
   }
   else {