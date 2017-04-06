b4dd7f49e4324cbb36e9b4704d3073185b5f0fbf
kana sugimoto <kana.sugi@nao.ac.jp>
2017-04-06 10:30:14 +0000

    New Development: No             JIRA Issue: Yes (CAS-8705)         Ready for Test: Yes      Interface Changes: No What Interface Changed:   Put in Release Notes: No              Module(s): hsd_k2jycal            Description: forgot to commit a file
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@40018 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/infrastructure/displays/singledish/__init__.py
+++ pipeline/pipeline/infrastructure/displays/singledish/__init__.py
@@ -4,4 +4,5 @@ from .clustering import ClusterDisplay
 from .tpimage import SDChannelAveragedImageDisplay
 from .spimage import SDSpectralImageDisplay
 from .imaging import SDImageDisplayFactory
+from .k2jy import K2JyHistDisplay
 #from .baselineall import SDBaselineAllDisplay
