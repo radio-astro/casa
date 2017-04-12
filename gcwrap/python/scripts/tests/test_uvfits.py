06cb172b41a24fb679079a6ab7b6cdea59de0a32
dave mehringer <dmehring@zia.aoc.nrao.edu>
2017-04-12 15:09:38 +0000

    New Development: Yes/No             JIRA Issue: No         Ready for Test: Yes/No      Interface Changes: Yes/No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: Removing test because of incomplete input data set
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@40053 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/tests/test_uvfits.py
+++ gcwrap/python/scripts/tests/test_uvfits.py
@@ -179,18 +179,6 @@ class uvfits_test(unittest.TestCase):
         self.assertFalse(exportuvfits(msname, fitsname, overwrite=False))
         self.assertTrue(exportuvfits(msname, fitsname, overwrite=True))
             
-    def test_varying_receptor_angles(self):
-        """CAS-8744 Test that selected spws with varying receptor angles will not be written"""
-        vis = datapath + "receptor_angle_test.ms"
-        myms = mstool()
-        myms.open(vis)
-        fitsfile = "blah.fits"
-        self.assertRaises(myms.tofits, fitsfile=fitsfile, overwrite=True)
-        self.assertTrue(myms.tofits(fitsfile=fitsfile, spw="1,2", overwrite=True))
-        for i in (0,1,2):
-            self.assertTrue(myms.tofits(fitsfile=fitsfile, spw=str(i), overwrite=True))
-        myms.done()
-
     def test_badscan(self):
         """CAS-10054: Tests intermittent incorrect scan number in last row of single-scan dataset"""
         myms = mstool()
