5f886ad05a49a42619b52b3108e41b601ba6739c
darrell schiebel <ds@nrao.edu>
2017-05-01 19:55:32 +0000

    New Development: no             JIRA Issue: CAS-9079       Ready to Release: yes      Interface Changes: none What Interface Changed: none          Test Programs: none   Put in Release Notes: no
               Description: fixes for documentation generation from the release mirror
    
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@40146 ac9c0da5-131f-0410-9009-a9c195f183e6

--- gcwrap/python/scripts/init_docs.py
+++ gcwrap/python/scripts/init_docs.py
@@ -5,6 +5,7 @@ import subprocess
 import sys
 import xml.etree.cElementTree as ET
 import urllib2
+from urlparse import urlparse
 
 class __doc(object):
     "command-line Plone help"
@@ -12,7 +13,10 @@ class __doc(object):
     def __init__( self ):
         self.local_toc = None
         self.remote_toc = None
-        self.remote_toc_url = 'https://casa.nrao.edu/PloneResource/stable/toc.xml'
+        self.remote_source_url = "https://casaweb-snapshot.cv.nrao.edu/casa5-0/casa5-0"
+        self.remote_source_url_components = urlparse(self.remote_source_url)
+        self.remote_toc_url = 'https://%s/PloneResource/stable/toc.xml' % self.remote_source_url_components[1]
+
         self.local_toc_url = None if casa['dirs']['doc'] is None else casa['dirs']['doc'] + '/casa.nrao.edu/casadocs/toc.xml'
 
     def __call__( self, sec=None ):
@@ -50,9 +54,9 @@ class __doc(object):
                     print "------------------------------------------------------------------------------"
                     show_toc(self.remote_toc)
         else:
-            path = casa['dirs']['doc'] + "/casa.nrao.edu/casadocs"
+            path = casa['dirs']['doc'] + "/casa.nrao.edu"
             if sec is None:
-                homepage = path + "/stable.html"
+                homepage = "%s/%s.html" % (path,self.remote_source_url_components[2])
                 if os.path.exists(path):
                     return webbrowser.open("file://" + homepage)
                 else:
@@ -89,11 +93,13 @@ class __doc(object):
             now = datetime.datetime.now( ).isoformat('-')
             os.rename(path, path + "." + now)
 
+        print "               source:  %s" % self.remote_source_url
+        print "    table of contents:  %s" % self.remote_toc_url
+        print "--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---"
         print "this will take some time..."
         print "relax..."
         print "do not hit ^C ..."
         print "do not expect output..."
-        url = "https://casa.nrao.edu/casadocs/stable"
 
         wgetcmd = "wget"
 
@@ -104,9 +110,19 @@ class __doc(object):
                                   "--level=inf", "--page-requisites", "-e", "robots=off", "--wait=0", "--quota=inf", "--reject",
                                   '*_form,RSS,*login*,logged_in,*logout*,logged_out,createObject*,select_default_page,selectViewTemplate*,object_cut,object_copy,object_rename,delete_confirmation,content_status_*,addtoFavorites,pdf.html,print.html',
                                   "--exclude-directories='search,*com_mailto*'", "--directory-prefix=" + casa['dirs']['doc'],
-                                  "--convert-links", url], stderr=subprocess.STDOUT, stdout=open(os.devnull,"w") )
+                                  "--convert-links", self.remote_source_url], stderr=subprocess.STDOUT, stdout=open(os.devnull,"w") )
         toc = subprocess.call( [ wgetcmd, self.remote_toc_url, "-O", self.local_toc_url], stderr=subprocess.STDOUT, stdout=open(os.devnull,"w") )
-        return [tree, toc]
+        if self.remote_source_url_components[1] != 'casa.nrao.edu':
+            orig = os.getcwd()
+            os.chdir(casa['dirs']['doc'])
+            if os.path.exists(self.remote_source_url_components[1]):
+                if os.path.exists('casa.nrao.edu'):
+                    os.remove('casa.nrao.edu')
+                os.symlink(self.remote_source_url_components[1],'casa.nrao.edu')
+            else:
+                print "warning, could not find mirror (%s/%s)" % (casa['dirs']['doc'],self.remote_source_url_components[1])
+            os.chdir(orig)
+        return (tree, toc)
 
 
 doc = __doc( )
