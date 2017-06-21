44d11819e01f65057d1812a2ff78d8354204e034
lindsey davis <ldavis@nrao.edu>
2017-06-21 19:30:33 +0000

    Fixed small issue in the QA2 / QA0 either or logic
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@40350 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/h/tasks/flagging/flagdeterbase.py
+++ pipeline/pipeline/h/tasks/flagging/flagdeterbase.py
@@ -421,15 +421,19 @@ class FlagDeterBase(basetask.StandardTaskTemplate):
                             'flagging for %s disabled.' % (inputs.fileonline, 
                                                            inputs.ms.basename))
             else:
-                # QA0 flag
+                # QA0 / QA2 flag
                 if inputs.qa0 or inputs.qa2:
                     cmdlist = self._read_flagfile(inputs.fileonline)
 
-                    flag_cmds.extend([cmd for cmd in cmdlist if ('QA0' in cmd)])
-                    flag_cmds.append("mode='summary' name='qa0'")
+                    # QA0 flag
+                    if inputs.qa0:
+                        flag_cmds.extend([cmd for cmd in cmdlist if ('QA0' in cmd)])
+                        flag_cmds.append("mode='summary' name='qa0'")
 
-                    flag_cmds.extend([cmd for cmd in cmdlist if ('QA2' in cmd)])
-                    flag_cmds.append("mode='summary' name='qa2'")
+                    # QA2 flag
+                    if inputs.qa2:
+                        flag_cmds.extend([cmd for cmd in cmdlist if ('QA2' in cmd)])
+                        flag_cmds.append("mode='summary' name='qa2'")
                     
                     # All other online flags
                     flag_cmds.extend([cmd for cmd in cmdlist if not ('QA0' in cmd) and not('QA2' in cmd)])
