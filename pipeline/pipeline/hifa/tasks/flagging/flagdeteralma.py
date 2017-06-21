4655d604ea2189f228e3c5bb685935ba027ae157
lindsey davis <ldavis@nrao.edu>
2017-06-21 19:21:05 +0000

    Added support for QA2 flags.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@40344 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifa/tasks/flagging/flagdeteralma.py
+++ pipeline/pipeline/hifa/tasks/flagging/flagdeteralma.py
@@ -16,8 +16,9 @@ class FlagDeterALMAInputs(flagdeterbase.FlagDeterBaseInputs):
     # new property for ACA correlator
     fracspwfps = basetask.property_with_default('fracspwfps', 0.048387)
 
-    # New property for QA0 flags
+    # New property for QA0 / QA2 flags
     qa0 = basetask.property_with_default('qa0', True)
+    qa2 = basetask.property_with_default('qa2', True)
 
     @basetask.log_equivalent_CASA_call
     def __init__(self, context, vis=None, output_dir=None, flagbackup=None,
@@ -25,7 +26,7 @@ class FlagDeterALMAInputs(flagdeterbase.FlagDeterBaseInputs):
                  intents=None, edgespw=None, fracspw=None, 
                  fracspwfps=None, online=None, fileonline=None,
                  template=None, filetemplate=None, hm_tbuff=None, 
-                 tbuff=None, qa0=None):
+                 tbuff=None, qa0=None, qa2=None):
         # set the properties to the values given as input arguments
         self._init_properties(vars())
 
