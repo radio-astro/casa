af0571ba67dbcacadeef44be34f61ad2beb45dbf
brian kent <bkent@nrao.edu>
2017-07-03 19:06:04 +0000

    CAS-10314.  Major alteration to flagging heuristics in hifv_checkflag.  rflag and tfcrop are used with new modes "bpd", "allcals", and "target".
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@40447 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifv/tasks/flagging/checkflag.py
+++ pipeline/pipeline/hifv/tasks/flagging/checkflag.py
@@ -49,46 +49,168 @@ class Checkflag(basetask.StandardTaskTemplate):
     def prepare(self):
         
         LOG.info("Checking RFI flagging of BP and Delay Calibrators")
-        
+        m = self.inputs.context.observing_run.measurement_sets[0]
+        tint = m.get_vla_max_integration_time()
+        pols = m.polarizations[0]
+
+        if (self.inputs.checkflagmode == 'bpd'):
+            fieldselect = self.inputs.context.evla['msinfo'][m.name].checkflagfields
+            scanselect = self.inputs.context.evla['msinfo'][m.name].testgainscans
+
+        if (self.inputs.checkflagmode == 'allcals'):
+            fieldselect = self.inputs.context.evla['msinfo'][m.name].calibrator_field_select_string
+            scanselect = self.inputs.context.evla['msinfo'][m.name].calibrator_scan_select_string
+
+            checkflagfields = self.inputs.context.evla['msinfo'][m.name].checkflagfields
+            testgainscans = self.inputs.context.evla['msinfo'][m.name].testgainscans
+
+            fieldselect = fieldselect.split(',')
+            scanselect = scanselect.split(',')
+
+            checkflagfields = checkflagfields.split(',')
+            testgainscans = testgainscans.split(',')
+
+            fieldselect = [fieldid for fieldid in fieldselect if fieldid not in checkflagfields]
+            scanselect = [scan for scan in scanselect if scan not in testgainscans]
+
+            fieldselect = ','.join(fieldselect)
+            scanselect = ','.join(scanselect)
+
+        if (self.inputs.checkflagmode == 'target'):
+            fieldsobj = m.get_fields(intent='TARGET')
+            fieldids = [field.id for field in fieldsobj]
+            fieldselect = ','.join([str(fieldid) for fieldid in fieldids])
+            scanselect = ''
+
+        if (self.inputs.checkflagmode == 'bpd' or
+            self.inputs.checkflagmode == 'allcals' or
+            self.inputs.checkflagmode == 'target'):
+
+            if ('RL' in pols.corr_type_string) and ('LR' in pols.corr_type_string):
+                for correlation in ['ABS_RL', 'ABS_LR']:
+
+                    method_args = {'mode': 'rflag',
+                                   'field': fieldselect,
+                                   'correlation': correlation,
+                                   'scan': scanselect,
+                                   'ntime': 'scan',
+                                   'datacolumn': 'corrected',
+                                   'flagbackup': True}
+
+                    checkflag_result = self._do_checkflag(**method_args)
+
+                extendflag_result = self._do_extendflag(field=fieldselect,
+                                                        scan=scanselect)
+
+            for correlation in ['REAL_RR', 'REAL_LL']:
+                method_args = {'mode': 'rflag',
+                               'field': fieldselect,
+                               'correlation': correlation,
+                               'scan': scanselect,
+                               'ntime': 'scan',
+                               'datacolumn': 'residual',
+                               'flagbackup': True}
+
+                checkflag_result = self._do_checkflag(**method_args)
+
+            extendflag_result = self._do_extendflag(field=fieldselect,
+                                                        scan=scanselect)
+
+            if ('RL' in pols.corr_type_string) and ('LR' in pols.corr_type_string):
+                for correlation in ['ABS_LR', 'ABS_RL']:
+                    method_args = {'mode': 'tfcrop',
+                                   'field': fieldselect,
+                                   'correlation': correlation,
+                                   'scan':scanselect,
+                                   'ntime': tint,
+                                   'datacolumn': 'corrected',
+                                   'flagbackup': True}
+
+                    tfcrop_result = self._do_tfcropflag(**method_args)
+
+                extendflag_result = self._do_extendflag(field=fieldselect,
+                                                        scan=scanselect)
+
+            for correlation in ['ABS_LL', 'ABS_RR']:
+                method_args = {'mode': 'tfcrop',
+                               'field': fieldselect,
+                               'correlation': correlation,
+                               'scan': scanselect,
+                               'ntime': tint,
+                               'datacolumn': 'corrected',
+                               'flagbackup': True}
+
+                tfcrop_result = self._do_tfcropflag(**method_args)
+
+            extendflag_result = self._do_extendflag(field=fieldselect,
+                                                        scan=scanselect)
+
+            # Do a second time on targets
+            if (self.inputs.checkflagmode == 'target'):
+                for correlation in ['ABS_LL', 'ABS_RR']:
+                    method_args = {'mode': 'tfcrop',
+                                   'field': fieldselect,
+                                   'correlation': correlation,
+                                   'scan': scanselect,
+                                   'ntime': tint,
+                                   'datacolumn': 'corrected',
+                                   'flagbackup': True}
+
+                    tfcrop_result = self._do_tfcropflag(**method_args)
+
+                extendflag_result = self._do_extendflag(field=fieldselect,
+                                                        scan=scanselect)
+
+            return extendflag_result
+
         # Values from pipeline context
         m = self.inputs.context.observing_run.measurement_sets[0]
         checkflagfields = self.inputs.context.evla['msinfo'][m.name].checkflagfields
         corrstring = m.get_vla_corrstring()
         testgainscans = self.inputs.context.evla['msinfo'][m.name].testgainscans
         
-        method_args = {'field'       : checkflagfields,
+        method_args = {'mode'        : 'rflag',
+                       'field'       : checkflagfields,
                        'correlation' : 'ABS_' + corrstring,
-                       'scan'        : testgainscans}
+                       'scan'        : testgainscans,
+                       'ntime'       : 'scan',
+                       'datacolumn'  : 'corrected',
+                       'flagbackup'  : False}
         
         if (self.inputs.checkflagmode == 'semi'):
             calibrator_field_select_string = self.inputs.context.evla['msinfo'][m.name].calibrator_field_select_string
             calibrator_scan_select_string = self.inputs.context.evla['msinfo'][m.name].calibrator_scan_select_string
         
-            method_args = {'field'       : calibrator_field_select_string,
+            method_args = {'mode'        : 'rflag',
+                           'field'       : calibrator_field_select_string,
                            'correlation' : 'ABS_' + corrstring,
-                           'scan'        : calibrator_scan_select_string}
+                           'scan'        : calibrator_scan_select_string,
+                           'ntime'       : 'scan',
+                           'datacolumn'  : 'corrected',
+                           'flagbackup'  : False}
         
         checkflag_result = self._do_checkflag(**method_args)
         
         return checkflag_result
     
-    def _do_checkflag(self, field=None, correlation=None, scan=None):
+    def _do_checkflag(self, mode='rflag', field=None, correlation=None, scan=None,
+                      ntime='scan', datacolumn='corrected', flagbackup=False):
         
         task_args = {'vis'          : self.inputs.vis,
-                     'mode'         : 'rflag',
+                     'mode'         : mode,
                      'field'        : field,
                      'correlation'  : correlation,
                      'scan'         : scan,
-                     'ntime'        : 'scan',
+                     'ntime'        : ntime,
                      'combinescans' : False,
-                     'datacolumn'   : 'corrected',
+                     'datacolumn'   : datacolumn,
                      'winsize'      : 3,
                      'timedevscale' : 4.0,
                      'freqdevscale' : 4.0,
                      'action'       : 'apply',
                      'display'      : '',
                      'extendflags'  : False,
-                     'flagbackup'   : False,
+                     'flagbackup'   : flagbackup,
                      'savepars'     : True}
                      
         job = casa_tasks.flagdata(**task_args)
@@ -99,3 +221,53 @@ class Checkflag(basetask.StandardTaskTemplate):
 
     def analyse(self, results):
         return results
+
+    def _do_extendflag(self, mode='extend', field=None,  scan=None,
+                      ntime='scan', extendpols=True, flagbackup=False):
+
+        task_args = {'vis': self.inputs.vis,
+                     'mode': mode,
+                     'field': field,
+                     'scan': scan,
+                     'ntime': ntime,
+                     'combinescans': False,
+                     'extendpols': extendpols,
+                     'growtime': 100.0,
+                     'growfreq': 60.0,
+                     'action': 'apply',
+                     'display': '',
+                     'extendflags': False,
+                     'flagbackup': flagbackup,
+                     'savepars': False}
+
+        job = casa_tasks.flagdata(**task_args)
+
+        self._executor.execute(job)
+
+        return CheckflagResults([job])
+
+    def _do_tfcropflag(self, mode='tfcrop', field=None, correlation=None, scan=None,
+                      ntime=0.45, datacolumn='corrected', flagbackup=True):
+
+        task_args = {'vis': self.inputs.vis,
+                     'mode': mode,
+                     'field': field,
+                     'correlation': correlation,
+                     'scan': scan,
+                     'ntime': ntime,
+                     'combinescans': False,
+                     'datacolumn': datacolumn,
+                     'freqcutoff': 3.0,
+                     'freqfit': 'line',
+                     'flagdimension': 'freq',
+                     'action': 'apply',
+                     'display': '',
+                     'extendflags': False,
+                     'flagbackup': flagbackup,
+                     'savepars': True}
+
+        job = casa_tasks.flagdata(**task_args)
+
+        self._executor.execute(job)
+
+        return CheckflagResults([job])
