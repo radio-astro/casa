6cba54dd612d87ee8d1d454419f1f8c6b290bb23
stewart williams <sjw@roe.ac.uk>
2017-10-13 11:00:12 +0000

    CAS-10731: Register VLASS template using new renderer registration mechanism
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@40905 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hif/tasks/makeimages/__init__.py
+++ pipeline/pipeline/hif/tasks/makeimages/__init__.py
@@ -13,6 +13,33 @@ pipelineqa.registry.add_handler(qa.MakeImagesQAHandler())
 pipelineqa.registry.add_handler(qa.MakeImagesListQAHandler())
 qaadapter.registry.register_to_imaging_topic(resultobjects.MakeImagesResult)
 
-weblog.add_renderer(MakeImages, 
+
+def _get_imaging_mode(context):
+    # NB: we only check the first entry in the clean list and assume any that
+    # follow will also be VLASS
+    try:
+        if 'VLASS' in context.clean_list_pending[0]['heuristics'].imaging_mode:
+            # this key should match the key against which the renderer is
+            # registered
+            return 'VLASS'
+    except IndexError:
+        pass
+    except KeyError:
+        pass
+
+    return None
+
+
+# no key/key_fn is specified, so this will be used for all non-VLASS data
+weblog.add_renderer(MakeImages,
                     clean_renderer.T2_4MDetailsTcleanRenderer(description='Calculate clean products'),
                     group_by=weblog.UNGROUPED)
+
+
+# if this is VLASS data, use a different weblog template
+weblog.add_renderer(MakeImages,
+                    clean_renderer.T2_4MDetailsTcleanRenderer(description='Calculate clean products',
+                                                              uri='vlass_tclean.mako'),
+                    group_by=weblog.UNGROUPED,
+                    key='VLASS',
+                    key_fn=_get_imaging_mode)
