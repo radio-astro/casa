9ea4650da8ce77aa4a59a86c88674be010a1c6ba
Joe Masters <jmasters@nrao.edu>
2017-11-02 18:23:03 +0000

    CAS-10731: Added stats and pbcor residual image to hifv_pbcor weblog.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@41020 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifv/tasks/pbcor/display.py
+++ pipeline/pipeline/hifv/tasks/pbcor/display.py
@@ -29,15 +29,17 @@ class PbcorimagesSummary(object):
         plot_wrappers = []
 
         for pbcorimagename in self.result.pbcorimagenames:
-            #plot_wrappers.append(displays.SkyDisplay().plot(self.context, pbcorimagename,
-            plot_wrappers.append(sky.SkyDisplay().plot(self.context, pbcorimagename,
-                                                            reportdir=stage_dir, intent='',
-                                                            collapseFunction='mean'))
-
-            with casatools.ImageReader(pbcorimagename) as image:
-                stats = image.statistics(robust=True)
-                self.result.max = stats.get('max')[0]
-                self.result.min = stats.get('min')[0]
-                self.result.sigma = stats.get('sigma')[0]
+            if 'residual.pbcor' in pbcorimagename:
+                plot_wrappers.append(sky.SkyDisplay().plot(self.context, pbcorimagename,
+                                                           reportdir=stage_dir, intent='',
+                                                           collapseFunction='mean'))
+                with casatools.ImageReader(pbcorimagename) as image:
+                    self.result.residual_stats = image.statistics(robust=True)
+            else:
+                plot_wrappers.append(sky.SkyDisplay().plot(self.context, pbcorimagename,
+                                                           reportdir=stage_dir, intent='',
+                                                           collapseFunction='mean'))
+                with casatools.ImageReader(pbcorimagename) as image:
+                    self.result.pbcor_stats = image.statistics(robust=True)
 
         return [p for p in plot_wrappers if p is not None]
