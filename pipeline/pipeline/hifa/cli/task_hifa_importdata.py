1a98cabae5058d02d23f27dbbdce4ded9c779c2f
lindsey davis <ldavis@nrao.edu>
2017-11-28 17:34:03 +0000

    Added support for the asimaging paramter to the hifa_importdata CASA interface.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@41097 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifa/cli/task_hifa_importdata.py
+++ pipeline/pipeline/hifa/cli/task_hifa_importdata.py
@@ -6,7 +6,7 @@ import pipeline.h.cli.utils as utils
 
 
 def hifa_importdata(vis=None, session=None, pipelinemode=None, asis=None, process_caldevice=None, overwrite=None,
-                    nocopy=None, bdfflags=None, lazy=None, dbservice=None, ocorr_mode=None, createmms=None, dryrun=None,
+                    nocopy=None, bdfflags=None, asimaging=None, lazy=None, dbservice=None, ocorr_mode=None, createmms=None, dryrun=None,
                     acceptresults=None):
     # create a dictionary containing all the arguments given in the
     # constructor
