a46d1c3aea36014101253bf3423f8099a4c9187d
brian kent <bkent@nrao.edu>
2017-12-19 19:45:16 +0000

    CAS-10985.  Updated KTypegaincal Inputs to vdp.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@41139 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hif/tasks/gaincal/ktypegaincal.py
+++ pipeline/pipeline/hif/tasks/gaincal/ktypegaincal.py
@@ -2,33 +2,53 @@ from __future__ import absolute_import
 
 import pipeline.infrastructure.basetask as basetask
 import pipeline.infrastructure as infrastructure
+import pipeline.infrastructure.vdp as vdp
 from . import common
 from . import gaincalworker
 
 LOG = infrastructure.get_logger(__name__)
 
+class KTypeGaincalInputs(common.VdpCommonGaincalInputs):
+    combine = vdp.VisDependentProperty(default='')
+    minsnr  = vdp.VisDependentProperty(default=3)
+    solint  = vdp.VisDependentProperty(default='int')
+    solnorm = vdp.VisDependentProperty(default=False)
+    gaintype = vdp.VisDependentProperty(default='K')
 
-
-class KTypeGaincalInputs(common.CommonGaincalInputs):
-    # simple vis-independent properties with default values
-    combine = basetask.property_with_default('combine', '')
-    minsnr  = basetask.property_with_default('minsnr', 3)
-    solint  = basetask.property_with_default('solint', 'int')
-    solnorm = basetask.property_with_default('solnorm', False)
-    gaintype = basetask.property_with_default('gaintype', 'K')
-
-    def __init__(self, context, output_dir=None,
-                 # 
-                 vis=None, caltable=None, 
-                 # data selection arguments
+    def __init__(self, context, output_dir=None, vis=None, caltable=None,
                  field=None, spw=None, antenna=None, uvrange=None, intent=None,
-                 # solution parameters
                  smodel=None, gaintype=None, calmode=None, solint=None,
-		 combine=None, refant=None, minblperant=None, minsnr=None,
-		 solnorm=None, append=None, scan=None,
-                 # preapply calibrations
+                 combine=None, refant=None, minblperant=None, minsnr=None,
+                 solnorm=None, append=None, scan=None,
                  opacity=None, parang=None):
-        self._init_properties(vars())
+
+        self.context = context
+        self.vis = vis
+        self.output_dir = output_dir
+
+        self.field = field
+        self.spw = spw
+        self.antenna = antenna
+        self.uvrange = uvrange
+        self.intent = intent
+        self.scan = scan
+
+        self.gaintype = gaintype
+
+        self.smodel = smodel
+        self.calmode = calmode
+        self.solint = solint
+        self.combine = combine
+        self.refant = refant
+        self.minblperant = minblperant
+        self.minsnr = minsnr
+        self.solnorm = solnorm
+        self.append = append
+        self.scan = scan
+        self.opacity = opacity
+        self.parang = parang
+
+        self.caltable = caltable
 
 
 class KTypeGaincal(gaincalworker.GaincalWorker):
