dc42dd41a00926bffc47acb9cfad98c880fad69e
lindsey davis <ldavis@nrao.edu>
2018-01-05 16:41:41 +0000

    Converted the hif_mstransform task to use the new vdp parameters.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@41151 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hif/tasks/mstransform/mstransform.py
+++ pipeline/pipeline/hif/tasks/mstransform/mstransform.py
@@ -8,83 +8,49 @@ import pipeline.infrastructure as infrastructure
 import pipeline.infrastructure.basetask as basetask
 import pipeline.infrastructure.callibrary as callibrary
 import pipeline.infrastructure.tablereader as tablereader
+import pipeline.infrastructure.vdp as vdp
 from pipeline.infrastructure import casa_tasks
 
 LOG = infrastructure.get_logger(__name__)
 
 # Define the minimum set of parameters required to split out
 # the TARGET data from the complete and fully calibrated
-# original MS. Other parameters will be added as more
+# original MS. Other parameters will be added here  as more
 # capabilites are added to hif_mstransform.
 
-class MstransformInputs(basetask.StandardInputs):
+class MstransformInputs(vdp.StandardInputs):
 
-    def __init__(self, context, output_dir=None, vis=None,
-        outputvis=None, field=None, intent=None, spw=None):
-
-        # set the properties to the values given as input arguments
-        self._init_properties(vars())
-
-    # Revisit this later
-    #    For now use a simple default naming convention
-    #    Borrow the output file name list handling technique from
-    #    the hif_flagdata interface
-    @property
+    @vdp.VisDependentProperty
     def outputvis(self):
-        if type(self.vis) is types.ListType:
-            return self._handle_multiple_vis('outputvis')
+        vis_root = os.path.splitext(self.vis)[0]
+        return vis_root + '_target.ms'
 
-        if not self._outputvis:
-            vis_root = os.path.splitext(self.vis)[0]
-            return vis_root + '_target.ms'
-
-        if type(self._outputvis) is types.ListType:
-            idx = self._my_vislist.index(self.vis)
-            return self._outputvis[idx]
-
-        return self._outputvis
-
-    @outputvis.setter
+    @outputvis.convert
     def outputvis(self, value):
-        if value in (None, ''):
-            value = []
-        elif type(value) is types.StringType:
-            value = list(value.replace('[','').replace(']','').replace("'","").split(','))
-        self._outputvis = value
+        if isinstance(value, str):
+            return list(value.replace('[', '').replace(']', '').replace("'", "").split(','))
+        else:
+            return value
 
-    # Find all the fields with TARGET intent
-    @property
+    # By default find all the fields with TARGET intent
+    @vdp.VisDependentProperty
     def field(self):
-        # If field was explicitly set, return that value
-        if self._field is not None:
-            return self._field
 
-        # If invoked with multiple mses, return a list of fields
-        if type(self.vis) is types.ListType:
-            return self._handle_multiple_vis('field')
-
-        # Otherwise return each field in the current ms that has been observed
+        # Find fields in the current ms that have been observed
         # with the desired intent
         fields = self.ms.get_fields(intent=self.intent)
 
-        # CAS-9499: MOUS uid://A001/X88f/X128 aborts during
-        # hif_uvcontfit with AttributeError: 'Field' object has no
-        # attribute 'source'.
-        #
-        # This bug revealed a problem that occurs when an observation
-        # is terminated: the final scan can be a TARGET scan but not
-        # contain any data for the science spectral windows, only for
-        # the square law detectors. The following code removes fields
-        # that do not contain data for the requested Inputs spectral
-        # windows - the science spectral windows by default.
-        #
+        # When an observation is terminated the final scan can be a TARGET scan but may
+        # not contain any TARGET data for the science spectral windows, e.g. there is square
+        # law detector data but nothing else. The following code removes fields that do not contain
+        # data for the requested spectral windows - the science spectral windows by default.
+        
         if fields:
             fields_by_id = sorted(fields, key=operator.attrgetter('id'))
             last_field = fields_by_id[-1]
 
-            # While we're here, remove any fields that are not
-            # linked with a source. This should not occur since the
-            # CAS-9499 tablereader bug was fixed, but check anyway.
+            # While we're here, remove any fields that are not linked with a source. This should
+            # not occur since the CAS-9499 tablereader bug was fixed, but check anyway.
             if getattr(last_field, 'source', None) is None:
                 fields.remove(last_field)
                 LOG.info('Truncated observation detected (no source for field): '
@@ -104,67 +70,65 @@ class MstransformInputs(basetask.StandardInputs):
         field_ids = set([f.id for f in fields])
 
         # Fields with different intents may have the same name. Check for this
-        # and return the IDs if necessary
+        # and return the ids rather than the names if necessary to resolve any
+        # ambiguities
         if len(unique_field_names) is len(field_ids):
             return ','.join(unique_field_names)
         else:
             return ','.join([str(i) for i in field_ids])
 
-    @field.setter
-    def field(self, value):
-        self._field = value
-
     # Select TARGET data by default
-    @property
-    def intent(self):
-        return self._intent
+    intent = vdp.VisDependentProperty(default='TARGET')
 
-    @intent.setter
-    def intent(self, value):
-        if value is None:
-            value = 'TARGET'
-        self._intent = value
-
-    # Find all the the spws TARGET intent. These may be a subset of the
-    # science windows which included calibration spws.
-    @property
+    # Find all the spws with TARGET intent. These may be a subset of the
+    # science spws which include calibration spws.
+    @vdp.VisDependentProperty
     def spw(self):
-        if self._spw is not None:
-            return self._spw
-
-        if type(self.vis) is types.ListType:
-            return self._handle_multiple_vis('spw')
-
         science_target_intents = set(self.intent.split(','))
         science_target_spws = []
 
-        science_spws = [spw for spw in self.ms.get_spectral_windows(self._spw)]
+        science_spws = [spw for spw in self.ms.get_spectral_windows(science_windows_only=True)]
         for spw in science_spws:
             if spw.intents.intersection(science_target_intents):
                 science_target_spws.append(spw)
+
         return ','.join([str(spw.id) for spw in science_target_spws])
 
-    @spw.setter
+    @spw.convert
     def spw(self, value):
-        self._spw = value
+        science_target_intents = set(self.intent.split(','))
+        science_target_spws = []
+
+        science_spws = [spw for spw in self.ms.get_spectral_windows(task_arg=value, science_windows_only=True)]
+        for spw in science_spws:
+            if spw.intents.intersection(science_target_intents):
+                science_target_spws.append(spw)
+
+        return ','.join([str(spw.id) for spw in science_target_spws])
+
+    def __init__(self, context, output_dir=None, vis=None,
+        outputvis=None, field=None, intent=None, spw=None):
+
+        # set the properties to the values given as input arguments
+        self.context = context
+        self.vis = vis
+        self.output_dir = output_dir
+        self.outputvis = outputvis
+        self.field = field
+        self.intent = intent
+        self.spw = spw
 
     def to_casa_args(self):
+
+        # Get parameter dictionary.
         d = super(MstransformInputs, self).to_casa_args()
 
-        # Filter out unwanted parameters
-        #     Note that the trailing , is required
-        #     Leave the commented out code in place
-        #     as an example.
-        #for ignore in ('field', 'spw', ):
-            #if ignore in d:
-                #del d[ignore]
-
         # Force the data column to be 'corrected' and the
-        # new (with casa 4.6) reindex parameter to False 
+        # new (with casa 4.6) reindex parameter to be False 
         d['datacolumn'] = 'corrected'
         d['reindex'] = False
-        return d
 
+        return d
 
 class Mstransform(basetask.StandardTaskTemplate):
     Inputs = MstransformInputs
@@ -177,8 +141,6 @@ class Mstransform(basetask.StandardTaskTemplate):
             outputvis=inputs.outputvis)
 
         # Run CASA task
-        #    Does this need a try / except block 
-
         mstransform_args = inputs.to_casa_args()
         mstransform_job = casa_tasks.mstransform(**mstransform_args)
         self._executor.execute(mstransform_job)
@@ -186,6 +148,7 @@ class Mstransform(basetask.StandardTaskTemplate):
         return result
 
     def analyse(self, result):
+
         # Check for existence of the output vis. 
         if not os.path.exists(result.outputvis):
             LOG.debug('Error creating target MS %s' % (os.path.basename(result.outputvis)))
