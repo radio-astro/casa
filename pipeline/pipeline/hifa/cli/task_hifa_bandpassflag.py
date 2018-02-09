5fbe8bbae36e77dbb722097999767380d7e57bc9
lindsey davis <ldavis@nrao.edu>
2018-02-09 21:20:26 +0000

    Fixed class definition.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@41242 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifa/cli/task_hifa_bandpassflag.py
+++ pipeline/pipeline/hifa/cli/task_hifa_bandpassflag.py
@@ -19,7 +19,7 @@ def hifa_bandpassflag(
     # constructor
     all_inputs = vars()
 
-    task_name = 'ALMABandpassflag'
+    task_name = 'Bandpassflag'
 
     ##########################################################################
     #                                                                        #
