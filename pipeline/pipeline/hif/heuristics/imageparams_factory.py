2355e3a39f5ce84eba602b3276ea09b375808f89
Joe Masters <jmasters@nrao.edu>
2018-03-09 15:24:36 +0000

    CAS-11192: add new image heuristics class and mode for VLASS SE taper.
    This is similar to the heuristics settings for SE continuum.
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@41292 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hif/heuristics/imageparams_factory.py
+++ pipeline/pipeline/hif/heuristics/imageparams_factory.py
@@ -1,6 +1,7 @@
 from .imageparams_alma import ImageParamsHeuristicsALMA
 from .imageparams_vlass_quick_look import ImageParamsHeuristicsVlassQl
 from .imageparams_vlass_single_epoch_continuum import ImageParamsHeuristicsVlassSeCont
+from .imageparams_vlass_single_epoch_taper import ImageParamsHeuristicsVlassSeTaper
 from .imageparams_vlass_single_epoch_cube import ImageParamsHeuristicsVlassSeCube
 from .imageparams_vla import ImageParamsHeuristicsVLA
 
@@ -17,9 +18,11 @@ class ImageParamsHeuristicsFactory(object):
             return ImageParamsHeuristicsVlassQl(vislist, spw, observing_run, imagename_prefix, proj_params, contfile, linesfile)
         elif imaging_mode == 'VLASS-SE-CONT':  # single epoch continuum
             return ImageParamsHeuristicsVlassSeCont(vislist, spw, observing_run, imagename_prefix, proj_params, contfile, linesfile)
+        elif imaging_mode == 'VLASS-SE-TAPER':  # single epoch taper
+            return ImageParamsHeuristicsVlassSeTaper(vislist, spw, observing_run, imagename_prefix, proj_params, contfile, linesfile)
         elif imaging_mode == 'VLASS-SE-CUBE':  # single epoch cube
             return ImageParamsHeuristicsVlassSeCube(vislist, spw, observing_run, imagename_prefix, proj_params, contfile, linesfile)
         elif imaging_mode == 'VLA' or imaging_mode == 'JVLA' or imaging_mode == 'EVLA':  # VLA but not VLASS
             return ImageParamsHeuristicsVLA(vislist, spw, observing_run, imagename_prefix, proj_params, contfile, linesfile)
         else:
-            raise Exception('Unknown imaging mode: %s' % (imaging_mode))
+            raise Exception('Unknown imaging mode: %s' % imaging_mode)
