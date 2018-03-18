4478f2a4fecafb3d9a877d018ea21a530f58c46e
lindsey davis <ldavis@nrao.edu>
2018-03-18 19:38:03 +0000

    Fixed a bug in the definition of the asis parameter that caused PPR execution of hifa_restoredata to fail.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@41303 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hifa/tasks/restoredata/almarestoredata.py
+++ pipeline/pipeline/hifa/tasks/restoredata/almarestoredata.py
@@ -10,7 +10,7 @@ LOG = infrastructure.get_logger(__name__)
 
 
 class ALMARestoreDataInputs(restoredata.RestoreDataInputs):
-    asis = vdp.VisDependentProperty('SBSummary ExecBlock Antenna Station Receiver Source CalAtmosphere CalWVR')
+    asis = vdp.VisDependentProperty(default='SBSummary ExecBlock Antenna Station Receiver Source CalAtmosphere CalWVR')
 
     def __init__(self, context, copytoraw=None, products_dir=None, rawdata_dir=None, output_dir=None, session=None,
                  vis=None, bdfflags=None, lazy=None, asis=None, ocorr_mode=None):
@@ -28,6 +28,7 @@ class ALMARestoreData(restoredata.RestoreData):
     # now but should simplify parameters in future
     def _do_importasdm(self, sessionlist, vislist):
         inputs = self.inputs
+
         container = vdp.InputsContainer(almaimportdata.ALMAImportData, inputs.context, vis=vislist, session=sessionlist,
                                         save_flagonline=False, lazy=inputs.lazy, bdfflags=inputs.bdfflags,
                                         dbservice=False, asis=inputs.asis, ocorr_mode=inputs.ocorr_mode)
