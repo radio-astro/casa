5a055bada2cbcd68617edacab88beac0ede682ef
dirk muders <dmuders@mpifr-bonn.mpg.de>
2018-04-29 18:29:13 +0000

    Avoid ALMA heuristics for robust and uvtaper.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@41419 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hif/heuristics/imageparams_vla.py
+++ pipeline/pipeline/hif/heuristics/imageparams_vla.py
@@ -9,3 +9,11 @@ class ImageParamsHeuristicsVLA(ImageParamsHeuristicsALMA):
     def __init__(self, vislist, spw, observing_run, imagename_prefix='', proj_params=None, contfile=None, linesfile=None):
         ImageParamsHeuristicsALMA.__init__(self, vislist, spw, observing_run, imagename_prefix, proj_params, contfile, linesfile)
         self.imaging_mode = 'VLA'
+
+    def robust(self, beam=None):
+
+        return 0.5
+
+    def uvtaper(self, beam_natural=None, protect_long=None):
+
+        return []
