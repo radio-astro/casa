d80c764aebc9e966fc8586f94230b64e9f9083d3
takeshi nakazato <takeshi.nakazato@nao.ac.jp>
2018-05-25 07:19:50 +0000

    explicitly set always_rerender value
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@41509 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hsd/tasks/baseline/__init__.py
+++ pipeline/pipeline/hsd/tasks/baseline/__init__.py
@@ -9,4 +9,4 @@ from .baseline import SDBaseline
 
 qaadapter.registry.register_to_miscellaneous_topic(baseline.SDBaselineResults)
 
-weblog.add_renderer(SDBaseline, renderer.T2_4MDetailsSingleDishBaselineRenderer(), group_by=weblog.UNGROUPED)
+weblog.add_renderer(SDBaseline, renderer.T2_4MDetailsSingleDishBaselineRenderer(always_rerender=False), group_by=weblog.UNGROUPED)
