cf89f09bbd4ed8bb69b1dc06afd72fae7f84145e
dirk muders <dmuders@mpifr-bonn.mpg.de>
2018-05-30 20:40:09 +0000

    CAS-11211:
    Initialize sensitivity dictionary with dummy robust and uvtaper values.
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@41522 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/infrastructure/launcher.py
+++ pipeline/pipeline/infrastructure/launcher.py
@@ -133,7 +133,7 @@ class Context(object):
         self.clean_list_pending = []
         self.clean_list_info = {}
         self.sensitivities = []
-        self.per_spw_cont_sensitivities_all_chan = {}
+        self.per_spw_cont_sensitivities_all_chan = {'robust': None, 'uvtaper': None}
 
         LOG.trace('Creating report directory \'%s\'' % self.report_dir)
         utils.mkdir_p(self.report_dir)
