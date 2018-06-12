1168b227d155d35040b46fb245bb690d23cfd6d6
takeshi nakazato <takeshi.nakazato@nao.ac.jp>
2018-06-12 02:00:59 +0000

    CAS-7720, CAS-11046 Parallelize hsd_flagdata
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@41590 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hsd/tasks/flagging/__init__.py
+++ pipeline/pipeline/hsd/tasks/flagging/__init__.py
@@ -2,10 +2,13 @@ from __future__ import absolute_import
 
 import pipeline.h.tasks.flagging.renderer as super_renderer
 import pipeline.infrastructure.renderer.weblog as weblog
-from .flagdeteralmasd import FlagDeterALMASingleDish
+from .flagdeteralmasd import FlagDeterALMASingleDish, HpcFlagDeterALMASingleDish
 
 # Use generic deterministic flagging renderer for ALMA SD
 # deterministic flagging.
 weblog.add_renderer(FlagDeterALMASingleDish,
+                    super_renderer.T2_4MDetailsFlagDeterBaseRenderer(description='ALMA SD deterministic flagging'),
+                    group_by=weblog.UNGROUPED)
+weblog.add_renderer(HpcFlagDeterALMASingleDish,
                     super_renderer.T2_4MDetailsFlagDeterBaseRenderer(description='ALMA SD deterministic flagging'),
                     group_by=weblog.UNGROUPED)
