50d8ab286def5b039a17aed32d4c47096a278891
stewart williams <sjw@roe.ac.uk>
2018-07-16 09:30:00 +0000

    Introduce enable_fd_logs function to log file descriptors before, during, and after CASA task execution.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@41727 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/infrastructure/utils.py
+++ pipeline/pipeline/infrastructure/utils.py
@@ -5,6 +5,7 @@ import copy
 import datetime
 import decimal
 import errno
+import functools
 import glob
 import inspect
 import itertools
@@ -14,6 +15,8 @@ import os
 import platform
 import re
 import string
+import subprocess
+import threading
 import types
 import uuid
 import xml.dom.minidom as minidom
@@ -375,6 +378,52 @@ def enable_memstats():
 
     jobrequest.PREHOOKS.append(get_hook_fn('Memory usage before '))
     jobrequest.POSTHOOKS.append(get_hook_fn('Memory usage after '))
+
+
+def enable_fd_logs(interval_secs=60):
+    """
+    Log file descriptors to the CASA log every n seconds.
+
+    :param interval_secs: logging cadence in seconds (default=60)
+    :return:
+    """
+    if platform.system() == 'Darwin':
+        LOG.error('Cannot list file descriptors on MacOS')
+        return
+
+    import pipeline.infrastructure.jobrequest as jobrequest
+
+    pid = os.getpid()
+    def list_file_descriptors(msg, job_name):
+        try:
+            out = subprocess.check_output(['ls', '-l', '/proc/{}/fd'.format(pid)])
+        except subprocess.CalledProcessError:
+            LOG.info('Could not list file descriptors for PID {}'.format(pid))
+        else:
+            LOG.info('File descriptors {} {}:\n{}'.format(msg, job_name, out))
+
+    def get_hook_fn(msg, start=False, cancel=False):
+        def log_file_descriptors(jobrequest):
+            job_name = jobrequest.fn.__name__
+            if start:
+                if enable_fd_logs.interval:
+                    enable_fd_logs.interval.stop()
+                LOG.info('Logging file descriptors every {} seconds during {}'.format(interval_secs, job_name))
+                enable_fd_logs.interval = Interval(interval_secs, list_file_descriptors, args=['during', job_name])
+                enable_fd_logs.interval.start()
+
+            if cancel and enable_fd_logs.interval:
+                enable_fd_logs.interval.stop()
+                enable_fd_logs.interval = None
+
+            list_file_descriptors(msg, job_name)
+
+        return log_file_descriptors
+
+    jobrequest.PREHOOKS.append(get_hook_fn('before', start=True))
+    jobrequest.POSTHOOKS.append(get_hook_fn('after', cancel=True))
+
+enable_fd_logs.interval = None
 
 
 def get_calfroms(context, vis, caltypes=None):
@@ -1454,3 +1503,48 @@ def write_errorexit_file(path, root, extension):
         return errorfile
     else:
         return ''
+
+
+class Interval(object):
+    def __init__(self, interval, function, args=None, kwargs=None):
+        """
+        Runs the function at a specified interval with given arguments.
+        """
+        if args is None:
+            args = []
+        if kwargs is None:
+            kwargs = {}
+        self.interval = interval
+        self.function = functools.partial(function, *args, **kwargs)
+        self.running = False
+        self._timer = None
+
+    def __call__(self):
+        """
+        Handler function for calling the partial and continuting.
+        """
+        self.running = False  # mark not running
+        self.start()  # reset the timer for the next go
+        self.function()  # call the partial function
+
+    def start(self):
+        """
+        Starts the interval and lets it run.
+        """
+        if self.running:
+            # Don't start if we're running!
+            return
+
+            # Create the timer object, start and set state.
+        self._timer = threading.Timer(self.interval, self)
+        self._timer.start()
+        self.running = True
+
+    def stop(self):
+        """
+        Cancel the interval (no more function calls).
+        """
+        if self._timer:
+            self._timer.cancel()
+        self.running = False
+        self._timer = None
