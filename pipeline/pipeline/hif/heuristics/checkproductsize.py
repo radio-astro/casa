e864b560362def6d03461061757dc36ab7e3cfb2
dirk muders <dmuders@mpifr-bonn.mpg.de>
2018-08-10 12:06:45 +0000

    CAS-11363:
    Adjusted PB mitigation calculation.
    
    
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@41880 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/pipeline/hif/heuristics/checkproductsize.py
+++ pipeline/pipeline/hif/heuristics/checkproductsize.py
@@ -126,7 +126,7 @@ class CheckProductSizeHeuristics(object):
 
             # Calculate PB level at which the largest cube size of all targets
             # is equal to the maximum allowed cube size.
-            PB_mitigation = math.exp(-math.log(2.0) * 2.2064 * self.inputs.maxcubesize / maxcubesize / 1.05)
+            PB_mitigation = math.exp(-math.log(2.0) * 2.2064 * self.inputs.maxcubesize / maxcubesize / 1.01)
             # Cap at PB=0.7
             PB_mitigation = min(PB_mitigation, 0.7)
             # Cap at PB=0.2
@@ -236,7 +236,7 @@ class CheckProductSizeHeuristics(object):
 
                 # Calculate PB level at which the largest cube size of all targets
                 # is equal to the maximum allowed cube size.
-                PB_mitigation = math.exp(-math.log(2.0) * 2.2064 * self.inputs.maxcubesize / maxcubesize / 1.05)
+                PB_mitigation = math.exp(-math.log(2.0) * 2.2064 * self.inputs.maxcubesize / maxcubesize / 1.01)
                 # Cap at PB=0.7
                 PB_mitigation = min(PB_mitigation, 0.7)
                 # Cap at PB=0.2
