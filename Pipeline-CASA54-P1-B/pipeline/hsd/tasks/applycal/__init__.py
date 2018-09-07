6a106284a887edaa8339f285d6a0e96b50fea718
yoshino.akira <yoshino.akira@nrao.edu>
2018-09-07 01:43:08 +0000

    CAS-11570: Modified applycal.py and __init__.py of hsd/tasks/applycal/ to remove link to DataSets of weblog.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project@41998 ac9c0da5-131f-0410-9009-a9c195f183e6

--- Pipeline-CASA54-P1-B/pipeline/hsd/tasks/applycal/__init__.py
+++ Pipeline-CASA54-P1-B/pipeline/hsd/tasks/applycal/__init__.py
@@ -4,11 +4,14 @@ import pipeline.infrastructure.renderer.qaadapter as qaadapter
 import pipeline.infrastructure.renderer.weblog as weblog
 # from pipeline.hif.tasks.applycal import qa
 # from pipeline.hif.tasks.applycal import ApplycalResults
-from pipeline.h.tasks.applycal import ApplycalResults
-from . import renderer
-from .applycal import SDApplycal, HpcSDApplycal
+#from pipeline.h.tasks.applycal import ApplycalResults
 
-qaadapter.registry.register_to_flagging_topic(ApplycalResults)
+from . import renderer
+from .applycal import SDApplycal, HpcSDApplycal, SDApplycalResults
+#from .applycal import SDApplycal, HpcSDApplycal
+
+qaadapter.registry.register_to_flagging_topic(SDApplycalResults)
+#qaadapter.registry.register_to_flagging_topic(ApplycalResults)
 
 weblog.add_renderer(SDApplycal, renderer.T2_4MDetailsSDApplycalRenderer(always_rerender=False), group_by=weblog.UNGROUPED)
 weblog.add_renderer(HpcSDApplycal, renderer.T2_4MDetailsSDApplycalRenderer(always_rerender=False), group_by=weblog.UNGROUPED)
